52d94dd601c3cfc8fc0c15dbc87677f1a155aa04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package tj-probability-package/0.1.1/tj_probability_package-0.1.1.tar.gz

---
+++ packages/tj-probability-package/tj_probability_package-0.1.1.tar.gz/tj_probability_package-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tj_probability_package',
+      version='0.1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['tj_probability_package'],
+      author = "Akintayo Jabar",
+      author_email = "tayojabar@gmail.com",
+      zip_safe=False)
