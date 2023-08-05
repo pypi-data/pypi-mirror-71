fe208c5482b54f7f35a312022f7c5146480061be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:36 +0000

    Add package starlette-x-bugsnag/0.1.1/starlette-x-bugsnag-0.1.1.tar.gz

---
+++ packages/starlette-x-bugsnag/starlette-x-bugsnag-0.1.1.tar.gz/starlette-x-bugsnag-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['starlette_x_bugsnag']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bugsnag>=3.6.1,<4.0.0',
+ 'importlib-metadata>=1.6.1,<2.0.0',
+ 'starlette>=0.13.4,<0.14.0']
+
+setup_kwargs = {
+    'name': 'starlette-x-bugsnag',
+    'version': '0.1.1',
+    'description': 'Shiny Bugsnag integration for Starlette framework',
+    'long_description': '# starlette-x-bugsnag\n\nShiny Bugsnag integration for Starlette framework ✨\n\n![test](https://github.com/2tunnels/starlette-x-bugsnag/workflows/test/badge.svg?branch=master)\n![version](https://img.shields.io/pypi/v/starlette-x-bugsnag.svg)\n![pyversions](https://img.shields.io/pypi/pyversions/starlette-x-bugsnag.svg)\n![black](https://img.shields.io/badge/code%20style-black-000000.svg)\n![license](https://img.shields.io/pypi/l/starlette-x-bugsnag)\n',
+    'author': 'Vlad Dmitrievich',
+    'author_email': 'me@2tunnels.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/2tunnels/starlette-x-bugsnag',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
