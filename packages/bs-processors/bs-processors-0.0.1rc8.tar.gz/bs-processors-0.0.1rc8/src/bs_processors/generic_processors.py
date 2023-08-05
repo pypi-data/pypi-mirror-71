4d8901803edb8e3bda182b93caf2de4ecebf8be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:36 +0000

    Add package bs-processors/0.0.1rc8/bs-processors-0.0.1rc8.tar.gz

---
+++ packages/bs-processors/bs-processors-0.0.1rc8.tar.gz/bs-processors-0.0.1rc8/src/bs_processors/generic_processors.py
@@ -0,0 +1,274 @@
+"""
+Generic processor factories
+
+This module contains basic types of processor factories that can be used to
+generate typical processors ( like filter, flatten, local_modify ... )
+"""
+
+import functools
+from typing import Callable, Any, List, Sequence
+
+from .processor_util import single_to_multiple
+from .xml_util import set_new_children, process_children, copy_element_type
+
+# configure the documented entities
+__pdoc__={}
+
+
+def single_filter_proc(should_filter: Callable[[Any], bool], elm) -> List[Any]:
+    """
+    Conditionally filters an element based on the passed predicate
+
+    * **should_filter**: predicate that returns True if the element should be filtered
+    * **elm**: the element to check
+    * **return**: an empty array if the element should be filtered or an array with the passed element
+
+    >>> def should_filter(elm):
+    ...     if elm.name in ['span', 'a']:
+    ...         return True
+    ...     return False
+    ...
+    >>> from bs4 import BeautifulSoup as bs
+    >>> doc = bs('<html><div>a<span>b</span> <a></div> <p><span>x</span></p><a></html>','html.parser')
+    >>> filtered = single_filter_proc(should_filter, doc.html)
+    >>> filtered
+    [<html><div>a </div> <p></p></html>]
+
+    """
+    if should_filter(elm):
+        return []
+    else:
+        new_children = process_children(lambda child: single_filter_proc(should_filter, child), elm)
+        set_new_children(elm, new_children)
+
+        return [elm]
+
+
+def filter_factory(should_filter):
+    """
+    Adaptor for single_filter_proc to accept multiple elements
+    """
+    return single_to_multiple(functools.partial(single_filter_proc, should_filter))
+
+
+def single_unwrap_proc(should_unwrap: Callable[[Any], bool], elm) -> List[Any]:
+    """
+    Conditionally unwraps an element and pushes all its unwrapped children to its parent
+
+    * **should_unwrap**: predicate that returns true if the element should be unwrapped
+    * **elm**: the element to be conditionally unwrapped
+    * **return**: either a list containing the element (if it wasn't unwrapped) or a list with all
+     the unwrapped children
+
+    >>> from bs4 import BeautifulSoup
+    >>> def should_unwrap(elm):
+    ...     return elm.name == "x"
+    ...
+
+    >>> doc = "<root><x >hello</x> after x <b> in b</b></root>"
+    >>> elm = BeautifulSoup(doc, "xml")
+    >>> # single_unwrap_proc(should_unwrap, elm.root)
+    [<root>hello after x <b> in b</b></root>]
+
+    >>> doc = "<root><x >hello <a> in a</a></x></root>"
+    >>> elm = BeautifulSoup(doc, "xml")
+    >>> single_unwrap_proc(should_unwrap, elm.root)
+    [<root>hello <a> in a</a></root>]
+
+    >>> doc = "<root><x >hello <a> in a <x>in x2</x> after</a></x> 22 <b>in b</b> end</root>"
+    >>> elm = BeautifulSoup(doc, "xml")
+    >>> single_unwrap_proc(should_unwrap, elm.root)
+    [<root>hello <a> in a in x2 after</a> 22 <b>in b</b> end</root>]
+    """
+
+    # Note: elm.unwrap() would do the job but the return value is not
+    # what I need (it is the node that was unwrapped instead of the
+    # children) so we need to do it manually
+    new_children = process_children(lambda child: single_unwrap_proc(should_unwrap, child), elm)
+
+    if not should_unwrap(elm):
+        set_new_children(elm, new_children)
+        return [elm]
+
+    # insert the new children in the parent
+    # TODO see if I need to do it this way !!!
+    current = elm
+    for child in new_children:
+        current.insert_after(child)
+        current = child
+    elm.extract()  # remove current element
+    return new_children
+
+
+def unwrap_factory(should_unwrap):
+    """
+    Adaptor for single_unwrap_proc to accept multiple elements
+    """
+    return single_to_multiple(functools.partial(single_unwrap_proc, should_unwrap))
+
+
+def single_flatten_proc(flatten_children: Callable[[Any], bool], is_internal: Callable[[Any], bool], elm) -> List[Any]:
+    """
+    Flattens an element pulling out 'block' like elements that 'want' to be top level
+
+    * **flatten_children**: Predicate that specifies whether the direct children of the current element can be
+        flatten (i.e. taken outside of this element) (e.g. html, body and a do not allow their children to escape
+        outside)
+    * **is_internal**: Predicate that specifies whether a child 'wants' to stay inside its parent or not (i.e.
+        whether a child behaves like a block and wants out or an inline and stays in)
+    * **elm**: the element to be flatten
+    * **return**: a list of flattened elements
+
+    >>> from bs4 import BeautifulSoup
+    >>> def flatten_children(elm):
+    ...     if elm.name is None or elm.name in {'a', 'body', 'html'}:
+    ...         return False
+    ...     return True
+    ...
+    >>> def is_internal(elm):
+    ...     if elm.name is None:
+    ...         return True
+    ...     if elm.name in { 'div', 'p', 'br'}:
+    ...         return False
+    ...     if elm.name == 'a' and "block_a" in elm['class']:
+    ...         return False
+    ...     return True
+    ...
+
+    >>> doc = '<div id="1"> a <br id="2"> b</div>'
+    >>> s = BeautifulSoup(doc, "html.parser")
+    >>> elm = s.div
+    >>> single_flatten_proc(flatten_children, is_internal, elm)
+    [<div id="1"> a </div>, <br id="2"/>, <div> b</div>]
+
+    >>> doc = '<div id="1"><i>it</i><br id="2"><b></b></div>'
+    >>> s = BeautifulSoup(doc, "html.parser")
+    >>> elm = s.div
+    >>> single_flatten_proc(flatten_children, is_internal, elm)
+    [<div id="1"><i>it</i></div>, <br id="2"/>, <div><b></b></div>]
+
+    >>> doc = '<div id="1">a<a class="block_a">b</a>c</div>'
+    >>> s = BeautifulSoup(doc, "html.parser")
+    >>> elm = s.div
+    >>> single_flatten_proc(flatten_children, is_internal, elm)
+    [<div id="1">a</div>, <a class="block_a">b</a>, <div>c</div>]
+
+    >>> doc = '<div id="1"><a class="block_a"><div>inside a</div></a></div>'
+    >>> s = BeautifulSoup(doc, "html.parser")
+    >>> elm = s.div
+    >>> single_flatten_proc(flatten_children, is_internal, elm)
+    [<div id="1"></div>, <a class="block_a"><div>inside a</div></a>]
+    """
+    new_children = process_children(lambda child: single_flatten_proc(flatten_children, is_internal, child), elm)
+
+    if not flatten_children(elm):
+        # all children should go inside
+        set_new_children(elm, new_children)
+        return [elm]
+
+    elm.clear()
+    # this element pops out external children
+    # remember the current parent
+    parent = elm
+
+    result = []
+    for child in new_children:
+        if is_internal(child):
+            if parent is None:
+                # we need a new element like elm
+                parent = copy_element_type(elm)
+            parent.append(child)
+        else:
+            # we need to pop the element at a higher level, if we have a valid parent
+            # append it first
+            if parent is not None:
+                result.append(parent)
+                parent = None
+            result.append(child)
+
+    if parent is not None:
+        result.append(parent)
+
+    return result
+
+
+def flatten_factory(flatten_children, is_internal):
+    """
+    Adaptor for single_filter_proc to accept multiple elements
+    """
+    return single_to_multiple(functools.partial(single_filter_proc, should_filter))
+
+    return single_to_multiple(functools.partial(single_flatten_proc, flatten_children, is_internal))
+
+
+def single_local_modify(modify_func: Callable[[Any], bool], elm):
+    """
+    Runs a modification function on each element and all tag children
+
+    * **modify_func**: the modification function
+    * **elm**: the top level element
+    * **return**: an array containing the original element
+    """
+    modify_func(elm)
+    process_children(lambda child: single_local_modify(modify_func, child), elm)
+    return [elm]
+
+
+def local_modify_factory(modify_func):
+    """
+    Adaptor for single_local_modify to accept multiple elements
+    """
+    return single_to_multiple(functools.partial(single_local_modify, modify_func))
+
+
+def single_join_children(join_children: Callable[[Any, Any], Any], elm)-> [Any]:
+    """
+    Processor that joins children
+
+    * **join_children**: a function that may join two children. If the children are
+    joined the function should return an array containing the joined element, if the
+    children are not joined the function should return an array containing the passed
+    children.
+    * **elm**: the root element
+    * **return**: a sequence of elements
+    """
+    new_children = process_children( join_children, elm)
+
+    def reducer(accumulator, new_child):
+        if len(accumulator) > 0:
+            # try to see if we can join the last child with the new child
+            result = accumulator[:-1] + list(join_children(accumulator[-1], new_child))
+        else:
+            # this is the first element just store it (noting to join it to)
+            result = [new_child]
+        return result
+
+    # try to join adjacent children
+    joined_children = functools.reduce(reducer, new_children, [])
+    set_new_children(elm, joined_children)
+    return [elm]
+
+
+def join_children_factory(join_children):
+    """
+    Adaptor for single_join_children to accept multiple elements
+    """
+    return single_to_multiple(functools.partial(single_join_children, join_children))
+
+
+def lateral_effect(lateral_effect_func, elms: Sequence[Any]) -> Sequence[Any]:
+    lateral_effect_func()
+    return elms
+
+
+__pdoc__["lateral_effect"] = False  # do not document 
+
+
+def lateral_effect_factory(lateral_effect_func):
+    """
+    A processor factory that runs a lateral effect ( provided function) and the returns the
+    passed argument
+    """
+    return functools.partial(lateral_effect, lateral_effect_func)
+
+
---
+++ packages/bs-processors/bs-processors-0.0.1rc8.tar.gz/bs-processors-0.0.1rc8/src/bs_processors/utils/file_util.py
@@ -0,0 +1,186 @@
+"""
+Utilities for applying processors to files
+"""
+import shutil
+from fnmatch import fnmatch
+from typing import Callable, List, Any, Sequence
+from bs4 import BeautifulSoup
+import os
+from os import path, walk
+import logging
+import re
+
+_log = logging.getLogger("bs-processors")
+
+
+def process_directory(processor: Callable[[List[Any]], List[Any]], parser_type: str,
+                      input_dir: str, output_dir: str,
+                      file_selector: Callable[[str], bool]):
+    """
+    Processes a directory with the specified processor
+
+    :param processor: a file processor
+    :param parser_type: processor 'html.parser', 'html', 'xml' ( BeautifulSoup parser)
+    :param input_dir: the input directory
+    :param output_dir: the output directory
+    :param file_selector: something that can be transformed into a file_name predicate
+        if the predicate is true than the file will be processed if not the file will be
+        copied from input dir to output dir, see to_file_selector_predicate for details
+        about the file selector.
+    """
+
+    for dirpath, dirnames, filenames in walk(input_dir):
+        rel_path = dirpath[len(input_dir):]
+        if len(rel_path) > 0 and rel_path[0] == path.sep:
+            rel_path= rel_path[1:]  # remove start '/'
+
+        current_output_dir = path.join(output_dir, rel_path)
+
+        if not path.exists(current_output_dir):
+            os.makedirs(current_output_dir)
+
+        for fname in filenames:
+            input_fname = path.join(dirpath, fname)
+            output_fname = path.join(current_output_dir, fname)
+            if file_selector(input_fname):
+                _log.debug(f"processing '{input_fname}' into '{output_fname}'")
+                process_file(processor, parser_type, input_fname, output_fname)
+            else:
+                _log.debug(f"copying '{input_fname}' into '{output_fname}'")
+                shutil.copy(input_fname, output_fname)
+
+
+def process_file(processor: Callable[[List[Any]], List[Any]], parser_type: str, input_file: str, output_file: str):
+    """
+    Processes a file with the passed processor and saves the result in the output file
+
+    * **processor**: the processor to be applied
+    * **parser_type**: BeautifulSoup parser type ( 'html', 'xml', 'html.parser', etc)
+    * **input_file**: the input file name
+    * **output_file**: the result file name
+    """
+    with open(input_file, "rt") as f:
+        soup = BeautifulSoup(f, parser_type)
+
+    result = processor([soup])
+
+    output_len = len(result)
+
+    if output_len == 0:
+        _log.warning(f"processing '{input_file}' did NOT generate any output")
+        return
+
+    if output_len > 1:
+        _log.warning(f"processing '{input_file}' generated multiple output elements saving only the first one")
+
+    result = result[0]
+
+    if result.name != '[document]':
+        _log.warning(f"processing '{input_file}' did not yield a beautiful soup element creating one")
+        soup = BeautifulSoup(features=parser_type)
+        result = soup.append(result)
+
+    directory_name, f_name = path.split(output_file)
+
+    if not path.exists(directory_name):
+        os.makedirs(directory_name)
+
+    with open(output_file, "wt") as f:
+        f.write(result.prettify())
+
+
+def process_html_file(processor: Callable[[List[Any]], List[Any]], input_file: str, output_file: str):
+    process_file(processor, 'html.parser', input_file, output_file)
+
+
+def to_file_selector_predicate(pred):
+    """
+    Creates a file selector predicate from a variety of arguments
+
+    * **pred**: something that can be transformed in a file name predicate
+         * None: will match everything
+         * a str: will be interpreted as a unix file pattern (e.g. *.txt )
+         * a sequence: will be interpreted as a sequence of unix file patterns
+                (e.g. [*.txt, *.py]
+         * a regular expression, will create a predicate with re.fullmath (i.e. full match
+            of the full file name)
+         * a predicate that takes a string (the full file name)
+
+    * **return**: a file name predicate
+
+    >>> pm = to_file_selector_predicate('*.txt')
+    >>> pm('abc/def.txt')
+    True
+    >>> pm('abc/def.doc')
+    False
+    >>> pm = to_file_selector_predicate(['*.txt', '*.doc'])
+    >>> pm('abc/def.doc')
+    True
+    >>> pm('abc/def.txt')
+    True
+    >>> pm('abc/def.tt')
+    False
+    >>> pm = to_file_selector_predicate(re.compile("(abc)|(def+)"))
+    >>> pm("abc")
+    True
+    >>> pm("abcd")
+    False
+    >>> pm("def")
+    True
+    >>> pm("deffff")
+    True
+    >>> pm("something")
+    False
+    >>> pm = to_file_selector_predicate(lambda x: x.endswith("txt"))
+    >>> pm("abc.txt")
+    True
+    >>> pm("abc.tt")
+    False
+
+    """
+    if pred is None:
+        return True  # select everything
+
+    # pred is a string
+    if isinstance(pred, str):
+        return pattern_match_pred([pred])
+
+    # pred is a list like object
+    elif isinstance(pred, (tuple, list, set, frozenset)):
+        return pattern_match_pred(pred)
+    # pred is a regex
+    elif isinstance(pred, re.Pattern):
+        return lambda fname: pred.fullmatch(fname) is not None
+    # pred must be a predicate, use it as is
+    else:
+        return pred
+
+
+def pattern_match_pred(patterns: Sequence[str]) -> Callable[[str], bool]:
+    """
+    Creates a unix file pattern match predicate from a sequence of patterns
+
+    * **patterns**: sequence of patterns
+    * **return**: predicate
+
+    >>> pm = pattern_match_pred(["*.exe", "*.txt", "*.do?"])
+    >>> pm( "abc.txt")
+    True
+    >>> pm( "User/bubu/xyz.txt")
+    True
+    >>> pm( "abc.txta")
+    False
+    >>> pm('abc.exe')
+    True
+    >>> pm('abc.ex')
+    False
+    >>> pm('abc.doc')
+    True
+    """
+    def inner(file_name: str) -> bool:
+        for pattern in patterns:
+            if fnmatch(file_name, pattern):
+                return True
+        return False
+
+    return inner
