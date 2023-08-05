0290d1ee8bc5d029d731c05c7597b7d41f3875ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package dcg-probability/1.0/dcg_probability-1.0.tar.gz

---
+++ packages/dcg-probability/dcg_probability-1.0.tar.gz/dcg_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dcg_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dcg_probability'],
+      author = 'Dane Gunther',
+      author_email = 'danecgunther@gmail.com',
+      zip_safe=False)
