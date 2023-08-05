23abb64eea96f21092252a7411f4e65ceadf33b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:10 +0000

    Add package django-admin-toolbox/1.0.0.dev28/django-admin-toolbox-1.0.0.dev28.tar.gz

---
+++ packages/django-admin-toolbox/django-admin-toolbox-1.0.0.dev28.tar.gz/django-admin-toolbox-1.0.0.dev28/admin_toolbox/settings.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+
+ADMIN_TOOLBOX = getattr(settings, 'ADMIN_TOOLBOX', {})
+
+sidebar = ADMIN_TOOLBOX.get('sidebar', {
+    'default': 'admin_toolbox.builders.AppsListBuilder',
+})
+
+breadcrumbs = ADMIN_TOOLBOX.get('breadcrumbs', 'auto')
