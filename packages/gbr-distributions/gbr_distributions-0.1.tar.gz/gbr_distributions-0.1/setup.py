ec106f499597921eac73550f4495d94a04134ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:15 +0000

    Add package gbr-distributions/0.1/gbr_distributions-0.1.tar.gz

---
+++ packages/gbr-distributions/gbr_distributions-0.1.tar.gz/gbr_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gbr_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gbr_distributions'],
+      author = 'ritesh_vesalapu',
+      author_email = 'riteshvesalapu@gmail.com',
+      zip_safe=False)
