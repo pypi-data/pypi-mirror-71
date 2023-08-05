d56a660ba22af3f3ae95e13a5c696bf5772862c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:45:09 +0000

    Add package gusbi-distributions/0.1/gusbi_distributions-0.1.tar.gz

---
+++ packages/gusbi-distributions/gusbi_distributions-0.1.tar.gz/gusbi_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gusbi_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gusbi_distributions'],
+      author='Lawrence Agyepong',
+      author_email='agyeponglawrence101@gmail.com',
+      zip_safe=False)
