4fe4ba643f1db0b38bb57e8bda0427c345c00964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package moleculer-client/0.1.3/moleculer-client-0.1.3.tar.gz

---
+++ packages/moleculer-client/moleculer-client-0.1.3.tar.gz/moleculer-client-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+from os import path
+from readme_renderer import rst
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = rst.render(f.read())
+
+setup(
+    name='moleculer-client',
+    version='0.1.3',
+    description='Simple Client in python to communicate with MoleculerJs Microservices using NATS',
+    long_description=long_description,
+    author='Caio Filus Felisbino',
+    author_email='caio.filus@gmail.com',
+    url='https://github.com/CaioFilus/moleculer-client',
+    license='MIT',
+    packages=['moleculer_client'],
+    install_requires=[
+        'nats-python==0.7.0',
+    ],
+    include_package_data=True,
+    keywords=['python', 'microservices', 'NATs'],
+)
