b6ea3a428fb3dec200f888a0db3a66e404b32c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:58 +0000

    Add package picomc/0.3.0/picomc-0.3.0-py3-none-any.whl

---
+++ packages/picomc/picomc-0.3.0-py3-none-any.whl/picomc/logging.py
@@ -0,0 +1,17 @@
+import logging
+
+logger = logging.getLogger("picomc.cli")
+
+
+def initialize(debug):
+    if debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("[%(levelname)-4s] %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    logger.debug("Logging succesfully initialized.")
