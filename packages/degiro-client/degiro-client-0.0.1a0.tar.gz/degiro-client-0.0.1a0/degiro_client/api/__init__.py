fff2c581f40fbe2e802a91b5ab4bf8b9b65e4b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:50 +0000

    Add package degiro-client/0.0.1a0/degiro-client-0.0.1a0.tar.gz

---
+++ packages/degiro-client/degiro-client-0.0.1a0.tar.gz/degiro-client-0.0.1a0/degiro_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from degiro_client.api.config_api import ConfigApi
+from degiro_client.api.login_api import LoginApi
+from degiro_client.api.pa_api import PaApi
+from degiro_client.api.trading_api import TradingApi
