3e3aed4b5557e71805df91d4086c68e311926331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:39 +0000

    Add package itk-totalvariation/0.2.0/itk_totalvariation-0.2.0-cp37-cp37m-win_amd64.whl

---
+++ packages/itk-totalvariation/itk_totalvariation-0.2.0-cp37-cp37m-win_amd64.whl/itk/Configuration/TotalVariationConfig.py
@@ -0,0 +1,8 @@
+depends = ('ITKPyBase', 'ITKImageSources', 'ITKImageFilterBase', 'ITKCommon', )
+templates = (
+  ('ProxTVImageFilter', 'itk::ProxTVImageFilter', 'itkProxTVImageFilterIF2IF2', True, 'itk::Image< float,2 >, itk::Image< float,2 >'),
+  ('ProxTVImageFilter', 'itk::ProxTVImageFilter', 'itkProxTVImageFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+  ('ProxTVImageFilter', 'itk::ProxTVImageFilter', 'itkProxTVImageFilterID2ID2', True, 'itk::Image< double,2 >, itk::Image< double,2 >'),
+  ('ProxTVImageFilter', 'itk::ProxTVImageFilter', 'itkProxTVImageFilterID3ID3', True, 'itk::Image< double,3 >, itk::Image< double,3 >'),
+)
+snake_case_functions = ('prox_tv_image_filter', )
