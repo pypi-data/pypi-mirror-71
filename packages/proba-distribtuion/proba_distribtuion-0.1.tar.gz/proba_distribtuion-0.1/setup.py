90672faef5fa88440e45a31a3ea41b3a1a0278f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:18 +0000

    Add package proba-distribtuion/0.1/proba_distribtuion-0.1.tar.gz

---
+++ packages/proba-distribtuion/proba_distribtuion-0.1.tar.gz/proba_distribtuion-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='proba_distribtuion',
+      version='0.1',
+      description='A simple package for Gaussian and Binomial Distributions',
+      packages=['proba_distribtuion'],
+      student_email='ndxloc1995@gmail.com',
+      zip_safe=False)
