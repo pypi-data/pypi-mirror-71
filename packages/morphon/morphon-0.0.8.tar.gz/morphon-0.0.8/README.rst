ebd63da6667020a193efbf3178cf0e8bdeda6a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:51 +0000

    Add package morphon/0.0.8/morphon-0.0.8.tar.gz

---
+++ packages/morphon/morphon-0.0.8.tar.gz/morphon-0.0.8/README.rst
@@ -0,0 +1,33 @@
+
+morphon
+-------
+
+Neuron morphology processing scripts:
+
+- ``mcheck``  test morphology reconstruction for structural consistency
+- ``mfind``   find singularities in morphological reconstruction
+- ``mmeter``  perform morphometric analysis
+- ``mmod``    modify morphology
+- ``mrep``    repair or correct neuron morphology
+- ``msholl``  Sholl analysis of morphological reconstruction
+- ``mview``   view neuron morphology
+
+See command line help for details.
+
+
+Installation
+~~~~~~~~~~~~
+
+Install the latest release:
+
+.. code-block:: python
+
+   pip install morphon
+
+Install a development version:
+
+.. code-block:: python
+
+   pip install git+git://github.com/a1eko/morphon
+
+See ``pip`` documentation for installation alternatives.
---
+++ packages/morphon/morphon-0.0.8.tar.gz/morphon-0.0.8/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setuptools.setup(name="morphon",
+    version="0.0.8",
+    description="Neuron morphology processing scripts",
+    long_description=readme(),
+    author="Alexander Kozlov",
+    author_email="akozlov@kth.se",
+    url="https://github.com/a1eko/morphon",
+    packages=setuptools.find_packages(),
+    scripts=[
+        "apps/mcheck",
+        "apps/mfind",
+        "apps/mmeter",
+        "apps/mmod",
+        "apps/mrep",
+        "apps/msholl",
+        "apps/mview",
+    ],
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX",
+    ],
+    keywords=[
+        "neuron",
+        "morphology",
+        "reconstruction",
+        "morphometry",
+        "modification",
+        "repair"],
+)
