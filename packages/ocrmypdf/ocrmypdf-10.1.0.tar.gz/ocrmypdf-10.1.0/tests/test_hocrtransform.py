4487e53538ec6b7290e3bc9c4c8c613b1cdbfe80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:42 +0000

    Add package ocrmypdf/10.1.0/ocrmypdf-10.1.0.tar.gz

---
+++ packages/ocrmypdf/ocrmypdf-10.1.0.tar.gz/ocrmypdf-10.1.0/tests/test_hocrtransform.py
@@ -0,0 +1,44 @@
+# © 2015 James R. Barlow: github.com/jbarlow83
+#
+# This file is part of OCRmyPDF.
+#
+# OCRmyPDF is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OCRmyPDF is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OCRmyPDF.  If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+from PIL import Image
+
+from ocrmypdf import hocrtransform
+from ocrmypdf._exec.tesseract import HOCR_TEMPLATE
+from ocrmypdf.helpers import check_pdf
+
+# pylint: disable=redefined-outer-name
+
+
+@pytest.fixture
+def blank_hocr(tmp_path):
+    filename = tmp_path / "blank.hocr"
+    filename.write_text(HOCR_TEMPLATE)  # pylint: disable=E1101
+    return filename
+
+
+def test_mono_image(blank_hocr, outdir):
+    im = Image.new('1', (8, 8), 0)
+    for n in range(8):
+        im.putpixel((n, n), 1)
+    im.save(outdir / 'mono.tif', format='TIFF')
+
+    hocr = hocrtransform.HocrTransform(str(blank_hocr), 300)
+    hocr.to_pdf(str(outdir / 'mono.pdf'), image_filename=str(outdir / 'mono.tif'))
+
+    check_pdf(str(outdir / 'mono.pdf'))
