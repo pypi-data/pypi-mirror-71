ab4b4be448d47a566766466342bc58bfa354d330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:33 +0000

    Add package rsnd-distributions/0.1/rsnd_distributions-0.1.tar.gz

---
+++ packages/rsnd-distributions/rsnd_distributions-0.1.tar.gz/rsnd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rsnd_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['rsnd_distributions'],
+      author = 'Roshan Madipalli',
+      author_email = 'roshan26102000@gmail.com',
+      zip_safe=False)
