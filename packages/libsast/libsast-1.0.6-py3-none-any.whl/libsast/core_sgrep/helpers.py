07c45db5769e2a629bfc0da2ba4e0511d620ca2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:30 +0000

    Add package libsast/1.0.6/libsast-1.0.6-py3-none-any.whl

---
+++ packages/libsast/libsast-1.0.6-py3-none-any.whl/libsast/core_sgrep/helpers.py
@@ -0,0 +1,34 @@
+# -*- coding: utf_8 -*-
+"""Semantic Grep Helpers."""
+import multiprocessing
+
+from semgrep import semgrep_main
+
+try:
+    CPU_COUNT = multiprocessing.cpu_count()
+except NotImplementedError:
+    CPU_COUNT = 1  # CPU count is not implemented on Windows
+
+
+def call_semgrep(paths, scan_rules):
+    """Call Semgrep."""
+    return semgrep_main.main(
+        target=paths,
+        pattern=None,
+        lang=None,
+        config=scan_rules,
+        debugging_json=False,
+        no_rewrite_rule_ids=False,
+        jobs=CPU_COUNT,
+        include=[],
+        include_dir=[],
+        exclude=[],
+        exclude_dir=[],
+        json_format=True,
+        sarif=False,
+        output_destination=None,
+        quiet=True,
+        strict=False,
+        exit_on_error=False,
+        autofix=False,
+        dangerously_allow_arbitrary_code_execution_from_rules=False)
