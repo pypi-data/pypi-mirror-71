1273def9f78e403953ad0d8859bb5e604cfcee85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package iminuit/1.4.0/iminuit-1.4.0-cp37-cp37m-win32.whl

---
+++ packages/iminuit/iminuit-1.4.0-cp37-cp37m-win32.whl/iminuit/_minuit_methods.py
@@ -0,0 +1,128 @@
+from warnings import warn
+from iminuit.util import InitialParamWarning
+from iminuit import util as mutil
+import numpy as np
+
+
+def pedantic(self, parameters, kwds, errordef):
+    def w(msg):
+        warn(msg, InitialParamWarning, stacklevel=3)
+
+    for vn in parameters:
+        if vn not in kwds:
+            w("Parameter %s does not have initial value. Assume 0." % vn)
+        if "error_" + vn not in kwds and "fix_" + mutil.param_name(vn) not in kwds:
+            w(
+                "Parameter %s is floating but does not have initial step size. Assume 1."
+                % vn
+            )
+    for vlim in mutil.extract_limit(kwds):
+        if mutil.param_name(vlim) not in parameters:
+            w(
+                "%s is given. But there is no parameter %s. Ignore."
+                % (vlim, mutil.param_name(vlim))
+            )
+    for vfix in mutil.extract_fix(kwds):
+        if mutil.param_name(vfix) not in parameters:
+            w(
+                "%s is given. But there is no parameter %s. Ignore."
+                % (vfix, mutil.param_name(vfix))
+            )
+    for verr in mutil.extract_error(kwds):
+        if mutil.param_name(verr) not in parameters:
+            w(
+                "%s float. But there is no parameter %s. Ignore."
+                % (verr, mutil.param_name(verr))
+            )
+    if errordef is None:
+        w("errordef is not given. Default to 1.")
+
+
+def profile(self, vname, bins, bound, subtract_min):
+    # center value
+    val = np.linspace(bound[0], bound[1], bins, dtype=np.double)
+    result = np.empty(bins, dtype=np.double)
+    pos = self.var2pos[vname]
+    n = val.shape[0]
+    arg = list(self.args)
+    if self.use_array_call:
+        varg = np.array(arg, dtype=np.double)
+        for i in range(n):
+            varg[pos] = val[i]
+            result[i] = self.fcn(varg)
+    else:
+        for i in range(n):
+            arg[pos] = val[i]
+            result[i] = self.fcn(*arg)
+    if subtract_min:
+        result -= self.fval
+    return val, result
+
+
+def draw_profile(self, vname, x, y, band, text):
+    from matplotlib import pyplot as plt
+
+    plt.plot(x, y)
+    plt.xlabel(vname)
+    plt.ylabel("FCN")
+
+    if vname in self.values:
+        v = self.values[vname]
+        plt.axvline(v, color="k", linestyle="--")
+
+        vmin = None
+        vmax = None
+        if (vname, 1) in self.merrors:
+            vmin = v + self.merrors[(vname, -1)]
+            vmax = v + self.merrors[(vname, 1)]
+        if vname in self.errors:
+            vmin = v - self.errors[vname]
+            vmax = v + self.errors[vname]
+
+        if vmin is not None and band:
+            plt.axvspan(vmin, vmax, facecolor="0.8")
+
+        if text:
+            plt.title(
+                ("%s = %.3g" % (vname, v))
+                if vmin is None
+                else ("%s = %.3g - %.3g + %.3g" % (vname, v, v - vmin, vmax - v)),
+                fontsize="large",
+            )
+
+    return x, y
+
+
+def draw_contour(self, x, y, bins, bound):
+    from matplotlib import pyplot as plt
+
+    vx, vy, vz = self.contour(x, y, bins, bound, subtract_min=True)
+
+    v = [self.errordef * (i + 1) for i in range(4)]
+
+    CS = plt.contour(vx, vy, vz, v)
+    plt.clabel(CS, v)
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.axhline(self.values[y], color="k", ls="--")
+    plt.axvline(self.values[x], color="k", ls="--")
+    return vx, vy, vz
+
+
+def draw_mncontour(self, x, y, nsigma, numpoints):
+    from matplotlib import pyplot as plt
+    from matplotlib.contour import ContourSet
+
+    c_val = []
+    c_pts = []
+    for sigma in range(1, nsigma + 1):
+        pts = self.mncontour(x, y, numpoints, sigma)[2]
+        # close curve
+        pts.append(pts[0])
+        c_val.append(sigma)
+        c_pts.append([pts])  # level can have more than one contour in mpl
+    cs = ContourSet(plt.gca(), c_val, c_pts)
+    plt.clabel(cs)
+    plt.xlabel(x)
+    plt.ylabel(y)
+    return cs
---
+++ packages/iminuit/iminuit-1.4.0-cp37-cp37m-win32.whl/iminuit/tests/test_util.py
@@ -0,0 +1,220 @@
+from iminuit.util import (
+    fitarg_rename,
+    true_param,
+    param_name,
+    extract_iv,
+    extract_limit,
+    extract_error,
+    extract_fix,
+    remove_var,
+    arguments_from_docstring,
+    Matrix,
+    FMin,
+    Param,
+    MError,
+    Params,
+    MigradResult,
+)
+import pytest
+
+
+def test_fitarg_rename():
+    fitarg = {"x": 1, "limit_x": (2, 3), "fix_x": True, "error_x": 10}
+
+    def ren(x):
+        return "z_" + x
+
+    newfa = fitarg_rename(fitarg, ren)
+    assert "z_x" in newfa
+    assert "limit_z_x" in newfa
+    assert "error_z_x" in newfa
+    assert "fix_z_x" in newfa
+    assert len(newfa) == 4
+
+
+def test_fitarg_rename_strprefix():
+    fitarg = {"x": 1, "limit_x": (2, 3), "fix_x": True, "error_x": 10}
+    newfa = fitarg_rename(fitarg, "z")
+    assert "z_x" in newfa
+    assert "limit_z_x" in newfa
+    assert "error_z_x" in newfa
+    assert "fix_z_x" in newfa
+    assert len(newfa) == 4
+
+
+def test_true_param():
+    assert true_param("N") is True
+    assert true_param("limit_N") is False
+    assert true_param("error_N") is False
+    assert true_param("fix_N") is False
+
+
+def test_param_name():
+    assert param_name("N") == "N"
+    assert param_name("limit_N") == "N"
+    assert param_name("error_N") == "N"
+    assert param_name("fix_N") == "N"
+
+
+def test_extract_iv():
+    d = dict(k=1.0, limit_k=1.0, error_k=1.0, fix_k=1.0)
+    ret = extract_iv(d)
+    assert "k" in ret
+    assert "limit_k" not in ret
+    assert "error_k" not in ret
+    assert "fix_k" not in ret
+
+
+def test_extract_limit():
+    d = dict(k=1.0, limit_k=1.0, error_k=1.0, fix_k=1.0)
+    ret = extract_limit(d)
+    assert "k" not in ret
+    assert "limit_k" in ret
+    assert "error_k" not in ret
+    assert "fix_k" not in ret
+
+
+def test_extract_error():
+    d = dict(k=1.0, limit_k=1.0, error_k=1.0, fix_k=1.0)
+    ret = extract_error(d)
+    assert "k" not in ret
+    assert "limit_k" not in ret
+    assert "error_k" in ret
+    assert "fix_k" not in ret
+
+
+def test_extract_fix():
+    d = dict(k=1.0, limit_k=1.0, error_k=1.0, fix_k=1.0)
+    ret = extract_fix(d)
+    assert "k" not in ret
+    assert "limit_k" not in ret
+    assert "error_k" not in ret
+    assert "fix_k" in ret
+
+
+def test_remove_var():
+    dk = dict(k=1, limit_k=1, error_k=1, fix_k=1)
+    dm = dict(m=1, limit_m=1, error_m=1, fix_m=1)
+    dn = dict(n=1, limit_n=1, error_n=1, fix_n=1)
+    d = {}
+    d.update(dk)
+    d.update(dm)
+    d.update(dn)
+
+    d = remove_var(d, ["k", "m"])
+    assert set(d.keys()) & set(dk.keys()) == set()
+    assert set(d.keys()) & set(dm.keys()) == set()
+
+
+def test_arguments_from_docstring():
+    s = "f(x, y, z)"
+    ok, a = arguments_from_docstring(s)
+    assert ok
+    assert a == ["x", "y", "z"]
+    # this is a hard one
+    s = "Minuit.migrad( int ncall_me =10000, [resume=True, int nsplit=1])"
+    ok, a = arguments_from_docstring(s)
+    assert ok
+    assert a == ["ncall_me", "resume", "nsplit"]
+
+
+def test_Matrix():
+    x = Matrix(("a", "b"), [[1, 2], [3, 4]])
+    assert x[0] == (1, 2)
+    assert x[1] == (3, 4)
+    assert x == ((1, 2), (3, 4))
+    assert repr(x) == "((1, 2), (3, 4))"
+    with pytest.raises(TypeError):
+        x[0][0] = 1
+
+
+def test_Param():
+    keys = "number name value error is_const is_fixed has_limits has_lower_limit has_upper_limit lower_limit upper_limit".split()  # noqa: E501
+    values = 3, "foo", 1.2, 3.4, False, False, True, True, False, 42, None
+    p = Param(*values)
+
+    assert p.has_lower_limit is True
+    assert p.has_upper_limit is False
+    assert p["has_lower_limit"] is True
+    assert p["lower_limit"] == 42
+    assert p["upper_limit"] is None
+    assert "upper_limit" in p
+    assert "foo" not in p
+
+    assert list(p) == keys
+    assert p.keys() == tuple(keys)
+    assert p.values() == values
+    assert p.items() == tuple((k, v) for k, v in zip(keys, values))
+
+    assert (
+        str(p)
+        == "Param(number=3, name='foo', value=1.2, error=3.4, is_const=False, is_fixed=False, has_limits=True, has_lower_limit=True, has_upper_limit=False, lower_limit=42, upper_limit=None)"  # noqa: E501
+    )
+
+
+def test_MError():
+    keys = "name is_valid lower upper lower_valid upper_valid at_lower_limit at_upper_limit at_lower_max_fcn at_upper_max_fcn lower_new_min upper_new_min nfcn min".split()  # noqa: E501
+    values = (
+        "Foo",
+        True,
+        0.1,
+        1.2,
+        True,
+        True,
+        False,
+        False,
+        False,
+        False,
+        0.1,
+        1.2,
+        42,
+        0.2,
+    )
+
+    m = MError(*values)
+
+    assert m.keys() == tuple(keys)
+    assert m.values() == values
+    assert m.name == "Foo"
+    assert m["name"] == "Foo"
+
+
+def test_FMin():
+    keys = "fval edm tolerance nfcn ncalls up is_valid has_valid_parameters has_accurate_covar has_posdef_covar has_made_posdef_covar hesse_failed has_covariance is_above_max_edm has_reached_call_limit".split()  # noqa: E501
+    values = (
+        0.2,
+        1e-3,
+        0.1,
+        10,
+        10,
+        1.2,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+        False,
+    )
+
+    f = FMin(*values)
+
+    assert f.keys() == tuple(keys)
+    assert f.values() == values
+    assert f.fval == 0.2
+    assert f["fval"] == 0.2
+
+
+def test_MigradResult():
+    fmin = FMin(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+    params = Params([], None)
+    mr = MigradResult(fmin, params)
+    assert mr.fmin is fmin
+    assert mr[0] is fmin
+    assert mr.params is params
+    assert mr[1] is params
+    a, b = mr
+    assert a is fmin
+    assert b is params
