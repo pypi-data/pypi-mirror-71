a95b96d353f621b1d904fc20721e68815e7a8d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:07 +0000

    Add package wj-distribution/0.1/wj_distribution-0.1.tar.gz

---
+++ packages/wj-distribution/wj_distribution-0.1.tar.gz/wj_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='wj_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['wj_probability'],
+      author = 'Lim Wai Jian',
+      author_email = 'jitsinkrs@hotmail.com',
+      zip_safe=False)
