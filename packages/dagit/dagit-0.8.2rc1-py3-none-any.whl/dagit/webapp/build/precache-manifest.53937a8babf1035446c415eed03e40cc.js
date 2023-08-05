4dc9dd1fe15622597f9e8b2387e3c4e0d17615d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:07 +0000

    Add package dagit/0.8.2rc1/dagit-0.8.2rc1-py3-none-any.whl

---
+++ packages/dagit/dagit-0.8.2rc1-py3-none-any.whl/dagit/webapp/build/precache-manifest.53937a8babf1035446c415eed03e40cc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3469451ed6c431a9f64dd73c546101ae",
+    "url": "/32219f99e918112537ac.worker.js"
+  },
+  {
+    "revision": "286fca9d8cb20cf479d8ac52fd643ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2ac21183877af923a6e",
+    "url": "/static/css/2.6e707ff0.chunk.css"
+  },
+  {
+    "revision": "e2ac21183877af923a6e",
+    "url": "/static/js/2.ecd72b59.chunk.js"
+  },
+  {
+    "revision": "50fbb10c2f10877dfae25577c57ea2a6",
+    "url": "/static/js/2.ecd72b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233babf4ba6251e4d51c",
+    "url": "/static/js/main.94cd1ef6.chunk.js"
+  },
+  {
+    "revision": "1ce0accb9006b5881ad3",
+    "url": "/static/js/runtime-main.cffdbaad.js"
+  },
+  {
+    "revision": "9468a8ebaf94983770942fae8c470448",
+    "url": "/static/media/icon-dependency-arrow.9468a8eb.svg"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/static/media/icons-20.ca7934b3.eot"
+  }
+]);
