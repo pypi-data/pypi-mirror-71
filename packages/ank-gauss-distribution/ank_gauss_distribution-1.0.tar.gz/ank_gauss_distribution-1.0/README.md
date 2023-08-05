4393ae78576f92450811f6d9981681c18cf3a2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:55 +0000

    Add package ank-gauss-distribution/1.0/ank_gauss_distribution-1.0.tar.gz

---
+++ packages/ank-gauss-distribution/ank_gauss_distribution-1.0.tar.gz/ank_gauss_distribution-1.0/README.md
@@ -0,0 +1,9 @@
+Gaussian Class distribution made by Ankit Dongre
+Used to find out gaussian class distribution
+Methods:
+----
+pdf()
+args: k
+returns probability distribution 
+------
+
---
+++ packages/ank-gauss-distribution/ank_gauss_distribution-1.0.tar.gz/ank_gauss_distribution-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ank_gauss_distribution',
+      version='1.0',
+      description='ank_gauss_distribution',
+      packages=['ank_gauss_distribution'],
+      zip_safe=False)
