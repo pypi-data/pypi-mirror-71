fa0151493c3fddf2287936438f223d56a1b6209a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:13 +0000

    Add package classyconf/0.2.0/classyconf-0.2.0.tar.gz

---
+++ packages/classyconf/classyconf-0.2.0.tar.gz/classyconf-0.2.0/classyconf/__init__.py
@@ -0,0 +1,16 @@
+from .casts import Boolean, Identity, List, Option, Tuple
+from .configuration import (
+    NOT_SET,
+    Configuration,
+    Value,
+    as_boolean,
+    as_is,
+    as_list,
+    as_option,
+    as_tuple,
+    evaluate,
+)
+from .loaders import EnvFile, Environment, IniFile, EnvPrefix
+
+
+__version__ = "0.2.0"
