ab6ed4a38ac03684fcedba1cb983e79145031f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:33 +0000

    Add package nidmd/0.2.1/nidmd-0.2.1.tar.gz

---
+++ packages/nidmd/nidmd-0.2.1.tar.gz/nidmd-0.2.1/nidmd/datasets/__init__.py
@@ -0,0 +1,6 @@
+"""
+Dynamic Mode Decomposition for nidmd
+"""
+from .atlas import Atlas
+
+__all__ = ['Atlas']
---
+++ packages/nidmd/nidmd-0.2.1.tar.gz/nidmd-0.2.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='nidmd',
+     version='0.2.1',
+     author="Arnaud Dhaene (EPFL)",
+     author_email="arnaud.dhaene@epfl.ch",
+     description="Dynamic Mode Decomposition of time-series fMRI",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/arnauddhaene/nidmd",
+     packages=setuptools.find_packages(),
+     include_package_data=True,
+     install_requires=['statsmodels', 'pandas', 'numpy', 'plotly', 'nilearn', 'matplotlib',
+                       'scikit-learn', 'scipy', 'sklearn'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
