fcd49bfc00f8c539bfae9c4392a31cb15f93b692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:37 +0000

    Add package akw-distributions/0.2/akw_distributions-0.2.tar.gz

---
+++ packages/akw-distributions/akw_distributions-0.2.tar.gz/akw_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='akw_distributions',
+      version='0.2',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['akw_distributions'],
+      author='Ariel Kexuan Wang',
+      author_email='wangkx2017@gmail.com',
+      zip_safe=False)
