73b7a4a126c0e99f5802e9c9006974f75b3ebca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:12 +0000

    Add package gaussnomial/0.1/gaussnomial-0.1.tar.gz

---
+++ packages/gaussnomial/gaussnomial-0.1.tar.gz/gaussnomial-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussnomial',
+      version='0.1',
+      author='shashi kumar soni',
+      author_email='skssunny30@gmail.com',
+      description='Gaussian distributions',
+      packages=['gaussnomial'],
+      zip_safe=False)
