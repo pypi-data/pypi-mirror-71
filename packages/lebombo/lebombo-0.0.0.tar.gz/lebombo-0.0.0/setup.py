126be441d320b24a99ad5989485484d4a54878ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:08 +0000

    Add package lebombo/0.0.0/lebombo-0.0.0.tar.gz

---
+++ packages/lebombo/lebombo-0.0.0.tar.gz/lebombo-0.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="lebombo",
+    version="0.0.0",
+    license='MIT',
+    author="TaeHwan-Jung",
+    author_email="nlkey2022@gmail.com",
+    description="",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+)
