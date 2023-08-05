4d9fb50b4d911b38f238a39167b5b2ec6d83c1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:10 +0000

    Add package sqlalchemy-function/0.0.8/sqlalchemy_function-0.0.8-py3-none-any.whl

---
+++ packages/sqlalchemy-function/sqlalchemy_function-0.0.8-py3-none-any.whl/sqlalchemy_function/__init__.py
@@ -0,0 +1,10 @@
+"""SQLAlchemy-Function
+
+This package defines the following bases:
+
+1. `FunctionMixin`: for creating Function models.
+2. `FunctionRelator`: a base for models which relate to Function models.
+"""
+
+from .function_mixin import FunctionMixin
+from .function_relator import FunctionRelator
