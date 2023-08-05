d5c3f691e821594b671518a1cc7f057ab5928a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:04 +0000

    Add package EDAspy/0.1.2/EDAspy-0.1.2.tar.gz

---
+++ packages/EDAspy/EDAspy-0.1.2.tar.gz/EDAspy-0.1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EDAspy",
+    version="0.1.2",
+    author="Vicente P. Soloviev",
+    author_email="vicente.perez.soloviev@gmail.com",
+    description="Estimation of Distribution Algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    download_url="https://github.com/VicentePerezSoloviev/EDAspy/archive/0.1.2.tar.gz",
+    url="https://github.com/VicentePerezSoloviev/EDAspy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    keywords=['EDA', 'estimation', 'bayesian', 'evolutionary', 'algorithm', 'optimization'],
+    python_requires='>=3.6',
+    license="LGPLv2+",
+)
