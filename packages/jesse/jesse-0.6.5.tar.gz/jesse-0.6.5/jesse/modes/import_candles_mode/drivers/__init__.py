71cf57524e0441547fd4a9aa6f786bf20ed887ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:35 +0000

    Add package jesse/0.6.5/jesse-0.6.5.tar.gz

---
+++ packages/jesse/jesse-0.6.5.tar.gz/jesse-0.6.5/jesse/modes/import_candles_mode/drivers/__init__.py
@@ -0,0 +1,15 @@
+from .binance import Binance
+from .binance_futures import BinanceFutures
+from .bitfinex import Bitfinex
+from .coinbase import Coinbase
+from .kraken import Kraken
+from .testnet_binance_futures import TestnetBinanceFutures
+
+drivers = {
+    'Binance': Binance,
+    'Binance Futures': BinanceFutures,
+    'Testnet Binance Futures': TestnetBinanceFutures,
+    'Bitfinex': Bitfinex,
+    'Coinbase': Coinbase,
+    'Kraken': Kraken
+}
