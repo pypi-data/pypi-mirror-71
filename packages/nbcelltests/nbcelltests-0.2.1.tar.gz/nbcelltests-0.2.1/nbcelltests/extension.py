ed0fb19d0f5762fbce58f0436476496935e19992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:44 +0000

    Add package nbcelltests/0.2.1/nbcelltests-0.2.1.tar.gz

---
+++ packages/nbcelltests/nbcelltests-0.2.1.tar.gz/nbcelltests-0.2.1/nbcelltests/extension.py
@@ -0,0 +1,103 @@
+# *****************************************************************************
+#
+# Copyright (c) 2019, the nbcelltests authors.
+#
+# This file is part of the nbcelltests library, distributed under the terms of
+# the Apache License 2.0.  The full license can be found in the LICENSE file.
+#
+import json
+import os
+import os.path
+import nbformat
+import sys
+import tornado.gen
+from tornado.concurrent import run_on_executor
+from concurrent.futures import ThreadPoolExecutor
+from notebook.base.handlers import IPythonHandler
+from notebook.utils import url_path_join
+try:
+    from backports.tempfile import TemporaryDirectory
+except ImportError:
+    from tempfile import TemporaryDirectory
+
+from .test import runWithHTMLReturn as runTest
+from .lint import runWithHTMLReturn as runLint
+
+
+class RunCelltestsHandler(IPythonHandler):
+    executor = ThreadPoolExecutor(4)
+
+    def initialize(self, rules=None, executable=None):
+        self.rules = rules
+        self.executable = executable
+
+    def get(self):
+        self.finish({'status': 0, 'test': self.rules})
+
+    @run_on_executor
+    def _run(self, body, path, name):
+        with TemporaryDirectory() as tempdir:
+            path = os.path.abspath(os.path.join(tempdir, name))
+            node = nbformat.from_dict(body.get('model'))
+            nbformat.write(node, path)
+            ret = runTest(path, executable=self.executable, rules=self.rules)
+            return ret
+
+    @tornado.gen.coroutine
+    def post(self):
+        body = json.loads(self.request.body)
+        path = os.path.join(os.getcwd(), body.get('path'))
+        name = os.path.basename(path)
+        ret = yield self._run(body, path, name)
+        self.finish({'status': 0, 'test': ret})
+
+
+class RunLintsHandler(IPythonHandler):
+    executor = ThreadPoolExecutor(4)
+
+    def initialize(self, rules=None, executable=None):
+        self.rules = rules
+        self.executable = executable
+
+    def get(self):
+        self.finish({'status': 0, 'linters': self.rules})
+
+    @run_on_executor
+    def _run(self, body, path, name):
+        with TemporaryDirectory() as tempdir:
+            path = os.path.abspath(os.path.join(tempdir, name))
+            node = nbformat.from_dict(body.get('model'))
+            nbformat.write(node, path)
+            ret, status = runLint(path, executable=self.executable, rules=self.rules)
+            return ret, status
+            self.finish({'status': status, 'lint': ret})
+
+    @tornado.gen.coroutine
+    def post(self):
+        body = json.loads(self.request.body)
+        path = os.path.join(os.getcwd(), body.get('path'))
+        name = os.path.basename(path)
+        ret, status = yield self._run(body, path, name)
+        self.finish({'status': status, 'lint': ret})
+
+
+def _load_jupyter_server_extension(nb_server_app):
+    """
+    Called when the extension is loaded.
+
+    Args:
+        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+    """
+    web_app = nb_server_app.web_app
+
+    host_pattern = '.*$'
+    base_url = web_app.settings['base_url']
+    # host_pattern = '.*$'
+    print('Installing nbcelltests handler on path %s' % url_path_join(base_url, 'celltests'))
+
+    rules = nb_server_app.config.get('JupyterLabCelltests', {}).get('rules', {})
+    test_executable = nb_server_app.config.get('JupyterLabCelltests', {}).get('test_executable', [sys.executable, '-m', 'pytest', '-v'])
+    lint_executable = nb_server_app.config.get('JupyterLabCelltests', {}).get('lint_executable', [sys.executable, '-m', 'flake8', '--ignore=W391'])
+
+    web_app.add_handlers(host_pattern, [(url_path_join(base_url, 'celltests/test/run'), RunCelltestsHandler, {'rules': rules, 'executable': test_executable})])
+    web_app.add_handlers(host_pattern, [(url_path_join(base_url, 'celltests/lint/run'), RunLintsHandler, {'rules': rules, 'executable': lint_executable})])
---
+++ packages/nbcelltests/nbcelltests-0.2.1.tar.gz/nbcelltests-0.2.1/setup.py
@@ -0,0 +1,91 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+from jupyter_packaging import (
+    create_cmdclass, install_npm, ensure_targets,
+    combine_commands, get_version
+)
+
+pjoin = path.join
+
+name = 'nbcelltests'
+here = path.abspath(path.dirname(__file__))
+jshere = path.join(here, 'js')
+version = get_version(pjoin(here, name, '_version.py'))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+requires = [
+    'jupyterlab>=1.0.0',
+    'nbval>=0.9.1',
+    'nbconvert',
+    'pytest>=4.4.0',
+    'pytest-cov',
+    'pytest-html>=1.20.0',
+    'flake8',
+    'parameterized',
+]
+
+dev_requires = requires + [
+    'bump2version',
+    'mock',
+    'autopep8',
+    'pytest-xdist ; sys_platform != "win32"',
+    'beautifulsoup4'
+]
+
+data_spec = [
+    # Lab extension installed by default:
+    ('share/jupyter/lab/extensions',
+     'js/lab-dist',
+     'jupyterlab_celltests-*.tgz'),
+    # Config to enable server extension by default:
+    ('etc/jupyter',
+     'jupyter-config',
+     '**/*.json'),
+]
+
+
+cmdclass = create_cmdclass('js', data_files_spec=data_spec)
+cmdclass['js'] = combine_commands(
+    install_npm(jshere, build_cmd='build:all'),
+    ensure_targets([
+        pjoin(jshere, 'lib', 'index.js'),
+        pjoin(jshere, 'style', 'index.css')
+    ]),
+)
+
+
+setup(
+    name=name,
+    version=version,
+    description='Cell-by-cell tests for JupyterLab',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jpmorganchase/nbcelltests',
+    author='The nbcelltests authors',
+    license='Apache 2.0',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Jupyter',
+    ],
+
+    cmdclass=cmdclass,
+
+    keywords='jupyter jupyterlab',
+    packages=find_packages(),
+    install_requires=requires,
+    extras_require={
+        'dev': dev_requires
+    },
+    include_package_data=True,
+    zip_safe=False,
+
+)
