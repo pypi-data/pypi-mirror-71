bcc2c274d172a7caf6968e43451bbb9aec1b15ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:21 +0000

    Add package libxduauth/1.3.1/libxduauth-1.3.1.tar.gz

---
+++ packages/libxduauth/libxduauth-1.3.1.tar.gz/libxduauth-1.3.1/libxduauth/sites/ids.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+from requests import Session
+
+from ..utils.page import parse_form_hidden_inputs
+
+
+class IDSSession(Session):
+
+    def __init__(
+            self, target, username, password,
+            *args, **kwargs
+    ):
+        super(IDSSession, self).__init__()
+        self.headers.update({'User-Agent': 'Mobile'})
+        page = self.get(
+            'http://ids.xidian.edu.cn/authserver/login',
+            params={'service': target}
+        ).text
+        params = parse_form_hidden_inputs(BeautifulSoup(page, "lxml"))
+        self.post(
+            'http://ids.xidian.edu.cn/authserver/login',
+            params={'service': target},
+            data=dict(params, **{
+                'username': username,
+                'password': password
+            })
+        )
