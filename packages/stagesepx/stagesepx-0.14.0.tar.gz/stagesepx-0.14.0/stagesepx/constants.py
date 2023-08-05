62064989670994938c64f2cccf915a5185dfcc6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:19 +0000

    Add package stagesepx/0.14.0/stagesepx-0.14.0.tar.gz

---
+++ packages/stagesepx/stagesepx-0.14.0.tar.gz/stagesepx-0.14.0/stagesepx/constants.py
@@ -0,0 +1,15 @@
+# encoding
+CHARSET = r"utf-8"
+
+# path and name
+CUT_RESULT_FILE_NAME = r"cut_result.json"
+REPORT_FILE_NAME = r"report.html"
+
+# report
+BACKGROUND_COLOR = r"#fffaf4"
+UNSTABLE_FLAG = r"-1"
+UNKNOWN_STAGE_FLAG = r"-2"
+IGNORE_FLAG = r"-3"
+
+# ext
+FFMPEG = r"ffmpeg"
