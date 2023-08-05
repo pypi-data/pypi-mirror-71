57988c2c1e472a68126e0eaaed2b6ffb50c2a7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:20 +0000

    Add package bioluigi/0.0.0/bioluigi-0.0.0.tar.gz

---
+++ packages/bioluigi/bioluigi-0.0.0.tar.gz/bioluigi-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='bioluigi',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'bioluigi = bioluigi.cli:main']},
+      install_requires=['click', 'luigi', 'requests', 'babel'])
