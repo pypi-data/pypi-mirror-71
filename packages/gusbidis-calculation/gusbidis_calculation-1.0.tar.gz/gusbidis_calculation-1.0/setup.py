d04709f262ed401ca239d56cbb22cc49ea0cab28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:52 +0000

    Add package gusbidis-calculation/1.0/gusbidis_calculation-1.0.tar.gz

---
+++ packages/gusbidis-calculation/gusbidis_calculation-1.0.tar.gz/gusbidis_calculation-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gusbidis_calculation',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gusbidis_calculation'],
+      author = 'Christabel Ajaero',
+      author_email = 'ajaero.christabel@gmail.com',
+    zip_safe=False)
