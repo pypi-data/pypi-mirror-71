4baa6da660077cbe1c82f3465331e995b2d5b9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package client-render/0.6/client-render-0.6.tar.gz

---
+++ packages/client-render/client-render-0.6.tar.gz/client-render-0.6/setup.py
@@ -0,0 +1,42 @@
+import os
+
+from setuptools import setup, find_packages
+
+requires = []
+with open('REQUIREMENTS', 'r') as f:
+    requires.extend(f.readlines())
+
+
+def _read(file_name):
+    with open(os.path.join(os.path.dirname(__file__), file_name)) as f:
+        return f.read()
+
+
+setup(
+    version="0.6",
+    name="client-render",
+    license='MIT',
+    description=_read('DESCRIPTION'),
+    author="Nikolay Telepenin",
+    author_email="telepenin.nikolay@gmail.com",
+    url="https://github.com/telepenin/client-render/",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    long_description=_read('README.md'),
+    install_requires=requires,
+    set_build_info=os.path.dirname(__file__),
+)
