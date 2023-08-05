042bbd583f93e12c02413e6f7a6ab383db44988b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package terminus-client-python/0.0.21/terminus-client-python-0.0.21.tar.gz

---
+++ packages/terminus-client-python/terminus-client-python-0.0.21.tar.gz/terminus-client-python-0.0.21/setup.py
@@ -0,0 +1,50 @@
+import setuptools
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+requires = [
+   'requests'
+]
+
+test_requirements = [
+    'pytest>=3',
+    'pytest-cov',
+    'pytest-mock',
+    'pytest-xdist',
+]
+
+extras_require = {
+    "dataframe": [
+        "numpy >= 1.13.0",
+        "pandas >= 0.23.0",
+    ],
+}
+
+setuptools.setup(
+    name="terminus-client-python",
+    version="0.0.21",
+    author="TerminusDB group",
+    author_email="terminusdatabase@gmail.com",
+    description="Python client for Terminus DB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_data={'': ['LICENSE']},
+    url="https://github.com/terminusdb/terminus-client-python",
+    packages=setuptools.find_packages(exclude=['tests']),
+    include_package_data=True,
+    install_requires=requires,
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=test_requirements,
+    extras_require=extras_require,
+    python_requires='>=3.6',
+)
