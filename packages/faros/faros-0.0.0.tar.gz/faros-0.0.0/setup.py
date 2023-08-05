77b0827cfe6ebf7c287cf5e9700893f5fff7d4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:11 +0000

    Add package faros/0.0.0/faros-0.0.0.tar.gz

---
+++ packages/faros/faros-0.0.0.tar.gz/faros-0.0.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="faros",
+    version="0.0.0",
+    author="Faros",
+    author_email="contact@faros.ai",
+    description="Faros API SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/faros-ai/faros-python-client",
+    packages=setuptools.find_packages(),
+    keywords=["faros", "api", "graphql", "sdk"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    install_requires=[
+        "graphqlclient>=0.2.4"
+    ],
+    python_requires='>=3.6'
+)
