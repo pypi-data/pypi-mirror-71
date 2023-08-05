89d7ba6d33cbf814b0bdf25f3026cf2f7923bf7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:40 +0000

    Add package pyyGit/0.2.4/pyyGit-0.2.4.tar.gz

---
+++ packages/pyyGit/pyyGit-0.2.4.tar.gz/pyyGit-0.2.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='pyyGit',
+    version='0.2.4',
+    description='the missing Python git module',
+    license='DBAD',
+    packages=['pyGit'],
+    author='ittussarom retals mail ynohtna',
+    author_email='thetechrobo@outlook.com',
+    keywords=['git', 'easy', 'thetechrobo'],
+    url='https://github.com/TheTechRobo/PyGit',
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+    project_urls={
+        'Documentation': 'https://github.com/thetechrobo/PyGit/wiki',
+        'Source': 'https://github.com/thetechrobo/pygit',
+        'Tracker': 'https://github.com/thetechrobo/pygit/issues',
+    },
+    python_requires='>=3',
+)
