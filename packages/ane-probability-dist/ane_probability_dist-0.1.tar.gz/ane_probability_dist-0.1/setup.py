0ff1daa20f7cc15ad8893feb55200772e0a32bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:28 +0000

    Add package ane-probability-dist/0.1/ane_probability_dist-0.1.tar.gz

---
+++ packages/ane-probability-dist/ane_probability_dist-0.1.tar.gz/ane_probability_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ane_probability_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ane_probability_dist'],
+      zip_safe=False)
