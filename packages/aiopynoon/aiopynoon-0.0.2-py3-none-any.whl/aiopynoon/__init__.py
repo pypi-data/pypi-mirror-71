0574d5dd17f240084c085563e7a71b90aea474d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:07 +0000

    Add package aiopynoon/0.0.2/aiopynoon-0.0.2-py3-none-any.whl

---
+++ packages/aiopynoon/aiopynoon-0.0.2-py3-none-any.whl/aiopynoon/__init__.py
@@ -0,0 +1,13 @@
+"""
+
+Basic connectivity and control for Noon Home Room Director and Extension switches.
+
+Note that this API is not supported by Noon, and is subject to change or withdrawal at any time.
+
+"""
+
+__author__ = "Alistair Galbraith"
+__copyright__ = "Copyright 2020, Alistair Galbraith"
+__version__ = "0.0.2"
+
+from .noon import Noon
