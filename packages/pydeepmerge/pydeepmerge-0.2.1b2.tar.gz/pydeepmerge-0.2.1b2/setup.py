dda0302c1a3202427c7655e95e1b79b1e25840cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:53 +0000

    Add package pydeepmerge/0.2.1b2/pydeepmerge-0.2.1b2.tar.gz

---
+++ packages/pydeepmerge/pydeepmerge-0.2.1b2.tar.gz/pydeepmerge-0.2.1b2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+description = \
+    'A lightweight library to perform deep merges of python dictionaries'
+
+setup(
+    name='pydeepmerge',
+    version='0.2.1b2',
+    license='MIT',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/taliamax/pydeepmerge',
+    maintainer='Natalia Maximo',
+    maintainer_email='iam@natalia.dev',
+    python_requires=">=3.5",
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
