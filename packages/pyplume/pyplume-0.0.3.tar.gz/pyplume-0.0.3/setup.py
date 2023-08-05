b2a7ca23b666bebc228c5dacbcae356c2c7ae44c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package pyplume/0.0.3/pyplume-0.0.3.tar.gz

---
+++ packages/pyplume/pyplume-0.0.3.tar.gz/pyplume-0.0.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyplume",
+    version="0.0.3",
+    author="Anthony Walker",
+    author_email="walkanth@oregonstate.edu",
+    license='BSD 3-clause "New" or "Revised License"',
+    description="This package contains files to build models for exhaust plume analysis and methods to analyze the results.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SoftwareDevEngResearch/plume-generation-and-analysis",
+    entry_points={
+        'console_scripts': [
+            'pyplume.mech=pyplume.mech:mechCLI',
+            'pyplume.model=pyplume.model:modelCLI',
+            'pyplume.figs=pyplume.figs:figsCLI',
+            'pyplume.stats=pyplume.stats:statsCLI',
+        ]
+    },
+    packages=['pyplume'],
+    include_package_data=True,
+    package_data={'pyplume': ['mechanisms/*','originals/*','tests/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pytest','cantera','h5py','matplotlib']
+)
