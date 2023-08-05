abf267b44060912ab4af9dcbcd0a518bb7ea6fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:28 +0000

    Add package video-utils/2.0.9/video_utils-2.0.9-py3-none-any.whl

---
+++ packages/video-utils/video_utils-2.0.9-py3-none-any.whl/video_utils/cprint.py
@@ -0,0 +1,11 @@
+from colorama import Fore, Style
+
+
+def cprint(colour, message):
+    colours = {
+        "green": Fore.GREEN,
+        "blue": Fore.BLUE,
+        "red": Fore.RED,
+        "yellow": Fore.YELLOW
+    }
+    print(colours[colour] + str(message) + Style.RESET_ALL)
