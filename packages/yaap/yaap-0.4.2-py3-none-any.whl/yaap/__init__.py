9d1afc6caadd3aae670143f1778556afed59835b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:26 +0000

    Add package yaap/0.4.2/yaap-0.4.2-py3-none-any.whl

---
+++ packages/yaap/yaap-0.4.2-py3-none-any.whl/yaap/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["Argument", "ValueArgument", "ListArgument",
+           "Bool", "Str", "Path", "Int", "Float",
+           "StrList", "PathList", "IntList", "FloatList", "Yaap"]
+__version__ = "0.4.2"
+
+from .argument import *
+from .yaap import *
