eeea92962c1d04ae7f70a998b6d012d7818a96be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:24 +0000

    Add package emmp-distributions/0.1/emmp_distributions-0.1.tar.gz

---
+++ packages/emmp-distributions/emmp_distributions-0.1.tar.gz/emmp_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='emmp_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['emmp_distributions'],
+      author = 'Emily Madrigal',
+      author_email = 'emily.mape.98@gmail.com',
+      zip_safe=False)
