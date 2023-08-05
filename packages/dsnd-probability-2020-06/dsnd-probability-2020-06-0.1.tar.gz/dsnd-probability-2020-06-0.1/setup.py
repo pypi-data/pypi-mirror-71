57281a28ebcd0200e54d276cc5b4287a1c112b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:32 +0000

    Add package dsnd-probability-2020-06/0.1/dsnd-probability-2020-06-0.1.tar.gz

---
+++ packages/dsnd-probability-2020-06/dsnd-probability-2020-06-0.1.tar.gz/dsnd-probability-2020-06-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd-probability-2020-06',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd-probability-2020-06'],
+      author = 'Lais Marques',
+      zip_safe=False)
