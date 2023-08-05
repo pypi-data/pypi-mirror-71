27d0e88faac26eb2c820285ba0b5e1a84fcb0818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:42 +0000

    Add package deephit-probability/0.2/deephit-probability-0.2.tar.gz

---
+++ packages/deephit-probability/deephit-probability-0.2.tar.gz/deephit-probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='deephit-probability',
+      version='0.2',
+      description='gaussian and binomial distribution',
+      packages=['deephit-probability'],
+      author='Deepak Rajpurohit',
+      author_email='ait.17bcs4172@gmail.com',
+      zip_safe=False)
