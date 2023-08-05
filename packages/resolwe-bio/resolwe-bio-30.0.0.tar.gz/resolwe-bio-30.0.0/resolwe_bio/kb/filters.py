4ae5e5b7afb1fba17bc9e31b535683ba7d3e94ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:10 +0000

    Add package resolwe-bio/30.0.0/resolwe-bio-30.0.0.tar.gz

---
+++ packages/resolwe-bio/resolwe-bio-30.0.0.tar.gz/resolwe-bio-30.0.0/resolwe_bio/kb/filters.py
@@ -0,0 +1,29 @@
+""".. Ignore pydocstyle D400.
+
+=======
+Filters
+=======
+
+"""
+import django_filters as filters
+
+from resolwe.flow.filters import TEXT_LOOKUPS
+
+from .models import Mapping
+
+
+class MappingFilter(filters.FilterSet):
+    """Filter the mapping endpoint."""
+
+    class Meta:
+        """Filter configuration."""
+
+        model = Mapping
+        fields = {
+            "source_db": TEXT_LOOKUPS[:],
+            "source_id": TEXT_LOOKUPS[:],
+            "source_species": TEXT_LOOKUPS[:],
+            "target_db": TEXT_LOOKUPS[:],
+            "target_id": TEXT_LOOKUPS[:],
+            "target_species": TEXT_LOOKUPS[:],
+        }
---
+++ packages/resolwe-bio/resolwe-bio-30.0.0.tar.gz/resolwe-bio-30.0.0/tests/urls.py
@@ -0,0 +1,27 @@
+from django.urls import include, path
+
+from rest_framework import routers
+
+from resolwe.api_urls import api_router as resolwe_router
+from resolwe.elastic import routers as search_routers
+from resolwe.flow.views import EntityViewSet
+from resolwe_bio.kb.views import (FeatureSearchViewSet, FeatureAutocompleteViewSet, FeatureViewSet,
+                                  MappingViewSet, MappingSearchViewSet)
+
+api_router = routers.DefaultRouter(trailing_slash=False)  # pylint: disable=invalid-name
+api_router.register(r'sample', EntityViewSet)
+api_router.register(r'kb/feature/admin', FeatureViewSet)
+api_router.register(r'kb/mapping/admin', MappingViewSet)
+
+search_router = search_routers.SearchRouter(trailing_slash=False)  # pylint: disable=invalid-name
+search_router.register(r'kb/feature/search', FeatureSearchViewSet, 'kb_feature_search')
+search_router.register(r'kb/feature/autocomplete', FeatureAutocompleteViewSet, 'kb_feature_autocomplete')
+search_router.register(r'kb/mapping/search', MappingSearchViewSet, 'kb_mapping_search')
+
+urlpatterns = [  # pylint: disable=invalid-name
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # XXX: Temporary fix to work with Resolwe 2.0.0, which requires 'resolwe-api' namespace to be available when
+    # reporting errors when running processes.
+    path('api-resolwe/', include((resolwe_router.urls, 'resolwe-api'))),
+    path('api/', include((api_router.urls + search_router.urls + resolwe_router.urls, 'resolwebio-api'))),
+]
