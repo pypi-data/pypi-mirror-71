f198e528f0960b02c372399241b9d1c5200859b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:17 +0000

    Add package datatorch/0.1.5/datatorch-0.1.5.tar.gz

---
+++ packages/datatorch/datatorch-0.1.5.tar.gz/datatorch-0.1.5/datatorch/agent/agent.py
@@ -0,0 +1,20 @@
+import logging
+
+from datatorch.core import BASE_URL
+
+
+logger = logging.getLogger(__name__)
+
+
+class Agent(object):
+    def __init__(self, id: str, host: str = BASE_URL):
+        self.id = id
+        self.host = host
+        self.api_url = f"{host}/api"
+        self.logger = logger
+
+        self.logger.debug(f"Initializing agent with ID {id}")
+        self.connect()
+
+    def connect(self):
+        self.logger.debug(f"Attempting connection to {self.api_url}")
