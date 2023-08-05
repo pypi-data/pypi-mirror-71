5f26ec47b7e5f38e024ff05366a0176597e82304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:20 +0000

    Add package Mayos-Package/1.0.0/Mayos_Package-1.0.0.tar.gz

---
+++ packages/Mayos-Package/Mayos_Package-1.0.0.tar.gz/Mayos_Package-1.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="Mayos_Package",
+    version="1.0.0",
+    author="nakamotomayos",
+    author_email="mayos@gmail.com",
+    description="mayos example package",
+    long_description="This is the longer description and will appear in the web.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
