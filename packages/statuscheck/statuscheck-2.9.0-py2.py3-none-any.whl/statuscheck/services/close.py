a8b7726aea21235c4eedddb01c211dce27f7082f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:30 +0000

    Add package statuscheck/2.9.0/statuscheck-2.9.0-py2.py3-none-any.whl

---
+++ packages/statuscheck/statuscheck-2.9.0-py2.py3-none-any.whl/statuscheck/services/close.py
@@ -0,0 +1,8 @@
+from statuscheck.services._statuspage import BaseStatusPageAPI
+
+
+class ServiceAPI(BaseStatusPageAPI):
+    name = "Close"
+    domain_id = "8vgwlwbg3zbc"
+    status_url = "https://status.close.com/"
+    service_url = "http://close.com/"
