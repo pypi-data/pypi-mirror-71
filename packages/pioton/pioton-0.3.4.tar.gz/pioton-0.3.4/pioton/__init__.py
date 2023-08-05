042b898296877dbe0fcf17655e8ec76bee8ee563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:56 +0000

    Add package pioton/0.3.4/pioton-0.3.4.tar.gz

---
+++ packages/pioton/pioton-0.3.4.tar.gz/pioton-0.3.4/pioton/__init__.py
@@ -0,0 +1,27 @@
+from .pioton import show_syntax,\
+               _translate_print,\
+  8            _translate_for,\
+  9            _translate_and,\
+ 10            _translate_or,\
+ 11            _translate_bools,\
+ 12            _translate_def,\
+ 13            _translate_if,\
+ 14            _translate_with_as,\
+ 15            _translate_open_close,\
+ 16            _translate_none,\
+ 17            _translate_assert,\
+ 18            _translate_imports,\
+ 19            _translate_await,\
+ 20            _translate_break,\
+ 21            _translate_del,\
+ 22            _translate_async,\
+ 23            _translate_class,\
+ 24            _translate_continue,\
+ 25            _translate_raise,\
+ 26            _translate_try_except_finally,\
+ 27            _translate_global,\
+ 28            _translate_not,\
+ 29            _translate_is,\
+ 30            _translate_pass,\
+ 31            _translate_while,\
+ 32            _translate_yield,\
---
+++ packages/pioton/pioton-0.3.4.tar.gz/pioton-0.3.4/setup.py
@@ -0,0 +1,71 @@
+from distutils.core import setup
+
+long_desc = """
+Píotón - Write Python code in Irish in IPython
+
+---
+
+Installation:
+
+pip: pip install pioton 
+
+github: clone the repo, move the contents of the pioton/ folder somewhere accessible by your PythonPATH
+
+---
+
+USAGE Transpiled:
+
+write your píotón code in one or more scripts with a '.pag' (Python as Gaeilge) extension, and run 
+
+tiomsaitheoir file.pag [file2.pag [...]]
+
+which will transpile the píotón scripts into pure Python '.py' files. Running 
+
+tiomsaitheoir -h
+
+will provide further details and examples, and running 
+
+tiomsaitheoir -s
+
+will show an overview of the syntax for píotón code 
+
+USAGE IPython:
+
+simply import píotón in any IPython environment (including Jupyter Notebooks) and start writing your code as Gaeilge.
+Call `pioton.show_syntax()` to print a full description of the new irish syntax.
+
+THIS IS FOR FUN - DO NOT USE THIS FOR ANYTHING IMPORTANT
+
+I can't stress that enough, I really didn't make sure it's robust at all, it's against Python standards, any code written in it will be completely unmaintainable, etc.
+
+---
+"""
+
+setup(
+  name = 'pioton',         # How you named your package folder (MyLib)
+  packages = ['pioton'],   # Chose the same as "name"
+  scripts = ['bin/tiomsaitheoir'],
+  version = '0.3.4',      # Start with a small number and increase it with every change you make
+  license='GPL',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'píotón language is a superset of Python allowing it to be written in Irish. Not for use in any important work',   # Give a short description about your library
+  long_description = long_desc,
+  author = 'Luke Roantree',                   # Type in your name
+  author_email = 'luke@roantree.com',      # Type in your E-Mail
+  url = 'https://github.com/LukeRoantree4815162342/pioton',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/LukeRoantree4815162342/pioton/archive/v_030.tar.gz',    # I explain this later on
+  keywords = ['IPython', 'Gaeilge', 'Irish'],   # Keywords that define your package best
+  install_requires=['ipython'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License (GPL)',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
