b52a0708103b64497a55f1ece8ba8c6aba840119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:10 +0000

    Add package gachengoh-distributions/1.2/gachengoh_distributions-1.2.tar.gz

---
+++ packages/gachengoh-distributions/gachengoh_distributions-1.2.tar.gz/gachengoh_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gachengoh_distributions',
+      version='1.2',
+      description='Gaussian distributions',
+      packages=['gachengoh_distributions'],
+      author = 'Tyller Gachengo',
+      author_email = 'gachengomarugu@gmail.com',
+    zip_safe=False)
