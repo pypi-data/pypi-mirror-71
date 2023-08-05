837de78259fac423a4f2d4039079c1ee478ae17f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:49 +0000

    Add package odoo12-addon-easy-my-coop-loan/12.0.1.0.1.99.dev54/odoo12-addon-easy_my_coop_loan-12.0.1.0.1.99.dev54.tar.gz

---
+++ packages/odoo12-addon-easy-my-coop-loan/odoo12-addon-easy_my_coop_loan-12.0.1.0.1.99.dev54.tar.gz/odoo12-addon-easy_my_coop_loan-12.0.1.0.1.99.dev54/odoo/addons/easy_my_coop_loan/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2019      Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Houssine BAKKALI - <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Easy My Coop Bond and Subordinated Loan Issues",
+    "version": "12.0.1.0.1",
+    "depends": ["easy_my_coop"],
+    "author": "Coop IT Easy SCRLfs",
+    "category": "Cooperative management",
+    "website": "http://www.coopiteasy.be",
+    "license": "AGPL-3",
+    "summary": """
+    This module allows to manage the bonds and
+    subordinated loans subscription life cycle.
+    """,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/loan_view.xml",
+        "views/partner_view.xml",
+        "views/menus.xml",
+        "data/mail_template_data.xml",
+    ],
+    "demo": ["demo/coop.xml"],
+    "installable": True,
+}
