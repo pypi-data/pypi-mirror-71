d3a689fb578c70b21d8b9ba118f0dbe125932e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:36 +0000

    Add package h1distributions/0.2/h1distributions-0.2.tar.gz

---
+++ packages/h1distributions/h1distributions-0.2.tar.gz/h1distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='h1distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
