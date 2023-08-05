0de60fff5242cee8faee3b780819270864c1c645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:56 +0000

    Add package distributions-AnishPratheepkumar/0.1/distributions_AnishPratheepkumar-0.1.tar.gz

---
+++ packages/distributions-AnishPratheepkumar/distributions_AnishPratheepkumar-0.1.tar.gz/distributions_AnishPratheepkumar-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_AnishPratheepkumar',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['distributions_AnishPratheepkumar'],
+      zip_safe=False)
