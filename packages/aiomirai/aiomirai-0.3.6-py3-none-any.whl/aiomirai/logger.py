25aca1db8adae5d2ab5ad7a06fe01734cdffc6b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:07 +0000

    Add package aiomirai/0.3.6/aiomirai-0.3.6-py3-none-any.whl

---
+++ packages/aiomirai/aiomirai-0.3.6-py3-none-any.whl/aiomirai/logger.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+
+default_handler = logging.StreamHandler(sys.stdout)
+default_handler.setFormatter(logging.Formatter(
+    '[%(asctime)s] [Mirai %(name)s] %(levelname)s: %(message)s'
+))
+
+
+def Logger(name: str, level: int) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.addHandler(default_handler)
+    logger.setLevel(level)
+    return logger
+
+
+Api = Logger('Api', logging.INFO)
+Receiver = Logger('Receiver', logging.INFO)
