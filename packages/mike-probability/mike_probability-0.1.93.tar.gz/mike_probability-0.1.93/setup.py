0c8b1c65bc402819c49d1ddb8a8ca53ceeb9f8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:28 +0000

    Add package mike-probability/0.1.93/mike_probability-0.1.93.tar.gz

---
+++ packages/mike-probability/mike_probability-0.1.93.tar.gz/mike_probability-0.1.93/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mike_probability',
+      version='0.1.93',
+      description='Gaussian and Binomial distributions ',
+      packages=['mike_probability'],
+      author= 'Micheal Akinkuotu',
+      author_email='michealakinkuotu73@gmail.com',
+      zip_safe=False)
