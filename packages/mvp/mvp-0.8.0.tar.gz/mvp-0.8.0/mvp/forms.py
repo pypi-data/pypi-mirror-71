7c43d25d9cc3aded27f30833e15553ccafe42d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:02 +0000

    Add package mvp/0.8.0/mvp-0.8.0.tar.gz

---
+++ packages/mvp/mvp-0.8.0.tar.gz/mvp-0.8.0/mvp/forms.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+# Standard library imports
+import os
+
+# Third party imports
+from psforms import *
+from psforms.validators import required
+from psforms.fields import *
+
+
+class PostRenderForm(Form):
+
+    meta = FormMetaData(
+        title='Post Render',
+        description='Post Render Callbacks',
+        header=False,
+        labels_on_top=False,
+        columns=2,
+    )
+
+
+class PlayblastForm(Form):
+
+    meta = FormMetaData(
+        header=True,
+        icon=os.path.join(os.path.dirname(__file__), 'icon.png'),
+        title='Review',
+        subforms_as_groups=True,
+    )
+    preset = StringOptionField('Preset', options=['Current Settings'])
+    capture_mode = ButtonOptionField(
+        'Capture Mode',
+        options=['sequence', 'snapshot'],
+        label_on_top=False
+    )
+    filename = SaveFileField('Filename', validators=(required,))
+    camera = StringOptionField('Camera')
+    resolution = Int2Field(
+        'Resolution',
+        range1=(0, 8192),
+        range2=(0, 8192),
+        default=(960, 540)
+    )
+    postrender = PostRenderForm()
+
+
+class NewPresetForm(Form):
+
+    meta = FormMetaData(
+        title='New Preset',
+        description='Create a new preset from the selected panel',
+        header=True,
+    )
+
+    panel = StringOptionField('Panel')
+    name = StringField('Preset Name', validators=(required,))
+
+
+class DelPresetForm(Form):
+
+    meta = FormMetaData(
+        title='Delete Preset',
+        description='Are you sure you want to delete this preset?',
+        header=True,
+    )
