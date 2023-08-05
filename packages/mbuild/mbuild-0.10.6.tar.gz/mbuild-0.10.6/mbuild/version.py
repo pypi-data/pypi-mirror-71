75c30337b3630e3b242104ca201d12460717db3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package mbuild/0.10.6/mbuild-0.10.6.tar.gz

---
+++ packages/mbuild/mbuild-0.10.6.tar.gz/mbuild-0.10.6/mbuild/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '0.10.6'
+short_version = '0.10.6'
+full_version = '0.10.6'
+git_revision = 'f948167a48dae6a604ae3065fbca1a3b9050366d'
+release = True
