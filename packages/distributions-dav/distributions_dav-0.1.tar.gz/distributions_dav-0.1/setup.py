58b072bf7467d9af8a5eb94d76a7d3fd298f1263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:21 +0000

    Add package distributions-dav/0.1/distributions_dav-0.1.tar.gz

---
+++ packages/distributions-dav/distributions_dav-0.1.tar.gz/distributions_dav-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_dav',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_dav'],
+      author = 'David Vindas',
+      author_email = 'isdavid506@gmail.com',
+      zip_safe=False)
