446cb4057bfa9d59d675f6176054e3feb22191c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:21 +0000

    Add package distributions-gandb/0.1/distributions_gandb-0.1.tar.gz

---
+++ packages/distributions-gandb/distributions_gandb-0.1.tar.gz/distributions_gandb-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_gandb',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_gandb'],
+      author='Siddhesh More',
+      zip_safe=False)
