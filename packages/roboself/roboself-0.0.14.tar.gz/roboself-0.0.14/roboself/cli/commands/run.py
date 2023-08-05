01c81d958bd9db4fafa816db4c69fc9ce93cefa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:56 +0000

    Add package roboself/0.0.14/roboself-0.0.14.tar.gz

---
+++ packages/roboself/roboself-0.0.14.tar.gz/roboself-0.0.14/roboself/cli/commands/run.py
@@ -0,0 +1,37 @@
+import os
+
+import click
+
+from roboself.client import Client
+from roboself.config import RawConfig
+from roboself.runtime import Runtime
+
+
+@click.command()
+def run():
+    """Runs the current skill.
+
+    Loads the local configuration and sends it to the runtime. Then it waits for calls
+    to execute actions.
+    """
+    click.echo("Loading configuration ...")
+
+    # TODO: fetch the skill name from env
+    raw_config = RawConfig(name="noname")
+    raw_config.load()
+
+    click.echo("Connecting to the roboself runtime ...")
+    # TODO: fetch the API Key from env
+    api_key = os.environ.get("ROBOSELF_API_KEY")
+    if not api_key:
+        print("No environment ROBOSELF_API_KEY variable found ")
+        return
+
+    client = Client(api_key)
+    runtime = Runtime(client=client, raw_config=raw_config)
+
+    click.echo("Connecting the skill runtime ...")
+    runtime.connect()
+
+    click.echo("Waiting for action requests ...")
+
