3655189eae8df3a98dbaf6bc215ab39e741d9b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:51 +0000

    Add package toga-android/0.3.0.dev20/toga-android-0.3.0.dev20.tar.gz

---
+++ packages/toga-android/toga-android-0.3.0.dev20.tar.gz/toga-android-0.3.0.dev20/toga_android/factory.py
@@ -0,0 +1,31 @@
+from .app import App, MainWindow
+from .paths import paths
+
+from .widgets.box import Box
+from .widgets.button import Button
+from .widgets.label import Label
+from .widgets.numberinput import NumberInput
+from .widgets.selection import Selection
+from .widgets.textinput import TextInput
+from .icons import Icon
+from .window import Window
+
+
+def not_implemented(feature):
+    print("[Android] Not implemented: {}".format(feature))
+
+
+__all__ = [
+    "App",
+    "Box",
+    "Button",
+    "Icon",
+    "Label",
+    "MainWindow",
+    "NumberInput",
+    "Selection",
+    "TextInput",
+    "Window",
+    "not_implemented",
+    "paths",
+]
