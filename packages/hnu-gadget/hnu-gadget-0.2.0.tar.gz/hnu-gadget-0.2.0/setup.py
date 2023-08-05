bbb393262e8b2e191501e6a89d6058d72d662be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:28 +0000

    Add package hnu-gadget/0.2.0/hnu-gadget-0.2.0.tar.gz

---
+++ packages/hnu-gadget/hnu-gadget-0.2.0.tar.gz/hnu-gadget-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hnu_gadget']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.23.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['hnu-gadget = hnu_gadget.main:main']}
+
+setup_kwargs = {
+    'name': 'hnu-gadget',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': '',
+    'author': 'renantamashiro',
+    'author_email': 'tamashirorenan@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://www.github.com/renantamashiro/hnu-gadget',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
