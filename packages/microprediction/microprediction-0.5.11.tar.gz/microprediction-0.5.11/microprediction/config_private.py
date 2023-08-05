1be88728dddb216d20ab058eeba8f7f82016e2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:16 +0000

    Add package microprediction/0.5.11/microprediction-0.5.11.tar.gz

---
+++ packages/microprediction/microprediction-0.5.11.tar.gz/microprediction-0.5.11/microprediction/config_private.py
@@ -0,0 +1,3 @@
+TRAFFIC_WRITE_KEY = 'a9f1566e48a366068bf933467noreal'      # <----  Use your own hereT
+THREE_BODY_WRITE_KEY = '3efc6d1e0c1c17603486f2d16notreal'  # <----  Use your own here
+BASE_URL = 'https://www.microprediction.org'
