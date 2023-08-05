6f1040f7c8c42b83cca6cbeb28728bff8a09e3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:47 +0000

    Add package mPowerGait/0.0.2/mPowerGait-0.0.2.tar.gz

---
+++ packages/mPowerGait/mPowerGait-0.0.2.tar.gz/mPowerGait-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mPowerGait',
+    version='0.0.2',
+    description='python package wrapper for mpower data pipeline',
+    py_modules=["pdkit_wrapper"],
+    package_dir={'': 'PDkit'},
+    install_requires=["numpy",
+                      "pandas==1.0.3",
+                      "scipy",
+                      "pdkit==1.2",
+                      "scikit-learn",
+                      "tsfresh",
+                      "matplotlib"]
+)
