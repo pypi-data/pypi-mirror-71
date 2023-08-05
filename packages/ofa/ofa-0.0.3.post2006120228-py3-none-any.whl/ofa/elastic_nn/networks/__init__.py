80d407496b907072bace7514c3a90a1a893ae841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:03 +0000

    Add package ofa/0.0.3-2006120228/ofa-0.0.3.post2006120228-py3-none-any.whl

---
+++ packages/ofa/ofa-0.0.3.post2006120228-py3-none-any.whl/ofa/elastic_nn/networks/__init__.py
@@ -0,0 +1,4 @@
+# Once for All: Train One Network and Specialize it for Efficient Deployment
+# Han Cai, Chuang Gan, Tianzhe Wang, Zhekai Zhang, Song Han
+# International Conference on Learning Representations (ICLR), 2020.
+
