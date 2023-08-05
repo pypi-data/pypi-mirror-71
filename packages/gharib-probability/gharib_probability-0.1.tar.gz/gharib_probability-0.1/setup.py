73ffe9c19a07761e430c604b23b16528b46628c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:45 +0000

    Add package gharib-probability/0.1/gharib_probability-0.1.tar.gz

---
+++ packages/gharib-probability/gharib_probability-0.1.tar.gz/gharib_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'gharib_probability',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages= ['gharib_probability'],
+      author = 'Ahmed Gharib',
+      author_email = 'a.gharib89@yahoo.com',
+      zip_safe = False)
