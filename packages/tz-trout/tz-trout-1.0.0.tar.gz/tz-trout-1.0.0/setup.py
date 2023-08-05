d8c000eee4094d83586031356fec6ab4487caeca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:21 +0000

    Add package tz-trout/1.0.0/tz-trout-1.0.0.tar.gz

---
+++ packages/tz-trout/tz-trout-1.0.0.tar.gz/tz-trout-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='tz-trout',
+    version='1.0.0',
+    url='http://github.com/closeio/tz-trout',
+    license='MIT',
+    author='Close.io',
+    author_email='engineering@close.io',
+    maintainer='Close.io',
+    maintainer_email='engineering@close.io',
+    description='Helps you figure out the time zone based on an address or a phone number.',
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=['tztrout',],
+    package_data={'tztrout': ['data/*']},
+    install_requires=['phonenumbers>=8.3.0', 'python-dateutil', 'pytz',],
+    tests_require=['mock', 'pytest'],
+)
