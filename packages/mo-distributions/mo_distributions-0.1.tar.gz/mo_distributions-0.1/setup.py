1fb7908709db0b7558df9dde001f97ded8736f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:29 +0000

    Add package mo-distributions/0.1/mo_distributions-0.1.tar.gz

---
+++ packages/mo-distributions/mo_distributions-0.1.tar.gz/mo_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mo_distributions',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['mo_distributions'],
+      author = 'Mojeed Akande',
+      author_email = 'mojeeedakande@gmail.com',
+      zip_safe=False)
