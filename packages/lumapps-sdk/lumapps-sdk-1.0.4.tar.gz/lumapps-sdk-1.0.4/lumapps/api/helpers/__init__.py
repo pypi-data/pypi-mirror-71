170c3da3adbf555475b0840ddc070722e30064e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:43 +0000

    Add package lumapps-sdk/1.0.4/lumapps-sdk-1.0.4.tar.gz

---
+++ packages/lumapps-sdk/lumapps-sdk-1.0.4.tar.gz/lumapps-sdk-1.0.4/lumapps/api/helpers/__init__.py
@@ -0,0 +1,16 @@
+from lumapps.api.helpers.widgets import (  # noqa
+    content_is_community,
+    content_is_template,
+    find_all_widgets,
+    find_all_widgets_and_containers,
+    find_widget,
+    find_widget_and_container,
+    new_lumapps_uuid,
+    set_new_lumapps_uuids,
+    copy_with_new_lumapps_uuids,
+    iter_with_key,
+    iter_with_key_value,
+    find_one_with_key_value,
+    replace_matching_key_val,
+    replace_key_val
+)
