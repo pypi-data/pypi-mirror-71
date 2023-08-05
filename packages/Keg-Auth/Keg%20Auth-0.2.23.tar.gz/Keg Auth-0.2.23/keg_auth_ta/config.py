37b9fdb9d128621363b2fc223ab3f3475be9d684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package Keg-Auth/0.2.23/Keg%20Auth-0.2.23.tar.gz

---
+++ packages/Keg-Auth/Keg%20Auth-0.2.23.tar.gz/Keg Auth-0.2.23/keg_auth_ta/config.py
@@ -0,0 +1,26 @@
+from blazeutils.strings import randchars
+
+
+class DefaultProfile(object):
+    SECRET_KEY = randchars()
+
+    # These three just get rid of warnings on the console.
+    KEG_KEYRING_ENABLE = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SITE_NAME = 'Keg Auth Demo'
+    SITE_ABBR = 'KA Demo'
+
+    AUTO_EXPAND_MENU = True
+
+
+class TestProfile(object):
+    # Make tests faster
+    PASSLIB_CRYPTCONTEXT_KWARGS = dict(schemes=['plaintext'])
+
+    MAIL_DEFAULT_SENDER = 'sender@example.com'
+
+    # These settings reflect what is needed in CI.  For local development, use
+    # keg_auth_ta-config.py to override.
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost/postgres'
