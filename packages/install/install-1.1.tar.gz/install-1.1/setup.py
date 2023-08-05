4ce9f2d1e503f9ad8737529b1052b540034b7a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:51 +0000

    Add package install/1.1/install-1.1.tar.gz

---
+++ packages/install/install-1.1.tar.gz/install-1.1/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "install",
+    version = "1.1",
+    author = "Eugene Kolo",
+    author_email = "eugene@kolobyte.com",
+    description = "Install packages from within code",
+    license = "MIT",
+    keywords = "install packages from within code",
+    url = "https://pypi.python.org/pypi/install",
+    packages=['install'],
+    scripts=['bin/install'],
+    long_description=read('README'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
