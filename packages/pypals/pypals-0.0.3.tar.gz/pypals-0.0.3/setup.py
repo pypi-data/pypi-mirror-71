6d08cada6015c29d57a4bc2a698b1bb0f07558eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:18 +0000

    Add package pypals/0.0.3/pypals-0.0.3.tar.gz

---
+++ packages/pypals/pypals-0.0.3.tar.gz/pypals-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+import os
+
+# with open("README.md", "r") as fh:
+    # long_description = fh.read()
+
+setup(
+    name="pypals",
+    version="0.0.3",
+    author="@byteface",
+    author_email="byteface@gmail.com",
+    description="Terminal buddies to run your python snippets",
+    # long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/byteface/pypals",
+    download_url = 'https://github.com/byteface/pypals/archive/0.0.3.tar.gz',
+    license="GNU General Public License v3.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["pypals"],
+    include_package_data=True
+)
+
+try:
+    os.mkdir('pypals') # create empty dir for user pypals at cwd
+except:
+    print('done!')
