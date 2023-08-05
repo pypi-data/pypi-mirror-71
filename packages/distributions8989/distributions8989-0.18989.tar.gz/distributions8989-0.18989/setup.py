ce138a4ac31d9cdef3149bb4cc5253ae28cd8882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package distributions8989/0.18989/distributions8989-0.18989.tar.gz

---
+++ packages/distributions8989/distributions8989-0.18989.tar.gz/distributions8989-0.18989/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions8989',
+      version='0.18989',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
