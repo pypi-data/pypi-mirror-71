9320c27775f7cb5d11f97c2cb420ff6d7ec2de59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:53 +0000

    Add package hp-money/1.0.8/hp_money-1.0.8.tar.gz

---
+++ packages/hp-money/hp_money-1.0.8.tar.gz/hp_money-1.0.8/README.md
@@ -0,0 +1,13 @@
+# hp_money
+
+>
+> version: 1.0.8
+>
+> author: Qingxu Kuang
+>
+> date: 2020-04-26
+>
+> update: 2020-06-12
+
+### Usage:
+    - To be continue
---
+++ packages/hp-money/hp_money-1.0.8.tar.gz/hp_money-1.0.8/hp_money/forex_rate/__init__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from decimal import (
+    Decimal,
+    ROUND_HALF_UP
+)
+
+
+class ForexRate(object):
+
+    def __init__(self, base_currency, target_currency, rate, origin=False):
+
+        # 汇率 = 原汇率 * 10的8次方
+        self.weight = Decimal('1') if origin else Decimal(pow(10, 8))
+        # 精度，保持小数点后0位
+        self.accuracy = Decimal('1')
+
+        self.rate = (Decimal(rate) * self.weight).quantize(
+            self.accuracy,
+            rounding=ROUND_HALF_UP
+        )
+        self.base_currency = base_currency
+        self.target_currency = target_currency
+
+    def __str__(self):
+
+        return f'1 {self.base_currency} = {self.rate} / {self.weight} {self.target_currency}'
+
+    def __repr__(self):
+
+        return self.__str__()
+
+    def reverse(self):
+
+        rate_value_reversed = (
+            Decimal('1') * self.weight / self.rate
+        ) * self.weight
+
+        rate_reversed = rate_value_reversed.quantize(
+            self.accuracy,
+            rounding=ROUND_HALF_UP,
+        ) / self.weight
+
+        return ForexRate(
+            self.target_currency,
+            self.base_currency,
+            rate_reversed
+        )
+
+    @property
+    def value(self):
+        return self.rate
