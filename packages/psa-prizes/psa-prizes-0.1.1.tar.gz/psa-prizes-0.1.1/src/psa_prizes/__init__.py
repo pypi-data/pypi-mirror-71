0f9acc33f5614341cc1595d15346a90a83ecb6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:42 +0000

    Add package psa-prizes/0.1.1/psa-prizes-0.1.1.tar.gz

---
+++ packages/psa-prizes/psa-prizes-0.1.1.tar.gz/psa-prizes-0.1.1/src/psa_prizes/__init__.py
@@ -0,0 +1,19 @@
+"""
+Entry point for the PSA Prizes package.
+This module imports all functions to make them easily callable for the end user.
+"""
+
+from psa_prizes.psa_scrape import *
+
+
+# Automate package version update.
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
