4896b36d1b0c6eeb31960a810210c54f87883ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:07 +0000

    Add package cmif/2020.6.16/cmif-2020.6.16-py3-none-any.whl

---
+++ packages/cmif/cmif-2020.6.16-py3-none-any.whl/cmif/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from . import demo
+from . import local
+from . import build
+from . import model
+from . import retrieve
+from . import authority
+from . import service
+from . import extract
