b6c5199a98c6311f668a318be2772dfb27e24615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:24 +0000

    Add package distributionsss/0.1/distributionsss-0.1.tar.gz

---
+++ packages/distributionsss/distributionsss-0.1.tar.gz/distributionsss-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsss',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsss'],
+      zip_safe=False)
