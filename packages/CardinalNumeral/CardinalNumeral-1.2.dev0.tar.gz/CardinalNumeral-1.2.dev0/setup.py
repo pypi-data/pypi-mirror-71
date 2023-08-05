4383518b0a5e627ce177b48e6ffeb154fb4e6e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:32 +0000

    Add package CardinalNumeral/1.2.dev0/CardinalNumeral-1.2.dev0.tar.gz

---
+++ packages/CardinalNumeral/CardinalNumeral-1.2.dev0.tar.gz/CardinalNumeral-1.2.dev0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(
+    name='CardinalNumeral',
+    version='1.2dev',
+    # packages=[
+	# 	'cardinal_numeral',
+	# 	'cardinal_numeral.language.english.sound',
+	# 	'cardinal_numeral.language.english',
+	# 	'cardinal_numeral.language.vietnamese',
+	# 	'cardinal_numeral.language.vietnamese.sound',
+	# 	'cardinal_numeral.tts'
+	# ],
+	packages = find_packages(include=['cardinal_numeral', 'cardinal_numeral.*']),
+    license='Coffee License',
+	package_dir={'cardinal_numeral': './cardinal_numeral'},
+	install_requires = [
+		'pygame', 'pyogg', 'tinytag'
+	],
+	include_package_data=True
+    # long_description=open('README.txt').read(),
+)
