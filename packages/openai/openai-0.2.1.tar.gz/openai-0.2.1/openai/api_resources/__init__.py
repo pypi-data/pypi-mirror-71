4253fb32648dd1fb09eb4bbea0369c312b669111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:29 +0000

    Add package openai/0.2.1/openai-0.2.1.tar.gz

---
+++ packages/openai/openai-0.2.1.tar.gz/openai-0.2.1/openai/api_resources/__init__.py
@@ -0,0 +1,5 @@
+from openai.api_resources.completion import Completion
+from openai.api_resources.engine import Engine
+from openai.api_resources.error_object import ErrorObject
+from openai.api_resources.snapshot import Snapshot
+from openai.api_resources.tag import Tag
