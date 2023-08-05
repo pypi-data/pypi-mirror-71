8ec1a409933ce388bb9235d7cfd32ef1d4f4404c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:23 +0000

    Add package theverse/0.1.1/theverse-0.1.1.tar.gz

---
+++ packages/theverse/theverse-0.1.1.tar.gz/theverse-0.1.1/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Change Log
+
+
+
+## v0.1.1 (2020-06-13)
+
+* Fixed missing subpackages in `setup.py`.
+
+
+
+## v0.1.0 (2020-06-13)
+
+* Initial release.
---
+++ packages/theverse/theverse-0.1.1.tar.gz/theverse-0.1.1/theverse/classes/dimensions.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020, Geoffrey M. Poore
+# All rights reserved.
+#
+# Licensed under the BSD 3-Clause License:
+# http://opensource.org/licenses/BSD-3-Clause
+#
+
+
+'''
+Units for dimensional analysis checks.
+'''
+
+
+import astropy.units.si as si
+
+
+mass = si.kg
+length = si.m
+time = si.s
+speed = length/time
