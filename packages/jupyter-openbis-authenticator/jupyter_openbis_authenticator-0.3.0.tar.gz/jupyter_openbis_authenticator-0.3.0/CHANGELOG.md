8b05704d8e3a17601527bd29d9c5abf2e2675826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:29 +0000

    Add package jupyter-openbis-authenticator/0.3.0/jupyter_openbis_authenticator-0.3.0.tar.gz

---
+++ packages/jupyter-openbis-authenticator/jupyter_openbis_authenticator-0.3.0.tar.gz/jupyter_openbis_authenticator-0.3.0/CHANGELOG.md
@@ -0,0 +1,8 @@
+## changes on 0.3.0.a
+
+first attempt to include kerberos
+
+## changes on 0.2.6
+
+* method logout_url() removed
+
---
+++ packages/jupyter-openbis-authenticator/jupyter_openbis_authenticator-0.3.0.tar.gz/jupyter_openbis_authenticator-0.3.0/setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='jupyter_openbis_authenticator',
+    version='0.3.0',
+    author='SIS | ID | ETH Zuerich',
+    author_email='swen@ethz.ch',
+    description='An authenticator for Jupyterhub which authenticates against openBIS.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://sissource.ethz.ch/sispub/jupyter-openbis-integration/',
+    license='Apache Software License Version 2.0',
+    packages=['jupyter_openbis_authenticator'],
+    install_requires=[
+        'pytest',
+        'jupyterhub>=0.8.0',
+        'pybis>=1.5.0'
+    ],
+    zip_safe=True
+)
