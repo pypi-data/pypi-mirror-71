b56e93505dfeec675ca916d663f763a18e98fe28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:59 +0000

    Add package daigle-dist/0.1.3/daigle_dist-0.1.3.tar.gz

---
+++ packages/daigle-dist/daigle_dist-0.1.3.tar.gz/daigle_dist-0.1.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='daigle_dist',
+      version="0.1.3",
+      author="Christopher Daigle",
+      author_email="pcjdaigle@gmail.com",
+      description="Gaussian and Binomial Distributions",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/ChristopherDaigle/daigle_dist.git",
+      packages=['daigle_dist'],
+      classifiers=["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent"],
+      python_requires='>=3.6',
+      zip_safe=False)
