33a8b6fa691ae52e80e13e9a470d2ac9f6f2ab6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:00 +0000

    Add package racm-distributions/0.1/racm_distributions-0.1.tar.gz

---
+++ packages/racm-distributions/racm_distributions-0.1.tar.gz/racm_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='racm_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['racm_distributions'],
+      author='Raquel Cerda',
+      zip_safe=False)
