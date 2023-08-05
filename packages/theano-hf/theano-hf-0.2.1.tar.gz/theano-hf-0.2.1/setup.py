ea7d18da5d6ec385836a5b908a82340258d3e22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:31 +0000

    Add package theano-hf/0.2.1/theano-hf-0.2.1.tar.gz

---
+++ packages/theano-hf/theano-hf-0.2.1.tar.gz/theano-hf-0.2.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="theano-hf",
+    install_requires=["theano", "numpy"],
+    version="0.2.1",
+    author="Kiruya Momochi",
+    author_email="kyaru@cock.li",
+    description="General purpose Hessian-free optimization in Theano",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KiruyaMomochi/theano-hf-py3",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
