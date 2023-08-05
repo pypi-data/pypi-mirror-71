c64d2b40171f44bd9d028ebd5cb8c11481423161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package facturapyme/1.0.0/facturapyme-1.0.0.tar.gz

---
+++ packages/facturapyme/facturapyme-1.0.0.tar.gz/facturapyme-1.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="facturapyme",
+    version="1.0.0",
+    author="Blackxel",
+    author_email="dev@blackxel.com",
+    description="Python SDK para integrar API de facturapy.me",
+    url="https://github.com/blackxel/facturapyme-sdk-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+)
