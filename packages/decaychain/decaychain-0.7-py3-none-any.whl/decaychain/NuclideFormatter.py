29086f35d05701128f7b2629ce70ae2969c77fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:20 +0000

    Add package decaychain/0.7/decaychain-0.7-py3-none-any.whl

---
+++ packages/decaychain/decaychain-0.7-py3-none-any.whl/decaychain/NuclideFormatter.py
@@ -0,0 +1,19 @@
+import re
+
+def NuclideFormatter(nuclide):
+    ## Formats a nuclide into the correct format for ICRP-07 matching.
+    user_nuclide = nuclide
+    ## finds the atomic mass
+    atomic_mass = int("".join(filter(str.isdigit, user_nuclide)))
+    ## regular expressions to find m and find element which must be in format "Aa".
+    meta_regex = r"[0-9]+[mn]"
+    element_regex = r"[A-Z][a-z]*"
+    meta = re.search(meta_regex,user_nuclide)
+    if meta:
+        meta_state = re.findall(meta_regex, user_nuclide)[0][-1]
+    else:
+        meta_state = ''
+    element = re.findall(element_regex, user_nuclide)[0]
+    element_f = element[0].upper() + element[1:].lower()
+    nuclide_out = str(element_f) + "-" + str(atomic_mass) +  meta_state
+    return nuclide_out
