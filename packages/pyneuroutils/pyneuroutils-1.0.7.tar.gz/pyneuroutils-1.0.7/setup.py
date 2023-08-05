a14d52a4eaf29a93c0f5d1f747850e4ec27697eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:52 +0000

    Add package pyneuroutils/1.0.7/pyneuroutils-1.0.7.tar.gz

---
+++ packages/pyneuroutils/pyneuroutils-1.0.7.tar.gz/pyneuroutils-1.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='pyneuroutils',
+    version='1.0.7',
+    packages=['pyneuroutils.statistics'],
+    url='https://github.com/xnejed07/pyneuroutils',
+    license='MIT',
+    author='Petr Nejedly',
+    author_email='xnejed07@gmail.com',
+    description='', install_requires=['numpy', 'scikit-learn', 'matplotlib', 'pandas']
+)
