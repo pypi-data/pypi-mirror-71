fe5613d0fc69226a055a4a2322509ae4a21cd173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:22 +0000

    Add package xeo-simple-calc/0.0.3/xeo_simple_calc-0.0.3-py3-none-any.whl

---
+++ packages/xeo-simple-calc/xeo_simple_calc-0.0.3-py3-none-any.whl/xeosimplecalc/__init__.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def float_divide(a, b):
+    return a / b
+def int_divide(a, b):
+    return int(a/b)
