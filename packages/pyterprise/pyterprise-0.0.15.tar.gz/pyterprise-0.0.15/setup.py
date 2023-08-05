3766a3dcfee8a29b08e61b8fa4461db36d2a6d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:20 +0000

    Add package pyterprise/0.0.15/pyterprise-0.0.15.tar.gz

---
+++ packages/pyterprise/pyterprise-0.0.15.tar.gz/pyterprise-0.0.15/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyterprise",
+    version="0.0.15",
+    author="James Fotherby",
+    author_email="fotherby1@gmail.com",
+    description="A small client library for the Terraform Enterprise REST API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Fotherbyy/PyTerprise",
+    install_requires=['requests'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
