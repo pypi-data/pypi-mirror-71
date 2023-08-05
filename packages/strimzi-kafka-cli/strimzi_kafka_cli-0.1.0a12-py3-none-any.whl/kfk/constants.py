f26b73f94eca83200de2d2e807d233939db4bfb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:36 +0000

    Add package strimzi-kafka-cli/0.1.0a12/strimzi_kafka_cli-0.1.0a12-py3-none-any.whl

---
+++ packages/strimzi-kafka-cli/strimzi_kafka_cli-0.1.0a12-py3-none-any.whl/kfk/constants.py
@@ -0,0 +1,17 @@
+import platform
+import sys
+
+from pathlib import Path
+
+BASE_FOLDER = ".strimzi-kafka-cli"
+BASE_PATH = str(Path.home()) + "/" + BASE_FOLDER
+STRIMZI_VERSION = "0.18.0"
+STRIMZI_PATH = BASE_PATH + "/strimzi-{version}".format(version=STRIMZI_VERSION)
+STRIMZI_RELEASE_URL = "https://github.com/strimzi/strimzi-kafka-operator/releases/download/{version}/strimzi-{version}.tar.gz".format(
+    version=STRIMZI_VERSION)
+KUBECTL_VERSION = "v1.18.0"
+KUBECTL_PATH = BASE_PATH + ("/kubectl" if platform.system().lower() != "windows" else "kubectl.exe")
+PROCESSOR_TYPE = "amd64" if sys.maxsize > 2 ** 32 else "386"
+KUBECTL_RELEASE_URL = "https://storage.googleapis.com/kubernetes-release/release/{version}/bin/{operating_system}/{processor_type}/kubectl".format(
+    version=KUBECTL_VERSION, operating_system=platform.system().lower(), processor_type=PROCESSOR_TYPE)
+SPACE = " "
