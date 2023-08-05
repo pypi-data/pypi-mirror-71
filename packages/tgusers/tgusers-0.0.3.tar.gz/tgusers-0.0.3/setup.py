d5189b2a7234d0be7c9aff660122e645550e8ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:25 +0000

    Add package tgusers/0.0.3/tgusers-0.0.3.tar.gz

---
+++ packages/tgusers/tgusers-0.0.3.tar.gz/tgusers-0.0.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from os import path
+
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+
+
+setup(
+    name='tgusers',
+    description="Mini framework for creating bots with tracking the user's position in the so-called \"rooms\". Based on aiogram.",
+    version='0.0.3',
+    url='https://github.com/drogi17/TgUsers',
+    author='ic_it',
+    author_email='',
+    license='GNU 3',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+    packages=['tgusers'],
+    install_requires=[
+        'aiogram==2.9.2',
+        'psycopg2==2.8.5',
+    ],
+    python_requires=">=3.6",
+)
