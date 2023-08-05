3901a55ac445cd1b58d92d970d3e1c43825ba0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:50 +0000

    Add package probability-dists/1.1/probability_dists-1.1.tar.gz

---
+++ packages/probability-dists/probability_dists-1.1.tar.gz/probability_dists-1.1/probability_dists/__init__.py
@@ -0,0 +1,3 @@
+from .GaussianDistribution import Gaussian
+from .BinomialDistribution import Binomial
+# TODO: import the Binomial class from the Binomialdistribution module
---
+++ packages/probability-dists/probability_dists-1.1.tar.gz/probability_dists-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_dists',
+      version='1.1',
+      description='Probability distributions: Gaussian & Binomial',
+      packages=['probability_dists'],
+      zip_safe=False)
