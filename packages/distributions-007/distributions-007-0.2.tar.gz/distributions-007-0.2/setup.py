173516d927687478e3b9732264cac60aed2c9279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:18 +0000

    Add package distributions-007/0.2/distributions-007-0.2.tar.gz

---
+++ packages/distributions-007/distributions-007-0.2.tar.gz/distributions-007-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-007',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions-007'],
+      zip_safe=False)
