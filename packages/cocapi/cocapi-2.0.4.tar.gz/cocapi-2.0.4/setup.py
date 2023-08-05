680f945eadd0bcf5f748b59ba62fcef90340e73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:35 +0000

    Add package cocapi/2.0.4/cocapi-2.0.4.tar.gz

---
+++ packages/cocapi/cocapi-2.0.4.tar.gz/cocapi-2.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools  # type: ignore
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cocapi",
+    version="2.0.4",
+    author="Tony Benoy",
+    setup_requires=['wheel'],
+    author_email="me@tonybenoy.com",
+    description="A python wrapper around clash of clans api",
+    long_description="A python wrapper around clash of clans api with support for filters",
+    long_description_content_type="text/markdown",
+    url="https://github.com/tonybenoy/cocapi",
+    install_requires=["httpx"],
+    keywords="Clash of Clans SuperCell API COC",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ),
+)
