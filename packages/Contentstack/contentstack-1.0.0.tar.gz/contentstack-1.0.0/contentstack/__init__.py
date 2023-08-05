e8fcdc6e278b36c66136e4dd2d982811638e4cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:07 +0000

    Add package Contentstack/1.0.0/contentstack-1.0.0.tar.gz

---
+++ packages/Contentstack/contentstack-1.0.0.tar.gz/contentstack-1.0.0/contentstack/__init__.py
@@ -0,0 +1,13 @@
+from .entry import Entry
+from .asset import Asset
+from .contenttype import ContentType
+from .https_connection import HTTPSConnection
+from .stack import Stack
+from .utility import Utils
+# from contentstack import *
+
+__author__ = 'Contentstack'
+__status__ = 'debug'
+__version__ = '1.0.0'
+__endpoint__ = 'cdn.contentstack.io'
+__email__ = 'mshaileshr@gmail.com'
