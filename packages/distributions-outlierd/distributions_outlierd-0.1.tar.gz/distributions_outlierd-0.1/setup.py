dc21660289d80c50e966fde5364601e0a67e1df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:58 +0000

    Add package distributions-outlierd/0.1/distributions_outlierd-0.1.tar.gz

---
+++ packages/distributions-outlierd/distributions_outlierd-0.1.tar.gz/distributions_outlierd-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_outlierd',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_outlierd'],
+      author='Deivid',
+      zip_safe=False)
