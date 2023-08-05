7f89fd1f1b0464124210a73d8d7b52abb3adcd8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:55 +0000

    Add package rulefit/0.3.1/rulefit-0.3.1.tar.gz

---
+++ packages/rulefit/rulefit-0.3.1.tar.gz/rulefit-0.3.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='rulefit',
+      version='0.3.1',
+      description='RuleFit algorithm',
+      author='Rohan George Thampi',
+      author_email='rohangt1591@gmail.com',
+      url='https://github.com/rohan-gt/rulefit',
+      packages=['rulefit'],
+      install_requires=[
+            'numpy>=1.16.1',
+            'pandas>=0.24.1',
+            'scikit-learn>=0.20.2']
+)
