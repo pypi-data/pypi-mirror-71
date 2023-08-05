c8afe9e7961cede262e163ac20416c951ec6d5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:08 +0000

    Add package GaBidistributions/1.2/GaBidistributions-1.2.tar.gz

---
+++ packages/GaBidistributions/GaBidistributions-1.2.tar.gz/GaBidistributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GaBidistributions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['GaBidistributions'],
+      author= 'bayode',
+      email='bayfag@yahoo.com',
+      zip_safe=False)
