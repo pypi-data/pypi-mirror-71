c45418d498a02db76c3ce2948db774dc9577b921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:04 +0000

    Add package odoo12-addon-theme-light/12.0.1.0.0.99.dev6/odoo12-addon-theme_light-12.0.1.0.0.99.dev6.tar.gz

---
+++ packages/odoo12-addon-theme-light/odoo12-addon-theme_light-12.0.1.0.0.99.dev6.tar.gz/odoo12-addon-theme_light-12.0.1.0.0.99.dev6/odoo/addons/theme_light/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2015-Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Houssine BAKKALI - <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Theme light",
+    "summary": "extract of the theme zen",
+    "license": "AGPL-3",
+    "category": "Website",
+    "version": "12.0.1.0.0",
+    "author": "Benjamin Dugardin," "Houssine BAKKALI," "Coop IT Easy SCRLfs",
+    "website": "https://www.coopiteasy.be",
+    "depends": ["base", "web", "website_theme_install"],
+    "data": ["views/layout_template.xml", "report/header_report_G002.xml"],
+    "installable": True,
+    "application": True,
+}
