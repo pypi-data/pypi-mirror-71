a461974dbb844cf4e6e7ceec19b30a2d13d55aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:40 +0000

    Add package RajatinoPackage/0.1/RajatinoPackage-0.1.tar.gz

---
+++ packages/RajatinoPackage/RajatinoPackage-0.1.tar.gz/RajatinoPackage-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='RajatinoPackage',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['RajatinoPackage'],
+      author = 'Rajat Baxi',
+      zip_safe=False)
