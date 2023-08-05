1f32f60ce604f5f691b85eb551fd645a47f4cbd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:48 +0000

    Add package geoparsepy/2.1.1/geoparsepy-2.1.1.tar.gz

---
+++ packages/geoparsepy/geoparsepy-2.1.1.tar.gz/geoparsepy-2.1.1/geoparsepy/__init__.py
@@ -0,0 +1,37 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+..
+	/////////////////////////////////////////////////////////////////////////
+	//
+	// (c) Copyright University of Southampton, 2020
+	//
+	// This software may not be used, sold, licensed, transferred, copied
+	// or reproduced in whole or in part in any manner or form or in or
+	// on any media by any person other than in accordance with the terms
+	// of the Licence Agreement supplied with the software, or otherwise
+	// without the prior written consent of the copyright owners.
+	//
+	// This software is distributed WITHOUT ANY WARRANTY, without even the
+	// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+	// PURPOSE, except where stated in the Licence Agreement supplied with
+	// the software.
+	//
+	// Created By : Stuart E. Middleton
+	// Created Date : 2020/06/12
+	// Created for Project : FloraGuard
+	//
+	/////////////////////////////////////////////////////////////////////////
+	//
+	// Dependancies : None
+	//
+	/////////////////////////////////////////////////////////////////////////
+	'''
+
+"""
+
+__all__ = ["geo_parse_lib","geo_preprocess_lib"]
+import geoparsepy.geo_parse_lib
+import geoparsepy.geo_preprocess_lib
+
