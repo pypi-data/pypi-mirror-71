97f2d05c595f5184f77f13fa1e8c081b6204ffae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:41 +0000

    Add package dsnd-proba/0.1/dsnd_proba-0.1.tar.gz

---
+++ packages/dsnd-proba/dsnd_proba-0.1.tar.gz/dsnd_proba-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_proba',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_proba'],
+      author = "Syed Mustaqhim",
+      author_email = "syedmustaqhim777@gmail.com", 
+      zip_safe=False)
