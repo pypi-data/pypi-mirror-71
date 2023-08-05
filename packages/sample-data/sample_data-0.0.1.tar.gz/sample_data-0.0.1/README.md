9efee35f2edcf3caa5581b8d63b18500f6a34fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:04 +0000

    Add package sample-data/0.0.1/sample_data-0.0.1.tar.gz

---
+++ packages/sample-data/sample_data-0.0.1.tar.gz/sample_data-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Sample data
+
+This is a collection of sample data for testing
---
+++ packages/sample-data/sample_data-0.0.1.tar.gz/sample_data-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+print(setuptools.find_packages())
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sample_data", # Replace with your own username
+    version="0.0.1",
+    author="Joel Horowitz",
+    author_email="joelhoro@gmail.com",
+    description="A few sample datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joelhoro",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
