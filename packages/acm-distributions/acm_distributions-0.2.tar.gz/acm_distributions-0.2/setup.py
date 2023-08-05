c75f471900316aa04a1334f062a6c8676364b697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:37 +0000

    Add package acm-distributions/0.2/acm_distributions-0.2.tar.gz

---
+++ packages/acm-distributions/acm_distributions-0.2.tar.gz/acm_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='acm_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['acm_distributions'],
+      zip_safe=False)
