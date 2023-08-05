b7bd30a11d2fb0e02db1df0d362c39988e9a30cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:13 +0000

    Add package collageradiomics/0.0.1a6/collageradiomics-0.0.1a6.tar.gz

---
+++ packages/collageradiomics/collageradiomics-0.0.1a6.tar.gz/collageradiomics-0.0.1a6/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="collageradiomics",
+    version="0.0.1a6",
+    author="Toth Technology",
+    author_email="toth-tech@hillyer.me",
+    description="CoLliage Implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Toth-Technology/rad-i",
+    py_modules=["collageradiomics"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scikit-learn',
+        'scikit-build',
+        'mahotas',
+        'scipy',
+        'SimpleITK'
+    ],
+    python_requires='>=3.7',
+)
