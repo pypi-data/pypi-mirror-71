b0897fe5aab6e03c71ad292c973dad2e5b6804db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:09 +0000

    Add package django-data-browser/2.0.2/django_data_browser-2.0.2-py3-none-any.whl

---
+++ packages/django-data-browser/django_data_browser-2.0.2-py3-none-any.whl/data_browser/models.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from django.conf import settings
+from django.db import models
+from django.http import QueryDict
+from django.utils import crypto, timezone
+
+
+def get_id():
+    return crypto.get_random_string(length=12)
+
+
+class View(models.Model):
+    class Meta:
+        permissions = [("make_view_public", "Can make a saved view publicly available")]
+
+    id = models.CharField(primary_key=True, max_length=12, default=get_id)
+    created_time = models.DateTimeField(default=timezone.now)
+
+    name = models.CharField(max_length=64, blank=False)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    public = models.BooleanField(default=False)
+    public_slug = models.CharField(max_length=12, default=get_id, blank=False)
+
+    model_name = models.CharField(max_length=32, blank=False)
+    fields = models.TextField(blank=True)
+    query = models.TextField(blank=True)
+
+    def get_query(self):
+        from .query import Query
+
+        return Query.from_request(self.model_name, self.fields, QueryDict(self.query))
+
+    def __str__(self):
+        return f"{self.model_name} view: {self.name}"
