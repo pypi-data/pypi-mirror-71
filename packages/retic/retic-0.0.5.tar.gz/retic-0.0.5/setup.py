8fd9f2d657e0d870e072928b66ed2dee480e4a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package retic/0.0.5/retic-0.0.5.tar.gz

---
+++ packages/retic/retic-0.0.5.tar.gz/retic-0.0.5/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='retic',
+    version='0.0.5',
+    license='MIT',
+    description='Fastest, easiest and simple web framework for Python.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Braian Staimer',
+    author_email='braianflorian@gmail.com',
+    url='https://github.com/reticpy/retic',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    data_files=[('mo', ['retic/locale/en_US/LC_MESSAGES/base.mo']),
+                ('mo', ['retic/locale/en_US/LC_MESSAGES/error_messages.po',
+                        'retic/locale/en_US/LC_MESSAGES/success_responses.po'])],
+    download_url='https://github.com/reticpy/retic/archive/0.0.5.tar.gz',
+    keywords=['PYTHON', 'WEB FRAMEWORK', 'MACHINE LEARNING WEB FRAMEWORK'],
+    install_requires=[
+        "environs>=7.4.0",
+        "httpmethods>=1.0.5",
+        "Werkzeug>=1.0.0",
+        "repath>=0.9.0"
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
