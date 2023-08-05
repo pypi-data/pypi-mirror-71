29bea797df64cf31483ce3aea44883c6866516b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:20 +0000

    Add package elfs/0.3.2/ELFS-0.3.2.tar.gz

---
+++ packages/elfs/ELFS-0.3.2.tar.gz/ELFS-0.3.2/setup.py
@@ -0,0 +1,21 @@
+import sys
+import elfs
+
+help_text = """```
+usage: elfs [options] [command [initial-arguments ...]]
+
+Easy Launcher For (the) Shell
+
+optional arguments:
+  -h, --help        show this help message and exit
+  -c                Add the command to your spellbook
+  -cc name desc rs  Add the command to your spellbook with comments
+  -t name template  Add the command to a new script from a template
+  -d path           Add a directory path to your config
+  -e .ext path      Add an extension and the path to an executable for it
+  -l, --list        List entire collection (or specify: cmd, dir, ext, files)
+  -s, --search      Search entire collection for command
+  -n, --dry-run     Print command instead of executing it
+```"""
+elfs.setup(help_text)
+
