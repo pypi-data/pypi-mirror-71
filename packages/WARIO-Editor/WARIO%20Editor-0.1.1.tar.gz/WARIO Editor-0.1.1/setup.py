0a966fdb2f9d325a34760303b2fbcc1910e82983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package WARIO-Editor/0.1.1/WARIO%20Editor-0.1.1.tar.gz

---
+++ packages/WARIO-Editor/WARIO%20Editor-0.1.1.tar.gz/WARIO Editor-0.1.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="WARIO Editor",
+    version="0.1.1",
+    author="Thomas Mudway, Oliver Cook, Ron Harwood",
+    description="Pipeline editor and front end for WARIO",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/McMasterRS/WARIO-Editor",
+    packages=setuptools.find_packages(),
+    package_data={
+        "":["*.json", "*.ui"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'PyQt5',
+        'PyQtWebEngine',
+        'blinker',
+        'wario'
+    ],
+    python_requires='>=3.6',
+)
