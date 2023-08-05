8b3a532ac5cc01ececae05f6a99cfc09be325968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:28 +0000

    Add package sy-probability/1.0/sy_probability-1.0.tar.gz

---
+++ packages/sy-probability/sy_probability-1.0.tar.gz/sy_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sy_probability',
+      version='1.0',
+      description='Gaussian distributions and Binomial distribution',
+      packages= ['sy_probability'],
+      author = 'Amol Ambekar',
+      author_email = 'amolambekar10.aa@gmail.com',
+    zip_safe=False)
