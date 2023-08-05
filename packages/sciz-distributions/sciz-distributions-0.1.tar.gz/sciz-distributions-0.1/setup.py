f3b692f4c787f71e8a934e509f5abe1b14eebc0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:09 +0000

    Add package sciz-distributions/0.1/sciz-distributions-0.1.tar.gz

---
+++ packages/sciz-distributions/sciz-distributions-0.1.tar.gz/sciz-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sciz-distributions',
+      version='0.1',
+      author= 'Satish Kumar Mishra',
+      description='Gaussian and Binomimal distributions',
+      packages=['sciz-distributions'],
+      zip_safe=False)
