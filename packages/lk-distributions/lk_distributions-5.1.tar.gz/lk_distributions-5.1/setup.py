5504a29793b5efb0cca8ecf1364e9adb1bb31d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package lk-distributions/5.1/lk_distributions-5.1.tar.gz

---
+++ packages/lk-distributions/lk_distributions-5.1.tar.gz/lk_distributions-5.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lk_distributions',
+      version='5.1',
+      description='Gaussian distributions',
+      packages=['lk_distributions'],
+      author = 'Lokesh Kumar',
+      author_email = 'hifilokesh@gmail.com',
+      zip_safe=False)
