4bafa294a519428e44629a042d2c7c8a8d0895c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:01 +0000

    Add package aksdp/0.0.1.post1/aksdp-0.0.1.post1.tar.gz

---
+++ packages/aksdp/aksdp-0.0.1.post1.tar.gz/aksdp-0.0.1.post1/aksdp/dataset/dataset.py
@@ -0,0 +1,35 @@
+from logging import getLogger
+from aksdp.data import Data
+from typing import List
+
+logger = getLogger(__name__)
+
+
+class DataSet:
+    def __init__(self):
+        self.data = {}
+
+    def put(self, name: str, d: Data):
+        self.data[name] = d
+
+    def get(self, name: str) -> Data:
+        return self.data[name]
+
+    def keys(self) -> List[str]:
+        return self.data.keys()
+
+    def merge(self, ds: "DataSet"):
+        for k, v in ds.data.items():
+            self.data[k] = v
+
+    def save_all(self):
+        for k, v in self.data.items():
+            logger.debug(f"save dataset[{k}]...")
+            v.save()
+
+    def apply(self, task) -> "DataSet":
+        return task.main(self)
+
+    def __str__(self) -> str:
+        keys = ",".join(self.keys())
+        return f"DataSet({keys})"
---
+++ packages/aksdp/aksdp-0.0.1.post1.tar.gz/aksdp-0.0.1.post1/aksdp/graph/__init__.py
@@ -0,0 +1,3 @@
+from .graph import Graph as Graph
+from .graph import TaskStatus as TaskStatus
+from .concurrent_graph import ConcurrentGraph as ConcurrentGraph
