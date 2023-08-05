84e05187c8ea2f0a4b5f2977b583ff07313d3c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:33 +0000

    Add package distributions-sm-udacity/0.1/distributions_sm_udacity-0.1.tar.gz

---
+++ packages/distributions-sm-udacity/distributions_sm_udacity-0.1.tar.gz/distributions_sm_udacity-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_sm_udacity',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_sm_udacity'],
+      author='smilchsack',
+      zip_safe=False)
