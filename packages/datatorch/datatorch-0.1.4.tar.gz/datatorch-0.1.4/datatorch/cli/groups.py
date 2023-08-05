f30f200381ebd54e688255ab7b18b1344e81fa4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:55 +0000

    Add package datatorch/0.1.4/datatorch-0.1.4.tar.gz

---
+++ packages/datatorch/datatorch-0.1.4.tar.gz/datatorch-0.1.4/datatorch/cli/groups.py
@@ -0,0 +1,12 @@
+import click
+
+from .main import commands as main_commands
+
+
+@click.group()
+def main():
+    pass
+
+
+main.add_command(main_commands.login)
+main.add_command(main_commands.agent)
