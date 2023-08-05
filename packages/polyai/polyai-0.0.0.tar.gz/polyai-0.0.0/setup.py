ede5a96c6ef89e1159d22b853f55df16dad01d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:49 +0000

    Add package polyai/0.0.0/polyai-0.0.0.tar.gz

---
+++ packages/polyai/polyai-0.0.0.tar.gz/polyai-0.0.0/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(name='polyai',
+      version='0.0.0',
+      description='Polyaxon ai.',
+      maintainer="Polyaxon, Inc.",
+      maintainer_email="contact@polyaxon.com",
+      author="Polyaxon, Inc.",
+      author_email="contact@polyaxon.com",
+      url="https://github.com/polyaxon/polyaxon",
+      license="Apache 2.0",
+      platforms="any",
+      packages=find_packages(),
+      keywords=[
+          'polyaxon',
+          'polyset',
+          'deep-learning',
+          'machine-learning',
+          'workflow',
+          'events',
+          'hooks',
+          'kubernetes',
+      ],
+      install_requires=[],
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ],
+      tests_require=[],
+      cmdclass={'test': PyTest})
