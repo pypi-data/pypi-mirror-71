1ad37952eb29ed3bed8f5df441092ee8f41ced50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:05 +0000

    Add package odoo12-addon-easy-my-coop-ch/12.0.1.0.2.99.dev12/odoo12_addon_easy_my_coop_ch-12.0.1.0.2.99.dev12-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-easy-my-coop-ch/odoo12_addon_easy_my_coop_ch-12.0.1.0.2.99.dev12-py2.py3-none-any.whl/odoo/addons/easy_my_coop_ch/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2018 Open Architects Consulting SPRL.
+# Copyright 2018      Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Houssine BAKKALI - <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Easy My Coop Switzerland",
+    "summary": "Easy My Coop Switzerland localization",
+    "version": "12.0.1.0.2",
+    "license": "AGPL-3",
+    "depends": ["easy_my_coop", "easy_my_coop_website", "l10n_ch"],
+    "author": "Coop IT Easy SCRLfs",
+    "category": "Cooperative management",
+    "website": "https://www.coopiteasy.be",
+    "description": """
+    This is the swiss localization for the easy my coop module
+    """,
+    "data": [
+        "views/subscription_template.xml",
+        # "views/subscription_request_view.xml",
+    ],
+    "installable": True,
+}
