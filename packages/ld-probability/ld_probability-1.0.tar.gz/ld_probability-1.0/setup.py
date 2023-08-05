d2c2dd383ddb38450168710da4acb6d8f5aba160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:04 +0000

    Add package ld-probability/1.0/ld_probability-1.0.tar.gz

---
+++ packages/ld-probability/ld_probability-1.0.tar.gz/ld_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ld_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ld_probability'],
+      author = 'Lakpa Dindu',
+      author_email = 'lakpadindu@hotmail.com',
+      zip_safe=False)
