ff385cae0e8b2038a362836965a82321a5f8dce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:19 +0000

    Add package doulma/0.1.0rc1/doulma-0.1.0rc1.tar.gz

---
+++ packages/doulma/doulma-0.1.0rc1.tar.gz/doulma-0.1.0rc1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="doulma",
+    version="0.1.0-rc-1",
+    description="A Light Weight Python Utility Library.",
+    author="HussainARK",
+    author_email="hussain.ark@yahoo.com",
+    license="MIT",
+    url="https://github.com/HussainARK/doulma",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
