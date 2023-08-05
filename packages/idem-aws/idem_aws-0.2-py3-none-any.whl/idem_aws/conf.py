6615230255720c227bd8dc798592e2f289763504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:13 +0000

    Add package idem-aws/0.2/idem_aws-0.2-py3-none-any.whl

---
+++ packages/idem-aws/idem_aws-0.2-py3-none-any.whl/idem_aws/conf.py
@@ -0,0 +1,16 @@
+CLI_CONFIG = {}
+CONFIG = {
+    "be_verbose": {
+        "dyne": "idem",
+        "action": "store_true",
+        "help": "List full instance information rather than just names",
+        "default": False,
+        "type": bool,
+    }
+}
+SUBCOMMANDS = {}
+DYNE = {
+    "acct": ["acct"],
+    "exec": ["exec"],
+    "states": ["states"],
+}
