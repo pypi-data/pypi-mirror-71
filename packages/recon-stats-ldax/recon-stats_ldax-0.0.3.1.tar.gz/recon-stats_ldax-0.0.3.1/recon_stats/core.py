58fbd38aefc7d1986fee4dbb0c6046554e049429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:58 +0000

    Add package recon-stats-ldax/0.0.3.1/recon-stats_ldax-0.0.3.1.tar.gz

---
+++ packages/recon-stats-ldax/recon-stats_ldax-0.0.3.1.tar.gz/recon-stats_ldax-0.0.3.1/recon_stats/core.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" core.py
+
+Main recon_stats code
+
+"""
+__author__ = 'Scott Burns <scott.s.burns@vanderbilt.edu>'
+__copyright__ = 'Copyright 2012 Vanderbilt University. All Rights Reserved'
+
+import os
+from os.path import isdir, join
+
+from .io import Parser
+
+
+class Subject(object):
+    def __init__(self, name):
+        self.name = name
+        try:
+            self.stat_dir = join(os.environ['SUBJECTS_DIR'], name, 'stats')
+        except KeyError:
+            raise ValueError("SUBJECTS_DIR is not set")
+        if not isdir(self.stat_dir):
+            raise ValueError("This subject doesn't have a 'stats' dir at {self.stat_dir}")
+
+    def get_measures(self):
+        measures = []
+        for root, d, fnames in os.walk(self.stat_dir):
+            for fname in fnames:
+                fullname = join(root, fname)
+                if Parser.can_parse(fullname):
+                    p = Parser(fullname)
+                    measures.extend(p.measures)
+        self.measures = measures
+
+    def upload_dict(self):
+        if not hasattr(self, 'measures'):
+            self.get_measures()
+        data = {}
+        for m in self.measures:
+            data[m.name()] = m.value_as_str()
+        return data
---
+++ packages/recon-stats-ldax/recon-stats_ldax-0.0.3.1.tar.gz/recon-stats_ldax-0.0.3.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Scott Burns <scott.s.burns@vanderbilt.edu>'
+
+import os
+from recon_stats import __version__
+
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    setup(name='recon-stats_ldax',
+        author='Scott Burns',
+        author_email='scott.s.burns@gmail.com',
+        description='recon-stats is a simple package to parse stats'
+                    ' files from Freesurfer\recon-all',
+        license='BSD (3-clause)',
+        url='http://github.com/sburns/recon-stats',
+        version=__version__,
+        download_url='http://github.com/sburns/recon-stats',
+        long_description='long_description',
+        packages=find_packages(),
+        platforms='any',
+        classifiers=(
+                #'Development Status :: 1 - Planning',
+                'Development Status :: 2 - Pre-Alpha',
+                # 'Development Status :: 3 - Alpha',
+                #'Development Status :: 4 - Beta',
+                #'Development Status :: 5 - Production/Stable',
+                #'Development Status :: 6 - Mature',
+                #'Development Status :: 7 - Inactive',
+                'Intended Audience :: Developers',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: BSD License',
+                'Topic :: Software Development',
+                'Topic :: Scientific/Engineering',
+                'Operating System :: POSIX',
+                'Operating System :: Unix',
+                'Operating System :: MacOS',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 3.6',
+                'Topic :: Scientific/Engineering',)
+        )
