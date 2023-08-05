a2977f89f427c0f6398b3a45fcb4b1f848f56fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:02 +0000

    Add package testpackagedjwihdowaoidoiwah/0.8.6/testpackagedjwihdowaoidoiwah-0.8.6.tar.gz

---
+++ packages/testpackagedjwihdowaoidoiwah/testpackagedjwihdowaoidoiwah-0.8.6.tar.gz/testpackagedjwihdowaoidoiwah-0.8.6/testpackagedjwihdowaoidoiwah/__init__.py
@@ -0,0 +1,1 @@
+from testpackagedjwihdowaoidoiwah.helloworld import helloworld
---
+++ packages/testpackagedjwihdowaoidoiwah/testpackagedjwihdowaoidoiwah-0.8.6.tar.gz/testpackagedjwihdowaoidoiwah-0.8.6/testpackagedjwihdowaoidoiwah/helloworld.py
@@ -0,0 +1,7 @@
+class helloworld:
+  def __init__(self):
+    pass
+  def hi(self):
+    print('hieii')
+if __name__ == "__main__":
+  helloworld().hi()
