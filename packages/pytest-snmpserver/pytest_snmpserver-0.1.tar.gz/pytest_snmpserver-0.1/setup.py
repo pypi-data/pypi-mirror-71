032c80ddb82351be5939556139320011ba312912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package pytest-snmpserver/0.1/pytest_snmpserver-0.1.tar.gz

---
+++ packages/pytest-snmpserver/pytest_snmpserver-0.1.tar.gz/pytest_snmpserver-0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pytest_snmpserver",
+    version="0.1",
+    packages=find_packages(),
+    long_description="SNMP server as a pytest plugin",
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    install_requires=[],
+    entry_points={"pytest11": ["pytest_snmpserver = pytest_snmpserver.pytest_plugin"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    )
