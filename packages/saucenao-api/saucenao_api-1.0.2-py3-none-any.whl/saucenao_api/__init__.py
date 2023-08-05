168da6afa179d97cd023117e1511c5c56e7cd922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:36 +0000

    Add package saucenao-api/1.0.2/saucenao_api-1.0.2-py3-none-any.whl

---
+++ packages/saucenao-api/saucenao_api-1.0.2-py3-none-any.whl/saucenao_api/__init__.py
@@ -0,0 +1,12 @@
+from .saucenao_api import SauceNao
+from .containers import BasicSauce, BookSauce, VideoSauce
+
+
+__author__ = 'nomnoms12'
+__copyright__ = 'Copyright 2020, Taiga.sh Development'
+__credits__ = ['nomnoms12', 'FujiMakoto']
+__license__ = 'GNU General Public License v3.0'
+__version__ = '1.0.2'
+__maintainer__ = 'nomnoms12'
+__email__ = 'alexander.ign0918@gmail.com'
+__status__ = 'Production'
