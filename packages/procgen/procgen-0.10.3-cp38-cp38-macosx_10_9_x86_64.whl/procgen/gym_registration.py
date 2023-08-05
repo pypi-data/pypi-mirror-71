87646e8db98566740cef891071ed390debd86956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:38 +0000

    Add package procgen/0.10.3/procgen-0.10.3-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/procgen/procgen-0.10.3-cp38-cp38-macosx_10_9_x86_64.whl/procgen/gym_registration.py
@@ -0,0 +1,23 @@
+from gym.envs.registration import register
+from gym3 import ToGymEnv, ViewerWrapper, ExtractDictObWrapper
+from .env import ENV_NAMES, ProcgenGym3Env
+
+
+def make_env(render=False, **kwargs):
+    if render:
+        kwargs["render_mode"] = "rgb_array"
+    env = ProcgenGym3Env(num=1, num_threads=0, **kwargs)
+    env = ExtractDictObWrapper(env, key="rgb")
+    if render:
+        env = ViewerWrapper(env, tps=15, info_key="rgb")
+    gym_env = ToGymEnv(env)
+    return gym_env
+
+
+def register_environments():
+    for env_name in ENV_NAMES:
+        register(
+            id=f'procgen-{env_name}-v0',
+            entry_point='procgen.gym_registration:make_env',
+            kwargs={"env_name": env_name},
+        )
