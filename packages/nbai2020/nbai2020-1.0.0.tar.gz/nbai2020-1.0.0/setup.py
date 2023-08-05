15e228c8d1abd35f54b416bba2538d493debeb0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:47 +0000

    Add package nbai2020/1.0.0/nbai2020-1.0.0.tar.gz

---
+++ packages/nbai2020/nbai2020-1.0.0.tar.gz/nbai2020-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name='nbai2020',
+        version='1.0.0',
+        py_modules=['nbai2020'],
+        author='hhpython',
+        author_email='2475061609@qq.com',
+        ur1='https://github.com/desion/tidy_page',
+        description='A simple printer of nested lists'
+      )
