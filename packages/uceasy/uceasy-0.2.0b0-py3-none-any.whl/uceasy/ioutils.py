36cb6215cd5217c21cdc0e22b34fec9d37061fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:33 +0000

    Add package uceasy/0.2.0b0/uceasy-0.2.0b0-py3-none-any.whl

---
+++ packages/uceasy/uceasy-0.2.0b0-py3-none-any.whl/uceasy/ioutils.py
@@ -0,0 +1,30 @@
+import configparser
+import csv
+import os
+from typing import List, Dict, Optional
+
+
+def load_csv(path: str, delimiter: str = ",") -> List[List[str]]:
+    """Read the csv file content and return a list of rows."""
+    with open(path, "r") as f:
+        reader = csv.reader(f, delimiter=delimiter)
+
+        # Ignore header
+        next(reader)
+
+        return [row for row in reader]
+
+
+def dump_config_file(path: str, config: Dict[str, Dict[str, Optional[str]]]) -> None:
+    """Read a dictionary and create a .ini style configuration file."""
+    parser = configparser.ConfigParser(delimiters=(":"), allow_no_value=True)
+    parser.optionxform = str  # type: ignore
+    parser.read_dict(config)
+
+    with open(path, "w") as fl:
+        parser.write(fl, space_around_delimiters=False)
+
+
+def get_taxa_from_contigs(contigs: str) -> int:
+    """Return the taxa number from the contigs directory."""
+    return len([name for name in os.listdir(contigs)])
