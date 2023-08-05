e79c58e005b036681e035709e59f95568e186845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package gausbin-dist/1.0/gausbin_dist-1.0.tar.gz

---
+++ packages/gausbin-dist/gausbin_dist-1.0.tar.gz/gausbin_dist-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gausbin_dist',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['gausbin_dist'],
+      author='Lovro Vrcek',
+      zip_safe=False)
