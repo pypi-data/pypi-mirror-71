9668356ea962f490281a6a126bb02bef5838577d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:02 +0000

    Add package ont-fast5-api/3.1.4/ont-fast5-api-3.1.4.tar.gz

---
+++ packages/ont-fast5-api/ont-fast5-api-3.1.4.tar.gz/ont-fast5-api-3.1.4/ont_fast5_api/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '3.1.4'
+__version_info__ = tuple([int(num) for num in __version__.split('.')])
+CURRENT_FAST5_VERSION = 2.0
+
+# Set up a default NullHandler in case we don't end up using another one
+# Taken from http://docs.python-guide.org/en/latest/writing/logging/
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+from ont_fast5_api.compression_settings import register_plugin
+register_plugin()
