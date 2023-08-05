f8e64fa5d5729bef1cb714a1ea00382807d0935c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:11 +0000

    Add package Impostor/2.0.0/Impostor-2.0.0.tar.gz

---
+++ packages/Impostor/Impostor-2.0.0.tar.gz/Impostor-2.0.0/setup.py
@@ -0,0 +1,125 @@
+"""
+Shamelessly copied from django's setup.py and edited to fit
+"""
+import codecs
+import os
+import sys
+
+from setuptools import find_packages
+
+from distutils.command.install import INSTALL_SCHEMES
+from distutils.command.install_data import install_data
+from distutils.core import setup
+
+
+class OsxInstallData(install_data):
+    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
+    # for this in distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
+
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to the
+        # fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+
+if sys.platform == "darwin":
+    cmdclasses = {'install_data': OsxInstallData}
+else:
+    cmdclasses = {'install_data': install_data}
+
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+impostor_dir = 'impostor'
+
+for dirpath, dirnames, filenames in os.walk(impostor_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+# Small hack for working with bdist_wininst.
+# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
+if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
+    for file_info in data_files:
+        file_info[0] = '\\PURELIB\\%s' % file_info[0]
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name="Impostor",
+    version="2.0.0",
+    url='https://github.com/avallbona/Impostor/',
+    author='Marko Samastur',
+    author_email='markos@gaivo.net',
+    maintainer='Andreu Vallbona',
+    maintainer_email='avallbona@gmail.com',
+    description='Staff can login as a different user.',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    platforms=['any'],
+    packages=find_packages(exclude=['tests*']),
+    cmdclass=cmdclasses,
+    data_files=data_files,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities'
+    ],
+    install_requires=[
+        'Django>=1.11',
+    ],
+)
