e1b6703b72917f85111d72a827ff5c9dd346af36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package ft-udacity-dist/0.1/ft_udacity_dist-0.1.tar.gz

---
+++ packages/ft-udacity-dist/ft_udacity_dist-0.1.tar.gz/ft_udacity_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ft_udacity_dist',
+      version='0.1',
+      description='Gaussian and Binomial Distributions - Udacity Course',
+      packages=['ft_udacity_dist'],
+      zip_safe=False)
