651c7a0d9f55276d0a506ba4c90aae5a152b7f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:38 +0000

    Add package rediz/0.6.0/rediz-0.6.0.tar.gz

---
+++ packages/rediz/rediz-0.6.0.tar.gz/rediz-0.6.0/rediz/rediz_config_not_used.py
@@ -0,0 +1,21 @@
+
+
+
+GRAPH_COLLIDER_CONFIG = {"password":"jebBj8iyfcGQt7iaPcXEtSPyMZSV6NFB",
+                         "host":"redis-15521.c6721.us-east-1-mz.ec2.cloud.rlrcp.com",
+                         "port":"15521",
+                         "delays":[55],
+                         "max_ttl":5*60,
+                         "transactions_ttl":5*60,
+                         "num_predictions":1000,
+                         "obscurity":"gc-obscure"}
+
+REDIZ_TEST_CONFIG = {"password":"5r70RHtd2m0x3BHdLE8mp7APxoIJS1aH",
+                     "host":"redis-19709.c11.us-east-1-3.ec2.cloud.redislabs.com",
+                     "port":"19709",
+                     "delays": [1, 5],
+                     "obscurity": "0e4da9eb-obscurity-testing-only-e07adfersadfdf893c"
+                     }
+
+
+REDIZ_CONFIG = GRAPH_COLLIDER_CONFIG
