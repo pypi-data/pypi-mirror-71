f982685c3c361e2ea4a522238f346f5c4537a15f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:55 +0000

    Add package teradatasqlalchemy/17.0.0.0/teradatasqlalchemy-17.0.0.0-py3-none-any.whl

---
+++ packages/teradatasqlalchemy/teradatasqlalchemy-17.0.0.0-py3-none-any.whl/teradatasqlalchemy/data_type_converter.py
@@ -0,0 +1,18 @@
+# Copyright 2018 by Teradata Corporation. All rights reserved.
+
+#from teradata.datatypes import DefaultDataTypeConverter
+#
+#class TDDataTypeConverter(DefaultDataTypeConverter):
+#
+#    def __init__(self, *args, **kwargs):
+#        super(TDDataTypeConverter, self).__init__(*args, **kwargs)
+#
+#    def _process_data_type(self, dataType):
+#        if 'INTERVAL' in dataType:
+#            return dataType.replace('_', ' ')
+#        return dataType
+#
+#    def convertValue(self, dbType, dataType, typeCode, value):
+#        dataType = self._process_data_type(dataType)
+#        return super(TDDataTypeConverter, self).convertValue(
+#            dbType, dataType, typeCode, value)
---
+++ packages/teradatasqlalchemy/teradatasqlalchemy-17.0.0.0-py3-none-any.whl/teradatasqlalchemy/vernumber.py
@@ -0,0 +1,3 @@
+# Copyright 2020 by Teradata Corporation. All rights reserved.
+
+sVersionNumber = "17.0.0.0"
