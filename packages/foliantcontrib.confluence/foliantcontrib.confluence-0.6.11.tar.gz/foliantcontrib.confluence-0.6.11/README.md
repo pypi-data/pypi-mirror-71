b1d34208fbfa2dca978014ef7a396f3f06791867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:37 +0000

    Add package foliantcontrib.confluence/0.6.11/foliantcontrib.confluence-0.6.11.tar.gz

---
+++ packages/foliantcontrib.confluence/foliantcontrib.confluence-0.6.11.tar.gz/foliantcontrib.confluence-0.6.11/README.md
@@ -0,0 +1,367 @@
+[![](https://img.shields.io/pypi/v/foliantcontrib.confluence.svg)](https://pypi.org/project/foliantcontrib.confluence/) [![](https://img.shields.io/github/v/tag/foliant-docs/foliantcontrib.confluence.svg?label=GitHub)](https://github.com/foliant-docs/foliantcontrib.confluence)
+
+# Confluence backend for Foliant
+
+![Confluence page built with Foliant](https://raw.githubusercontent.com/foliant-docs/foliantcontrib.confluence/master/img/confluence.png)
+
+Confluence backend generates confluence articles and uploads them on your confluence server. It can create and edit pages in Confluence with content based on your Foliant project.
+
+It also has a feature of restoring the user inline comments, added for the article, even after the commented fragment was changed.
+
+This backend adds the `confluence` target for your Foliant `make` command.
+
+## Installation
+
+```bash
+$ pip install foliantcontrib.confluence
+```
+
+> The backend requires [Pandoc](https://pandoc.org/) to be installed on your system. Pandoc is needed to convert Markdown into HTML.
+
+## Usage
+
+To upload a Foliant project to Confluence server use `make confluence` command:
+
+```bash
+$ foliant make confluence
+Parsing config... Done
+Making confluence... Done
+────────────────────
+Result:
+https://my_confluence_server.org/pages/viewpage.action?pageId=123 (Page Title)
+```
+
+## Config
+
+You have to set up the correct config for this backend to work properly.
+
+Specify all options in `backend_config.confluence` section:
+
+```yaml
+backend_config:
+  confluence:
+    host: 'https://my_confluence_server.org'
+    login: user
+    password: user_password
+    id: 124443
+    title: Title of the page
+    space_key: "~user"
+    parent_id: 124442
+    parent_title: Parent
+    test_run: false
+    notify_watchers: false
+    toc: false
+    restore_comments: true
+    resolve_if_changed: false
+    pandoc_path: pandoc
+    codeblocks:
+        ...
+```
+
+`host`
+:   **Required** Host of your confluence server.
+
+`login`
+:   Login of the user who has permissions to create and update pages. If login is not supplied, it will be prompted during the build.
+
+`password`
+:   Password of the user. If the password is not supplied, it will be prompted during the build.
+
+`id`
+:   ID of the page where the content will be uploaded. *Only for already existing pages*
+
+`title`
+:   Title of the page to be created or updated.
+
+> Remember that page titles in the space have to be unique.
+
+`space_key`
+:   The space key where the page(s) will be created/edited. *Only for not yet existing pages*.
+
+`parent_id`
+:   ID of the parent page under which the new one(s) should be created. *Only for not yet existing pages*.
+
+`parent_title`
+:   Another way to define the parent of the page. Lower priority than `paren_di`. Title of the parent page under which the new one(s) should be created. The parent should exist under the space_key specified. *Only for not yet existing pages*.
+
+`test_run`
+:   If this option is true, Foliant will prepare the files for uploading to Confluence, but won't actually upload them. Use this option for testing your content before upload. The prepared files can be found in `.confluencecache/debug` folder. Default: `false`
+
+`notify_watchers`
+:   If `true` — watchers will be notified that the page has changed. Default: `false`
+
+`toc`
+:   Set to `true` to add a table of contents to the beginning of the document. Default: `false`
+
+`restore_comments`
+:   Attempt to restore inline comments near the same places after updating the page. Default: `true`
+
+`resolve_if_changed`
+:   Delete inline comment from the source if the commented text was changed. This will automatically mark the comment as resolved. Default: `false`
+
+`pandoc_path`
+:   Path to Pandoc executable (Pandoc is used to convert Markdown into HTML).
+
+`codeblocks`
+:   Configuration for converting Markdown code blocks into code-block macros. See details in **Code blocks processing** sections.
+
+## User's guide
+
+### Uploading articles
+
+By default, if you specify `id` or `space_key` and `title` in foliant.yml, the whole project will be built and uploaded to this page.
+
+If you wish to upload separate chapters into separate articles, you need to specify the respective `id` or `space_key` and `title` in *meta section* of the chapter.
+
+Meta section is a YAML-formatted field-value section in the beginning of the document, which is defined like this:
+
+```yaml
+---
+field: value
+field2: value
+---
+
+Your chapter md-content
+```
+
+If you want to upload a chapter into confluence, add its properties under the `confluence` key like this:
+
+```yaml
+---
+confluence:
+    title: My confluence page
+    space_key: "~user"
+---
+
+You chapter md-content
+```
+
+> **Important notice!**
+> Both modes work together. If you specify the `id1` in foliant.yml and `id2` in chapter's meta — the whole project will be uploaded to the page with `id1`, and the specific chapter will also be uploaded to the page with `id2`.
+
+### Creating pages
+
+If you want a new page to be created for content in your Foliant project, just supply in foliant.yml the space key and a title that does not yet exist in this space. Remember that in Confluence page titles are unique inside one space. If you use a title of an already existing page, the backend will attempt to edit it and replace its content with your project.
+
+Example config for this situation is:
+
+```yaml
+backend_config:
+  confluence:
+    host: https://my_confluence_server.org
+    login: user
+    password: pass
+    title: My unique title
+    space_key: "~user"
+```
+
+Now if you change the title in your config, confluence will *create a new page with the new title*, leaving the old one intact.
+
+If you want to change the title of your page, the answer is in the following section.
+
+### Updating pages
+
+Generally to update the page contents you may use the same config you used to create it (see the previous section). If the page with a specified title exists, it will be updated.
+
+Also, you can just specify the id of an existing page. After build its contents will be updated.
+
+```yaml
+backend_config:
+  confluence:
+    host: https://my_confluence_server.org
+    login: user
+    password: pass
+    id: 124443
+```
+
+This is also *the only* way to edit a page title. If `title` param is specified, the backend will attempt to change the page's title to the new one:
+
+```yaml
+backend_config:
+  confluence:
+    host: https://my_confluence_server.org
+    login: user
+    password: pass
+    id: 124443
+    title: New unique title
+```
+
+### Updating part of a page
+
+Confluence backend can also upload an article into the middle of a Confluence page, leaving all the rest of it intact. To do this you need to add an *Anchor* into your page in the place where you want Foliant content to appear.
+
+1. Go to Confluence web interface and open the article.
+2. Go to Edit mode.
+3. Put the cursor in the position where you want your Foliant content to be inserted and start typing `{anchor` to open the macros menu and locate the Anchor macro.
+4. Add an anchor with the name `foliant`.
+5. Save the page.
+
+Now if you upload content into this page (see two previous sections), Confluence backend will leave all text which was before and after the anchor intact, and add your Foliant content in the middle.
+
+You can also add two anchors: `foliant_start` and `foliant_end`. In this case, all text between these anchors will be replaced by your Foliant content.
+
+### Inserting raw confluence tags
+
+If you want to supplement your page with confluence macros or any other storage-specific HTML, you may do it by wrapping them in the `<raw_confluence></raw_confluence>` tag.
+
+For example, if you wish to add a table of contents into the middle of the document for some reason, you can do something like this:
+
+```html
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit dolorem nulla quam doloribus delectus voluptate.
+
+<raw_confluence><ac:structured-macro ac:macro-id="1" ac:name="toc" ac:schema-version="1"/></raw_confluence>
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, laboriosam cumque soluta sequi blanditiis, voluptatibus quaerat similique nihil debitis repellendus.
+```
+
+### Code blocks processing
+
+Since 0.6.9 backend converts Markdown code blocks into Confluence code-block macros. You can tune the macros appearance by specifying some options in `codeblocks` config section of Confluence backend
+
+```yaml
+backend_config:
+    confluence:
+        codeblocks:  # all are optional
+            theme: django
+            title: Code example
+            linenumbers: false
+            collapse: false
+```
+
+
+`theme`
+:   Color theme of the code blocks. Should be one of:
+
+* `emacs`,
+* `django`,
+* `fadetogrey`,
+* `midnight`,
+* `rdark`,
+* `eclipse`,
+* `confluence`.
+
+`title`
+:   Title of the code block.
+
+`linenumbers`
+:   Show line numbers in code blocks. Default: `false`
+
+`collapse`
+:   Collapse code blocks into a clickable bar. Default: `false`
+
+Right now Foliant only converts code blocks by backticks/tildes (tabbed code blocks are ignored for now):
+
+~~~
+This code block will be converted:
+
+```python
+def test2():
+    pass
+ ```
+~~~
+
+```
+And this:
+~~~
+def test3():
+    pass
+~~~
+```
+
+Syntax name, defined after backticks/tildes is converted into its Confluence counterpart. Right now following syntaxes are supported:
+
+* `actionscript`,
+* `applescript`,
+* `bash`,
+* `c`,
+* `c`,
+* `coldfusion`,
+* `cpp`,
+* `cs`,
+* `css`,
+* `delphi`,
+* `diff`,
+* `erlang`,
+* `groovy`,
+* `html`,
+* `java`,
+* `javascript`,
+* `js`,
+* `perl`,
+* `php`,
+* `powershell`,
+* `python`,
+* `xml`,
+* `yaml`.
+
+## Credits
+
+The following wonderful tools and libraries are used in foliantcontrib.confluence:
+
+- [Atlassian Python API wrapper](https://github.com/atlassian-api/atlassian-python-api),
+- [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/),
+- [PyParsing](https://github.com/pyparsing/pyparsing).
+
+# Confluence Preprocessor for Foliant
+
+Confluence preprocessor allows inserting content from Confluence server into your Foliant project.
+
+## Installation
+
+```bash
+$ pip install foliantcontrib.confluence
+```
+
+## Config
+
+To enable the preprocessor, add `confluence` to `preprocessors` section in the project config:
+
+```yaml
+preprocessors:
+    - confluence
+```
+
+The preprocessor has a number of options:
+
+```yaml
+preprocessors:
+    - confluence:
+        host: https://my_confluence_server.org
+        login: user
+        password: user_password
+        space_key: "~user"
+        pandoc_path: pandoc
+```
+
+`host`
+:   **Required** Host of your confluence server. If not stated — it would be taken from Confluence backend config.
+
+`login`
+:   Login of the user who has permissions to create and update pages. If login is not supplied, it would be taken from backend config, or prompted during the build.
+
+`password`
+:   Password of the user. If password is not supplied, it would be taken from backend config, or prompted during the build.
+
+`space_key`
+:   The space key where the page titles will be searched for.
+
+`pandoc_path`
+:   Path to Pandoc executable (Pandoc is used to convert Confluence content into Markdown).
+
+## Usage
+
+Add a `<confluence></confluence>` tag at the position in the document where the content from Confluence should be inserted. The page is defined by its `id` or `title`. If you are specifying page by title, you will also need to set `space_key` either in tag or in the preprocessor options.
+
+```html
+The following content is imported from Confluence:
+
+<confluence id="12345"></confluence>
+
+This is from Confluence too, but determined by page title (space key is defined in preprocessor config):
+
+<confluence title="My Page"></confluence>
+
+Here we are overriding space_key:
+
+<confluence space_key="ANOTHER_SPACE" title="My Page"></confluence>
+```
---
+++ packages/foliantcontrib.confluence/foliantcontrib.confluence-0.6.11.tar.gz/foliantcontrib.confluence-0.6.11/foliant/backends/confluence/confluence.py
@@ -0,0 +1,298 @@
+import shutil
+import os
+
+from pathlib import Path
+from getpass import getpass
+
+from atlassian import Confluence
+
+from foliant.utils import spinner, output
+from foliant.backends.base import BaseBackend
+from foliant.meta.generate import load_meta
+from foliant.preprocessors import flatten, unescapecode
+from foliant.preprocessors.utils.combined_options import (Options, val_type)
+
+from .classes import Page
+from .convert import (md_to_editor, process_images, confluence_unescape,
+                      editor_to_storage, add_comments, add_toc, set_up_logger,
+                      update_attachments, unique_name)
+
+# disabling confluence logger because it litters up output
+from unittest.mock import Mock
+import atlassian.confluence
+atlassian.confluence.log = Mock()
+
+
+CACHEDIR_NAME = '.confluencecache'
+ATTACHMENTS_DIR_NAME = 'attachments'
+ESCAPE_DIR_NAME = 'escaped'
+DEBUG_DIR_NAME = 'debug'
+REMOTE_ATTACHMENTS_DIR_NAME = 'remote_attachments'
+
+
+class BadParamsException(Exception):
+    pass
+
+
+def get_content_id_by_title(con: Confluence, title: str, space_key: str):
+    if not space_key:
+        raise BadParamsException('You have to add space_key if you specify '
+                                 'paret by title!')
+    try:
+        p = con.get_page_by_title(space_key, title)
+        if p and 'id' in p:
+            return p['id']
+    except:
+        pass
+    raise BadParamsException(f'Cannot find parent with title {title}')
+
+
+class Backend(BaseBackend):
+    _flat_src_file_name = '__all__.md'
+
+    targets = ('confluence')
+
+    required_preprocessors_after = [
+        'unescapecode',
+        {
+            'confluence_final': {
+                'cachedir': CACHEDIR_NAME,
+                'escapedir': ESCAPE_DIR_NAME
+            }
+        }
+    ]
+
+    defaults = {'mode': 'single',
+                'toc': False,
+                'pandoc_path': 'pandoc',
+                'restore_comments': True,
+                'resolve_if_changed': False,
+                'notify_watchers': False,
+                'test_run': False}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._cachedir = (self.project_path / CACHEDIR_NAME).resolve()
+        self._cachedir.mkdir(exist_ok=True)
+
+        self._debug_dir = self._cachedir / DEBUG_DIR_NAME
+        shutil.rmtree(self._debug_dir, ignore_errors=True)
+        self._debug_dir.mkdir(exist_ok=True)
+
+        self._flat_src_file_path = self._cachedir / self._flat_src_file_name
+        self._attachments_dir = self._cachedir / ATTACHMENTS_DIR_NAME
+        config = self.config.get('backend_config', {}).get('confluence', {})
+        self.options = {**self.defaults, **config}
+
+        self.logger = self.logger.getChild('confluence')
+
+        self.logger.debug(f'Backend inited: {self.__dict__}')
+        set_up_logger(self.logger)
+
+    def backup_debug_info(self):
+        '''Copy debug files from the cachedir to debug dir'''
+        _, _, files = next(os.walk(self._cachedir))
+        for file in files:
+            new_name = unique_name(self._debug_dir, file)
+            shutil.move(self._cachedir / file, self._debug_dir / new_name)
+
+    def _get_options(self, *configs) -> Options:
+        '''
+        Get a list of dictionaries, all of which will be merged in one and
+        transfered to an Options object with necessary checks.
+
+        Returns the resulting Options object.
+        '''
+        options = {}
+        for config in configs:
+            options.update(config)
+        options = Options(options,
+                          validators={'host': val_type(str),
+                                      'login': val_type(str),
+                                      'password': val_type(str),
+                                      'id': val_type([str, int]),
+                                      'parent_id': val_type([str, int]),
+                                      'title': val_type(str),
+                                      'space_key': val_type(str),
+                                      'pandoc_path': val_type(str),
+                                      },
+                          required=[('id',),
+                                    ('title', 'space_key')])
+        return options
+
+    def _connect(self, host: str, login: str, password: str) -> Confluence:
+        """Connect to Confluence server and test connection"""
+        self.logger.debug(f'Trying to connect to confluence server at {host}')
+        host = host.rstrip('/')
+        self.con = Confluence(host, login, password)
+        try:
+            res = self.con.get('rest/api/space')
+        except UnicodeEncodeError:
+            raise RuntimeError('Sorry, non-ACSII passwords are not supported')
+        if isinstance(res, str) or 'statusCode' in res:
+            raise RuntimeError(f'Cannot connect to {host}:\n{res}')
+
+    def _upload(self,
+                config: Options or dict,
+                content: str,
+                filename: str or Path):
+        '''Upload one md-file to Confluence. Filename needed to fix the images'''
+        title = config.get('title')
+        parent_id = None
+        if 'id' not in config:
+            if 'parent_id' in config:
+                parent_id = config['parent_id']
+            elif 'parent_title' in config:
+                parent_id = get_content_id_by_title(self.con,
+                                                    config['parent_title'],
+                                                    config.get('space_key'))
+
+        page = Page(self.con,
+                    config.get('space_key'),
+                    title,
+                    parent_id,
+                    config.get('id'))
+
+        new_content = md_to_editor(content, self._cachedir, config['pandoc_path'])
+
+        self.logger.debug('Converting HTML to Confluence storage format')
+        new_content = editor_to_storage(self.con, new_content)
+        with open(self._cachedir / '2_storage.html', 'w') as f:
+            f.write(new_content)
+        new_content, attachments = process_images(new_content,
+                                                  Path(filename).parent,
+                                                  self._attachments_dir)
+        if not config['test_run']:
+            update_attachments(page,
+                               attachments,
+                               self._cachedir / REMOTE_ATTACHMENTS_DIR_NAME)
+        new_content = confluence_unescape(new_content, self._cachedir / ESCAPE_DIR_NAME)
+
+        if config['toc']:
+            new_content = add_toc(new_content)
+
+        with open(self._cachedir / '3_unescaped_with_images.html', 'w') as f:
+            f.write(new_content)
+
+        if config['restore_comments']:
+            new_content = add_comments(page,
+                                       new_content,
+                                       config['resolve_if_changed'])
+
+        need_update = page.need_update(new_content, title)
+        with open(self._cachedir / '4_to_upload.html', 'w') as f:
+            f.write(new_content)
+        if need_update:
+            self.logger.debug('Ready to upload. Content saved for debugging to '
+                              f'{self._cachedir / "to_upload.html"}')
+            minor_edit = not config['notify_watchers']
+            if not config['test_run']:
+                page.upload_content(new_content, title, minor_edit)
+        else:
+            self.logger.debug(f'Page with id {page.id} and title "{page.title}"'
+                              " hadn't changed. Skipping.")
+
+        self.backup_debug_info()
+
+        if config['test_run']:
+            return 'TEST RUN ' + ("* " * need_update) + '{host}/pages/viewpage.action?pageId={id} ({title})'\
+                .format(host=config["host"].rstrip('/'), id=page.id, title=page.title)
+        else:
+            return ("* " * need_update) + '{host}/pages/viewpage.action?pageId={id} ({title})'\
+                .format(host=config["host"].rstrip('/'), id=page.id, title=page.title)
+
+    def _build(self):
+        '''
+        Main method. Builds confluence XHTML document from flat md source and
+        uploads it into the confluence server.
+        '''
+        self._connect(self.options['host'],
+                      self.options['login'],
+                      self.options['password'])
+        result = []
+        if 'id' in self.options or ('title' in self.options and 'space_key' in self.options):
+            self.logger.debug('Uploading flat project to confluence')
+            output(f'Building main project', self.quiet)
+
+            flatten.Preprocessor(
+                self.context,
+                self.logger,
+                self.quiet,
+                self.debug,
+                {'flat_src_file_name': self._flat_src_file_name,
+                 'keep_sources': True}
+            ).apply()
+
+            unescapecode.Preprocessor(
+                self.context,
+                self.logger,
+                self.quiet,
+                self.debug,
+                {}
+            ).apply()
+
+            shutil.move(self.working_dir / self._flat_src_file_name,
+                        self._flat_src_file_path)
+
+            with open(self._flat_src_file_path, encoding='utf8') as f:
+                md_source = f.read()
+                options = self._get_options(self.options)
+
+                self.logger.debug(f'Options: {options}')
+            result.append(self._upload(options, md_source, self._flat_src_file_path))
+
+        self.logger.debug('Searching metadata for confluence properties')
+
+        chapters = self.config['chapters']
+        meta = load_meta(chapters, self.working_dir)
+        for section in meta.iter_sections():
+
+            if 'confluence' not in section.data or \
+                    not isinstance(section.data['confluence'], dict):
+                self.logger.debug(f'No "confluence" section in {section}), skipping.')
+                continue
+
+            # getting common options from foliant.yml and merging them with meta fields
+            common_options = {}
+            uncommon_options = ['title', 'id', 'space_key', 'parent_id']
+            common_options = {k: v for k, v in self.options.items()
+                              if k not in uncommon_options}
+            try:
+                options = self._get_options(common_options, section.data['confluence'])
+            except Exception as e:
+                # output(f'Skipping section {section}, wrong params: {e}', self.quiet)
+                self.logger.debug(f'Skipping section {section}, wrong params: {e}')
+                continue
+            self.logger.debug(f'Building {section.chapter.filename}: {section.title}')
+            output(f'Building {section.title}', self.quiet)
+            md_source = section.get_source()
+
+            self.logger.debug(f'Options: {options}')
+            original_file = self.project_path / section.chapter.filename
+            result.append(self._upload(options, md_source, original_file))
+        if result:
+            return '\n' + '\n'.join(result)
+        else:
+            return 'nothing to upload'
+
+    def make(self, target: str) -> str:
+        with spinner(f'Making {target}', self.logger, self.quiet, self.debug):
+            output('', self.quiet)  # empty line for better output
+            try:
+                self.options = Options(self.options, required=['host'])
+                if "login" not in self.options:
+                    msg = f"Please input login for {self.options['host']}:\n"
+                    msg = '\n!!! User input required !!!\n' + msg
+                    self.options['login'] = input(msg)
+                if "password" not in self.options:
+                    msg = f"Please input password for {self.options['login']}:\n"
+                    msg = '\n!!! User input required !!!\n' + msg
+                    self.options['password'] = getpass(msg)
+                if target == 'confluence':
+                    return self._build()
+                else:
+                    raise ValueError(f'Confluence cannot make {target}')
+
+            except Exception as exception:
+                raise type(exception)(f'Build failed: {exception}')
