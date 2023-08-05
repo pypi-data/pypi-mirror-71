d7ef723c3466610cf91fec29e4c009379db03e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:00 +0000

    Add package frds/0.2.1/frds-0.2.1.tar.gz

---
+++ packages/frds/frds-0.2.1.tar.gz/frds-0.2.1/frds/data/data_manager.py
@@ -0,0 +1,146 @@
+import os
+from multiprocessing.shared_memory import SharedMemory
+from multiprocessing.managers import SharedMemoryManager
+from multiprocessing import Manager
+from importlib import import_module
+from numpy import recarray, copyto, dtype
+import pandas as pd
+from .dataset import Dataset
+from typing import NewType, Tuple, List, Dict
+
+SharedMemoryInfo = NewType(
+    'SharedMemoryInfo', Tuple[SharedMemory, tuple, dtype])
+
+
+class DataManager:
+
+    def __init__(self, obs=1000, config=None):
+        self._datasets = dict()
+        self.smm = SharedMemoryManager()
+        self.smm.start()
+        self.result = Manager().list()
+        self.conns = dict()
+        self.obs = obs
+        self.config = config
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.shutdown()
+
+    def shutdown(self):
+        self.smm.shutdown()
+        for conn in self.conns.values():
+            conn.close()
+
+    def _add_to_shared_memory(self, nparray: recarray) -> SharedMemory:
+        """Internal function to copy an array into shared memory.
+
+        Parameters
+        ----------
+        nparray : recarray
+            The array to be copied into shared memory.
+
+        Returns
+        -------
+        SharedMemoryName
+            The shared memory object.
+        """
+        shm = self.smm.SharedMemory(nparray.nbytes)
+        array = recarray(shape=nparray.shape, dtype=nparray.dtype, buf=shm.buf)
+        copyto(array, nparray)
+        return shm
+
+    def _download_dataset(self, dataset: Dataset) -> pd.DataFrame:
+        """Internal function to download the dataset.
+
+        Parameters
+        ----------
+        dataset : Dataset
+            Dataset information including source, library, table, vars, etc.
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame of the downloaded dataset.
+        """
+        # TODO: generic login data for different data sources
+        if dataset.source == 'wrds':
+            usr = self.config.get('wrds_username')
+            pwd = self.config.get('wrds_password')
+        # If there exists a connection for the data source, use it!
+        if (conn := self.conns.get(dataset.source, None)) is None:
+            module = import_module(f'frds.data.{dataset.source}')
+            conn = module.Connection(usr=usr, pwd=pwd)
+            self.conns.update({dataset.source: conn})
+        df = conn.get_table(library=dataset.library,
+                            table=dataset.table,
+                            columns=dataset.vars,
+                            date_cols=dataset.date_vars,
+                            obs=self.obs)
+        return df
+
+    def get_dataset(self, dataset: Dataset) -> SharedMemoryInfo:
+        """Get the shared memory object, shape and dtype of the given dataset.
+
+        Parameters
+        ----------
+        dataset : Dataset
+            Dataset information including source, library, table, vars, etc.
+
+        Returns
+        -------
+        SharedMemoryInfo
+            (SharedMemory, tuple, numpy.dtype), where the tuple is the shape.
+        """
+        if dataset in self._datasets:
+            return self._datasets.get(dataset)
+        dataset_name = f'{dataset.source}_{dataset.library}_{dataset.table}'
+        path = os.path.join(self.config.get('data_dir'), f'{dataset_name}.dta')
+        # If local data directory has the table
+        if os.path.exists(path):
+            df = next(pd.read_stata(path, chunksize=1))
+            # If required variables are all in the table
+            if set(dataset.vars).issubset(set(df.columns)):
+                df = pd.read_stata(path, columns=dataset.vars)
+            # Some variables are missing and need to be downloaded
+            else:
+                # Should find out the missing variables and only download them
+                # But since we don't know the identifiers, might as well just
+                # download everything and merge with existing one.
+                df = self._download_dataset(dataset)
+                df_in_datadir = pd.read_stata(path)
+                common_cols = [col for col in df.columns if col in
+                               df_in_datadir.columns]
+                df_to_save = df.merge(df_in_datadir, on=common_cols)
+                df_to_save.to_stata(path, write_index=False,
+                                    convert_dates={v: 'td' for v in dataset.date_vars})
+                del df_in_datadir
+                del df_to_save
+        # No such table in the local data directory
+        else:
+            # Download the entire table from source
+            df = self._download_dataset(dataset)
+            # Store the table in local data directory
+            df.to_stata(path, write_index=False, convert_dates={
+                v: 'td' for v in dataset.date_vars})
+        nparray = df.to_records(index=False)
+        shm = self._add_to_shared_memory(nparray)
+        self._datasets.update({dataset: (shm, nparray.shape, nparray.dtype)})
+        return shm, nparray.shape, nparray.dtype
+
+    def get_datasets(self, datasets: List[Dataset]) -> Dict[Dataset, SharedMemoryInfo]:
+        """Return a dictionary of {Dataset: SharedMemoryInfo} given a list of datasets.
+
+        Parameters
+        ----------
+        datasets : List[Dataset]
+            List of requried datasets.
+
+        Returns
+        -------
+        Dict[Dataset, SharedMemoryInfo]
+            Dictionary of Dataset to SharedMemoryInfo.
+        """
+        return {dataset: self.get_dataset(dataset) for dataset in datasets}
---
+++ packages/frds/frds-0.2.1.tar.gz/frds-0.2.1/frds/run.py
@@ -0,0 +1,79 @@
+import os
+from .ra import RA
+from .data import DataManager, Dataset
+from .data.data_manager import SharedMemoryInfo
+from frds.measures import *
+from frds import result_dir, data_dir, wrds_password, wrds_username
+from typing import List, Dict, Tuple, Set
+import inspect
+import frds
+
+
+def main(measures=None, gui=False, config=None, progress_callback=None):
+    if config is None:
+        config = dict(
+            wrds_username=wrds_username,
+            wrds_password=wrds_password,
+            result_dir=str(result_dir),
+            data_dir=str(data_dir),
+        )
+    if gui:
+        from importlib import import_module
+        measures = [import_module(
+            f'frds.measures.{measure}') for measure in measures]
+        progress = progress_callback.emit
+    else:
+        measures = [measure for _, measure in inspect.getmembers(
+            frds.measures, inspect.ismodule)]
+        progress = print
+
+    research_assistants = []
+
+    # Consolidate data sources and make one request for one table
+    progress('Checking datasets required for estimation...')
+    datasets = set(dataset for m in measures for dataset in m.datasets)
+    sources = set(dataset.source for dataset in datasets)
+    progress(f'Data sources required: {",".join(sources)}.')
+    progress('Preparing datasets...')
+    tables_required = set((dta.source, dta.library, dta.table)
+                          for m in measures for dta in m.datasets)
+    table_vars: Dict[Tuple, Set] = {table: set() for table in tables_required}
+    table_dates: Dict[Tuple, Set] = {table: set() for table in tables_required}
+    for table in tables_required:
+        for dta in datasets:
+            if (dta.source, dta.library, dta.table) == table:
+                table_vars.get(table).update(dta.vars)
+                table_dates.get(table).update(dta.date_vars)
+    datasets = []
+    for table in tables_required:
+        src, lib, tablename = table
+        datasets.append(Dataset(source=src,
+                                library=lib,
+                                table=tablename,
+                                vars=table_vars.get(table),
+                                date_vars=table_dates.get(table)))
+    progress('Creating data manager...')
+    with DataManager(obs=-1, config=config) as dm:
+        progress('Loading datasets...')
+        shminfo: Dict[Dataset, SharedMemoryInfo] = dm.get_datasets(datasets)
+        progress('Starting RAs...')
+        os.makedirs(config.get('result_dir'), exist_ok=True)
+        for m in measures:
+            # find the required tables for this measure m
+            tables_required = [(dta.source, dta.library, dta.table)
+                               for dta in m.datasets]
+            # find the related dataset in shared memory
+            datasets_related = {dta: shm for dta, shm in shminfo.items() if
+                                (dta.source, dta.library, dta.table) in tables_required}
+            task = datasets_related, m.name, m.datasets, m.estimate
+            (ra := RA(task, config)).start()
+            research_assistants.append(ra)
+            progress(f'RA for {m.name} has started working...')
+        progress('RAs are working on estimation...')
+        for ra in research_assistants:
+            ra.join()
+    progress(f'Completed! Results saved in {config.get("result_dir")}')
+
+
+if __name__ == "__main__":
+    main()
