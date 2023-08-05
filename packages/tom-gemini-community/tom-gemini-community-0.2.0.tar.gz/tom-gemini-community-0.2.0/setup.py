c840a940dc4d7c6f6d627a377fbbf7e76359b66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:33 +0000

    Add package tom-gemini-community/0.2.0/tom-gemini-community-0.2.0.tar.gz

---
+++ packages/tom-gemini-community/tom-gemini-community-0.2.0.tar.gz/tom-gemini-community-0.2.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='tom-gemini-community',
+    version='0.2.0',
+    author="Bryan Miller",
+    author_email="millerwbryan@gmail.com",
+    description="Gemini Observatory Community TOM Broker Modules",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TOMToolkit/tom_gemini_community",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    keywords=['tomtoolkit', 'astronomy', 'astrophysics', 'cosmology', 'science', 'fits', 'observatory', 'gemini'],
+    install_requires=[
+        'tomtoolkit',
+        'gsselect'
+    ]
+)
