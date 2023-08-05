ace763422d6e24ed7f52c130e167392100866fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:24 +0000

    Add package gibson2/0.0.5/gibson2-0.0.5-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/gibson2/gibson2-0.0.5-cp27-cp27mu-manylinux1_x86_64.whl/gibson2/core/render/build/CMakeFiles/GLFWRendererContext.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GLFWRendererContext.dir/glad/gl.cpp.o"
+  "CMakeFiles/GLFWRendererContext.dir/cpp/glfw_mesh_renderer.cpp.o"
+  "GLFWRendererContext.pdb"
+  "GLFWRendererContext.cpython-36m-x86_64-linux-gnu.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GLFWRendererContext.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ packages/gibson2/gibson2-0.0.5-cp27-cp27mu-manylinux1_x86_64.whl/gibson2/core/render/build/CMakeFiles/MeshRendererContext.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fei/Development/gibsonv2/gibson2/core/render/cpp/Mesh_renderer.cpp" "/home/fei/Development/gibsonv2/gibson2/core/render/build/CMakeFiles/MeshRendererContext.dir/cpp/Mesh_renderer.cpp.o"
+  "/home/fei/Development/gibsonv2/gibson2/core/render/glad/egl.cpp" "/home/fei/Development/gibsonv2/gibson2/core/render/build/CMakeFiles/MeshRendererContext.dir/glad/egl.cpp.o"
+  "/home/fei/Development/gibsonv2/gibson2/core/render/glad/gl.cpp" "/home/fei/Development/gibsonv2/gibson2/core/render/build/CMakeFiles/MeshRendererContext.dir/glad/gl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_GLAD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../glad"
+  "../glfw/include"
+  "../pybind11/include"
+  "/home/fei/anaconda2/envs/torch4/include/python3.6m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
