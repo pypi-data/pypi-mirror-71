5558928cf5c86bf5d9f964593b1e7890c87793a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:07 +0000

    Add package pyaware/3.1.0/pyaware-3.1.0-py3-none-any.whl

---
+++ packages/pyaware/pyaware-3.1.0-py3-none-any.whl/pyaware/mqtt/factories.py
@@ -0,0 +1,31 @@
+from typing import Callable
+
+
+def get_factory(factory: dict) -> Callable:
+    return globals()[factory["name"]]
+
+
+def telemetry_v1(data, timestamp, **kwargs) -> dict:
+    msg = {
+        "dateTime": timestamp,
+        "raw_values": data,
+        "parameterValues": []
+    }
+    for name, values in data.items():
+        msg["parameterValues"].append(
+            {"parameterName": name,
+             "data": {
+                 "sum": sum(values.values()),
+                 "latest": values[max(values)],
+                 "samples": len(values)
+             }}
+        )
+    return msg
+
+
+def telemetry_v2(data, meta, timestamp, **kwargs) -> dict:
+    msg = meta.copy()
+    msg["timestamp"] = timestamp
+    msg["values"] = data
+    msg["raw_values"] = data
+    return msg
