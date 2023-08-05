76afff28f139b32e4a6bf18f3ff7aa37467ce21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:26 +0000

    Add package polyencryption/0.0.8/polyencryption-0.0.8.tar.gz

---
+++ packages/polyencryption/polyencryption-0.0.8.tar.gz/polyencryption-0.0.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+readme = open("README.md", "r")
+changes = open("CHANGELOG.txt", "r")
+long_description = readme.read() + '\n\n' + changes.read()
+readme.close()
+changes.close()
+
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='polyencryption',
+    version='0.0.8',
+    description='a simple encryption algorithm that encrypts .txt files using a polyalphabetic cypher. NOTE- this software is not intended for or should be used for security purposes',
+    long_description=long_description,
+    url='https://github.com/skelegorg/polyencryption',
+    author='Andrew C',
+    author_email='cumminand@outlook.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='encryption',
+    packages=find_packages(),
+    install_requires=['secrets>=3']
+)
