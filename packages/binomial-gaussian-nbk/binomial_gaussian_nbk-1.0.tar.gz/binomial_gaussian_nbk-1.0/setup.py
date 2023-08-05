8c11092ee3e5e984373aa811e22797f3990aead6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:15 +0000

    Add package binomial-gaussian-nbk/1.0/binomial_gaussian_nbk-1.0.tar.gz

---
+++ packages/binomial-gaussian-nbk/binomial_gaussian_nbk-1.0.tar.gz/binomial_gaussian_nbk-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='binomial_gaussian_nbk',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['binomial_gaussian_nbk'],
+      author = 'Nishant Bhaskar',
+      email = 'open.nishant@gmail.com',
+      zip_safe=False)
