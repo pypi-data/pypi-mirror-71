d4aa238feb2ee0a9ca8a2b2e36c93e144a2ed079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:05 +0000

    Add package ljz-distributions/0.1/ljz_distributions-0.1.tar.gz

---
+++ packages/ljz-distributions/ljz_distributions-0.1.tar.gz/ljz_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ljz_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Jinze Li',
+      author_email = 'lijinze0704@gmail.com',
+      zip_safe=False)
