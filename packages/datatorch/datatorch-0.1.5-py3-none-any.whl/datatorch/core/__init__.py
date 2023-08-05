bdcf75a8209aa3bbf0dfabbce00392928aae81a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:17 +0000

    Add package datatorch/0.1.5/datatorch-0.1.5-py3-none-any.whl

---
+++ packages/datatorch/datatorch-0.1.5-py3-none-any.whl/datatorch/core/__init__.py
@@ -0,0 +1,8 @@
+import click
+
+from datatorch.core.settings import Settings
+
+__all__ = ["Settings", "LOG_STRING", "BASE_URL"]
+
+LOG_STRING = click.style("DataTorch", fg="red", bold=True)
+BASE_URL = "https://datatorch.io"
---
+++ packages/datatorch/datatorch-0.1.5-py3-none-any.whl/datatorch/core/folder.py
@@ -0,0 +1,20 @@
+import os
+
+from datatorch.utils.files import mkdir_exists
+from datatorch.core import env
+
+
+def global_path(mkdir=True):
+    user_dir = os.path.join(os.path.expanduser("~"), ".datatorch")
+    config_dir = os.getenv(env.CONFIG_DIR) or user_dir
+    if mkdir:
+        mkdir_exists(config_dir)
+    return config_dir
+
+
+def local_path(mkdir=True):
+    root_dir = os.getcwd()
+    path = os.path.join(root_dir, ".datatorch")
+    if mkdir:
+        mkdir_exists(path)
+    return path
