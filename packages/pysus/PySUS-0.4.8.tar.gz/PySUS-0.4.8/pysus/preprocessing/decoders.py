2376d15546aecdc13960d4c0a6c70432f0aecc07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:56 +0000

    Add package pysus/0.4.8/PySUS-0.4.8.tar.gz

---
+++ packages/pysus/PySUS-0.4.8.tar.gz/PySUS-0.4.8/pysus/preprocessing/decoders.py
@@ -0,0 +1,99 @@
+#-*- coding:utf-8 -*-
+u"""
+This module contains a set of functions to decode
+commonly encoded variables
+Created on 19/07/16
+by fccoelho
+license: GPL V3 or Later
+"""
+
+__docformat__ = 'restructuredtext en'
+import numpy as np
+from datetime import timedelta
+
+@np.vectorize
+def decodifica_idade_SINAN(idade, unidade='Y'):
+    """
+    Em tabelas do SINAN frequentemente a idade é representada como um inteiro que precisa ser parseado
+    para retornar a idade em uma unidade cronológica padrão.
+    :param unidade: unidade da idade: 'Y': anos, 'M' meses, 'D': dias, 'H': horas
+    :param idade: inteiro ou sequencia de inteiros codificados.
+    :return:
+    """
+    fator = {'Y': 1., 'M': 12., 'D': 365., 'H': 365*24.}
+    if idade >= 4000: #idade em anos
+        idade_anos = idade - 4000
+    elif idade >= 3000 and idade < 4000: #idade em meses
+        idade_anos = (idade-3000)/12.
+    elif idade >= 2000 and idade < 3000: #idade em dias
+        idade_anos = (idade-2000)/365.
+    elif idade >= 1000 and idade < 2000: # idade em horas
+        idade_anos = (idade-1000)/(365*24.)
+    else:
+        #print(idade)
+        idade_anos = np.nan
+        #raise ValueError("Idade inválida")
+    idade_dec = idade_anos*fator[unidade]
+    return idade_dec
+
+@np.vectorize
+def decodifica_idade_SIM(idade, unidade="D"):
+    """
+    Em tabelas do SIM a idade encontra-se codificada
+    :param idade: valor original da tabela do SIM
+    :param unidade: Unidade de saida desejada: 'Y': anos, 'M' meses, 'D': dias, 'H': horas. Valor default: 'D'
+    :return:
+    """
+    fator = {'Y': 365., 'M': 30., 'D': 1., 'H': 1/24.}
+    try:
+        if idade.startswith('1'):
+            idade = timedelta(hours=int(idade[1:])).days
+        elif idade.startswith('2'):
+            idade = timedelta(days=int(idade[1:])).days
+        elif idade.startswith('3'):
+            idade = timedelta(days=int(idade[1:]) * 30).days
+        elif idade.startswith('4'):
+            idade = timedelta(days=int(idade[1:]) * 365).days
+        else:
+            idade = np.nan
+    except ValueError:
+        idade = np.nan
+    return idade/fator.get(unidade, 1)
+
+
+def is_valid_geocode(geocodigo):
+    """
+    Returns True if the geocode is valid
+    :param geocodigo:
+    :return:
+    """
+    if len(str(geocodigo)) != 7:
+        raise ValueError('Geocode must have 7 digtis')
+    dig = int(str(geocodigo)[-1])
+    if dig == calculate_digit(geocodigo):
+        return True
+    else:
+        return False
+
+
+def calculate_digit(geocode):
+    """
+    Calcula o digito verificador do geocódigo de município com 6 dígitos
+    :param geocode: geocódigo com 6 dígitos
+    :return: dígito verificador
+    """
+    peso = [1, 2, 1, 2, 1, 2, 0]
+    soma = 0
+    geocode = str(geocode)
+    for i in range(6):
+        valor = int(geocode[i]) * peso[i]
+        soma += sum([int(d) for d in str(valor)]) if valor > 9 else valor
+    dv = 0 if soma % 10 == 0 else (10 - (soma % 10))
+    return dv
+
+
+def add_dv(geocodigo):
+    if len(str(geocodigo)) == 7:
+        return geocodigo
+    else:
+        return int(str(geocodigo) + str(calculate_digit(geocodigo)))
