f77b1a0257097070a2881eac657a200c8ac8957d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:00 +0000

    Add package DirkiboysGaussian/1.0/DirkiboysGaussian-1.0.tar.gz

---
+++ packages/DirkiboysGaussian/DirkiboysGaussian-1.0.tar.gz/DirkiboysGaussian-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='DirkiboysGaussian',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
