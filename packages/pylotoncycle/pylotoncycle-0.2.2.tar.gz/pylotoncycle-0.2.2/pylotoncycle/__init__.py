4cf15e6dcb006801b2b3ec75f5cc54fa40042985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:42 +0000

    Add package pylotoncycle/0.2.2/pylotoncycle-0.2.2.tar.gz

---
+++ packages/pylotoncycle/pylotoncycle-0.2.2.tar.gz/pylotoncycle-0.2.2/pylotoncycle/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from .pylotoncycle import PylotonCycle
+from .parser import *
+
+
+__all__ = [
+    'PylotonCycle',
+    'ParseCyclingMetrics',
+    'ParseOutdoorRunMetrics'
+]
