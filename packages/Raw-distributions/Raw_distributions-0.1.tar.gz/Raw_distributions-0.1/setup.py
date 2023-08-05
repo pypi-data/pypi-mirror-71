ac2cbfc7c726c7d8da756b7806c33d0f84e10e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:46 +0000

    Add package Raw-distributions/0.1/Raw_distributions-0.1.tar.gz

---
+++ packages/Raw-distributions/Raw_distributions-0.1.tar.gz/Raw_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Raw_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      author= 'Rawan Galal',
+      packages=['Raw_distributions'],
+      zip_safe=False)
