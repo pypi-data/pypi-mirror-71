11664080bf0ae265936fc03a78874307ab010e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package distributions-gaussian-binomial-exercise/1.0/distributions_gaussian_binomial_exercise-1.0.tar.gz

---
+++ packages/distributions-gaussian-binomial-exercise/distributions_gaussian_binomial_exercise-1.0.tar.gz/distributions_gaussian_binomial_exercise-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_gaussian_binomial_exercise',
+      version='1.0',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['distributions_gaussian_binomial_exercise'],
+      author='JL',
+    zip_safe=False)
