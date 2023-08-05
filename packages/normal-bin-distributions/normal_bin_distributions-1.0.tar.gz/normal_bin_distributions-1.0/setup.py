a7acbaea0dd7bc50d646692ccf86ca9adefdbe3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package normal-bin-distributions/1.0/normal_bin_distributions-1.0.tar.gz

---
+++ packages/normal-bin-distributions/normal_bin_distributions-1.0.tar.gz/normal_bin_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='normal_bin_distributions',
+      version='1.0',
+      description='Binomial and Gaussian distributions',
+      packages=['normal_bin_distributions'],
+      author = 'Victor Emenike',
+      zip_safe=False)
