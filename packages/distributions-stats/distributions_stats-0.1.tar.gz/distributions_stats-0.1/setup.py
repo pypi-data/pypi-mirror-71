6af16652095a97b19b8a20edfdcef9c60096f9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:23 +0000

    Add package distributions-stats/0.1/distributions_stats-0.1.tar.gz

---
+++ packages/distributions-stats/distributions_stats-0.1.tar.gz/distributions_stats-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_stats',
+      version='0.1',
+      description='Basic statistical distributions',
+      packages=['distributions_stats'],
+      author='Mateus Cichelero',
+      author_email='eng.cichelero@gmail.com',
+      zip_safe=False)
