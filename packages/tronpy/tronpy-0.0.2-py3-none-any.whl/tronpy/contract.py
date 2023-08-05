e47ce5a0abb1d1dfcb6e054c54c5968438e32a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:48 +0000

    Add package tronpy/0.0.2/tronpy-0.0.2-py3-none-any.whl

---
+++ packages/tronpy/tronpy-0.0.2-py3-none-any.whl/tronpy/contract.py
@@ -0,0 +1,231 @@
+from tronpy import patch_abi
+
+from typing import Union, Optional, Any
+from eth_abi import encode_single, decode_single
+from Crypto.Hash import keccak
+
+from tronpy import keys
+import tronpy
+
+
+def keccak256(data: bytes) -> bytes:
+    hasher = keccak.new(digest_bits=256)
+    hasher.update(data)
+    return hasher.digest()
+
+
+def assure_bytes(value: Union[str, bytes]) -> bytes:
+    if isinstance(value, (str,)):
+        return bytes.fromhex(value)
+    if isinstance(value, (bytes,)):
+        return value
+    raise ValueError("bad bytes format")
+
+
+class Contract(object):
+    def __init__(
+        self,
+        addr=None,
+        *,
+        bytecode: Union[str, bytes],
+        name: str = None,
+        abi: Optional[dict] = None,
+        user_resource_percent: int = 100,
+        origin_energy_limit: int = 1,
+        origin_address: str = None,
+        owner_address: str = "410000000000000000000000000000000000000000",
+        client=None,
+    ):
+        self.contract_address = addr
+        self.bytecode = assure_bytes(bytecode)
+        self.name = name
+        self.abi = abi or []
+
+        self.user_resource_percent = user_resource_percent
+        self.origin_energy_limit = origin_energy_limit
+
+        self.origin_address = origin_address
+        self.owner_address = owner_address
+
+        self._functions = None
+        self._client = client
+
+        super().__init__()
+
+    def __str__(self):
+        return "<Contract {} {}>".format(self.name, self.contract_address)
+
+    def deploy(self) -> 'TransactionBuilder':
+        if self.contract_address:
+            raise RuntimeError("this contract has already deployed to {}".format(self.contract_address))
+
+        if self.origin_address != self.owner_address:
+            raise RuntimeError("origin address and owner address mismatch")
+
+        return self._client.trx._build_transaction(
+            "CreateSmartContract",
+            {
+                "owner_address": keys.to_hex_address(self.owner_address),
+                "new_contract": {
+                    "origin_address": keys.to_hex_address(self.origin_address),
+                    "abi": {"entrys": self.abi},
+                    "bytecode": self.bytecode.hex(),
+                    "call_value": 0,  # TODO
+                    "name": self.name,
+                    "consume_user_resource_percent": self.user_resource_percent,
+                    "origin_energy_limit": self.origin_energy_limit,
+                },
+            },
+        )
+
+    @property
+    def functions(self):
+        if self._functions is None:
+            if self.abi:
+                self._functions = ContractFunctions(self)
+                return self._functions
+            raise ValueError("can not call a contract without ABI")
+        return self._functions
+
+
+class ContractFunctions(object):
+    def __init__(self, contract):
+        self._contract = contract
+        super().__init__()
+
+    def __getitem__(self, method: str):
+        for method_abi in self._contract.abi:
+            if method_abi["type"] == "Function" and method_abi["name"] == method:
+                return ContractMethod(method_abi, self._contract)
+
+        raise KeyError("contract has no method named '{}'".format(method))
+
+    def __getattr__(self, method: str):
+        try:
+            return self[method]
+        except KeyError:
+            raise AttributeError("contract has no method named '{}'".format(method))
+
+    def __dir__(self):
+        return [method["name"] for method in self._contract.abi if method["type"] == "Function"]
+
+    def __iter__(self):
+        yield from [self[method] for method in dir(self)]
+
+
+class ContractMethod(object):
+    def __init__(self, abi: dict, contract: Contract):
+
+        self._abi = abi
+        self._contract = contract
+        self._owner_address = contract.owner_address
+        self._client = contract._client
+
+        self.inputs = abi.get("inputs", [])
+        self.outputs = abi.get("outputs", [])
+
+        self.call_value = 0
+        self.call_token_value = 0
+        self.call_token_id = 0
+
+        super().__init__()
+
+    def __str__(self):
+        return self.function_type
+
+    def with_owner(self, addr: str):
+        self._owner_address = addr
+        return self
+
+    def with_transfer(self, amount: int):
+        self.call_value = amount
+        return self
+
+    def with_asset_transfer(self, amount: int, token_id: int):
+        self.call_token_value = amount
+        self.call_token_id = token_id
+        return self
+
+    def call(self, *args, **kwargs):
+        return self.__call__(*args, **kwargs)
+
+    def parse_output(self, raw: str) -> Any:
+        parsed_result = decode_single(self.output_type, bytes.fromhex(raw))
+        if len(self.outputs) == 1:
+            return parsed_result[0]
+        return parsed_result
+
+    def __call__(self, *args, **kwargs):
+        parameter = ""
+
+        if args and kwargs:
+            raise ValueError("do not mix positional arguments and keyword arguments")
+
+        if len(self.inputs) == 0:
+            if args or kwargs:
+                raise TypeError("{} expected {} arguments".format(self.name, len(self.inputs)))
+        elif args:
+            if len(args) != len(self.inputs):
+                raise TypeError("wrong number of arguments, require {} got {}".format(len(self.inputs), len(args)))
+            parameter = encode_single(self.input_type, args).hex()
+        elif kwargs:
+            if len(kwargs) != len(self.inputs):
+                raise TypeError("wrong number of arguments, require {} got {}".format(len(self.inputs), len(args)))
+            args = []
+            for arg in self.inputs:
+                try:
+                    args.append(kwargs[arg['name']])
+                except KeyError:
+                    raise TypeError("missing argument '{}'".format(arg['name']))
+            parameter = encode_single(self.input_type, args).hex()
+        else:
+            raise TypeError("wrong number of arguments, require {}".format(len(self.inputs)))
+
+        if self._abi.get("stateMutability", None).lower() in ['view', 'pure']:
+            # const call, contract ret
+            ret = self._client.trigger_const_smart_contract_function(
+                self._owner_address, self._contract.contract_address, self.function_signature, parameter,
+            )
+
+            return self.parse_output(ret)
+
+        else:
+            return self._client.trx._build_transaction(
+                "TriggerSmartContract",
+                {
+                    "owner_address": keys.to_hex_address(self._owner_address),
+                    "contract_address": keys.to_hex_address(self._contract.contract_address),
+                    "data": self.function_signature_hash + parameter,
+                    "call_token_value": self.call_token_value,
+                    "call_value": self.call_value,
+                    "token_id": self.call_token_id,
+                },
+            )
+
+    @property
+    def name(self):
+        return self._abi["name"]
+
+    @property
+    def input_type(self):
+        return "(" + (",".join(arg["type"] for arg in self.inputs)) + ")"
+
+    @property
+    def output_type(self):
+        return "(" + (",".join(arg["type"] for arg in self.outputs)) + ")"
+
+    @property
+    def function_signature(self):
+        return self.name + self.input_type
+
+    @property
+    def function_signature_hash(self) -> str:
+        return keccak256(self.function_signature.encode())[:4].hex()
+
+    @property
+    def function_type(self):
+        types = ', '.join(arg["type"] + ' ' + arg.get("name", '') for arg in self.inputs)
+        ret = 'function {}({})'.format(self.name, types)
+        if self.outputs:
+            ret += ' returns ({})'.format(', '.join(arg["type"] + ' ' + arg.get("name", '') for arg in self.outputs))
+        return ret
---
+++ packages/tronpy/tronpy-0.0.2-py3-none-any.whl/tronpy/tron.py
@@ -0,0 +1,484 @@
+from typing import Union
+import time
+import requests
+from pprint import pprint
+import json
+import hashlib
+from decimal import Decimal
+from urllib.parse import urljoin
+
+from tronpy import keys
+from tronpy.contract import Contract
+from tronpy.keys import PrivateKey
+from tronpy.providers import HTTPProvider
+from tronpy.defaults import conf_for_name
+from tronpy.exceptions import (
+    BadSignature,
+    BadKey,
+    BadHash,
+    BlockNotFound,
+    TaposError,
+    UnknownError,
+    TransactionError,
+    ValidationError,
+    ApiError,
+    AddressNotFound,
+    TransactionNotFound,
+)
+
+TAddress = Union[str, bytes]
+
+
+def current_timestamp() -> int:
+    return int(time.time() * 1000)
+
+
+class TransactionRet(dict):
+    def __init__(self, iterable, client: "Tron"):
+        super().__init__(iterable)
+
+        self._client = client
+        self._txid = self["txid"]
+
+    def wait(self, timeout=30, interval=1.6) -> dict:
+        end_time = time.time() + timeout * 1_0000
+        while time.time() < end_time:
+            try:
+                return self._client.get_transaction_info(self._txid)
+            except TransactionNotFound:
+                time.sleep(interval)
+
+        raise TransactionNotFound("timeout and can not find the transaction")
+
+
+class Transaction(object):
+    def __init__(self, raw_data: dict, client: "Tron" = None):
+        self._raw_data = raw_data
+        self._signature = []
+        self._client = client
+        self.txid = ""
+        self._permission = None
+
+        super().__init__()
+
+        sign_weight = self._client.get_sign_weight(self)
+        if "transaction" not in sign_weight:
+            self._client._handle_api_error(sign_weight)
+            return  # unreachable
+
+        self.txid = sign_weight["transaction"]["transaction"]["txID"]
+        # when account not exist on-chain
+        self._permission = sign_weight.get("permission", None)
+
+    def to_json(self) -> dict:
+        return {"txID": self.txid, "raw_data": self._raw_data, "signature": self._signature}
+
+    def inspect(self) -> "Transaction":
+        pprint(self.to_json())
+        return self
+
+    def sign(self, priv_key: PrivateKey) -> "Transaction":
+        assert self.txid, "txID not calculated"
+
+        if self._permission is not None:
+            addr_of_key = priv_key.public_key.to_hex_address()
+            for key in self._permission["keys"]:
+                if key["address"] == addr_of_key:
+                    break
+            else:
+                raise BadKey(
+                    "provided private key is not in the permission list",
+                    "provided {}".format(priv_key.public_key.to_base58check_address()),
+                    "required {}".format(self._permission),
+                )
+        sig = priv_key.sign_msg_hash(bytes.fromhex(self.txid))
+        self._signature.append(sig.hex())
+        return self
+
+    def broadcast(self) -> TransactionRet:
+        return TransactionRet(self._client.broadcast(self), client=self._client)
+
+    def __str__(self):
+        return json.dumps(self.to_json(), indent=2)
+
+
+class TransactionBuilder(object):
+    def __init__(self, inner: dict, client: "Tron", contract: Contract = None):
+        self._client = client
+        self._raw_data = {
+            "contract": [inner],
+            "timestamp": current_timestamp(),
+            "expiration": current_timestamp() + 60_000,
+            "ref_block_bytes": None,
+            "ref_block_hash": None,
+        }
+        self._contract = contract
+
+        super().__init__()
+
+    def permission_id(self, perm_id: int) -> "TransactionBuilder":
+        self._raw_data["contract"][0]["Permission_id"] = perm_id
+        return self
+
+    def memo(self, memo: Union[str, bytes]) -> "TransactionBuilder":
+        data = memo.encode() if isinstance(memo, (str,)) else memo
+        self._raw_data["data"] = data.hex()
+        return self
+
+    def fee_limit(self, value: int) -> "TransactionBuilder":
+        self._raw_data["fee_limit"] = value
+        return self
+
+    def build(self, options=None, **kwargs) -> Transaction:
+        ref_block_id = self._client.get_latest_solid_block_id()
+        # last 2 byte of block number part
+        self._raw_data["ref_block_bytes"] = ref_block_id[12:16]
+        # last half part of block hash
+        self._raw_data["ref_block_hash"] = ref_block_id[16:32]
+
+        txn = Transaction(self._raw_data, client=self._client)
+        return txn
+
+
+class Trx(object):
+    """The Trx(transaction) API"""
+
+    def __init__(self, tron):
+        self._tron = tron
+
+    @property
+    def client(self) -> "Tron":
+        return self._tron
+
+    def _build_transaction(self, type_: str, obj: dict, contract: Contract = None) -> dict:
+        inner = {
+            "parameter": {"value": obj, "type_url": "type.googleapis.com/protocol.{}".format(type_)},
+            "type": type_,
+        }
+        return TransactionBuilder(inner, client=self.client)
+
+    def transfer(self, from_: TAddress, to: TAddress, amount: int) -> TransactionBuilder:
+        return self._build_transaction(
+            "TransferContract",
+            {"owner_address": keys.to_hex_address(from_), "to_address": keys.to_hex_address(to), "amount": amount},
+        )
+
+    # TRC10 asset
+
+    def asset_transfer(self, from_: TAddress, to: TAddress, amount: int, token_id: int) -> TransactionBuilder:
+        return self._build_transaction(
+            "TransferAssetContract",
+            {
+                "owner_address": keys.to_hex_address(from_),
+                "to_address": keys.to_hex_address(to),
+                "amount": amount,
+                "asset_name": str(token_id).encode().hex(),
+            },
+        )
+
+    def asset_issue(
+        self,
+        owner: TAddress,
+        abbr: str,
+        total_supply: int,
+        *,
+        url: str,
+        name: str = None,
+        description: str = "",
+        start_time: int = None,
+        end_time: int = None,
+        precision: int = 6,
+        frozen_supply: list = None,
+        trx_num: int = 1,
+        num: int = 1,
+    ) -> TransactionBuilder:
+        if name is None:
+            name = abbr
+
+        if start_time is None:
+            # use default expiration
+            start_time = current_timestamp() + 60_000
+
+        if end_time is None:
+            # use default expiration
+            end_time = current_timestamp() + 60_000 + 1
+
+        if frozen_supply is None:
+            frozen_supply = []
+
+        return self._build_transaction(
+            "AssetIssueContract",
+            {
+                "owner_address": keys.to_hex_address(owner),
+                "abbr": abbr.encode().hex(),
+                "name": name.encode().hex(),
+                "total_supply": total_supply,
+                "precision": precision,
+                "url": url.encode().hex(),
+                "description": description.encode().hex(),
+                "start_time": start_time,
+                "end_time": end_time,
+                "frozen_supply": frozen_supply,
+                "trx_num": trx_num,
+                "num": num,
+                "public_free_asset_net_limit": 0,
+                "free_asset_net_limit": 0,
+            },
+        )
+
+    # Account
+
+    def account_permission_update(self, owner: TAddress, perm: dict) -> 'TransactionBuilder':
+        return self._build_transaction(
+            'AccountPermissionUpdate', dict(owner_address=keys.to_hex_address(owner), **perm)
+        )
+
+    def account_update(self, owner: TAddress, name: str) -> 'TransactionBuilder':
+        return self._build_transaction(
+            'UpdateAccount', {"owner_address": keys.to_hex_address(owner), "account_name": name.encode().hex(),}
+        )
+
+    def freeze_balance(
+        self, owner: TAddress, amount: int, resource: str = 'ENERGY', *, receiver: TAddress = None
+    ) -> 'TransactionBuilder':
+        payload = {
+            "owner_address": keys.to_hex_address(owner),
+            'frozen_balance': amount,
+            'resource': resource,
+        }
+        if receiver is not None:
+            payload['receiver_address'] = keys.to_hex_address(receiver)
+        return self._build_transaction('FreezeBalance', payload)
+
+    def unfreeze_balance(
+        self, owner: TAddress, resource: str = 'ENERGY', receiver: TAddress = None
+    ) -> 'TransactionBuilder':
+        payload = {
+            "owner_address": keys.to_hex_address(owner),
+            'resource': resource,
+        }
+        if receiver is not None:
+            payload['receiver_address'] = keys.to_hex_address(receiver)
+        return self._build_transaction('UnfreezeBalance', payload)
+
+
+class Tron(object):
+    # Address API
+    is_address = staticmethod(keys.is_address)
+    is_base58check_address = staticmethod(keys.is_base58check_address)
+    is_hex_address = staticmethod(keys.is_hex_address)
+
+    to_base58check_address = staticmethod(keys.to_base58check_address)
+    to_hex_address = staticmethod(keys.to_hex_address)
+    to_canonical_address = staticmethod(keys.to_base58check_address)
+
+    def __init__(self, provider: HTTPProvider = None, *, network: str = "mainnet"):
+
+        self._trx = Trx(self)
+        if provider is None:
+            self.provider = HTTPProvider(conf_for_name(network))
+        else:
+            self.provider = provider
+
+    @property
+    def trx(self):
+        return self._trx
+
+    def _handle_api_error(self, payload: dict):
+        if payload.get("result", None) is True:
+            return
+        if "Error" in payload:
+            # class java.lang.NullPointerException : null
+            raise ApiError(payload["Error"])
+        if "code" in payload:
+            try:
+                msg = bytes.fromhex(payload["message"]).decode()
+            except Exception:
+                msg = payload["message"]
+
+            if payload["code"] == "SIGERROR":
+                raise BadSignature(msg)
+            elif payload["code"] == "TAPOS_ERROR":
+                raise TaposError(msg)
+            elif payload["code"] in ["TRANSACTION_EXPIRATION_ERROR", "TOO_BIG_TRANSACTION_ERROR"]:
+                raise TransactionError(msg)
+            elif payload["code"] == "CONTRACT_VALIDATE_ERROR":
+                raise ValidationError(msg)
+            raise UnknownError(msg, payload["code"])
+        if "result" in payload and isinstance(payload['result'], (dict,)):
+            return self._handle_api_error(payload["result"])
+
+    # Address utilities
+
+    def generate_address(self, priv_key=None) -> dict:
+        if priv_key is None:
+            priv_key = PrivateKey.random()
+        return {
+            'base58check_address': priv_key.public_key.to_base58check_address(),
+            'hex_address': priv_key.public_key.to_hex_address(),
+            'private_key': priv_key.hex(),
+            'public_key': priv_key.public_key.hex(),
+        }
+
+    def get_address_from_passphrase(self, passphrase: str) -> dict:
+        priv_key = PrivateKey.from_passphrase(passphrase.encode())
+        return self.generate_address(priv_key)
+
+    # Account query
+
+    def get_account(self, addr: TAddress) -> dict:
+        ret = self.provider.make_request(
+            'wallet/getaccount', {"address": keys.to_base58check_address(addr), "visible": True}
+        )
+        if ret:
+            return ret
+        else:
+            raise AddressNotFound("account not found on-chain")
+
+    def get_account_resource(self, addr: TAddress) -> dict:
+        ret = self.provider.make_request(
+            'wallet/getaccountresource', {"address": keys.to_base58check_address(addr), "visible": True}
+        )
+        if ret:
+            return ret
+        else:
+            raise AddressNotFound("account not found on-chain")
+
+    def get_account_balance(self, addr: TAddress) -> Decimal:
+        info = self.get_account(addr)
+        return Decimal(info.get("balance")) / 1_000_000
+
+    def get_account_permission(self, addr: TAddress) -> dict:
+        addr = keys.to_base58check_address(addr)
+        # will check account existence
+        info = self.get_account(addr)
+        # For old accounts prior to AccountPermissionUpdate, these fields are not set.
+        # So default permission is for backward compatibility.
+        default_witness = None
+        if info.get("is_witness", None):
+            default_witness = {
+                "type": "Witness",
+                "id": 1,
+                "permission_name": "witness",
+                "threshold": 1,
+                "keys": [{"address": addr, "weight": 1}],
+            }
+        return {
+            "owner": info.get(
+                "owner_permission",
+                {"permission_name": "owner", "threshold": 1, "keys": [{"address": addr, "weight": 1}],},
+            ),
+            "actives": info.get(
+                "active_permission",
+                [
+                    {
+                        "type": "Active",
+                        "id": 2,
+                        "permission_name": "active",
+                        "threshold": 1,
+                        "operations": "7fff1fc0033e0100000000000000000000000000000000000000000000000000",
+                        "keys": [{"address": addr, "weight": 1}],
+                    }
+                ],
+            ),
+            "witness": info.get("witness_permission", default_witness),
+        }
+
+    # Block query
+
+    def get_latest_solid_block(self) -> dict:
+        return self.provider.make_request('walletsolidity/getnowblock')
+
+    def get_latest_solid_block_id(self) -> str:
+        info = self.provider.make_request('wallet/getnodeinfo')
+        return info["solidityBlock"].split(",ID:", 1)[-1]
+
+    def get_block(self, id_or_num: Union[str, int]) -> dict:
+        if isinstance(id_or_num, (int,)):
+            block = self.provider.make_request('wallet/getblockbynum', {'num': id_or_num, 'visible': True})
+        elif isinstance(id_or_num, (str,)):
+            block = self.provider.make_request('wallet/getblockbyid', {'value': id_or_num, 'visible': True})
+        else:
+            raise TypeError("can not infer type of {}".format(id_or_num))
+
+        if block:
+            return block
+        else:
+            raise BlockNotFound
+
+    def get_transaction(self, txn_id: str) -> dict:
+        if len(txn_id) != 64:
+            raise BadHash("wrong transaction hash length")
+
+        ret = self.provider.make_request('wallet/gettransactionbyid', {"value": txn_id, "visible": True})
+        self._handle_api_error(ret)
+        if ret:
+            return ret
+        raise TransactionNotFound
+
+    def get_transaction_info(self, txn_id: str) -> dict:
+        if len(txn_id) != 64:
+            raise BadHash("wrong transaction hash length")
+
+        ret = self.provider.make_request('wallet/gettransactioninfobyid', {"value": txn_id, "visible": True})
+        self._handle_api_error(ret)
+        if ret:
+            return ret
+        raise TransactionNotFound
+
+    # Smart contract
+
+    def get_contract(self, addr: TAddress):
+        addr = keys.to_base58check_address(addr)
+        info = self.provider.make_request('wallet/getcontract', {"value": addr, "visible": True})
+
+        try:
+            self._handle_api_error(info)
+        except ApiError:
+            # your java's null pointer exception sucks
+            raise AddressNotFound("contract address not found")
+
+        cntr = Contract(
+            addr=addr,
+            bytecode=info["bytecode"],
+            name=info.get("name", ""),
+            abi=info["abi"].get("entrys", []),
+            origin_energy_limit=info.get("origin_energy_limit", 0),
+            user_resource_percent=info["consume_user_resource_percent"],
+            client=self,
+        )
+        return cntr
+
+    def deploy_contract(self, owner: TAddress, contract: Contract) -> 'TransactionBuilder':
+        contract._client = self
+        contract.owner_address = owner
+        contract.origin_address = owner
+        contract.contract_address = None
+
+        return contract.deploy()
+
+    def trigger_const_smart_contract_function(
+        self, owner_address: TAddress, contract_address: TAddress, function_selector: str, parameter: str,
+    ) -> str:
+        ret = self.provider.make_request(
+            'wallet/triggerconstantcontract',
+            {
+                "owner_address": keys.to_base58check_address(owner_address),
+                "contract_address": keys.to_base58check_address(contract_address),
+                "function_selector": function_selector,
+                "parameter": parameter,
+                "visible": True,
+            },
+        )
+        self._handle_api_error(ret)
+        return ret["constant_result"][0]
+
+    # Transaction handling
+
+    def broadcast(self, txn: Transaction):
+        paylaod = self.provider.make_request("/wallet/broadcasttransaction", txn.to_json())
+        self._handle_api_error(paylaod)
+        return paylaod
+
+    def get_sign_weight(self, txn: TransactionBuilder) -> str:
+        return self.provider.make_request('wallet/getsignweight', txn.to_json())
