ec879397b3f3f4259cddca75a4265e3a200936d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:10 +0000

    Add package distribution-basics/0.1/distribution_basics-0.1.tar.gz

---
+++ packages/distribution-basics/distribution_basics-0.1.tar.gz/distribution_basics-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution_basics',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distribution_basics'],
+      zip_safe=False)
