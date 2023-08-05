b278f32b8265b2964c72791414a5730190658c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:59 +0000

    Add package sayan-distributions/1.0/sayan_distributions-1.0.tar.gz

---
+++ packages/sayan-distributions/sayan_distributions-1.0.tar.gz/sayan_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sayan_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['sayan_distributions'],
+      author = 'Osayanhu Imoisili',
+      author_email = 'jerryosaimo@gmail.com',
+      zip_safe=False)
