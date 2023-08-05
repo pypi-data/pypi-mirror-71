57ed2bd60baedc55d0c4a8bd846b8e0b7ea23b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:10 +0000

    Add package gbdis/0.1/gbdis-0.1.tar.gz

---
+++ packages/gbdis/gbdis-0.1.tar.gz/gbdis-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gbdis',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Gaussbinomial'],
+      zip_safe=False)
