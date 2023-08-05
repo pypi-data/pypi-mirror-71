4b403389fc1483d8e6cdf43de51187a4b676ff18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:40 +0000

    Add package bi-gaus-probab/0.1/bi-gaus-probab-0.1.tar.gz

---
+++ packages/bi-gaus-probab/bi-gaus-probab-0.1.tar.gz/bi-gaus-probab-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bi-gaus-probab',
+      version='0.1',
+      description='Binomail & Gaussian distributions',
+      packages=['bi-gaus-probab'],
+      author = "Ankit Sharma",
+      author_email = 'ankit220694@gmail.com',
+      zip_safe=False)
