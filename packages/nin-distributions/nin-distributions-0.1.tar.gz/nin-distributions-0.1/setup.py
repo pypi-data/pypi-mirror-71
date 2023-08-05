843fa7a3706118f9aa6546572816aa6ee2a60af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:10 +0000

    Add package nin-distributions/0.1/nin-distributions-0.1.tar.gz

---
+++ packages/nin-distributions/nin-distributions-0.1.tar.gz/nin-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nin-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['nin-distributions'],
+      zip_safe=False)
