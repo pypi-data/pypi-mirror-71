01ee7e43f3358e84f475e4ac988b0638fc4046b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:52 +0000

    Add package prob-distrs/0.5/prob_distrs-0.5.tar.gz

---
+++ packages/prob-distrs/prob_distrs-0.5.tar.gz/prob_distrs-0.5/prob_distrs/__init__.py
@@ -0,0 +1,6 @@
+from .GaussianDistribution import Gaussian
+from .BinomialDistribution import Binomial
+from .BernoulliDistribution import Bernoulli
+from .PoissonDistribution import Poisson
+from .ExponentialDistribution import Exponential
+from .UniformDistribution import Uniform
