a9baf90a7f27ac931165005ea0ff523cdbf27b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:01 +0000

    Add package RG-distributions/0.1/RG_distributions-0.1.tar.gz

---
+++ packages/RG-distributions/RG_distributions-0.1.tar.gz/RG_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='RG_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      author= 'Rawan Galal',
+      packages=['RG_distributions'],
+      zip_safe=False)
