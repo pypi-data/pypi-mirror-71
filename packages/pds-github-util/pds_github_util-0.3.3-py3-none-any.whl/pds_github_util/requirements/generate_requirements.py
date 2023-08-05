cee1d446697570c0d91b4c425b2f98403c4d2eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:56 +0000

    Add package pds-github-util/0.3.3/pds_github_util-0.3.3-py3-none-any.whl

---
+++ packages/pds-github-util/pds_github_util-0.3.3-py3-none-any.whl/pds_github_util/requirements/generate_requirements.py
@@ -0,0 +1,28 @@
+import argparse
+from pds_github_util.requirements.requirements import Requirements
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Create new snapshot release')
+    parser.add_argument('--organization', dest='organization',
+                        help='github organization owning the repo (e.g. NASA-PDS)')
+    parser.add_argument('--repository', dest='repository',
+                        help='github repository name')
+    parser.add_argument('--dev', dest='dev',
+                        nargs = '?',
+                        const = True, default = False,
+                        help = "Generate requirements with impacts related to latest dev/snapshot version")
+    parser.add_argument('--output', dest='output',
+                        help='markdown output file name')
+    parser.add_argument('--token', dest='token',
+                        help='github personal access token')
+    args = parser.parse_args()
+
+    requirements = Requirements(args.organization, args.repository, token=args.token, dev=args.dev)
+    requirement_file = requirements.write_requirements(args.output)
+    print(requirement_file)
+
+
+if __name__ == "__main__":
+    main()
