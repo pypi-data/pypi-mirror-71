e961bc61e25639b51d8f769cd8968dd0fba098cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:55 +0000

    Add package phcli/0.0.21/phcli-0.0.21.tar.gz

---
+++ packages/phcli/phcli-0.0.21.tar.gz/phcli-0.0.21/command/phcommand.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""alfredyang@pharbers.com.
+
+This module document the usage of class phCommand,
+which help users to create, update, and publish the jobs they created.
+"""
+import click
+from phcontext.phcontextfacade import PhContextFacade
+
+
+@click.command()
+@click.option("--cmd", prompt="Your command is", help="The command that you want to process.",
+              type=click.Choice(["create", "combine", "dag", "publish", "run", "submit", "status"]))
+@click.option("--path", prompt="Your config and python job file directory", help="The concert job you want the process.")
+def ph_command(cmd, path):
+    """The Pharbers Max Job Command Line Interface (CLI)
+
+        --cmd Args: \n
+            create: to generate a job template \n
+            combine: to combine job into a job sequence \n
+            publish: to publish job to pharbers IPaaS \n
+
+        --path Args: \n
+            the dictionary that specify the py and yaml file
+    """
+    context = PhContextFacade(cmd, path)
+    click.get_current_context(context.execute())
---
+++ packages/phcli/phcli-0.0.21.tar.gz/phcli-0.0.21/phcli/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from command.phcommand import ph_command
+
+
+def main():
+    return ph_command()
+
+
+if __name__ == '__main__':
+    main()
