9199dc140959648b249d967cc249f7672cc2059d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:49 +0000

    Add package odoo12-addon-easy-my-coop-loan/12.0.1.0.1.99.dev54/odoo12_addon_easy_my_coop_loan-12.0.1.0.1.99.dev54-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-easy-my-coop-loan/odoo12_addon_easy_my_coop_loan-12.0.1.0.1.99.dev54-py2.py3-none-any.whl/odoo/addons/easy_my_coop_loan/models/partner.py
@@ -0,0 +1,29 @@
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Houssine BAKKALI <houssine@coopiteasy.be>
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    loan_line_ids = fields.One2many(
+        comodel_name="loan.issue.line",
+        inverse_name="partner_id",
+        string="Loans",
+    )
+    is_loaner = fields.Boolean(
+        string="Loaner", compute="_compute_is_loaner", store=True
+    )
+
+    @api.multi
+    @api.depends("loan_line_ids", "loan_line_ids.state")
+    def _compute_is_loaner(self):
+        for partner in self:
+            sudo_partner = partner.sudo()
+            loans = sudo_partner.loan_line_ids.filtered(
+                lambda l: l.state in ["subscribed", "waiting", "paid"]
+            )
+            sudo_partner.is_loaner = bool(loans)
