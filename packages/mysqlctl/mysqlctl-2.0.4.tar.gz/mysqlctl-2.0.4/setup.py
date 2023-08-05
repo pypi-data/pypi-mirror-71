e228bdab0cf619021d40c4f56c76878a828600f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:07 +0000

    Add package mysqlctl/2.0.4/mysqlctl-2.0.4.tar.gz

---
+++ packages/mysqlctl/mysqlctl-2.0.4.tar.gz/mysqlctl-2.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name = 'mysqlctl',
+    version = '2.0.4',
+    author = '王哈哈',
+    author_email = 'mail65656@163.com',
+    description = '用来操作MySQL增删改查',
+    long_description = '用来操作MySQL增删改查',
+    packages = setuptools.find_packages(),
+    platforms = 'any',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    install_requires=[
+        # 你的库依赖的第三方库（也可以指定版本）'
+        'pymysql'
+    ],
+)
+
