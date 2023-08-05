eab6bcb1321b8ae4905742c8a98ac63d9e2b7d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:03 +0000

    Add package gplib/0.16.0/gplib-0.16.0.tar.gz

---
+++ packages/gplib/gplib-0.16.0.tar.gz/gplib-0.16.0/gplib/fitting_methods/fitting_method.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright 2019 Ibai Roman
+#
+#    This file is part of GPlib.
+#
+#    GPlib is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    GPlib is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with GPlib. If not, see <http://www.gnu.org/licenses/>.
+
+
+class FittingMethod(object):
+    """
+
+    """
+
+    def fit(self, model, folds, budget=None, verbose=False):
+        """
+
+        :param model:
+        :type model:
+        :param folds:
+        :type folds:
+        :param budget:
+        :type budget:
+        :param verbose:
+        :type verbose:
+        :return:
+        :rtype:
+        """
+
+        raise NotImplementedError("Not Implemented. This is an interface.")
