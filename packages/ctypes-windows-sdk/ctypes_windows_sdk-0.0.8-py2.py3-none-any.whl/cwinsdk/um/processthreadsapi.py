ac06a2ecce38757f4040ffae1dbf5a73d0dddbba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:21 +0000

    Add package ctypes-windows-sdk/0.0.8/ctypes_windows_sdk-0.0.8-py2.py3-none-any.whl

---
+++ packages/ctypes-windows-sdk/ctypes_windows_sdk-0.0.8-py2.py3-none-any.whl/cwinsdk/um/processthreadsapi.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from ctypes import POINTER
+from ctypes.wintypes import BOOL, HANDLE, DWORD
+
+from .. import windll
+
+OpenProcessToken = windll.advapi32.OpenProcessToken
+OpenProcessToken.argtypes = [HANDLE, DWORD, POINTER(HANDLE)]
+OpenProcessToken.restype = BOOL
+
+GetCurrentProcess = windll.kernel32.GetCurrentProcess
+GetCurrentProcess.argtypes = []
+GetCurrentProcess.restype = HANDLE
