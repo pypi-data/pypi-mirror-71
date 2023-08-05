6de9e8b5aea98a489b697c8be4165049f947d6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:57 +0000

    Add package phcli/0.0.22/phcli-0.0.22.tar.gz

---
+++ packages/phcli/phcli-0.0.22.tar.gz/phcli-0.0.22/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="phcli",
+    version="0.0.22",
+    author="Alfred Yang",
+    author_email="alfredyang@pharbers.com",
+    description="pharbers dag scheduler config",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+
+    package_data={
+        '': ['template/*.yaml', 'template/*.tmp']
+    },
+    entry_points={
+        'console_scripts': [
+            'phcli = command.phcommand:ph_command',
+        ],
+    }
+)
