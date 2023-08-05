dd7e1741ebf56c53cc9f509a87afd802571f3fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:52 +0000

    Add package pydeepmerge/0.2.2b4/pydeepmerge-0.2.2b4.tar.gz

---
+++ packages/pydeepmerge/pydeepmerge-0.2.2b4.tar.gz/pydeepmerge-0.2.2b4/pydeepmerge/__init__.py
@@ -0,0 +1,15 @@
+'''
+A lightweight package to perform deep merges of Mapping objects
+'''
+
+import pydeepmerge.strategies as strategies
+import pydeepmerge._deep_merge as _deep_merge
+
+from pydeepmerge._deep_merge import deep_merge  # noqa: F401
+
+# Since prefer_right is a merge strategy, it should be
+# in the strategies subpackage, but it's also the default
+# merge strategy, and recursive, so if it is not
+# in the _deep_merge module it'll cause a circular
+# import
+strategies.prefer_right = _deep_merge.prefer_right
