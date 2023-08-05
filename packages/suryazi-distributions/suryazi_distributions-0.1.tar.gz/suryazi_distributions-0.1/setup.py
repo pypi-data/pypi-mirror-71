c35cc019eb4b18995c6a842c69a7abeaae56d4c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:38 +0000

    Add package suryazi-distributions/0.1/suryazi_distributions-0.1.tar.gz

---
+++ packages/suryazi-distributions/suryazi_distributions-0.1.tar.gz/suryazi_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='suryazi_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['suryazi_distributions'],
+      zip_safe=False)
