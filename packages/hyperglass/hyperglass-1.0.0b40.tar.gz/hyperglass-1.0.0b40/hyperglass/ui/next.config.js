0ae7558933ae6e274182559884fce3c3f446bd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:59 +0000

    Add package hyperglass/1.0.0b40/hyperglass-1.0.0b40.tar.gz

---
+++ packages/hyperglass/hyperglass-1.0.0b40.tar.gz/hyperglass-1.0.0b40/hyperglass/ui/next.config.js
@@ -0,0 +1,21 @@
+const aliases = require("./.alias");
+const envVars = require("/tmp/hyperglass.env.json");
+const { configFile } = envVars;
+const config = require(String(configFile));
+
+module.exports = {
+  webpack(config) {
+    const { alias } = config.resolve;
+    config.resolve.alias = {
+      ...alias,
+      ...aliases
+    };
+    return config;
+  },
+  poweredByHeader: false,
+  env: {
+    _NODE_ENV_: config.NODE_ENV,
+    _HYPERGLASS_URL_: config._HYPERGLASS_URL_,
+    _HYPERGLASS_CONFIG_: config._HYPERGLASS_CONFIG_
+  }
+};
