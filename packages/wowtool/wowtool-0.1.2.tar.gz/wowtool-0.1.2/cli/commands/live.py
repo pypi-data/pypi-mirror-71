0a8855983358ec650a5d18dabea895a3fce8b105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:27 +0000

    Add package wowtool/0.1.2/wowtool-0.1.2.tar.gz

---
+++ packages/wowtool/wowtool-0.1.2.tar.gz/wowtool-0.1.2/cli/commands/live.py
@@ -0,0 +1,38 @@
+import click
+import json
+import os
+from pathlib import Path
+from wowpy.livestreams import Livestream
+from cli.utils import exception_handler
+
+PROFILE_HELP = """
+`wowtool` profile, can be set with WOWTOOL_PROFILE env var.
+profile settings in ~/.wow/config.json
+"""
+
+
+@click.group()
+def live():
+    """
+    Livestream commands.
+    """
+    pass
+
+
+@live.command(name='query')
+@click.option('--id', required=True, help='Livestream id.')
+@exception_handler
+def query(id):
+    """Get data for a Livestream"""
+
+    # Load wowza api keys
+    home_path = str(Path.home())
+    credentials_path = home_path + '/.wow/config.json'
+    if os.path.exists(credentials_path):
+        with open(credentials_path) as json_file:
+            credentials_data = json.load(json_file)
+            os.environ["WSC_ACCESS_KEY"] = credentials_data['WSC_ACCESS_KEY']
+            os.environ["WSC_API_KEY"] = credentials_data['WSC_API_KEY']
+
+    response = Livestream.get_livestream(id)
+    click.echo(json.dumps(response))
