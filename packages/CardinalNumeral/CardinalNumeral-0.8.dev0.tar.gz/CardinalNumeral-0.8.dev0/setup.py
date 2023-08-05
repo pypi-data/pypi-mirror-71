769dcf58344356da5634d11084d466fe1a9d31bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:28 +0000

    Add package CardinalNumeral/0.8.dev0/CardinalNumeral-0.8.dev0.tar.gz

---
+++ packages/CardinalNumeral/CardinalNumeral-0.8.dev0.tar.gz/CardinalNumeral-0.8.dev0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='CardinalNumeral',
+    version='0.8dev',
+    packages=['cardinal_numeral','cardinal_numeral.language.english', 'cardinal_numeral.language.vietnamese','cardinal_numeral.tts'],
+    license='Coffee License',
+	package_dir={'cardinal_numeral': './cardinal_numeral'},
+
+	# include_package_data=True
+    # long_description=open('README.txt').read(),
+)
