69665a4f18041d77c9b0f4989f0f57fd8a5022cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:45 +0000

    Add package probabilty-dist-2types/0.1/probabilty_dist_2types-0.1.tar.gz

---
+++ packages/probabilty-dist-2types/probabilty_dist_2types-0.1.tar.gz/probabilty_dist_2types-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probabilty_dist_2types',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probabilty_dist_2types'],
+      zip_safe=False)
