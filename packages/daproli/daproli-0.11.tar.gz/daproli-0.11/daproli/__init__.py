656ee73149f2ad093ba972611851125782ddc821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:34 +0000

    Add package daproli/0.11/daproli-0.11.tar.gz

---
+++ packages/daproli/daproli-0.11.tar.gz/daproli-0.11/daproli/__init__.py
@@ -0,0 +1,18 @@
+from .processor import \
+    map, \
+    filter, \
+    split, \
+    expand, \
+    combine, \
+    join
+
+from .transformer import \
+    MapTransformer, \
+    FilterTransformer, \
+    SplitTransformer, \
+    ExpandTransformer, \
+    CombineTransformer, \
+    JoinTransformer, \
+    DataTransformer, \
+    MultiTransformer, \
+    PipelineTransformer
