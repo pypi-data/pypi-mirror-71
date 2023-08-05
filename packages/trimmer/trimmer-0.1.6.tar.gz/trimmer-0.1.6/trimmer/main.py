ba904ef3d2092a7b46c60754292f30d1e59594b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:37 +0000

    Add package trimmer/0.1.6/trimmer-0.1.6.tar.gz

---
+++ packages/trimmer/trimmer-0.1.6.tar.gz/trimmer-0.1.6/trimmer/main.py
@@ -0,0 +1,18 @@
+from cliglue import CliBuilder, argument, parameter, flag
+
+from .trim_source import trim_from_source
+from .version import __version__
+
+
+def main():
+    CliBuilder('trimmer', version=__version__, help='MP3 song normalizer',
+               run=trim_from_source, help_on_empty=True).has(
+        argument('source', help='song source (youtube URL or MP3 file)'),
+        parameter('artist', help='song artist', type=str),
+        parameter('title', help='song title', type=str),
+        parameter('trim-start', help='trim given seconds at the beginning', type=float),
+        parameter('trim-end', help='trim given seconds at the end', type=float),
+        flag('no-normalize', help='skip normalizing volume level'),
+        flag('no-trim', help='skip trimming silence at the edges of song'),
+        flag('no-fade', help='skip applying fade-in & fade-out'),
+    ).run()
