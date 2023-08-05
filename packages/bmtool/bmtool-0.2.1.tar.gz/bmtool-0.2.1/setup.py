022d80d8b92ae42ea47633ee029b95dce92f16ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:26 +0000

    Add package bmtool/0.2.1/bmtool-0.2.1.tar.gz

---
+++ packages/bmtool/bmtool-0.2.1.tar.gz/bmtool-0.2.1/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bmtool",
+    version="0.2.1",
+    author="Tyler Banks",
+    author_email="tbanks@mail.missouri.edu",
+    description="BMTools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tjbanks/bmtools",
+    download_url='',
+    license='MIT',
+    install_requires=[
+        'bmtk',
+        'click',
+        'clint',
+        'h5py',
+        'matplotlib',
+        'networkx',
+        'numpy',
+        'pandas',
+        'questionary',
+        'pynmodlt'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=['tests']),
+    entry_points={
+        'console_scripts': [
+            'bmtools = bmtools.cli.manage:cli',
+            'bmtool = bmtools.cli.manage:cli'
+        ]
+    }
+)
