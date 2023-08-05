d0f76be731e08ffc54c8de7f41b436ed3353cf3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:29 +0000

    Add package geortree/0.7.0/geortree-0.7.0.tar.gz

---
+++ packages/geortree/geortree-0.7.0.tar.gz/geortree-0.7.0/README.md
@@ -0,0 +1,8 @@
+[![Build Status](https://travis-ci.org/gflorio/pytimeset.svg?branch=master)](https://travis-ci.org/gflorio/pytimeset)
+[![Coverage Status](https://coveralls.io/repos/github/GFlorio/pytimeset/badge.svg?branch=master)](https://coveralls.io/github/GFlorio/pytimeset?branch=master)
+[![PyPI version](https://badge.fury.io/py/pytimeset.svg)](https://badge.fury.io/py/pytimeset)
+[![PyPI pyversions](https://img.shields.io/pypi/pyversions/pytimeset.svg)](https://pypi.python.org/pypi/pytimeset/)
+
+# GeoRTree
+
+Reasonably efficient R-Tree implementation, adapted to work with geographic coordinates.
---
+++ packages/geortree/geortree-0.7.0.tar.gz/geortree-0.7.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='geortree',
+    version='0.7.0',
+    packages=['geortree'],
+    url='https://github.com/GFlorio/geortree',
+    license='MIT',
+    author='Gabriel Florio',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email='gabriel@gabrielflorio.com',
+    description='Reasonably efficient R-Tree implementation, adapted to work '
+                'with geographic coordinates.',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
