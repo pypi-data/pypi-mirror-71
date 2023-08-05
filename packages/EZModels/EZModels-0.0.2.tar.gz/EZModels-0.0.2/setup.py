27e97be34a1b446fb4d1358f138679adb957b8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:36 +0000

    Add package EZModels/0.0.2/EZModels-0.0.2.tar.gz

---
+++ packages/EZModels/EZModels-0.0.2.tar.gz/EZModels-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='EZModels',  
+    version='0.0.2',  
+
+    description='Streamlining Common Model Builds for Quick Development and Deployment', 
+    long_description=long_description,  
+    long_description_content_type='text/markdown',
+    author='Preston Ward',  
+    author_email='prestonsward@gmail.com',  
+
+   
+    keywords='data science machine learning model building',  
+
+    packages=find_packages(),  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+ 
+)
