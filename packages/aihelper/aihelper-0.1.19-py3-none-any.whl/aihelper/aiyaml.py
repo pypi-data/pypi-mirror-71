819d184153866cb717c564583562f34c79ebbf0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:19 +0000

    Add package aihelper/0.1.19/aihelper-0.1.19-py3-none-any.whl

---
+++ packages/aihelper/aihelper-0.1.19-py3-none-any.whl/aihelper/aiyaml.py
@@ -0,0 +1,45 @@
+import os
+
+def write_basic_yaml():
+    return {
+        "DIRECTORY": None,
+        "WAVE LENGTHS": [None],
+        "RMS": False,
+    }
+
+def write_yaml():
+    return {
+        "DIRECTORY": "",
+        "WAVE LENGTHS": [None, None, None, None],
+        "RMS": False
+    }
+
+def write_acd_thermal_yaml():
+    return {
+        "DIRECTORY": "",
+        "WAVE LENGTHS": [None, None, None, None],
+        "RMS": False,
+        "MS Deadtime (Seconds)": 30
+    }
+
+def write_directories(data):
+    if data:
+        r_dict = {
+            "DIRECTORY": data.get("DIRECTORY"),
+            "BASELINE DIRECTORY": data.get("BASELINE DIRECTORY"),
+            "GRADIENT": data.get("GRADIENTS", [10]),
+            "ISOTHERMS": data.get("ISOTHERMS", [190, 220, 350]),
+            "WAVE LENGTHS": data.get("WAVE LENGTHS", [None, None, None, None]),
+            "RMS": data.get("RMS", True),
+            "TECHNIQUES": data.get("TECHNIQUES"),
+        }
+    else:
+        r_dict = {
+            "DIRECTORY": os.getcwd(),
+            "PROJECT": "DESCRIPTIVE PROJECT TITLE",
+            "TOPICS": ["TANGO", "VOYAGER", "APOSTOLIC", "UNICORN"],
+            "GRADIENTS": [10],
+            "ISOTHERMS": [190, 220, 350],
+            "TECHNIQUES": ["STA", "IR", "GC", "MS", "TG"],
+        }
+    return r_dict
