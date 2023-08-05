65c67deb8460954f84aee1cf52dc91959d7d8278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:51 +0000

    Add package featureflagtech/0.0.7/featureflagtech-0.0.7.tar.gz

---
+++ packages/featureflagtech/featureflagtech-0.0.7.tar.gz/featureflagtech-0.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+  name = 'featureflagtech',
+  version = '0.0.7',
+  author = 'Charlie Hodgkinson',
+  author_email = 'charlotte.hodgkinson4@gmail.com',
+  description = 'devlopment of python featureflag client',
+  long_description = open('README.md').read(),
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/featureflagtech/featureflagtechpython',
+  packages = ['featureflagtech'],
+  install_requires=[
+    'requests'
+  ],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
