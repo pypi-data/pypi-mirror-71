4fb6c0195aee784500f569a6c6bd1f2cc2e3707d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:31 +0000

    Add package django-github-webhooks/0.1.1/django-github-webhooks-0.1.1.tar.gz

---
+++ packages/django-github-webhooks/django-github-webhooks-0.1.1.tar.gz/django-github-webhooks-0.1.1/docs/conf.py
@@ -0,0 +1,64 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+cwd = os.getcwd()
+parent = os.path.dirname(cwd)
+sys.path.append(parent)
+
+import github_webhooks
+
+# -- Project information -----------------------------------------------------
+
+project = 'Django GitHub webhooks'
+copyright = '2020, stefanitsky'
+author = 'stefanitsky'
+
+# The full version, including alpha/beta/rc tags
+version = github_webhooks.__version__
+# The full version, including alpha/beta/rc tags.
+release = github_webhooks.__version__
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# The master toctree document.
+# Note: https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
---
+++ packages/django-github-webhooks/django-github-webhooks-0.1.1.tar.gz/django-github-webhooks-0.1.1/docs/index.rst
@@ -0,0 +1,34 @@
+.. Django GitHub webhooks documentation master file, created by
+   sphinx-quickstart on Sat Jun 13 18:32:29 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Django GitHub webhooks's documentation!
+==================================================
+
+.. image:: https://github.com/OpenWiden/django-github-webhooks/workflows/Python%20package/badge.svg?branch=master
+    :target: https://github.com/OpenWiden/django-github-webhooks/workflows/Python%20package/badge.svg?branch=master
+    :alt: Build status
+
+.. image:: https://codecov.io/gh/OpenWiden/django-github-webhooks/branch/master/graph/badge.svg
+    :target: https://codecov.io/gh/OpenWiden/django-github-webhooks
+    :alt: Code coverage
+
+.. image:: https://readthedocs.org/projects/django-github-webhooks/badge/?version=latest
+    :target: https://django-github-webhooks.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/OpenWiden/django-github-webhooks/shield.svg
+     :target: https://pyup.io/repos/github/OpenWiden/django-github-webhooks/
+     :alt: Updates
+
+Table Of Contents
+=================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   installation
+   quickstart
+   history
