794f42430d32e484e6ded97e4fb8c73768bf307c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:42 +0000

    Add package graphrepo/0.1.5/graphrepo-0.1.5.tar.gz

---
+++ packages/graphrepo/graphrepo-0.1.5.tar.gz/graphrepo-0.1.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="graphrepo",
+      version="0.1.5",
+      description="A tool that maps a Github repo to Neo4j and Helps Mining the Repo in the DB",
+      url="https://github.com/NullConvergence/GraphRepo",
+      license='Apache License',
+      python_requires='>=3.5',
+      packages=find_packages(),
+      package_dir={'graphrepo': 'graphrepo'})
