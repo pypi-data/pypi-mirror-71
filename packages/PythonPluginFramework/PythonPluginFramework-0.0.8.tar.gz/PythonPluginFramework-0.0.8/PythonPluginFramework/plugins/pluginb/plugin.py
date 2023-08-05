ef3a86d987e843dd7ee5a6a50a4995f22ed54e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:33 +0000

    Add package PythonPluginFramework/0.0.8/PythonPluginFramework-0.0.8.tar.gz

---
+++ packages/PythonPluginFramework/PythonPluginFramework-0.0.8.tar.gz/PythonPluginFramework-0.0.8/PythonPluginFramework/plugins/pluginb/plugin.py
@@ -0,0 +1,27 @@
+
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+context = None
+def init(context_):
+    global context
+    context = context_
+
+
+class MyTest:
+    def test(self):
+        print("Hello Test222!")
+
+
+config = {
+    'pluginid': 'Core::ATest',
+    "extensions" : [
+        {
+            "name": "PL::Test1",
+            "impl": MyTest()
+        }
+    ]
+}
