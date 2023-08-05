355a7c19ec74ca5ee2d7a1ed3f8e222b7a66319a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:08 +0000

    Add package GBa222distributions/1.2/GBa222distributions-1.2.tar.gz

---
+++ packages/GBa222distributions/GBa222distributions-1.2.tar.gz/GBa222distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GBa222distributions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['GBa222distributions'],
+      author= 'bayode',
+      email='bayfag@yahoo.com',
+      zip_safe=False)
