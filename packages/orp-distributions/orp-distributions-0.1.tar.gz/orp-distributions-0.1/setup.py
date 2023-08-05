18bc83b5d7d9a452d8e77c0b43f70ff6b4334fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package orp-distributions/0.1/orp-distributions-0.1.tar.gz

---
+++ packages/orp-distributions/orp-distributions-0.1.tar.gz/orp-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='orp-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['orp-distributions'],
+      author = 'Osvaldo Rosales',
+      zip_safe=False)
