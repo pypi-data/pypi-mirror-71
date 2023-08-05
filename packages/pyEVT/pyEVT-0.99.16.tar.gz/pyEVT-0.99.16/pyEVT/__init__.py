5ce19e562f701e2b78d3b5c84eb2f1abfcb52f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:53 +0000

    Add package pyEVT/0.99.16/pyEVT-0.99.16.tar.gz

---
+++ packages/pyEVT/pyEVT-0.99.16.tar.gz/pyEVT-0.99.16/pyEVT/__init__.py
@@ -0,0 +1,5 @@
+from .EvtExchanger import EvtExchanger
+EvtExchanger.Initialise()
+DeviceSelector=EvtExchanger.Device()
+#_evtLister = EvtExchanger().Device()
+#_evtLister.S
---
+++ packages/pyEVT/pyEVT-0.99.16.tar.gz/pyEVT-0.99.16/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="pyEVT", 
+    version="0.99.16",
+    author="Eise Hoekstra and Mark Span (primary developer)",
+    author_email="m.m.span@rug.nl",
+    description="Package to communicate with RUG developed hardware",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/markspan/pyEVT",
+    download_url = 'https://github.com/markspan/pyEVT/dist/pyEVT-0.99.15.win-amd64.zip',
+    packages=setuptools.find_packages(),
+	include_package_data=True,
+	package_data={"pyEVT": ['EventExchanger.dll', 'HidSharp.dll']},	
+	install_requires=[
+          'pythonnet',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+		"Intended Audience :: Science/Research",
+    ],
+    python_requires='>=3.6',
+)
