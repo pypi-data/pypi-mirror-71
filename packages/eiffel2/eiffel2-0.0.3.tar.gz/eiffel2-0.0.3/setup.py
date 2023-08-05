64555475fcbcf25375fa59fd0531824623ddc9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:50 +0000

    Add package eiffel2/0.0.3/eiffel2-0.0.3.tar.gz

---
+++ packages/eiffel2/eiffel2-0.0.3.tar.gz/eiffel2-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eiffel2", # Replace with your own username
+    version="0.0.3",
+    author="Eduardo Alejandro Lozano Garcia",
+    author_email="sagrario3098@gmail.com",
+    description="Neural Network visualization tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ale9806/Eiffel2",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
