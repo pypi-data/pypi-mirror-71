6e06fa6588a708381b153053e26212692b6a1757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:20 +0000

    Add package find-ff/570/find-ff-570.tar.gz

---
+++ packages/find-ff/find-ff-570.tar.gz/find-ff-570/libff/builtin/grep.py
@@ -0,0 +1,44 @@
+# -----------------------------------------------------------------------
+#
+# ff - a tool for finding files in the filesystem
+# Copyright (C) 2020 Lars Gustäbel <lars@gustaebel.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------
+
+from libff.plugin import *
+
+
+class Grep(Plugin):
+    """The "grep" plugin provides access to the lines and the line count of a
+       text file. It is useful for finding files that contain certain lines
+       or certain substrings.
+    """
+
+    use_cache = False
+
+    attributes = [
+        ("linecount", Number, "The number of lines in the file."),
+        ("lines", ListOfStrings, "The lines of the file.")
+    ]
+
+    def can_handle(self, entry):
+        return entry.text
+
+    def process(self, entry, cached):
+        with open(entry.path, errors="surrogateescape") as lines:
+            lines = list(lines)
+            yield "linecount", len(lines)
+            yield "lines", lines
