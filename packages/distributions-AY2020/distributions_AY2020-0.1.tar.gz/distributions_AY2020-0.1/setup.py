572e640bc8f297f119009cc752145f83c3111f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:01 +0000

    Add package distributions-AY2020/0.1/distributions_AY2020-0.1.tar.gz

---
+++ packages/distributions-AY2020/distributions_AY2020-0.1.tar.gz/distributions_AY2020-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_AY2020',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_AY2020'],
+      zip_safe=False)
