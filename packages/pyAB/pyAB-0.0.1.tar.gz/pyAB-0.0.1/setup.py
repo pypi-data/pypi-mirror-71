8b590f5ec97bad19b650305ead77e753d6afe089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:58 +0000

    Add package pyAB/0.0.1/pyAB-0.0.1.tar.gz

---
+++ packages/pyAB/pyAB-0.0.1.tar.gz/pyAB-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name='pyAB',
+      version='0.0.1',
+      description='A/B Testing using Bayesian & Frequentist Statistics',
+      url='https://github.com/AdiVarma27/pyAB',
+      author='Aditya Varma Kalidindi',
+      author_email='kadityavarma27@gmail.com',
+      packages=find_packages()
+    )
