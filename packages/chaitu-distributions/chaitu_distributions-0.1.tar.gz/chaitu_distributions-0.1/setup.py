614b4288d359c6503787bc532933e9b1f799d591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:39 +0000

    Add package chaitu-distributions/0.1/chaitu_distributions-0.1.tar.gz

---
+++ packages/chaitu-distributions/chaitu_distributions-0.1.tar.gz/chaitu_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='chaitu_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['chaitu_distributions'],
+      author="chaitanya_basava",
+      author_email="viswachaitanya.b16@iiits.in",
+      zip_safe=False)
