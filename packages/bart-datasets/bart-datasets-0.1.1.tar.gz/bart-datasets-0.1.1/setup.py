3669a381ea2ed4ba6f67fac427b954b7fc9abe4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:15 +0000

    Add package bart-datasets/0.1.1/bart-datasets-0.1.1.tar.gz

---
+++ packages/bart-datasets/bart-datasets-0.1.1.tar.gz/bart-datasets-0.1.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '.'}
+
+packages = \
+['bartdatasets', 'bartdatasets.extract', 'bartdatasets.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['google-api-python-client==1.9.1',
+ 'pyarrow>=0.17.1,<0.18.0',
+ 's3fs>=0.4.2,<0.5.0']
+
+setup_kwargs = {
+    'name': 'bart-datasets',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Cesar Augusto',
+    'author_email': 'cesarabruschetta@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1,<3.7',
+}
+
+
+setup(**setup_kwargs)
