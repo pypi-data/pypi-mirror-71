6564ab218870c7ef0dbfddcef2b75ccd7eb6b6f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:49 +0000

    Add package sv-practise-distributions/0.1/sv_practise_distributions-0.1.tar.gz

---
+++ packages/sv-practise-distributions/sv_practise_distributions-0.1.tar.gz/sv_practise_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sv_practise_distributions',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['sv_practise_distributions'],
+      zip_safe=False)
