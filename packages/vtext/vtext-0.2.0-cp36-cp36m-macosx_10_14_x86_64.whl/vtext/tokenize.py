616599bdb7dcf596f272ad1aa62ea09fbfa730bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:34 +0000

    Add package vtext/0.2.0/vtext-0.2.0-cp36-cp36m-macosx_10_14_x86_64.whl

---
+++ packages/vtext/vtext-0.2.0-cp36-cp36m-macosx_10_14_x86_64.whl/vtext/tokenize.py
@@ -0,0 +1,20 @@
+# Copyright 2019 vtext developers
+#
+# Licensed under the Apache License, Version 2.0,
+# <http://apache.org/licenses/LICENSE-2.0>. This file may not be copied,
+# modified, or distributed except according to those terms.
+
+from ._lib import BaseTokenizer
+from ._lib import UnicodeWordTokenizer
+from ._lib import RegexpTokenizer
+from ._lib import VTextTokenizer
+from ._lib import CharacterTokenizer
+
+
+__all__ = [
+    "BaseTokenizer",
+    "UnicodeWordTokenizer",
+    "RegexpTokenizer",
+    "VTextTokenizer",
+    "CharacterTokenizer",
+]
