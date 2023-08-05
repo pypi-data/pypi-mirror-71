72ab1ec003f68eb46c7054efa9184a4ed51048d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:40 +0000

    Add package beancount-reds-importers/0.1.2/beancount_reds_importers-0.1.2-py3-none-any.whl

---
+++ packages/beancount-reds-importers/beancount_reds_importers-0.1.2-py3-none-any.whl/beancount_reds_importers/example/fund_info.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# cusip info: https://www.quantumonline.com/search.cfm
+
+# eventually, the plan is to automate downloading of cusip and ticker info as needed. For now, manually
+# include a list here.
+
+cusip_map = {
+    '808524805': 'SCHF',
+    '012345678' : 'VGTEST',
+}
+
+ticker_map = {
+    'Schwab International Equity ETF':   'SCHF',
+    'Vanguard Test Fund' : 'VGTEST',
+}
+
+money_market = ['VMFXX']
+
+fund_info = {
+    'money_market': money_market,
+    'cusip_map': cusip_map,
+    'ticker_map': ticker_map,
+}
