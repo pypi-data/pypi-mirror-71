c9c64ba089a0cd29b0ba5ccd5fa9dd2500d0dd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:06 +0000

    Add package pfp-lgbt/1.0.1/pfp_lgbt-1.0.1.tar.gz

---
+++ packages/pfp-lgbt/pfp_lgbt-1.0.1.tar.gz/pfp_lgbt-1.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pfp_lgbt',
+    version='1.0.1',
+    author='Weilbyte',
+    description='Python API Wrapper for https://pfp.lgbt/',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Weilbyte/pfp_lgbt.py',
+    packages=setuptools.find_packages(exclude=['test']),
+    install_requires=[
+        'aiohttp'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.6',
+)
