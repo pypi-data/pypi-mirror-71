3d3e8b06adb44ce84d2515903bfa73e111605413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:31 +0000

    Add package credisdict/0.1.1/CRedisDict-0.1.1.tar.gz

---
+++ packages/credisdict/CRedisDict-0.1.1.tar.gz/CRedisDict-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CRedisDict",
+    version="0.1.1",
+    author="Joan Hérisson",
+    author_email="joan.herisson@univ-evry.fr",
+    description="Dictionnary with complex data stored in a Redis database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brsynth/CRedisDict",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'redis', 'json'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
