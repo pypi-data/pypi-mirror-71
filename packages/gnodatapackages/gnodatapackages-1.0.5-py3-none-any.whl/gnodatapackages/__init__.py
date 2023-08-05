5bb3573f6bdef1394318a046cbb1b53d89c6b3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:56 +0000

    Add package gnodatapackages/1.0.5/gnodatapackages-1.0.5-py3-none-any.whl

---
+++ packages/gnodatapackages/gnodatapackages-1.0.5-py3-none-any.whl/gnodatapackages/__init__.py
@@ -0,0 +1,7 @@
+from .gno_gen_stopwords import gno_generalstopwords
+from .gno_name_stopwords import gno_inpersonname_remove
+from .gno_name_stopwords import gno_honorifics_remove
+from .gno_coname_stopwords import gno_coname_stopwords
+
+def joke():
+    return (u'simi lan test test.')
