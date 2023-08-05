06359ca0041852d7bf520f99bead44368149fcdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:49 +0000

    Add package orp-probability/0.1/orp-probability-0.1.tar.gz

---
+++ packages/orp-probability/orp-probability-0.1.tar.gz/orp-probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='orp-probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['orp-probability'],
+      author = 'Osvaldo Rosales',
+      zip_safe=False)
