f03d9ff8cde4e48d49c772a8ba714c0a130b849b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:02 +0000

    Add package norm-bino/0.1/norm-bino-0.1.tar.gz

---
+++ packages/norm-bino/norm-bino-0.1.tar.gz/norm-bino-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='norm-bino',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['norm-bino'],
+      zip_safe=False)
