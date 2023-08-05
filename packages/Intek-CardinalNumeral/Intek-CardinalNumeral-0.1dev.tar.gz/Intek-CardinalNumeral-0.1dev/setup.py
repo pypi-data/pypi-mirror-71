3c5dc08752fa0f6532687dee09e953f7922bd86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:10 +0000

    Add package Intek-CardinalNumeral/0.1dev/Intek-CardinalNumeral-0.1dev.tar.gz

---
+++ packages/Intek-CardinalNumeral/Intek-CardinalNumeral-0.1dev.tar.gz/Intek-CardinalNumeral-0.1dev/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name='Intek-CardinalNumeral',
+    version='0.1dev',
+    packages=['cardinal_numeral',],
+    license='Coffee License',
+    # long_description=open('README.txt').read(),
+)
