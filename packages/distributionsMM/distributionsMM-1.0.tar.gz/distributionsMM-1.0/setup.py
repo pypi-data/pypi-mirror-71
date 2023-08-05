890e8809863a342b3175e4f71556b782d8663ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:09 +0000

    Add package distributionsMM/1.0/distributionsMM-1.0.tar.gz

---
+++ packages/distributionsMM/distributionsMM-1.0.tar.gz/distributionsMM-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributionsMM',
+      version='1.0',
+      description='distributions Gaussian and Binomial',
+      packages=['distributionsMM'],
+      author='MM',
+      zip_safe=False)
