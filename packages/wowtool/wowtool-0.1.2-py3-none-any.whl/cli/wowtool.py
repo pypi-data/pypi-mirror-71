ee14aba63ebf26f619646585367bf404f205f961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:27 +0000

    Add package wowtool/0.1.2/wowtool-0.1.2-py3-none-any.whl

---
+++ packages/wowtool/wowtool-0.1.2-py3-none-any.whl/cli/wowtool.py
@@ -0,0 +1,12 @@
+import click
+from cli.__version__ import VERSION
+from cli.commands.live import live
+
+
+@click.group()
+@click.version_option(version=VERSION)
+def cli():
+    pass
+
+
+cli.add_command(live)
