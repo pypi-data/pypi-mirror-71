488b4ebe91166c843c0873297ca8a6c3b159f26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:21 +0000

    Add package binary-quiz/0.1.3/binary_quiz-0.1.3.tar.gz

---
+++ packages/binary-quiz/binary_quiz-0.1.3.tar.gz/binary_quiz-0.1.3/app/main.py
@@ -0,0 +1,65 @@
+import click
+from click import ClickException
+
+from app.config import BEGIN_ANSWERS_TOKEN
+from app.generate import generate_answers, generate_problems
+from app.pdf import make_pdf
+
+
+@click.command()
+@click.argument("bits", type=click.INT)
+@click.argument("num-problems", type=click.INT)
+@click.option("--pdf", default=False, is_flag=True)
+@click.option("--silent", default=False, is_flag=True)
+@click.option("--include-answers", default=True, is_flag=True)
+@click.option("--output-filepath")
+@click.option("--num-columns", default=4)
+def main(
+    bits: int,
+    num_problems: int,
+    pdf: bool = False,
+    silent: bool = False,
+    include_answers: bool = True,
+    output_filepath: str = None,
+    num_columns: int = 4,
+) -> None:
+
+    validate_args(pdf, silent, output_filepath)
+
+    answers, problems = make_problems_and_answers(bits, num_problems)
+
+    if pdf:
+        make_pdf(
+            problems=problems,
+            answers=answers,
+            output_path=output_filepath or "problems.pdf",
+            include_answers=include_answers,
+            num_columns=num_columns,
+        )
+
+    if not silent:
+        if include_answers:
+            click.echo(BEGIN_ANSWERS_TOKEN.join((problems, answers)))
+        else:
+            click.echo(problems)
+
+
+def validate_args(pdf, silent, output_filepath):
+    if pdf and silent:
+        raise ClickException(
+            "please specify either `pdf` or `silent`, not both (otherwise there "
+            "won't be any outcome of running the app!"
+        )
+
+    if pdf and output_filepath and not output_filepath.endswith("pdf"):
+        raise ClickException("Please include an output filepath ending in '.pdf'")
+
+
+def make_problems_and_answers(bits, num_problems):
+    problems = generate_problems(bits, num_problems)
+    answers = generate_answers(problems)
+    problems_string = "\n\n".join(problems)
+    answers_string = "\n\n".join(
+        [f"{problem} | {answer} " for problem, answer in zip(problems, answers)]
+    )
+    return answers_string, problems_string
---
+++ packages/binary-quiz/binary_quiz-0.1.3.tar.gz/binary_quiz-0.1.3/app/pdf.py
@@ -0,0 +1,48 @@
+import os
+import subprocess
+import tempfile
+
+POSTSCRIPT_FILEPATH = "tempfile.ps"
+
+
+def make_pdf(
+    problems: str,
+    answers: str,
+    output_path: str,
+    include_answers: bool,
+    num_columns: int,
+) -> None:
+    if include_answers:
+        pdf_jobs = (("problems", problems), ("answers", answers))
+    else:
+        pdf_jobs = ("problems", problems)
+
+    for job_name, text in pdf_jobs:
+        if job_name == "answers":
+            output_path = make_answers_path(output_path)
+        with tempfile.NamedTemporaryFile(mode="w") as txt_file:
+            write_to_txtfile(txt_file, text)
+            make_postscript_file(txt_file, num_columns)
+            make_pdf_file(output_path)
+        os.unlink(POSTSCRIPT_FILEPATH)
+
+
+def make_answers_path(path):
+    path, filename = os.path.split(path)
+    new_filename = f"{filename.split('.pdf')[0]}-answers.pdf"
+    return os.path.join(path, new_filename)
+
+
+def write_to_txtfile(txt_file, text):
+    txt_file.write(text)
+    txt_file.flush()
+
+
+def make_pdf_file(output_path):
+    command = f"ps2pdf {POSTSCRIPT_FILEPATH} {output_path}"
+    subprocess.call(command, shell=True)
+
+
+def make_postscript_file(txt_file, num_columns):
+    command = f"enscript --columns={num_columns} --no-header --output={POSTSCRIPT_FILEPATH} {txt_file.name}"
+    subprocess.call(command, shell=True)
