39a85f2fd7ea7daf4727adc840968e5f83c0c876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:42 +0000

    Add package Djaq/0.1.3/Djaq-0.1.3.tar.gz

---
+++ packages/Djaq/Djaq-0.1.3.tar.gz/Djaq-0.1.3/djaq/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.3"
+__author__ = "Paul Wolf"
+__license__ = "MIT"
+
+from .query import DjangoQuery
+from .result import DQResult
---
+++ packages/Djaq/Djaq-0.1.3.tar.gz/Djaq-0.1.3/setup.py
@@ -0,0 +1,32 @@
+#!python
+from distutils.core import setup
+from setuptools import find_packages
+
+description = "A string-based Django query language"
+
+with open("README.txt") as file:
+    long_description = file.read()
+
+setup(
+    name="Djaq",
+    description=description,
+    url="https://github.com/paul-wolf/djaq",
+    author="Paul Wolf",
+    author_email="paul.wolf@yewleaf.com",
+    version="0.1.3",
+    packages=find_packages(exclude=["*~"]),
+    include_package_data=True,
+    license="MIT",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Django",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
