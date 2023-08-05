3c2fdbf1f2baffaf08292eb9d8a330de17225843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:48 +0000

    Add package dulo-probability/0.1/dulo_probability-0.1.tar.gz

---
+++ packages/dulo-probability/dulo_probability-0.1.tar.gz/dulo_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dulo_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dulo_probability'],
+      author='Job Dulo',
+      author_email='dulo.collins@gmail.com',
+      zip_safe=False)
