e181fc2dafdaf34fd667cbea3557cf3b9610af8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:16 +0000

    Add package daproli/0.12/daproli-0.12.tar.gz

---
+++ packages/daproli/daproli-0.12.tar.gz/daproli-0.12/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='daproli',
+      version='0.12',
+      url='https://github.com/ermshaua/daproli',
+      license='BSD 3-Clause License',
+      author='Arik Ermshaus',
+      author_email='arik@tutanota.de',
+      description='daproli is a small data processing library that attempts to make data transformation more declarative.',
+      packages=find_packages(exclude=['tests', 'examples']),
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      zip_safe=False)
