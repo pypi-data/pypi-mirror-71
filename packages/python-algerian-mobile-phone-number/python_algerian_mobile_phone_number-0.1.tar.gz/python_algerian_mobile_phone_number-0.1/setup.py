3792218875427d1764539c444a0cb5d4b864cf01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:04 +0000

    Add package python-algerian-mobile-phone-number/0.1/python_algerian_mobile_phone_number-0.1.tar.gz

---
+++ packages/python-algerian-mobile-phone-number/python_algerian_mobile_phone_number-0.1.tar.gz/python_algerian_mobile_phone_number-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_algerian_mobile_phone_number",
+    version="0.1",
+    author="Mohammed Amine BOURAS",
+    author_email="mohamine19@gmail.com",
+    description="Algerian mobile phone number package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mohamine18/python_algerian_mobile_phone_number",
+    packages=['python_algerian_mobile_phone_number'],
+)
