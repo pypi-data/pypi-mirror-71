aee309be3a9d1edec3fef2a00387f707ede1490b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:32 +0000

    Add package bitmex-tools/1.9/bitmex-tools-1.9.tar.gz

---
+++ packages/bitmex-tools/bitmex-tools-1.9.tar.gz/bitmex-tools-1.9/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bitmex-tools',
+    version='1.9',
+    description='Bitmex Tools',
+    author='Philippe Remy',
+    license='MIT',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'sortedcontainers',
+        'websocket-client==0.47.0'
+    ]
+)
