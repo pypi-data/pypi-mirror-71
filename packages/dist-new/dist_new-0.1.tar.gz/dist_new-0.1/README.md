8357005204de9fad3dc918dae914a0a7ec409e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:57 +0000

    Add package dist-new/0.1/dist_new-0.1.tar.gz

---
+++ packages/dist-new/dist_new-0.1.tar.gz/dist_new-0.1/README.md
@@ -0,0 +1,9 @@
+#dist_new package
+
+Summary of the package
+
+#Files
+
+Explanations of files in the package
+
+#installation 
---
+++ packages/dist-new/dist_new-0.1.tar.gz/dist_new-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_new',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_new'],
+      zip_safe=False)
