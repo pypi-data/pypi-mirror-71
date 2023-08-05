f9f07ed026b5f8212fbb48d0a94577c5d2024a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package pyplume/0.0.6/pyplume-0.0.6-py3-none-any.whl

---
+++ packages/pyplume/pyplume-0.0.6-py3-none-any.whl/pyplume/tests/testStatistics.py
@@ -0,0 +1,8 @@
+import sys,os,pytest,filecmp,shutil,pytest
+def testStatisticsMain():
+    """This is a temporary placeholder"""
+    assert True
+
+def runTests():
+    """Use this function to run pytests."""
+    pytest.main([__file__,"--verbose"])
