2e66fc853d5327fa0b1bae7412be7b6d3f75860e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:46 +0000

    Add package userprovided/0.5.5/userprovided-0.5.5-py3-none-any.whl

---
+++ packages/userprovided/userprovided-0.5.5-py3-none-any.whl/userprovided/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from userprovided import cloud
+from userprovided import date
+from userprovided import hash
+from userprovided import mail
+from userprovided import port
+from userprovided import url
+
+
+name = "userprovided"
+__version__ = "0.5.4"
+__author__ = "Rüdiger Voigt"
