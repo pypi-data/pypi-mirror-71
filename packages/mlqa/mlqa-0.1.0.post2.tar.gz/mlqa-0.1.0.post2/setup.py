2f1ecf13caf43219cb7df62868559639a6d13748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:11 +0000

    Add package mlqa/0.1.0.post2/mlqa-0.1.0.post2.tar.gz

---
+++ packages/mlqa/mlqa-0.1.0.post2.tar.gz/mlqa-0.1.0.post2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlqa",
+    version="0.1.0.post2",
+    author="Dogan Askan",
+    author_email="doganaskan@gmail.com",
+    description="A Package to perform QA on data flows for Machine Learning.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ddaskan/mlqa",
+    download_url="https://pypi.python.org/pypi/mlqa",
+    project_urls={
+        "Bug Tracker": "https://github.com/ddaskan/mlqa/issues",
+        "Documentation": "http://www.doganaskan.com/mlqa/",
+        "Source Code": "https://github.com/ddaskan/mlqa",
+    },
+    license='MIT',
+    keywords='qa ml ai data analysis machine learning quality assurance',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Scientific/Engineering',
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+    ],
+    python_requires='>=3.5',
+    install_requires=['numpy', 'pandas'],
+)
