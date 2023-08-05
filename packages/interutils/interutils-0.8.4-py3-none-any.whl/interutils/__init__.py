b204a6dff96aa3a7e2bc010499fba56f8396577e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package interutils/0.8.4/interutils-0.8.4-py3-none-any.whl

---
+++ packages/interutils/interutils-0.8.4-py3-none-any.whl/interutils/__init__.py
@@ -0,0 +1,3 @@
+from .interactive import clear, pr, ask, banner, choose, pause, cyan, generic_menu_loop, get_date
+from .net import get_net, is_mac, ping
+from .fs import choose_file, count_lines, file_volume, human_bytes, is_image, is_package
