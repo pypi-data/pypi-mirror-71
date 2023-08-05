90b696ed917161c70fe690bf3454c53c0e24cc8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:25 +0000

    Add package palindromicprimes/1.0.1/palindromicprimes-1.0.1.tar.gz

---
+++ packages/palindromicprimes/palindromicprimes-1.0.1.tar.gz/palindromicprimes-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+    name='palindromicprimes',
+    packages=['palindromicprimes'],
+    version='1.0.1',
+    license='MIT',
+    description='Python library to search through palindromic primes. Based on the A002385 number set. Created for the NahamCon CTF 2020.',
+    author='Noxtal',
+    author_email='contact.noxtal@gmail.com',
+    url='https://github.com/Noxtal/palindromicprimes/',
+    download_url='https://github.com/Noxtal/palindromicprimes/archive/1.0.1.zip',
+    keywords=['primes', 'palindromes', 'numbers'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+)
