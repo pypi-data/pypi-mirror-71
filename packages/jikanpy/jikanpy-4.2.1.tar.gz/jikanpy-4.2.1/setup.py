d9b052d6444f714907547b3af7d37d5772d05d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package jikanpy/4.2.1/jikanpy-4.2.1.tar.gz

---
+++ packages/jikanpy/jikanpy-4.2.1.tar.gz/jikanpy-4.2.1/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+CHANGELOG = (HERE / "CHANGELOG.md").read_text()
+
+setup(
+    name="jikanpy",
+    version="4.2.1",
+    description="Python wrapper for the Jikan API",
+    license="MIT",
+    long_description=README + CHANGELOG,
+    long_description_content_type="text/markdown",
+    author="Abhinav Kasamsetty",
+    package_data={"jikanpy": ["py.typed"]},
+    packages=["jikanpy"],
+    url="https://github.com/abhinavk99/jikanpy",
+    install_requires=["requests", "aiohttp", "simplejson"],
+    keywords=["jikan", "jikanpy", "api", "myanimelist"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+    zip_safe=False,
+    python_requires=">=3.6",
+)
