c8f2626e776c46de7293bd9503d7b7e535dd36ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:41 +0000

    Add package lx16a/0.0.2/lx16a-0.0.2.tar.gz

---
+++ packages/lx16a/lx16a-0.0.2.tar.gz/lx16a-0.0.2/README.md
@@ -0,0 +1,5 @@
+# lx16a
+
+> THIS IS WIP!
+
+Python library to control the Hiwonder LX-16a digital serial servo motor.
---
+++ packages/lx16a/lx16a-0.0.2.tar.gz/lx16a-0.0.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lx16a']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0',
+ 'construct>=2.10.56,<3.0.0',
+ 'loguru>=0.5.0,<0.6.0',
+ 'pyserial>=3.4,<4.0']
+
+setup_kwargs = {
+    'name': 'lx16a',
+    'version': '0.0.2',
+    'description': 'Python library to control the Hiwonder LX-16a digital serial servo motor.',
+    'long_description': '# lx16a\n\n> THIS IS WIP!\n\nPython library to control the Hiwonder LX-16a digital serial servo motor.',
+    'author': 'Phil Winder',
+    'author_email': 'phil@WinderResearch.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://winderresearch.com',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4',
+}
+
+
+setup(**setup_kwargs)
