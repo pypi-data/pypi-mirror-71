b30124c7af702fea853bd9e8526efb08a14b341f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:30 +0000

    Add package sid-distributions/0.1/sid-distributions-0.1.tar.gz

---
+++ packages/sid-distributions/sid-distributions-0.1.tar.gz/sid-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sid-distributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['sid-distributions'],
+      author=['Sidhartha Roy'],
+      zip_safe=False)
