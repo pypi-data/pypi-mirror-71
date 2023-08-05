2051e2a77811479f1b7c80dbab7bb452765c314b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:06 +0000

    Add package pfp-lgbt/1.0.1/pfp_lgbt-1.0.1-py3-none-any.whl

---
+++ packages/pfp-lgbt/pfp_lgbt-1.0.1-py3-none-any.whl/pfp_lgbt/handlers.py
@@ -0,0 +1,46 @@
+from .models import Flag
+from .util import byteToImageFile
+import asyncio
+
+async def handleFlags(response, host):
+    """Handles returning of Flag response value.
+
+    Args:
+        response (requests.Response): Request response
+        host (string): Base URL of the API host.
+
+    Returns:
+        list: List of all available flags, as classes
+    """
+    json = await response.json()
+    flags = []
+    for i in json:
+        flag = Flag(i, json[i]['defaultAlpha'], json[i]['tooltip'], f'{host}/icon/{i}')
+        flags.append(flag)
+    return flags
+
+async def handleIconBytes(response):
+    """Handles returning of iconBytes response value.
+
+    Args:
+        response (requests.Response): Request response
+
+    Returns:
+        bytes: Icon image in bytes
+    """
+    return await response.read()
+
+async def handleImageStatic(response, file_output):
+    """Handles returning of imageStatic response value
+
+    Args:
+        response (requests.Response): Request response
+        file_output (string): Optional path to save the output image
+
+    Returns:
+        bytes: Output image in bytes
+    """
+    if file_output is not None:
+        await byteToImageFile(await response.read(), file_output)
+    return await response.read()
+
