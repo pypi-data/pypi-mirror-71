63c471c7c7104e8da30f0a896e62c2702807013d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:31 +0000

    Add package PyOptDE/1.1/PyOptDE-1.1.tar.gz

---
+++ packages/PyOptDE/PyOptDE-1.1.tar.gz/PyOptDE-1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "PyOptDE", # Replace with your own username
+    version = "1.1",
+    author = "Lucas Resende",
+    author_email = "lucasresenderc@gmail.com",
+    description = "An implementation of the Differential Evolution algorithm.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/lucasresenderc/PyOptDE",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.6',
+    install_requires = [
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "numpy"
+    ]
+)
