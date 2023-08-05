c5f7c38baac1d185ace2c56882d36ca56b8bd25e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package codewatchman/0.0.4/codewatchman-0.0.4.tar.gz

---
+++ packages/codewatchman/codewatchman-0.0.4.tar.gz/codewatchman-0.0.4/codewatchman/WatchmanLog.py
@@ -0,0 +1,31 @@
+class WatchmanLog:
+    message = None
+    payload = None
+    is_valid = True
+    log_code = None
+
+    def __init__(self, log_code, message, payload):
+        self.message = message
+        self.payload = payload
+        self.log_code = log_code
+        self.validate()
+
+
+    def validate(self):
+        if self.message is None or type(self.message) is not str:
+            print("Invalid log data. Message is required.")
+            self.is_valid = False
+            return
+
+        if self.log_code is None or type(self.log_code) is not str:
+            print("Invalid log data. log_code is required and it has to be a string")
+            self.is_valid = False
+            return
+
+        if self.payload != None and type(self.payload) is not dict:
+            print("Invalid log data. Payload has to be a python dictionary.")
+            self.is_valid = False
+            return
+
+        self.is_valid = True
+
