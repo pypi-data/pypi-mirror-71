cf7d5cdfa9b4250021a10d8727b92cac47ee8ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:54 +0000

    Add package cris-distribution-test-package/1.9/cris_distribution_test_package-1.9.tar.gz

---
+++ packages/cris-distribution-test-package/cris_distribution_test_package-1.9.tar.gz/cris_distribution_test_package-1.9/README.md
@@ -0,0 +1,1 @@
+This is a testing package from Udacity course
---
+++ packages/cris-distribution-test-package/cris_distribution_test_package-1.9.tar.gz/cris_distribution_test_package-1.9/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='cris_distribution_test_package',
+      version='1.9',
+      description='Gaussian and binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
