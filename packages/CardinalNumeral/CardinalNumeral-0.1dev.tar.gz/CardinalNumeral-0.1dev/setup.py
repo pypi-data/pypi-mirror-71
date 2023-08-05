8266a5e1c8f070797ffe758cc2351ffd0cca6eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:01 +0000

    Add package CardinalNumeral/0.1dev/CardinalNumeral-0.1dev.tar.gz

---
+++ packages/CardinalNumeral/CardinalNumeral-0.1dev.tar.gz/CardinalNumeral-0.1dev/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name='CardinalNumeral',
+    version='0.1dev',
+    packages=['cardinal_numeral',],
+    license='Coffee License',
+    # long_description=open('README.txt').read(),
+)
