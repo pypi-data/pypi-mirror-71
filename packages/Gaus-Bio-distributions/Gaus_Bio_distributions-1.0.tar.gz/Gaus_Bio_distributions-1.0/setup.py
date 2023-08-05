f11676d69ede55e48658156f04fed6c409455ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:18 +0000

    Add package Gaus-Bio-distributions/1.0/Gaus_Bio_distributions-1.0.tar.gz

---
+++ packages/Gaus-Bio-distributions/Gaus_Bio_distributions-1.0.tar.gz/Gaus_Bio_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Gaus_Bio_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['Gaus_Bio_distributions'],
+      author = 'Francis Obasi',
+      author_email =  'obasichigoziefrancis@gmail.com',
+      zip_safe=False)
