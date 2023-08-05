ba4fd594792219660660c2c3cd8343c148f4ee7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:55 +0000

    Add package inoftvocal/0.88/inoftvocal-0.88.tar.gz

---
+++ packages/inoftvocal/inoftvocal-0.88.tar.gz/inoftvocal-0.88/inoft_vocal_framework/databases/dynamodb/dynamodb_core.py
@@ -0,0 +1,69 @@
+import logging
+
+import boto3
+from boto3.session import Session, ResourceNotExistsError
+
+from inoft_vocal_framework.databases.dynamodb.dynamodb_utils import Utils
+from inoft_vocal_framework.utils.static_logger import logger
+
+class DynamoDbCoreAdapter:
+    _EXISTING_DATABASE_CLIENTS = dict()
+
+    def __init__(self, table_name: str, region_name: str, primary_key_name="id", create_table=True):
+        self.table_name = table_name
+        self.primary_key_name = primary_key_name
+        self.create_table = create_table
+
+        self.utils = Utils()
+
+        # We store the database clients in a static variable, so that if we init the class with
+        # the same region_name, we do not need to wait for a new initialization of the client.
+        if region_name in self._EXISTING_DATABASE_CLIENTS.keys():
+            self.dynamodb = self._EXISTING_DATABASE_CLIENTS[region_name]
+            print(f"Re-using the already created dynamodb client for region {region_name}")
+        elif "default" in self._EXISTING_DATABASE_CLIENTS.keys():
+            self.dynamodb = self._EXISTING_DATABASE_CLIENTS["default"]
+            print(f"Re-using the already created dynamodb client for the default region")
+        else:
+            print(f"Initializing the {self}. For local development, make sure that you are connected to internet."
+                  f"\nOtherwise the framework will get stuck at initializing the {self}")
+
+            dynamodb_regions = Session().get_available_regions("dynamodb")
+            if region_name in dynamodb_regions:
+                self.dynamodb = boto3.client("dynamodb", region_name=region_name)
+                self._EXISTING_DATABASE_CLIENTS[region_name] = self.dynamodb
+            else:
+                self.dynamodb = boto3.client("dynamodb")
+                self._EXISTING_DATABASE_CLIENTS["default"] = self.dynamodb
+                logger.debug(f"Warning ! The specified dynamodb region_name {region_name} is not a valid region_name."
+                             f"The dynamodb client has been initialized without specifying the region.")
+
+            self.__create_table_if_not_exists()
+            print(f"Initialization of {self} completed successfully !")
+
+    def __create_table_if_not_exists(self) -> None:
+        """
+        Creates table in Dynamodb resource if it doesn't exist and create_table is set as True.
+        :raises: PersistenceException: When `create_table` fails on dynamodb resource.
+        """
+        if self.create_table:
+            try:
+                self.dynamodb.create_table(
+                    TableName=self.table_name,
+                    KeySchema=[
+                        {
+                            'AttributeName': self.primary_key_name,
+                            'KeyType': 'HASH'
+                        }
+                    ],
+                    AttributeDefinitions=[
+                        {
+                            'AttributeName': self.primary_key_name,
+                            'AttributeType': 'S'
+                        }
+                    ],
+                    BillingMode='PAY_PER_REQUEST',
+                )
+            except Exception as e:
+                if e.__class__.__name__ != "ResourceInUseException":
+                    raise Exception(f"Create table if not exists request failed: Exception of type {type(e).__name__} occurred {str(e)}")
---
+++ packages/inoftvocal/inoftvocal-0.88.tar.gz/inoftvocal-0.88/inoft_vocal_framework/databases/dynamodb/dynamodb_utils.py
@@ -0,0 +1,45 @@
+class Utils:
+    from boto3.dynamodb.types import TypeSerializer, TypeDeserializer
+    # The serializer and deserializer can be static, even in a lambda,
+    # since their classes will never change according to the user.
+    _serializer = None
+    _deserializer = None
+
+    @property
+    def serializer(self) -> TypeSerializer:
+        if Utils._serializer is None:
+            Utils._serializer = Utils.TypeSerializer()
+        return Utils._serializer
+    
+    @property
+    def deserializer(self) -> TypeDeserializer:
+        if Utils._deserializer is None:
+            Utils._deserializer = Utils.TypeDeserializer()
+        return Utils._deserializer
+
+    def python_to_dynamodb(self, python_object):
+        dynamodb_dict = self.serializer.serialize(python_object)
+        dynamodb_dict_keys = list(dynamodb_dict.keys())
+        if len(dynamodb_dict_keys) == 1:
+            if dynamodb_dict_keys[0] == "M":
+                # With a dict, DynamoDB is not expecting the first 'M' key (stands for Map)
+                return dynamodb_dict["M"]
+            else:
+                return dynamodb_dict
+        elif len(dynamodb_dict_keys) == 0:
+            return dynamodb_dict
+        else:
+            raise Exception(f"The number of keys of the dynamodb was superior to 1. This is NOT SUPPOSED TO HAPPEN !"
+                            f"\nPlease submit an issue on the GitHub page of the framework right away (https://github.com/Robinson04/inoft_vocal_framework)."
+                            f"\nThere is something really wrong on our side or on AWS side.")
+
+    def dynamodb_to_python(self, dynamodb_object):
+        if isinstance(dynamodb_object, list):
+            for i, item in enumerate(dynamodb_object):
+                dynamodb_object[i] = self.deserializer.deserialize(item)
+        elif isinstance(dynamodb_object, dict):
+            for key, item in dynamodb_object.items():
+                dynamodb_object[key] = self.deserializer.deserialize(item)
+            return dynamodb_object
+        else:
+            return self.deserializer.deserialize(dynamodb_object)
