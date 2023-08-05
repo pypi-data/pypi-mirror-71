c4820d81ec34c62f32f722d142cdafad9ac670c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:50 +0000

    Add package timesketch-api-client/20200612/timesketch-api-client-20200612.tar.gz

---
+++ packages/timesketch-api-client/timesketch-api-client-20200612.tar.gz/timesketch-api-client-20200612/timesketch_api_client/index.py
@@ -0,0 +1,113 @@
+# Copyright 2019 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Timesketch API client library."""
+from __future__ import unicode_literals
+
+import json
+import logging
+
+from . import error
+from . import resource
+
+
+logger = logging.getLogger('index_api')
+
+
+class SearchIndex(resource.BaseResource):
+    """Timesketch searchindex object.
+
+    Attributes:
+        id: The ID of the search index.
+        api: An instance of TimesketchApi object.
+    """
+
+    def __init__(self, searchindex_id, api, searchindex_name=None):
+        """Initializes the SearchIndex object.
+
+        Args:
+            searchindex_id: Primary key ID of the searchindex.
+            searchindex_name: Name of the searchindex (optional).
+        """
+        self.id = searchindex_id
+        self._labels = []
+        self._searchindex_name = searchindex_name
+        self._resource_uri = 'searchindices/{0:d}'.format(self.id)
+        super(SearchIndex, self).__init__(
+            api=api, resource_uri=self._resource_uri)
+
+    @property
+    def labels(self):
+        """Property that returns the SearchIndex labels."""
+        if self._labels:
+            return self._labels
+
+        data = self.lazyload_data()
+        objects = data.get('objects', [])
+        if not objects:
+            return self._labels
+
+        index_data = objects[0]
+        label_string = index_data.get('label_string', '')
+        if label_string:
+            self._labels = json.loads(label_string)
+        else:
+            self._labels = []
+
+        return self._labels
+
+    @property
+    def name(self):
+        """Property that returns searchindex name.
+
+        Returns:
+            Searchindex name as string.
+        """
+        if not self._searchindex_name:
+            searchindex = self.lazyload_data()
+            self._searchindex_name = searchindex['objects'][0]['name']
+        return self._searchindex_name
+
+    @property
+    def index_name(self):
+        """Property that returns Elasticsearch index name.
+
+        Returns:
+            Elasticsearch index name as string.
+        """
+        searchindex = self.lazyload_data()
+        return searchindex['objects'][0]['index_name']
+
+    @property
+    def status(self):
+        """Property that returns the index status.
+
+        Returns:
+            String with the index status.
+        """
+        data = self.data
+        timeline_object = data.get('objects', [{}])[0]
+        status_list = timeline_object.get('status')
+
+        if not status_list:
+            return 'Unknown'
+
+        status = status_list[0]
+        return status.get('status')
+
+    def delete(self):
+        """Deletes the index."""
+        resource_url = '{0:s}/searchindices/{1:d}/'.format(
+            self.api.api_root, self.id)
+        response = self.api.session.delete(resource_url)
+        return error.check_return_status(response, logger)
