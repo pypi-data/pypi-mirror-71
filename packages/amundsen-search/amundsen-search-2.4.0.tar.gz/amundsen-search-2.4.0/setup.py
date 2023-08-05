dacfa63ed9e236600521779e9b484d72070fda2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:36 +0000

    Add package amundsen-search/2.4.0/amundsen-search-2.4.0.tar.gz

---
+++ packages/amundsen-search/amundsen-search-2.4.0.tar.gz/amundsen-search-2.4.0/setup.py
@@ -0,0 +1,24 @@
+import os
+
+from setuptools import setup, find_packages
+
+__version__ = '2.4.0'
+
+requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+with open(requirements_path) as requirements_file:
+    requirements = requirements_file.readlines()
+
+setup(
+    name='amundsen-search',
+    version=__version__,
+    description='Search Service for Amundsen',
+    url='https://github.com/lyft/amundsensearchlibrary.git',
+    maintainer='Lyft',
+    maintainer_email='dp-tools@lyft.com',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    zip_safe=False,
+    dependency_links=[],
+    install_requires=requirements,
+    python_requires=">=3.6"
+)
