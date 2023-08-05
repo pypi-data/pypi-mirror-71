7c23b36e0a39170569558393b637cb08cf97f846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:01 +0000

    Add package gbd-probability/1.21/gbd-probability-1.21.tar.gz

---
+++ packages/gbd-probability/gbd-probability-1.21.tar.gz/gbd-probability-1.21/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gbd-probability',
+      version='1.21',
+      description='Gaussian & Binomial distributions',
+      author= 'MOHAMMED AZEEM',
+      packages=['gbd-probability'],
+      zip_safe=False)
