b89151d306d0fc151e417d520212d0ca977808fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:05 +0000

    Add package learning-just-chill/0.1/learning_just_chill-0.1.tar.gz

---
+++ packages/learning-just-chill/learning_just_chill-0.1.tar.gz/learning_just_chill-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='learning_just_chill',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['learning_just_chill'],
+      zip_safe=False)
