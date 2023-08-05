5c8e081bf38b5db8961f2bb2b2d6211e7bac2666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:22 +0000

    Add package rkd/0.0.1.dev49/rkd-0.0.1.dev49.tar.gz

---
+++ packages/rkd/rkd-0.0.1.dev49.tar.gz/rkd-0.0.1.dev49/docs/source/index.rst
@@ -0,0 +1,71 @@
+
+Riotkit Do (RKD)
+================
+
+RKD is delivered as a Python Package. To extend RKD with additional tasks you need to install them via PIP or (simpler) define without own code in makefile.py/makefile.yaml
+
+Bootstrap RKD in your project
+-----------------------------
+
+The preferred way to setup RKD is virtualenv.
+
+With requirements.txt and virtualenv you can fully control versioning of RKD and it's components provided by open-source community. You decide when you are ready to upgrade.
+
+.. code:: bash
+
+    virtualenv .venv
+    source .venv/bin/activate
+
+    echo "rkd<=0.5" > requirements.txt # better choose a stable tag and use fixed version for stability
+
+    # example of installing component from external package/repository
+    # later you need to activate it in makefile.yaml or in makefile.py - check later other chapter "Importing tasks"
+    # echo "rkt_ciutils<=3.0"
+
+    pip install -r requirements.txt
+    rkd :rkd:create-structure
+
+Conception
+----------
+
+Makefile, Gradle and other build systems are strictly for development, so RiotKit decided to create RKD **with DevOps in mind**.
+
+Everything could be done like in Makefile via YAML syntax, or in Python, because DevOps love Python! :)
+
+
+In effect a simple task executor with clear rules and early validation of input parameters was created.
+Each task specified to be run is treated like a separate application - has it's own parameters, by default inherits global settings but those could be overridden.
+**The RKD version and version of any installed tasks are managed by Python Packaging - DevOps needs to have strict control over dependencies and upgrades.**
+
+**Basic examples:**
+
+.. code:: bash
+
+    rkd :tasks
+
+    # runs two tasks ":sh" with different arguments
+    rkd :sh -c 'echo hello' :sh -c 'ps aux'
+
+    # runs different tasks in order
+    rkd :py:clean :py:build :py:publish --user=__token__ --password=123456
+
+    # allows to fail one of tasks in our pipeline (does not interrupt the pipeline when first task fails)
+    rkd :sh -c 'exit 1' --keep-going :sh -c 'echo hello'
+
+    # silent output, only tasks stdout and stderr is visible (for parsing outputs in scripts)
+    rkd --silent :sh -c "ps aux"
+
+
+.. image:: ../tasks.png
+
+.. image:: ../python.png
+
+
+.. toctree::
+   :maxdepth: 5
+   :caption: Contents:
+
+   quickstart
+   standardlib/index
+   usage/index
+
---
+++ packages/rkd/rkd-0.0.1.dev49.tar.gz/rkd-0.0.1.dev49/test/test_argparsing.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import unittest
+from argparse import ArgumentParser
+from rkd.argparsing import CommandlineParsingHelper
+from rkd.test import get_test_declaration
+
+
+class ArgParsingTest(unittest.TestCase):
+    def test_creates_grouped_arguments_into_tasks__task_after_flag(self):
+        """ Test parsing arguments """
+
+        parsed = CommandlineParsingHelper.create_grouped_arguments([
+            ':harbor:start', '--profile=test', '--fast-fail', ':status'
+        ])
+
+        self.assertEqual("[Task<:harbor:start (['--profile=test', '--fast-fail'])>, Task<:status ([])>]", str(parsed))
+
+    def test_creates_grouped_arguments_into_tasks__no_task_defined_goes_to_rkd_initialization(self):
+        parsed = CommandlineParsingHelper.create_grouped_arguments([
+            '--help'
+        ])
+
+        self.assertEqual("[Task<rkd:initialize (['--help'])>]", str(parsed))
+
+    def test_creates_grouped_arguments_into_tasks__tasks_only(self):
+        parsed = CommandlineParsingHelper.create_grouped_arguments([
+            ':harbor:start', ':harbor:status', ':harbor:stop'
+        ])
+
+        self.assertEqual("[Task<:harbor:start ([])>, Task<:harbor:status ([])>, Task<:harbor:stop ([])>]", str(parsed))
+
+    def test_add_env_variables_to_argparse(self):
+        parser = ArgumentParser(':test')
+        task = get_test_declaration()
+
+        CommandlineParsingHelper.add_env_variables_to_argparse(parser, task)
+        self.assertIn('Union (default: International Workers Association)', parser.description)
+
+    def test_add_env_variables_to_argparse__no_envs(self):
+        parser = ArgumentParser(':test')
+        task = get_test_declaration()
+
+        # empty the values
+        task.get_task_to_execute().get_declared_envs = lambda: {}
+
+        CommandlineParsingHelper.add_env_variables_to_argparse(parser, task)
+        self.assertNotIn('Union (default: International Workers Association)', parser.description)
+        self.assertIn('-- No environment variables declared --', parser.description)
+
+    def test_arguments_usage(self):
+        """Check that arguments are recognized"""
+
+        parsed = CommandlineParsingHelper.create_grouped_arguments([
+            ':strike:start', 'now'
+        ])
+
+        self.assertEqual("[Task<:strike:start (['now'])>]", str(parsed))
+
+    def test_arguments_usage_with_switch_before(self):
+        """Check that arguments are recognized - variant with an additional switch"""
+
+        parsed = CommandlineParsingHelper.create_grouped_arguments([
+            ':strike:start', '--general', 'now'
+        ])
+
+        self.assertEqual("[Task<:strike:start (['--general', 'now'])>]", str(parsed))
