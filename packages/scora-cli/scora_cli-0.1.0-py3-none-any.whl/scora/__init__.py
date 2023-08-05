9f7abd7c2b1616a375c742834aee0cc610831506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:48 +0000

    Add package scora-cli/0.1.0/scora_cli-0.1.0-py3-none-any.whl

---
+++ packages/scora-cli/scora_cli-0.1.0-py3-none-any.whl/scora/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for CLI App"""
+
+import click
+from pkgver import package_version
+from scora import commands
+
+
+__author__ = """Oncase"""
+__email__ = 'contato@oncase.com.br'
+__version__ = package_version
+
+
+@click.group()
+def cli():
+    pass
+
+
+# Add commands
+cli.add_command(commands.blocks)
