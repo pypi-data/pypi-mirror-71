5291cf212dc2cd368675c0346e62ae6eaf7245ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:11 +0000

    Add package panku/0.1/panku-0.1.tar.gz

---
+++ packages/panku/panku-0.1.tar.gz/panku-0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md",'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='panku',
+    scripts=['Panku/panku'],
+    version='0.1',
+    license='MIT',
+    author='Skynse',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    #package_data={'': ['./requirements.txt']},
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    author_email='',
+    description='Easy API-less reddit scraper',
+    python_requires='>=3.6',
+)
