fbfb3e56f57fff14c1eeb6adec4dbbd3fb835427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:19 +0000

    Add package gym-notebook-wrapper/0.0.3/gym_notebook_wrapper-0.0.3-py3-none-any.whl

---
+++ packages/gym-notebook-wrapper/gym_notebook_wrapper-0.0.3-py3-none-any.whl/gnwrapper/__init__.py
@@ -0,0 +1,55 @@
+import os
+
+from gym import Wrapper
+from IPython import display
+import matplotlib.pyplot as plt
+from pyvirtualdisplay import Display
+
+
+class VirtualDisplay(Wrapper):
+    """
+    Wrapper for running Xvfb
+    """
+    def __init__(self,env,size=(1024, 768)):
+        """
+        Wrapping environment and start Xvfb
+        """
+        super().__init__(env)
+        self._display = None
+        # To avoid starting multiple virtual display
+        if not os.getenv("DISPLAY",None):
+            self._display = Display(visible=0, size=size)
+            self._display.start()
+
+class Animation(VirtualDisplay):
+    """
+    Wrapper for running/rendering OpenAI gym environment on Notebook
+    """
+    def __init__(self,env,size=(1024, 768)):
+        """
+        Wrapping environment for Notebook
+
+        Parameters
+        ----------
+        env : gym.Env
+            Environment to be wrapped
+        size : array-like, optional
+            Virtual display size, whose default is (1024,768)
+        """
+        super().__init__(env,size)
+
+        self._img = None
+
+    def render(self,mode=None,**kwargs):
+        """
+        Render the environment on Notebook
+        """
+        display.clear_output(wait=True)
+        _img = self.env.render(mode='rgb_array',**kwargs)
+        if self._img is None:
+            self._img = plt.imshow(_img)
+        else:
+            self._img.set_data(_img)
+
+        plt.axis('off')
+        display.display(plt.gcf())
