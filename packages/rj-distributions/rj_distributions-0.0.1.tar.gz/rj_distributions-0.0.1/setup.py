cb4044599b1fa0b2c7e0acb246c58a62d85f9059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package rj-distributions/0.0.1/rj_distributions-0.0.1.tar.gz

---
+++ packages/rj-distributions/rj_distributions-0.0.1.tar.gz/rj_distributions-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rj_distributions',
+      version='0.0.1',
+      description='Gaussian distributions',
+      packages=['rj_distributions'],
+      author='Rijul Paul',
+      author_email='rijul90@gmail.com',
+      zip_safe=False)
