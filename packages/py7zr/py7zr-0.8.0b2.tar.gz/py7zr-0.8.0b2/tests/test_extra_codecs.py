f58934ba83236267759f61be4d24be999f1875b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:17 +0000

    Add package py7zr/0.8.0b2/py7zr-0.8.0b2.tar.gz

---
+++ packages/py7zr/py7zr-0.8.0b2.tar.gz/py7zr-0.8.0b2/tests/test_extra_codecs.py
@@ -0,0 +1,51 @@
+import os
+import pathlib
+
+import pytest
+
+import py7zr
+from py7zr.exceptions import UnsupportedCompressionMethodError
+
+try:
+    import zstandard as Zstd  # type: ignore  # noqa
+except ImportError:
+    Zstd = None
+
+testdata_path = pathlib.Path(os.path.dirname(__file__)).joinpath('data')
+os.umask(0o022)
+
+
+@pytest.mark.files
+def test_extract_bzip2(tmp_path):
+    archive = py7zr.SevenZipFile(testdata_path.joinpath('bzip2_2.7z').open(mode='rb'))
+    archive.extractall(path=tmp_path)
+    archive.close()
+
+
+@pytest.mark.files
+def test_extract_ppmd(tmp_path):
+    with pytest.raises(UnsupportedCompressionMethodError):
+        archive = py7zr.SevenZipFile(testdata_path.joinpath('ppmd.7z').open(mode='rb'))
+        archive.extractall(path=tmp_path)
+        archive.close()
+
+
+@pytest.mark.files
+def test_extract_deflate(tmp_path):
+    with py7zr.SevenZipFile(testdata_path.joinpath('deflate.7z').open(mode='rb')) as archive:
+        archive.extractall(path=tmp_path)
+
+
+@pytest.mark.files
+@pytest.mark.skipif(Zstd is None, reason="zstandard library does not exist.")
+def test_extract_zstd(tmp_path):
+    with py7zr.SevenZipFile(testdata_path.joinpath('zstd.7z').open(mode='rb')) as archive:
+        archive.extractall(path=tmp_path)
+
+
+@pytest.mark.files
+@pytest.mark.skipif(Zstd is None, reason="zstandard library does not exist.")
+@pytest.mark.skip(reason="Python lzma module does not allow BCJ only filter.")
+def test_extract_p7zip_zstd(tmp_path):
+    with py7zr.SevenZipFile(testdata_path.joinpath('p7zip-zstd.7z').open('rb')) as archive:
+        archive.extractall(path=tmp_path)
