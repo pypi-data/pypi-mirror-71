25949df10903ad2182f1d71bba9ee3a977c974fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:03 +0000

    Add package GB22distributions/1.1/GB22distributions-1.1.tar.gz

---
+++ packages/GB22distributions/GB22distributions-1.1.tar.gz/GB22distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GB22distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['GB22distributions'],
+      author= 'bayode',
+      email='bayfag@yahoo.com',
+      zip_safe=False)
