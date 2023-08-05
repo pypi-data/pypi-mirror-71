0516a0ff568b7fb24062e305a3b2cb7f00b9c36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package mlaricobar-probability/0.1/mlaricobar_probability-0.1.tar.gz

---
+++ packages/mlaricobar-probability/mlaricobar_probability-0.1.tar.gz/mlaricobar_probability-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mlaricobar_probability',
+      version='0.1',
+      description='Gaussian and binomial distribution',
+      packages=['mlaricobar_probability'],
+      author = "Michael Larico Barzola",
+      author_email = "mlaricobar@gmail.com",
+      zip_safe=False
+     )
