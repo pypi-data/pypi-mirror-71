431dcd52d617e12e73e877a8c47c4e4d152bfb47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:02 +0000

    Add package aiocronjob/0.2.3/aiocronjob-0.2.3-py3-none-any.whl

---
+++ packages/aiocronjob/aiocronjob-0.2.3-py3-none-any.whl/aiocronjob/build/precache-manifest.9d162726e4aac1983daa3a4523943a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b12e6673f02a2b42be000244fce0a2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f168b8ff248dc3ae1243",
+    "url": "/static/css/2.c34640d7.chunk.css"
+  },
+  {
+    "revision": "df8b07cbf1f5a7bde05b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f168b8ff248dc3ae1243",
+    "url": "/static/js/2.9d7e59d7.chunk.js"
+  },
+  {
+    "revision": "2ca78df4e200ab631a9acbb7a85ee5b8",
+    "url": "/static/js/2.9d7e59d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8b07cbf1f5a7bde05b",
+    "url": "/static/js/main.c3a146a0.chunk.js"
+  },
+  {
+    "revision": "608e1bc1a4ba6cf85865",
+    "url": "/static/js/runtime-main.c9a91e67.js"
+  }
+]);
