5ac6eeacfb1597ea7efbd656600b27def5804a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:48 +0000

    Add package reegis/0.2.0.dev0/reegis-0.2.0.dev0.tar.gz

---
+++ packages/reegis/reegis-0.2.0.dev0.tar.gz/reegis-0.2.0.dev0/reegis/demand_elec.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+""" Download and prepare entsoe load profile from opsd data portal.
+
+SPDX-FileCopyrightText: 2016-2019 Uwe Krien <krien@uni-bremen.de>
+
+SPDX-License-Identifier: MIT
+"""
+__copyright__ = "Uwe Krien <krien@uni-bremen.de>"
+__license__ = "MIT"
+
+
+# Python libraries
+import logging
+
+# External packages
+import pandas as pd
+
+# internal modules
+from reegis import entsoe
+from reegis import geometries
+from reegis import openego
+from reegis import bmwi as bmwi_data
+
+
+def get_entsoe_profile_by_region(region, year, name, annual_demand):
+    """
+
+    Parameters
+    ----------
+    region
+    year
+    name
+    annual_demand : str or numeric
+        A numeric annual value or a method to fetch the annual value.
+        Valid methods are: bmwi, entsoe, openego
+
+    Returns
+    -------
+    pandas.DataFrame : A table with a time series for each region. The unit
+        is MW for the internal methods or the same unit order as the input
+        of the annual_demand parameter.
+    Examples
+    --------
+    >>> fs=geometries.get_federal_states_polygon()
+    >>> d1=get_entsoe_profile_by_region(fs, 2014, 'federal_states', 'entsoe'
+    ...     )  # doctest: +SKIP
+    >>> int(d1.sum().sum())  # doctest: +SKIP
+    519757349
+    >>> d2=get_entsoe_profile_by_region(fs, 2014, 'federal_states', 'bmwi'
+    ...     )  # doctest: +SKIP
+    >>> int(d2.sum().sum())  # doctest: +SKIP
+    523988000
+    >>> d3=get_entsoe_profile_by_region(fs, 2014, 'federal_states', 200000
+    ...     )  # doctest: +SKIP
+    >>> round(d3.sum().sum())  # doctest: +SKIP
+    200000.0
+
+    """
+    logging.debug("Get entsoe profile {0} for {1}".format(name, year))
+
+    profile = entsoe.get_entsoe_load(year).reset_index(drop=True)["DE_load_"]
+    norm_profile = profile.div(profile.sum())
+    ego_demand = openego.get_ego_demand_by_region(region, name, grouped=True)
+
+    if annual_demand == "bmwi":
+        annual_demand = (
+            bmwi_data.get_annual_electricity_demand_bmwi(year) * 10 ** 6
+        )
+    elif annual_demand == "entsoe":
+        annual_demand = profile.sum()
+    elif annual_demand == "openego":
+        annual_demand = ego_demand.sum() * 10 ** 3
+    elif isinstance(annual_demand, (int, float)):
+        pass
+    else:
+        msg = (
+            "{0} of type {1} is not a valid input for 'annual_demand'.\n"
+            "Use 'bmwi', 'entsoe' or a float/int value."
+        )
+        raise ValueError(msg.format(annual_demand, type(annual_demand)))
+
+    demand_fs = ego_demand.div(ego_demand.sum()).mul(annual_demand)
+
+    return pd.DataFrame(
+        [demand_fs.values] * len(norm_profile), columns=demand_fs.index
+    ).mul(norm_profile, axis=0)
+
+
+if __name__ == "__main__":
+    pass
---
+++ packages/reegis/reegis-0.2.0.dev0.tar.gz/reegis-0.2.0.dev0/setup.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+
+"""Setup module of reegis."""
+
+from setuptools import setup, find_packages
+import os
+import reegis
+
+if not os.environ.get("READTHEDOCS") == "True":
+    requirements = [
+        "pandas >= 0.21.0",
+        "demandlib",
+        "tables",
+        "shapely",
+        "pvlib < 0.7",
+        "geopandas",
+        "requests",
+        "numpy",
+        "workalendar",
+        "pyproj",
+        "pytz",
+        "windpowerlib",
+        "python-dateutil",
+        "Rtree",
+        "xlrd",
+    ]
+else:
+    requirements = ["cycler"]
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="reegis",
+    version=reegis.__version__,
+    author="Uwe Krien",
+    author_email="krien@uni-bremen.de",
+    description="Open geospatial data model",
+    long_description=read("README.rst"),
+    long_description_content_type="text/x-rst",
+    package_dir={"reegis": "reegis"},
+    url="https://github.com/reegis/reegis",
+    packages=find_packages(),
+    namespace_package=["reegis"],
+    install_requires=requirements,
+    license="MIT",
+    extras_require={
+        "dev": [
+            "nose",
+            "cython",
+            "sphinx",
+            "sphinx_rtd_theme",
+            "matplotlib",
+            "descartes",
+            "request",
+        ]
+    },
+    package_data={
+        "reegis": [
+            os.path.join("data", "static", "*.csv"),
+            os.path.join("data", "static", "*.txt"),
+            os.path.join("data", "geometries", "*.csv"),
+            os.path.join("data", "geometries", "*.geojson"),
+            "*.ini",
+        ]
+    },
+)
