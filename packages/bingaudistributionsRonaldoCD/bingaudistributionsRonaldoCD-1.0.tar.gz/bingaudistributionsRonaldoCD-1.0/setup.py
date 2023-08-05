1e03aa1954f33a872c73fc0dd0618c225064dab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:32 +0000

    Add package bingaudistributionsRonaldoCD/1.0/bingaudistributionsRonaldoCD-1.0.tar.gz

---
+++ packages/bingaudistributionsRonaldoCD/bingaudistributionsRonaldoCD-1.0.tar.gz/bingaudistributionsRonaldoCD-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bingaudistributionsRonaldoCD',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['bingaudistributionsRonaldoCD'],
+      author = 'Ronaldo Cachi',
+      author_email = 'w.ronaldo.cd@gmail.com',
+      zip_safe=False)
