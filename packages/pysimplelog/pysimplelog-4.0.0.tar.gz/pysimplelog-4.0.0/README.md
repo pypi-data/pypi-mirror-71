134798899b76c711c5656517b5a638feed49071c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:11 +0000

    Add package pysimplelog/4.0.0/pysimplelog-4.0.0.tar.gz

---
+++ packages/pysimplelog/pysimplelog-4.0.0.tar.gz/pysimplelog-4.0.0/README.md
@@ -0,0 +1,17 @@
+# Simple Logger
+This package is a simple yet complete logging management system for python based applications. 
+* Logging to two streams is allowed, by default the first one is the standard output (terminal).
+* Logging text formatting (text color, text weight, background color) is allowed.
+* Adding as many logging levels and types as needed is possible.
+
+## Installation
+pip install pysimplelog
+
+## Online Documentation
+http://bachiraoun.github.io/pysimplelog/
+
+## Author
+Bachir Aoun
+
+
+
---
+++ packages/pysimplelog/pysimplelog-4.0.0.tar.gz/pysimplelog-4.0.0/setup.py
@@ -0,0 +1,100 @@
+"""
+This script will work from within the main package directory.
+
+python setup.py sdist bdist_wheel
+twine upload dist/fullrmc-... --universal
+
+
+"""
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+from distutils.util import convert_path
+import os, sys
+
+# set package path and name
+PACKAGE_PATH = '.'
+PACKAGE_NAME = 'pysimplelog'
+
+# check python version
+if sys.version_info[:2] < (2, 6):
+    raise RuntimeError("Python version 2.6.x and above is required.")
+
+# automatically create MANIFEST.in
+commands = [# include MANIFEST.in
+            '# include this file, to ensure we can recreate source distributions',
+            'include MANIFEST.in'
+            # exclude all .log files
+            '\n# exclude all logs',
+            'global-exclude *.log',
+            # exclude all other non necessary files
+            '\n# exclude all other non necessary files ',
+            'global-exclude .project',
+            'global-exclude .pydevproject',
+            # exclude all of the subversion metadata
+            '\n# exclude all of the subversion metadata',
+            'global-exclude *.svn*',
+            'global-exclude .svn/*',
+            'global-exclude *.git*',
+            'global-exclude .git/*',
+            # include all LICENCE files
+            '\n# include all license files found',
+            'global-include ./*LICENSE.*',
+            # include all README files
+            '\n# include all readme files found',
+            'global-include ./*README.*',
+            'global-include ./*readme.*']
+with open('MANIFEST.in','w') as fd:
+    for l in commands:
+        fd.write(l)
+        fd.write('\n')
+
+# declare classifiers
+CLASSIFIERS = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: GNU Affero General Public License v3
+Programming Language :: Python
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Topic :: Software Development
+Topic :: Software Development :: Build Tools
+Topic :: Scientific/Engineering
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: Unix
+Operating System :: MacOS
+"""
+
+# create descriptions
+LONG_DESCRIPTION = ["This is a pythonic simple yet complete system logger.",
+                    "It allows logging simultaneously to two streams, the first one is the system standard output by default and the second one is designated to be set to a file.",
+                    "In addition, pysimplelog is text colouring and attributes enabled when the stream allows it.",]
+DESCRIPTION      = [ LONG_DESCRIPTION[0] ]
+
+## get package info
+PACKAGE_INFO={}
+infoPath = convert_path('__pkginfo__.py')
+with open(infoPath) as fd:
+    exec(fd.read(), PACKAGE_INFO)
+
+
+# create meta data
+metadata = dict(name = PACKAGE_NAME,
+                packages=[PACKAGE_NAME],
+                package_dir={PACKAGE_NAME: '.'},
+                version= PACKAGE_INFO['__version__'] ,
+                author="Bachir AOUN",
+                author_email="bachir.aoun@e-aoun.com",
+                description = "\n".join(DESCRIPTION),
+                long_description = "\n".join(LONG_DESCRIPTION),
+                url = "http://bachiraoun.github.io/pysimplelog/",
+                download_url = "https://github.com/bachiraoun/simplelog",
+                license = 'GNU',
+                classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+                platforms = ["Windows", "Linux", "Mac OS-X", "Unix"], )
+
+# setup
+setup(**metadata)
