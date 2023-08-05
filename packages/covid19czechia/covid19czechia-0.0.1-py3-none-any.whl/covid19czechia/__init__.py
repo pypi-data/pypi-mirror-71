1f9556ed2d5639a7b30b3588c74d42876b34d006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:20 +0000

    Add package covid19czechia/0.0.1/covid19czechia-0.0.1-py3-none-any.whl

---
+++ packages/covid19czechia/covid19czechia-0.0.1-py3-none-any.whl/covid19czechia/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Webscraper for Czechia COVID19 data.
+ 
+Sources:
+    * https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19
+    * ...
+Todo:
+    * More sources
+    * Caching
+"""
+
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution("covid19czechia").version
+except:
+    __version__ = None
