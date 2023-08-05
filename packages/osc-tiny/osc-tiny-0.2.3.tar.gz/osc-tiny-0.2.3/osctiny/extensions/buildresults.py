720a17e47f78cd473a2c8830df00ed0d825589a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:38 +0000

    Add package osc-tiny/0.2.3/osc-tiny-0.2.3.tar.gz

---
+++ packages/osc-tiny/osc-tiny-0.2.3.tar.gz/osc-tiny-0.2.3/osctiny/extensions/buildresults.py
@@ -0,0 +1,70 @@
+"""
+Build result extension
+----------------------
+"""
+# pylint: disable=too-few-public-methods
+from __future__ import unicode_literals
+from six.moves.urllib.parse import urljoin
+
+from ..utils.base import ExtensionBase
+
+
+class Build(ExtensionBase):
+    """
+    Osc extension for retrieving build results
+    """
+    base_path = "/build"
+
+    def get(self, project, package=None, repo=None, **params):
+        """
+        Get build infos
+
+        If no parameters are supplied, a compact list of build states for all
+        packages in the project are returned.
+
+        If a ``package`` and/or ``repo`` are supplied, an extended list of build
+        results for these particular values is returned.
+
+        :param project: Project name
+        :param package: Package name
+        :param repo: Repository name
+        :param params: Additional parameters
+        :return: Objectified XML element
+        :rtype: lxml.objectify.ObjectifiedElement
+
+        .. versionadded:: 0.1.1
+        """
+        params["package"] = package
+        params["repository"] = repo
+        response = self.osc.request(
+            method="GET",
+            url=urljoin(self.osc.url, "{}/{}/_result".format(self.base_path,
+                                                             project)),
+            params=params
+        )
+
+        return self.osc.get_objectified_xml(response)
+
+    def get_binary_list(self, project, repo, arch, package, **params):
+        """
+        Get a list of built RPMs
+
+        :param project: Project name
+        :param repo: Repository name
+        :param arch: Architecture name
+        :param package: Package name
+        :param params: Additional parameters
+        :return: Objectified XML element
+        :rtype: lxml.objectify.ObjectifiedElement
+
+        .. versionadded:: 0.1.3
+        """
+        response = self.osc.request(
+            method="GET",
+            url=urljoin(self.osc.url, "{}/{}/{}/{}/{}".format(
+                self.base_path, project, repo, arch, package
+            )),
+            params=params
+        )
+
+        return self.osc.get_objectified_xml(response)
