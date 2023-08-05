d2b1f21fadf611060df342a82462b9e5a4cf7cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:28 +0000

    Add package peretz-counter/0.1.1/peretz_counter-0.1.1-py2.py3-none-any.whl

---
+++ packages/peretz-counter/peretz_counter-0.1.1-py2.py3-none-any.whl/content_counter/counter.py
@@ -0,0 +1,12 @@
+from content_fetcher import Fetcher
+
+class Counter:
+	def __init__(self, filename):
+		self.fetcher = Fetcher(filename)
+
+	def count_content(self):
+		return len(self.fetcher.get_content().split())
+
+if __name__ == '__main__':
+	instance = Counter('/home/peretz/dependencies/assets/content.txt')
+	print(instance.count_content())
