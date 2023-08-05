638b8a2038b1a652f8783a4210e3f08e0d7a2a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:18 +0000

    Add package django-data-browser/2.0.0/django-data-browser-2.0.0.tar.gz

---
+++ packages/django-data-browser/django-data-browser-2.0.0.tar.gz/django-data-browser-2.0.0/tests/test_admin.py
@@ -0,0 +1,197 @@
+import pytest
+from data_browser.admin import ViewAdmin, globals
+from data_browser.models import View
+from django.contrib import admin
+from django.contrib.admin.utils import flatten_fieldsets
+from django.contrib.auth.models import Permission, User
+
+
+@pytest.fixture
+def view(admin_user):
+    return View(model_name="app.model", fields="fa+0,fd-1,fn", query="bob__equals=fred")
+
+
+def test_open_view(view, rf):
+    expected = '<a href="/data_browser/query/app.model/fa+0,fd-1,fn.html?bob__equals=fred">view</a>'
+    assert ViewAdmin.open_view(view) == expected
+
+
+def test_public_link(view, rf, settings):
+    globals.request = rf.get("/")
+    assert ViewAdmin.public_link(view) == "N/A"
+    view.public = True
+    expected = f"http://testserver/data_browser/view/{view.public_slug}.csv"
+    assert ViewAdmin.public_link(view) == expected
+    settings.DATA_BROWSER_ALLOW_PUBLIC = False
+    assert (
+        ViewAdmin.public_link(view) == "Public URL's are disabled in Django settings."
+    )
+    globals.request = None
+
+
+def test_google_sheets_formula(view, rf, settings):
+    globals.request = rf.get("/")
+    assert ViewAdmin.google_sheets_formula(view) == "N/A"
+    view.public = True
+    expected = (
+        f'=importdata("http://testserver/data_browser/view/{view.public_slug}.csv")'
+    )
+    assert ViewAdmin.google_sheets_formula(view) == expected
+    settings.DATA_BROWSER_ALLOW_PUBLIC = False
+    assert (
+        ViewAdmin.google_sheets_formula(view)
+        == "Public URL's are disabled in Django settings."
+    )
+    globals.request = None
+
+
+def test_add_has_user(admin_client):
+    user = User.objects.get()
+    res = admin_client.get("/admin/data_browser/view/add/")
+    assert res.status_code == 200
+    assert res.context[0]["adminform"].form.initial["owner"] == user.pk
+
+
+def test_change_form_links_have_full_url(view, admin_client):
+    view.public = True
+    view.save()
+    res = admin_client.get(
+        f"http://testserver/admin/data_browser/view/{view.pk}/change/"
+    )
+    assert res.status_code == 200
+
+    all_fields = [f for fs in res.context[0]["adminform"] for l in fs for f in l]
+    for field in all_fields:
+        if field.is_readonly and field.field["name"] == "public_link":
+            expected = f"http://testserver/data_browser/view/{view.public_slug}.csv"
+            assert field.contents() == expected
+            break
+    else:
+        assert False
+
+
+@pytest.fixture
+def get_admin_details(rf):
+    def helper(admin_user, obj):
+        request = rf.get("/")
+        request.user = admin_user
+        view_admin = ViewAdmin(View, admin.site)
+        fields = set(flatten_fieldsets(view_admin.get_fieldsets(request, obj)))
+        read_only = set(view_admin.get_readonly_fields(request, obj))
+        editable = fields - read_only
+        read_only = read_only & fields
+        return editable, read_only
+
+    return helper
+
+
+@pytest.fixture
+def staff_user(admin_user):
+    admin_user.is_superuser = False
+    admin_user.user_permissions.add(Permission.objects.get(codename=f"change_view"))
+    return admin_user
+
+
+class TestAdminFieldsSuperUser:
+    def test_add_page_edit_everything(self, admin_user, get_admin_details):
+        editable, read_only = get_admin_details(admin_user, None)
+        assert editable == {
+            "description",
+            "fields",
+            "model_name",
+            "name",
+            "owner",
+            "public",
+            "public_slug",
+            "query",
+        }
+        assert read_only == {
+            "created_time",
+            "google_sheets_formula",
+            "id",
+            "open_view",
+            "public_link",
+        }
+
+    def test_private_view_edit_everything(self, admin_user, get_admin_details, view):
+        editable, read_only = get_admin_details(admin_user, view)
+        assert editable == {
+            "description",
+            "fields",
+            "model_name",
+            "name",
+            "owner",
+            "public",
+            "public_slug",
+            "query",
+        }
+        assert read_only == {
+            "created_time",
+            "google_sheets_formula",
+            "id",
+            "open_view",
+            "public_link",
+        }
+
+    def test_public_view_edit_everything(self, admin_user, get_admin_details, view):
+        view.public = True
+        editable, read_only = get_admin_details(admin_user, view)
+        assert editable == {
+            "description",
+            "fields",
+            "model_name",
+            "name",
+            "owner",
+            "public",
+            "public_slug",
+            "query",
+        }
+        assert read_only == {
+            "created_time",
+            "google_sheets_formula",
+            "id",
+            "open_view",
+            "public_link",
+        }
+
+
+class TestAdminFieldsStaffUser:
+    def test_add_page_no_public_fields(self, staff_user, get_admin_details):
+        editable, read_only = get_admin_details(staff_user, None)
+        assert editable == {
+            "description",
+            "fields",
+            "model_name",
+            "name",
+            "owner",
+            "query",
+        }
+        assert read_only == {"created_time", "id", "open_view"}
+
+    def test_private_view_no_public_fields(self, staff_user, get_admin_details, view):
+        editable, read_only = get_admin_details(staff_user, view)
+        assert editable == {
+            "description",
+            "fields",
+            "model_name",
+            "name",
+            "owner",
+            "query",
+        }
+        assert read_only == {"created_time", "id", "open_view"}
+
+    def test_public_view_readonly(self, staff_user, get_admin_details, view):
+        view.public = True
+        editable, read_only = get_admin_details(staff_user, view)
+        assert editable == set()
+        assert read_only == {
+            "created_time",
+            "description",
+            "fields",
+            "id",
+            "model_name",
+            "name",
+            "open_view",
+            "owner",
+            "query",
+        }
---
+++ packages/django-data-browser/django-data-browser-2.0.0.tar.gz/django-data-browser-2.0.0/tests/test_views.py
@@ -0,0 +1,282 @@
+import csv
+import json
+from datetime import datetime
+
+import data_browser.models
+import pytest
+from django.contrib.auth.models import User
+
+from . import models
+from .util import update_fe_fixture
+
+
+def dump(val):
+    print(json.dumps(val, indent=4, sort_keys=True))
+
+
+class ANY:  # pragma: no cover
+    def __init__(self, type):
+        self.type = type
+
+    def __eq__(self, other):
+        return isinstance(other, self.type)
+
+
+@pytest.fixture
+def products(db):
+    address = models.Address.objects.create(city="london")
+    producer = models.Producer.objects.create(name="Bob", address=address)
+    models.Product.objects.create(name="a", size=1, size_unit="g", producer=producer)
+    models.Product.objects.create(name="b", size=1, size_unit="g", producer=producer)
+    models.Product.objects.create(name="c", size=2, size_unit="g", producer=producer)
+
+
+@pytest.fixture
+def pivot_products(db):
+    address = models.Address.objects.create(city="london", street="bad")
+    producer = models.Producer.objects.create(name="Bob", address=address)
+    datetimes = [
+        datetime(2020, 1, 1),
+        datetime(2020, 2, 1),
+        datetime(2020, 2, 2),
+        datetime(2021, 1, 1),
+        datetime(2021, 1, 2),
+        datetime(2021, 1, 3),
+    ]
+    for dt in datetimes:
+        models.Product.objects.create(created_time=dt, name=str(dt), producer=producer)
+
+
+def test_query_html(admin_client, snapshot):
+    res = admin_client.get(
+        "/data_browser/query/tests.Product/size-0,name+1,size_unit.html?size__lt=2&id__gt=0"
+    )
+    assert res.status_code == 200
+    context = json.loads(res.context["ctx"])
+    context["config"]["version"] = "redacted"
+    context["initialState"]["version"] = "redacted"
+    snapshot.assert_match(context, "context")
+
+
+def test_query_html_no_perms(admin_user, admin_client, snapshot):
+    admin_user.is_superuser = False
+    admin_user.save()
+    res = admin_client.get("/data_browser/query//.html?")
+    assert res.status_code == 200
+    context = json.loads(res.context["ctx"])
+    context["config"]["version"] = "redacted"
+    context["initialState"]["version"] = "redacted"
+    snapshot.assert_match(context, "context")
+
+
+def test_query_ctx(admin_client, snapshot):
+    res = admin_client.get("/data_browser/query//.ctx?")
+    assert res.status_code == 200
+    context = res.json()
+    context["config"]["version"] = "redacted"
+    context["initialState"]["version"] = "redacted"
+    snapshot.assert_match(context, "context")
+    update_fe_fixture("frontend/src/context_fixture.json", context)
+
+
+@pytest.mark.usefixtures("products")
+def test_query_json_bad_fields(admin_client):
+    res = admin_client.get(
+        "".join(
+            [
+                "/data_browser/query/tests.Product/",
+                "size-0,name+1,size_unit,bob-2,is_onsale,pooducer__name,producer__name.json",
+                "?size__lt=2&id__gt=0&bob__gt=1&size__xx=1&size__lt=xx",
+            ]
+        )
+    )
+    assert res.status_code == 200
+    assert json.loads(res.content.decode("utf-8"))["rows"] == [
+        {
+            "size": 1,
+            "name": "a",
+            "size_unit": "g",
+            "is_onsale": False,
+            "producer__name": "Bob",
+        },
+        {
+            "size": 1,
+            "name": "b",
+            "size_unit": "g",
+            "is_onsale": False,
+            "producer__name": "Bob",
+        },
+    ]
+
+
+def test_query_html_bad_model(admin_client):
+    res = admin_client.get(
+        "/data_browser/query/tests.Bob/size-0,name+1,size_unit.html?size__lt=2&id__gt=0"
+    )
+    assert res.status_code == 404
+
+
+@pytest.mark.usefixtures("products")
+def test_query_csv(admin_client):
+    res = admin_client.get(
+        "/data_browser/query/tests.Product/size-0,name+1,size_unit.csv?size__lt=2&id__gt=0"
+    )
+    assert res.status_code == 200
+    print(res.content.decode("utf-8"))
+    rows = list(csv.reader(res.content.decode("utf-8").splitlines()))
+    dump(rows)
+    assert rows == [["size", "name", "size_unit"], ["1.0", "a", "g"], ["1.0", "b", "g"]]
+
+
+@pytest.mark.usefixtures("pivot_products")
+def test_query_csv_pivoted(admin_client):
+    res = admin_client.get(
+        "/data_browser/query/tests.Product/created_time__year,&created_time__month,id__count,id__max.csv?"
+    )
+    assert res.status_code == 200
+    print(res.content.decode("utf-8"))
+    rows = list(csv.reader(res.content.decode("utf-8").splitlines()))
+    dump(rows)
+    assert rows == [
+        ["created_time month", "January", "", "Feburary", ""],
+        ["created_time year", "id count", "id max", "id count", "id max"],
+        ["2020.0", "1.0", "1.0", "2.0", "3.0"],
+        ["2021.0", "3.0", "6.0", "", ""],
+    ]
+
+
+testdata = [
+    ("----"),
+    ("---b"),
+    ("--c-"),
+    ("--cb"),
+    ("-r--"),
+    ("-r-b"),
+    ("-rc-"),
+    ("-rcb"),
+    ("d---"),
+    ("d--b"),
+    ("d-c-"),
+    ("d-cb"),
+    ("dr--"),
+    ("dr-b"),
+    ("drc-"),
+    ("drcb"),
+]
+
+
+@pytest.mark.usefixtures("pivot_products")
+@pytest.mark.parametrize("key", testdata)
+def test_query_csv_pivot_permutations(admin_client, key, snapshot):
+    fields = []
+    if "r" in key:
+        fields.append("created_time__year")
+    if "c" in key:
+        fields.append("&created_time__month")
+    if "b" in key:
+        fields.extend(["id__count", "id__max"])
+    filters = "" if "d" in key else "id__equals=123"
+
+    res = admin_client.get(
+        f"/data_browser/query/tests.Product/{','.join(fields)}.csv?{filters}"
+    )
+    assert res.status_code == 200
+    print(res.content.decode("utf-8"))
+    rows = list(csv.reader(res.content.decode("utf-8").splitlines()))
+    dump(rows)
+    snapshot.assert_match(rows, "key")
+
+
+@pytest.mark.usefixtures("products")
+def test_query_json(admin_client, snapshot):
+    res = admin_client.get(
+        "/data_browser/query/tests.Product/size-0,name+1,size_unit.json?size__lt=2&id__gt=0"
+    )
+    assert res.status_code == 200
+    data = json.loads(res.content.decode("utf-8"))
+    data["version"] = "redacted"
+    snapshot.assert_match(data, "data")
+
+
+@pytest.mark.usefixtures("pivot_products")
+def test_query_json_pivot(admin_client, snapshot):
+    res = admin_client.get(
+        "/data_browser/query/tests.Product/created_time__year,&created_time__month,id__count,id__max.json?"
+    )
+    assert res.status_code == 200
+    data = json.loads(res.content.decode("utf-8"))
+    data["version"] = "redacted"
+    snapshot.assert_match(data, "data")
+
+
+@pytest.mark.usefixtures("products")
+def test_query_json_bad_model(admin_client):
+    res = admin_client.get(
+        "/data_browser/query/tests.Bob/size-0,name+1,size_unit.json?size__lt=2&id__gt=0"
+    )
+    assert res.status_code == 404
+
+
+@pytest.mark.usefixtures("products")
+def test_view_csv(admin_client, settings):
+    view = data_browser.models.View.objects.create(
+        model_name="tests.Product",
+        fields="size-0,name+1,size_unit",
+        query="size__lt=2&id__gt=0",
+        owner=User.objects.get(),
+    )
+
+    res = admin_client.get(f"/data_browser/view/{view.public_slug}.csv")
+    assert res.status_code == 404
+
+    view.public = True
+    view.save()
+    res = admin_client.get(f"/data_browser/view/{view.public_slug}.csv")
+    assert res.status_code == 200
+    print(res.content.decode("utf-8"))
+    rows = list(csv.reader(res.content.decode("utf-8").splitlines()))
+    dump(rows)
+    assert rows == [["size", "name", "size_unit"], ["1.0", "a", "g"], ["1.0", "b", "g"]]
+
+    settings.DATA_BROWSER_ALLOW_PUBLIC = False
+    res = admin_client.get(f"/data_browser/view/{view.public_slug}.csv")
+    assert res.status_code == 404
+    settings.DATA_BROWSER_ALLOW_PUBLIC = True
+
+    view.owner = User.objects.create(is_staff=True)
+    view.save()
+    res = admin_client.get(f"/data_browser/view/{view.public_slug}.csv")
+    assert res.status_code == 404
+
+
+@pytest.mark.usefixtures("products")
+def test_view_json(admin_client):
+    view = data_browser.models.View.objects.create(
+        model_name="tests.Product",
+        fields="size-0,name+1,size_unit",
+        query="size__lt=2&id__gt=0",
+        owner=User.objects.get(),
+    )
+
+    res = admin_client.get(f"/data_browser/view/{view.public_slug}.json")
+    assert res.status_code == 404
+
+    view.public = True
+    view.save()
+    res = admin_client.get(f"/data_browser/view/{view.public_slug}.json")
+    assert res.status_code == 200
+    data = json.loads(res.content.decode("utf-8"))
+    dump(data)
+    assert data == {
+        "rows": [
+            {"size": 1, "name": "a", "size_unit": "g"},
+            {"size": 1, "name": "b", "size_unit": "g"},
+        ],
+        "cols": [{}],
+        "body": [[{}, {}]],
+    }
+
+    view.owner = User.objects.create(is_staff=True)
+    view.save()
+    res = admin_client.get(f"/data_browser/view/{view.public_slug}.csv")
+    assert res.status_code == 404
