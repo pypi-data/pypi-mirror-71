20cd11fe10f9d3660315797b9acfd5b22a6804a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:25 +0000

    Add package distributions-DtoE/0.1/distributions_DtoE-0.1.tar.gz

---
+++ packages/distributions-DtoE/distributions_DtoE-0.1.tar.gz/distributions_DtoE-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_DtoE',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_DtoE'],
+      author = 'ReneeJia',
+      author_email = 'rj368@cornell.edu',
+      zip_safe=False)
