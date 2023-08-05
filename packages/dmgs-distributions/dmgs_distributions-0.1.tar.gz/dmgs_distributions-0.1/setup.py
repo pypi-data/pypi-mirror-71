80e22e63dfb165d75ec7a8eeb570b1cfa0e1070d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:00 +0000

    Add package dmgs-distributions/0.1/dmgs_distributions-0.1.tar.gz

---
+++ packages/dmgs-distributions/dmgs_distributions-0.1.tar.gz/dmgs_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dmgs_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dmgs_distributions'],
+      author='Daniel Guatibonza',
+      zip_safe=False)
