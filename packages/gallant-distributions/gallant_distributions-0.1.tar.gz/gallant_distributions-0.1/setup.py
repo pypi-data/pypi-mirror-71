03df574fe2d8df458769cfc6a0997ae33a7b8282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:10 +0000

    Add package gallant-distributions/0.1/gallant_distributions-0.1.tar.gz

---
+++ packages/gallant-distributions/gallant_distributions-0.1.tar.gz/gallant_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gallant_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'gallant decoy',
+      author_email ='gallantdecoy@gmail.com',
+      zip_safe=False)
