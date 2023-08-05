320cabd194c2493d9829fb0600d81f1940d4dc12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:20 +0000

    Add package tagword-crawler/0.2.0/tagword-crawler-0.2.0.tar.gz

---
+++ packages/tagword-crawler/tagword-crawler-0.2.0.tar.gz/tagword-crawler-0.2.0/tagword_crawler/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# Copyright 2018 The TagWord Authors. All Rights Reserved.
+# Name: TAGWORD CRAWLER爬虫软件
+# Contact: contact@tagword_crawler.cn
+# Description: TAGWORD CRAWLER软件
+# ========================================================
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
