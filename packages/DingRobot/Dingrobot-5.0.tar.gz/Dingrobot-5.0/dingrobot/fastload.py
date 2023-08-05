3189ec890d8d00b74804080b0e09698ff08795ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:53 +0000

    Add package DingRobot/5.0/Dingrobot-5.0.tar.gz

---
+++ packages/DingRobot/Dingrobot-5.0.tar.gz/Dingrobot-5.0/dingrobot/fastload.py
@@ -0,0 +1,8 @@
+import json
+def load(file_name):
+    bl = []
+    with open(file_name, "r") as rl:
+        bl_j = json.loads(rl.read())
+    for i in bl_j.keys():
+        bl.append(f"{i}=dingrobot.robot('{bl_j[i]['token']}','{bl_j[i]['key']}')")
+    return bl
