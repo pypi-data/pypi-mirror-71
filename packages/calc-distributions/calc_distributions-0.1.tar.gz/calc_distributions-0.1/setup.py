698a13606ff4ec58ce945ccbf87526f7f64e7f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:24 +0000

    Add package calc-distributions/0.1/calc_distributions-0.1.tar.gz

---
+++ packages/calc-distributions/calc_distributions-0.1.tar.gz/calc_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='calc_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['calc_distributions'],
+      author = "Sadeko Daniel",
+      author_email = "sadekodaniel@gmail.com",
+      zip_safe=False)
