eead1cf6b0a9d73020b0179c888fbb21254828f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:09 +0000

    Add package pktt/1.2/pktt-1.2.tar.gz

---
+++ packages/pktt/pktt-1.2.tar.gz/pktt-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pktt',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['pktt'],
+      author = 'Paul Tran',
+      author_email = 'tranpaul62@gmail.com',
+      zip_safe=False)
