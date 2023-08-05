9229c49ba8776fb73c62550c2dfd50fefa238357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:34 +0000

    Add package distributions-2/0.1/distributions_2-0.1.tar.gz

---
+++ packages/distributions-2/distributions_2-0.1.tar.gz/distributions_2-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_2',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_2'],
+      author= 'bayode',
+      email='bayfag@yahoo.com',
+      zip_safe=False)
