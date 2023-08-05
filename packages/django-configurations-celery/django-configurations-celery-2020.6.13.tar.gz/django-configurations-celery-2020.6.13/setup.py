25c0574eac1f16dfa9d44e267e45ef6a8053599a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:40 +0000

    Add package django-configurations-celery/2020.6.13/django-configurations-celery-2020.6.13.tar.gz

---
+++ packages/django-configurations-celery/django-configurations-celery-2020.6.13.tar.gz/django-configurations-celery-2020.6.13/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='django-configurations-celery',
+    version='2020.6.13',
+    install_requires=[
+        'django-configurations',
+        'setuptools',
+    ],
+    packages=[
+        'django_configurations_celery',
+    ],
+)
