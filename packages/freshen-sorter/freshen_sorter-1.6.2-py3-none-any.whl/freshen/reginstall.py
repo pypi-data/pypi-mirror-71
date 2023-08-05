1bdb5d2d8c5afe7c5aaf47350b30f2eb894db922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:31 +0000

    Add package freshen-sorter/1.6.2/freshen_sorter-1.6.2-py3-none-any.whl

---
+++ packages/freshen-sorter/freshen_sorter-1.6.2-py3-none-any.whl/freshen/reginstall.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath('.'))
+
+
+def install():
+    from context_menu import menus
+    import modules
+
+    pyLoc = sys.executable  # .replace('python.exe', 'pythonw.exe')
+    scriptLoc = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), 'main.py') # Location of parser to be called
+    menu = menus.ContextMenu('Sort Files', type='DIRECTORY_BACKGROUND')
+
+    extractCommand = menus.ContextCommand('Extract Files', python=modules.handleExtract)
+    extensionCommand = menus.ContextCommand('Sort by Extension', python=modules.handleExtension)
+    typeCommand = menus.ContextCommand('Sort by Type', python=modules.handleType)
+
+    dateMenu = menus.ContextMenu('Sort By Date')
+    dateMenu.add_items([
+        menus.ContextCommand('Day', python=modules.handleDate, params='D'),
+        menus.ContextCommand('Month', python=modules.handleDate, params='M'),
+        menus.ContextCommand('Year', python=modules.handleDate, params='Y')
+
+    ])
+
+    menu.add_items([
+        extractCommand,
+        extensionCommand,
+        typeCommand,
+        dateMenu
+    ])
+
+    menu.compile()
+
+
+def uninstall():
+    from context_menu import menus
+
+    menus.removeMenu('Sort Files', 'DIRECTORY_BACKGROUND')
+
+
+
+# if __name__ == "__main__":
+#     if is_admin():
+#         install()
+#     else:
+#         ctypes.windll.shell32.ShellExecuteW(
+#             None, "runas", sys.executable, __file__, None, 1)
+#
