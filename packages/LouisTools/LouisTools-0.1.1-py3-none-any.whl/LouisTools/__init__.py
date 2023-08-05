bf2ee8493950f22540d7c9f114c08b7e8e7367ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:46 +0000

    Add package LouisTools/0.1.1/LouisTools-0.1.1-py3-none-any.whl

---
+++ packages/LouisTools/LouisTools-0.1.1-py3-none-any.whl/LouisTools/__init__.py
@@ -0,0 +1,22 @@
+'''
+@version: Python 3.7.3
+@Author: Louis
+@Date: 2020-06-15 17:55:14
+@LastEditors: Louis
+@LastEditTime: 2020-06-15 17:55:14
+'''
+__project__ = "LouisTools"
+__version__ = "0.1.1"
+
+from LouisTools.louis_consts import *
+from LouisTools.louis_datetime import *
+from LouisTools.louis_decorators import *
+from LouisTools.louis_df import *
+from LouisTools.louis_email import *
+from LouisTools.louis_finance import *
+from LouisTools.louis_io import *
+from LouisTools.louis_keyrings import *
+from LouisTools.louis_log import *
+from LouisTools.louis_os import *
+from LouisTools.louis_re import *
+import LouisTools.HTMLTestRunner_py3
