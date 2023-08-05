ec23cb710cbc763b4bc354c830ab364432fea627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package hebrew-tokenizer/1.1.0/hebrew_tokenizer-1.1.0-py3-none-any.whl

---
+++ packages/hebrew-tokenizer/hebrew_tokenizer-1.1.0-py3-none-any.whl/hebrew_tokenizer/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# IMPORTS
+from .api import tokenize
+__version__ = "1.0.1"
+__all__ = ['tokenize']
+
---
+++ packages/hebrew-tokenizer/hebrew_tokenizer-1.1.0-py3-none-any.whl/hebrew_tokenizer/groups.py
@@ -0,0 +1,12 @@
+from enum import Enum, auto
+class Groups(Enum):
+    WHITESPACE = auto()
+    DATE = auto()
+    HOUR = auto()
+    NUMBER = auto()
+    URL = auto()
+    PUNCTUATION = auto()
+    ENGLISH = auto()
+    HEBREW = auto()
+    OTHER = auto()
+
