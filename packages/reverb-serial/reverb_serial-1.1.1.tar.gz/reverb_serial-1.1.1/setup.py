9dae07088276333383fb4039c02120f0afa142b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:24 +0000

    Add package reverb-serial/1.1.1/reverb_serial-1.1.1.tar.gz

---
+++ packages/reverb-serial/reverb_serial-1.1.1.tar.gz/reverb_serial-1.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="reverb_serial",
+    version="1.1.1",
+    license="GPL",
+    packages=find_packages(),
+    author="tjreverb",
+    author_email="tjreverb@gmail.com",
+    description="A fake serial library for testing purposes",
+    long_description=long_description,
+    url="https://github.com/TJREVERB/reverb_serial",
+    python_requires='>=3.6',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Other Audience",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+    ]
+)
