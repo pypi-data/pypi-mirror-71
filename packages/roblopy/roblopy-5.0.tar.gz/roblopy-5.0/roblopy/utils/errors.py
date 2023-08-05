1d3472d1e010642363429b27a17d64135f747212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:25 +0000

    Add package roblopy/5.0/roblopy-5.0.tar.gz

---
+++ packages/roblopy/roblopy-5.0.tar.gz/roblopy-5.0/roblopy/utils/errors.py
@@ -0,0 +1,11 @@
+class BadStatus(Exception):
+    pass
+
+class NotFound(Exception):
+    pass
+
+class BadRequest(Exception):
+    pass
+
+class ErrorMessage(Exception):
+    pass
---
+++ packages/roblopy/roblopy-5.0.tar.gz/roblopy-5.0/roblopy/utils/request.py
@@ -0,0 +1,41 @@
+import requests
+from .errors import *
+
+def get(url: str):
+    response = requests.get(url)
+
+    if "errorMessage" in response.json():
+        raise ErrorMessage(f"Error message is {response.json()['errorMessage']}. Data is {response.text}.")
+
+    if response.status_code == 200:
+        return response
+    elif response.status_code == 404:
+        raise NotFound(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
+    elif response.status_code == 400:
+        raise BadRequest(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
+    else:
+        raise BadStatus(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
+
+def no_json_get(url: str):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response
+    elif response.status_code == 404:
+        raise NotFound(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
+    elif response.status_code == 400:
+        raise BadRequest(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
+    else:
+        raise BadStatus(f"Got status code {response.status_code} from {url}. Data is {response.text}.")
+
+def no_data_get(url: str):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response
+    elif response.status_code == 404:
+        raise NotFound(f"Got status code {response.status_code} from {url}.")
+    elif response.status_code == 400:
+        raise BadRequest(f"Got status code {response.status_code} from {url}.")
+    else:
+        raise BadStatus(f"Got status code {response.status_code} from {url}.")
