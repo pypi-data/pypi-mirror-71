d5600e00e8380c521c28003e422f2511e626be9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:26 +0000

    Add package vidgear/0.1.8/vidgear-0.1.8-py3-none-any.whl

---
+++ packages/vidgear/vidgear-0.1.8-py3-none-any.whl/vidgear/gears/__init__.py
@@ -0,0 +1,11 @@
+# import the necessary packages
+from .camgear import CamGear
+from .netgear import NetGear
+from .pigear import PiGear
+from .screengear import ScreenGear
+from .videogear import VideoGear
+from .writegear import WriteGear
+
+__all__ = ["PiGear", "CamGear", "VideoGear", "ScreenGear", "WriteGear", "NetGear"]
+
+__author__ = "Abhishek Thakur (@abhiTronix) <abhi.una12@gmail.com>"
---
+++ packages/vidgear/vidgear-0.1.8-py3-none-any.whl/vidgear/gears/asyncio/__init__.py
@@ -0,0 +1,7 @@
+# import the necessary packages
+from .netgear_async import NetGear_Async
+from .webgear import WebGear
+
+__all__ = ["WebGear", "NetGear_Async"]
+
+__author__ = "Abhishek Thakur (@abhiTronix) <abhi.una12@gmail.com>"
