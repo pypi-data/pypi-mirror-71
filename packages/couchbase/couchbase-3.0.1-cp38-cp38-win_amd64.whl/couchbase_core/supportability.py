51633532c06eaf6ca0df667a98ec7e13f0ef9b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:56 +0000

    Add package couchbase/3.0.1/couchbase-3.0.1-cp38-cp38-win_amd64.whl

---
+++ packages/couchbase/couchbase-3.0.1-cp38-cp38-win_amd64.whl/couchbase_core/supportability.py
@@ -0,0 +1,96 @@
+import warnings
+
+from functools import wraps
+
+
+def deprecate_module_attribute(mod, deprecated=tuple()):
+    return warn_on_attribute_access(mod, deprecated, "deprecated")
+
+
+class Level(object):
+    def __new__(cls, function, *args, **kwargs):
+        """
+        Mark a function as {}
+
+        :param function: input function
+        :return: marked function
+        """.format(cls.__name__)
+
+        message = cls.__doc__+"\n"
+
+        func_name = getattr(function, '__qualname__', function.__name__)
+
+        result = cls.get_final_fn(function, message, func_name)
+        result.__doc__ = (function.__doc__+"\n\n" if function.__doc__ else "") + "    :warning: " + message % "This"
+        return result
+
+    @classmethod
+    def get_final_fn(cls, function, message, func_name):
+        @wraps(function)
+        def fn_wrapper(*args, **kwargs):
+            warnings.warn(message % "'{}'".format(func_name))
+            return function(*args, **kwargs)
+
+        return fn_wrapper
+
+
+class Uncommitted(Level):
+    """
+    %s is an uncommitted API call that is unlikely to change, but may still change as final consensus on its behavior has not yet been reached.
+    """
+
+
+uncommitted = Uncommitted
+
+
+class Volatile(Level):
+    """
+    %s is a volatile API call that is still in flux and may likely be changed.
+
+    It may also be an inherently private API call that may be exposed, but "YMMV" (your mileage may vary) principles apply.
+    """
+
+
+volatile = Volatile
+
+
+class Internal(Level):
+    """
+    %s is an internal API call.
+
+    Components external to Couchbase Python Client should not rely on it is not intended for use outside the module, even to other Couchbase components.
+    """
+
+    @classmethod
+    def get_final_fn(cls, function, *args):
+        return function
+
+
+internal = Internal
+
+
+class Committed(Level):
+    """
+    %s is guaranteed to be supported and remain stable between SDK versions.
+    """
+
+
+committed = Committed
+
+
+def warn_on_attribute_access(obj, applicable, status):
+    """Return a wrapped object that warns about deprecated accesses"""
+    applicable = set(applicable)
+
+    class Wrapper(object):
+        def __getattr__(self, attr):
+            if attr in applicable:
+                warnings.warn("Property %s is %s" % (attr, status))
+
+            return getattr(obj, attr)
+
+        def __setattr__(self, attr, value):
+            if attr in applicable:
+                warnings.warn("Property %s is %s" % (attr, status))
+            return setattr(obj, attr, value)
+    return Wrapper()
