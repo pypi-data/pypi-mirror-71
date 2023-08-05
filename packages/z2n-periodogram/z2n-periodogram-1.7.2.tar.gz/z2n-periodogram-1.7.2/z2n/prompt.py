eb15b6dfbd2d6a4e2be8912239f5e27f7031b115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:26 +0000

    Add package z2n-periodogram/1.7.2/z2n-periodogram-1.7.2.tar.gz

---
+++ packages/z2n-periodogram/z2n-periodogram-1.7.2.tar.gz/z2n-periodogram-1.7.2/z2n/prompt.py
@@ -0,0 +1,134 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Other Libraries
+import click
+from click_shell import shell
+
+# Owned Libraries
+from z2n import __docs__
+from z2n import __version__
+from z2n.plot import Plot
+
+# Instance of the Plot Object
+figure = Plot()
+
+# Variables to display on prompt
+__z2n__ = f'''
+        Z2n Software ({__version__}), a python program for periodograms analysis.
+        Copyright (C) 2020, and MIT License, by Yohan Alexander [UFS].
+        Type "help" for more information or "docs" for documentation.
+        '''
+
+__plt__ = f'''
+        Interactive plotting window of the Z2n Software ({__version__}).
+        Type "help" for more information.
+        '''
+
+
+@click.version_option(prog_name='Z2n Software', version=__version__)
+@shell(prompt=click.style('(z2n) >>> ', fg='blue', bold=True), intro=__z2n__)
+def z2n():
+    """
+    This program allows the user to calculate periodograms, given a time series,
+    using the Z2n statistics a la Buccheri et al. 1983.
+
+    The standard Z2n statistics calculates the phase of each arrival time and
+    the corresponding sinusoidal functions for each time. Be advised that this
+    is very computationally expensive if the number of frequency bins is high.
+    """
+
+
+@z2n.command()
+def docs() -> None:
+    """Open the documentation on the software."""
+    click.launch(__docs__)
+    click.echo(f"To read the documentation go to {__docs__}")
+
+
+@z2n.command()
+def plot() -> None:
+    """Open the interactive plotting window."""
+    if figure.data.z2n.size == 0:
+        click.secho("The periodogram was not calculated yet.", fg='yellow')
+        if click.confirm("Do you want to plot from a file"):
+            if not figure.plot_file():
+                plt()
+    else:
+        figure.plot_figure()
+        plt()
+
+
+@z2n.command()
+def run() -> None:
+    """Calculate the Z2n Statistics."""
+    if not figure.plot_periodogram():
+        plt()
+
+
+@z2n.command()
+def save() -> None:
+    """Save the periodogram on a file."""
+    if figure.data.z2n.size == 0:
+        click.secho("The periodogram was not calculated yet.", fg='yellow')
+    else:
+        figure.data.save_file()
+
+
+@shell(prompt=click.style('(plt) >>> ', fg='magenta', bold=True), intro=__plt__)
+def plt() -> None:
+    """Open the interactive periodogram plotting window."""
+
+
+@plt.command()
+def peak() -> None:
+    """Plot a vetical line on the peak."""
+    figure.plot_peak()
+
+
+@plt.command()
+def title() -> None:
+    """Change the title on the figure."""
+    figure.change_title()
+
+
+@plt.command()
+def xlabel() -> None:
+    """Change the label on the x axis."""
+    figure.change_xlabel()
+
+
+@plt.command()
+def xscale() -> None:
+    """Change the scale on the x axis."""
+    figure.change_xscale()
+
+
+@plt.command()
+def xlim() -> None:
+    """Change the limites on the x axis."""
+    figure.change_xlim()
+
+
+@plt.command()
+def ylabel() -> None:
+    """Change the label on the y axis."""
+    figure.change_ylabel()
+
+
+@plt.command()
+def yscale() -> None:
+    """Change the scale on the y axis."""
+    figure.change_yscale()
+
+
+@plt.command()
+def ylim() -> None:
+    """Change the limites on the y axis."""
+    figure.change_ylim()
+
+
+@plt.command()
+def save() -> None:
+    """Save the image on a file."""
+    figure.save_image()
