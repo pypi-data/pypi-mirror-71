445782cb4b1d4024d048a3b6e1bc336539ed1ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:10 +0000

    Add package reamber/0.0.12/reamber-0.0.12.tar.gz

---
+++ packages/reamber/reamber-0.0.12.tar.gz/reamber-0.0.12/reamber/base/RAConst.py
@@ -0,0 +1,51 @@
+class RAConst :
+
+    HR_TO_MIN   : float = 60.0
+    HR_TO_SEC   : float = 60.0 * 60.0
+    HR_TO_MSEC  : float = 60.0 * 60.0 * 1000.0
+    MIN_TO_HR   : float = 1.0 / 60.0
+    MIN_TO_SEC  : float = 60.0
+    MIN_TO_MSEC : float = 60.0 * 1000.0
+    SEC_TO_HR   : float = 1.0 / (60.0 * 60.0)
+    SEC_TO_MIN  : float = 1.0 / 60.0
+    SEC_TO_MSEC : float = 1000.0
+    MSEC_TO_HR  : float = 1.0 / (60.0 * 60.0 * 1000.0)
+    MSEC_TO_MIN : float = 1.0 / (60.0 * 1000.0)
+    MSEC_TO_SEC : float = 1.0 / 1000.0
+
+    @staticmethod
+    def hrToMin(hours): return float(hours * RAConst.HR_TO_MIN)
+    @staticmethod
+    def hrToSec(hours): return float(hours * RAConst.HR_TO_SEC)
+    @staticmethod
+    def hrToMSec(hours): return float(hours * RAConst.HR_TO_MSEC)
+    @staticmethod
+    def minToHr(mins): return float(mins * RAConst.MIN_TO_HR)
+    @staticmethod
+    def minToSec(mins): return float(mins * RAConst.MIN_TO_SEC)
+    @staticmethod
+    def minToMSec(mins): return float(mins * RAConst.MIN_TO_MSEC)
+    @staticmethod
+    def secToHr(secs): return float(secs * RAConst.SEC_TO_HR)
+    @staticmethod
+    def secToMin(secs): return float(secs * RAConst.SEC_TO_MIN)
+    @staticmethod
+    def secToMSec(secs): return float(secs * RAConst.SEC_TO_MSEC)
+    @staticmethod
+    def mSecToHr(msecs): return float(msecs * RAConst.MSEC_TO_HR)
+    @staticmethod
+    def mSecToMin(msecs): return float(msecs * RAConst.MSEC_TO_MIN)
+    @staticmethod
+    def mSecToSec(msecs): return float(msecs * RAConst.MSEC_TO_SEC)
+
+    # These are the colors to indicate snapping
+    DIVISION_COLORS = {1: "#969696"
+                     , 2: "#b02323"
+                     , 3: "#8a23b0"
+                     , 4: "#2399b0"
+                     , 6: "#b02378"
+                     , 8: "#b06c23"
+                     , 12: "#23b09d"
+                     , 16: "#3db023"
+                     , 24: "#94b023"
+                     , 32: "#23b052"}
