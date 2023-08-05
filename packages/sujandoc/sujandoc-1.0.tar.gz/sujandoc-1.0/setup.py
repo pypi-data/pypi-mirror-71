e5eb7f7772326ea8629cd43b41b4349371b76657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:58 +0000

    Add package sujandoc/1.0/sujandoc-1.0.tar.gz

---
+++ packages/sujandoc/sujandoc-1.0.tar.gz/sujandoc-1.0/setup.py
@@ -0,0 +1,5 @@
+import setuptools
+setuptools.setup(name="sujandoc",
+                 version="1.0",
+                 long_description="this is that",
+                 packages=setuptools.find_packages())
