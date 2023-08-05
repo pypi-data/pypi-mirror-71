7ff12f68d0a5fad26f224963adf5dad526f525d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:09 +0000

    Add package pexelsPy/1.0.1/pexelsPy-1.0.1.tar.gz

---
+++ packages/pexelsPy/pexelsPy-1.0.1.tar.gz/pexelsPy-1.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pexelsPy",
+    version="1.0.1",
+    author="Sravan Kumar",
+    author_email="demonlyf98@protonmail.com",
+    description="Use Pexels API v1 with Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kingsamurai123/pexels-api",
+    keywords='pexels api images photos videos python',
+    install_requires=['requests'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # python_requires='>=3',
+)
