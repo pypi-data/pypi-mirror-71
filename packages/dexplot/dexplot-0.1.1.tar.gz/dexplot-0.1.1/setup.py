db14d92248d3e996e95a4e39803f17a6e75a1d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:54 +0000

    Add package dexplot/0.1.1/dexplot-0.1.1.tar.gz

---
+++ packages/dexplot/dexplot-0.1.1.tar.gz/dexplot-0.1.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+from dexplot import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dexplot",
+    version=__version__,
+    author="Ted Petrou",
+    author_email="petrou.theodore@gmail.com",
+    description="Powerful and intuitive data visualization library using matplotlib for both long and wide data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="data visualization matplotlib pandas",
+    url="https://github.com/dexplo/dexplot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Framework :: Matplotlib"
+    ],
+    install_requires=['matplotlib>=3.1', 
+                      'pandas>=0.24', 
+                      'numpy>=1.15', 
+                      'scipy>=1.0'],
+    python_requires='>=3.6'
+)
