ee6b563eb18bcb1e8391a808498e0a5a03515d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:44 +0000

    Add package jtt-tm-utils/0.0.6/jtt-tm-utils-0.0.6.tar.gz

---
+++ packages/jtt-tm-utils/jtt-tm-utils-0.0.6.tar.gz/jtt-tm-utils-0.0.6/jtt_tm_utils/__init__.py
@@ -0,0 +1,10 @@
+__version__ ='0.0.6'
+__author__ = 'candyabc'
+__copyright__ = 'candyabc'
+__email__ = 'hfcandyabc@163.com'
+__license__ = 'maxwin'
+
+
+from .sync_basedata import data_manager
+
+# __all__=("JttTaskManager","JttTask")
