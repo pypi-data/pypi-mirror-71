176d79906e0e62bb6cf2994a5f13513dbf1e81ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:35 +0000

    Add package vkstartiii/0.2.1/vkstartiii-0.2.1.tar.gz

---
+++ packages/vkstartiii/vkstartiii-0.2.1.tar.gz/vkstartiii-0.2.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['vkstartiii']
+install_requires = \
+['vk_api>=11.8.0,<12.0.0']
+
+setup_kwargs = {
+    'name': 'vkstartiii',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'LooPeKa',
+    'author_email': 'loudmaks10@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
