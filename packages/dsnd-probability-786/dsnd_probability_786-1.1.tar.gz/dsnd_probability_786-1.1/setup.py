3347467d60b2298f5fc71f7474c9c3e585760462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:46 +0000

    Add package dsnd-probability-786/1.1/dsnd_probability_786-1.1.tar.gz

---
+++ packages/dsnd-probability-786/dsnd_probability_786-1.1.tar.gz/dsnd_probability_786-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_786',
+      version= '1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_786'],
+      author = 'Faiza Ahmed',
+      author_email = 'syeda.faiza.ahmed@gmail.com',
+   zip_safe=False)
+
+      
