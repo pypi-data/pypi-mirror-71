f56c7a353fbc4f26bb6bdd0f81b40c338a781b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:34 +0000

    Add package linformer-pytorch/0.4.0/linformer_pytorch-0.4.0.tar.gz

---
+++ packages/linformer-pytorch/linformer_pytorch-0.4.0.tar.gz/linformer_pytorch-0.4.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="linformer_pytorch", # Replace with your own username
+    version="0.4.0",
+    author="Peter Tatkowski",
+    author_email="tatp22@gmail.com",
+    description="An implementation of the Linformer in Pytorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tatp22/linformer-pytorch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'torch',
+    ],
+)
