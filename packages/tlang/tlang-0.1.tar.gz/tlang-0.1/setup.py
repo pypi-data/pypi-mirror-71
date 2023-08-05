28b959f23bd6d0e13ff627eea6f3a19f529b719f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:34 +0000

    Add package tlang/0.1/tlang-0.1.tar.gz

---
+++ packages/tlang/tlang-0.1.tar.gz/tlang-0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name = "tlang",
+    packages = ["tlang"],
+    version = "0.1",
+    license="Apache-2.0",
+    description = "Machine learning for systems",
+    author = "tlang",
+    author_email = "tianqi.tchen@gmail.com",
+    keywords = [],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+  ],
+)
