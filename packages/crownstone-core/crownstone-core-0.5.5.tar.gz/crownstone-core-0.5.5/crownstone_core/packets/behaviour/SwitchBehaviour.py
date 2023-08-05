c9e1fd0655930ce44a3982b78f9627bd6099dc34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:00 +0000

    Add package crownstone-core/0.5.5/crownstone-core-0.5.5.tar.gz

---
+++ packages/crownstone-core/crownstone-core-0.5.5.tar.gz/crownstone-core-0.5.5/crownstone_core/packets/behaviour/SwitchBehaviour.py
@@ -0,0 +1,84 @@
+from crownstone_core.packets.behaviour.BehaviourBase import BehaviourBase
+from crownstone_core.packets.behaviour.BehaviourTypes import BehaviourType
+from crownstone_core.packets.behaviour.PresenceDescription import BehaviourPresence, BehaviourPresenceType, \
+    DEFAULT_PRESENCE_DELAY
+
+DEFAULT_PRESENCE = BehaviourPresence().setSpherePresence(BehaviourPresenceType.someoneInSphere)
+
+
+class SwitchBehaviour(BehaviourBase):
+    """
+    Implements packet generation for SwitchBehaviours
+    """
+
+    def __init__(self, profileIndex=0, behaviourType=BehaviourType.behaviour, intensity=None, activeDays=None,
+                 time=None, presence=None, endCondition=None, idOnCrownstone=None):
+        super().__init__(profileIndex, behaviourType, intensity, activeDays, time, presence, endCondition,
+                         idOnCrownstone)
+
+    def ignorePresence(self):
+        self.presence = None
+        return self
+
+    def setPresenceIgnore(self):
+        return self.ignorePresence()
+
+    def setPresenceSomebody(self):
+        self.setPresenceSomebodyInSphere()
+        return self
+
+    def setPresenceNobody(self):
+        self.setPresenceNobodyInSphere()
+        return self
+
+    def setPresenceSomebodyInSphere(self):
+        self.presence = BehaviourPresence().setSpherePresence(BehaviourPresenceType.someoneInSphere)
+        return self
+
+    def setPresenceNobodyInSphere(self):
+        self.presence = BehaviourPresence().setSpherePresence(BehaviourPresenceType.nobodyInSphere)
+        return self
+
+    def setPresenceInSphere(self):
+        self.setPresenceSomebodyInSphere()
+        return self
+
+    def setPresenceInLocations(self, locationIds):
+        self.setPresenceSomebodyInLocations(locationIds)
+        return self
+
+    def setPresenceSomebodyInLocations(self, locationIds, delay=DEFAULT_PRESENCE_DELAY):
+        self.presence = BehaviourPresence().setLocationPresence(BehaviourPresenceType.somoneInLocation, locationIds,
+                                                                delay)
+        return self
+
+    def setPresenceNobodyInLocations(self, locationIds, delay=DEFAULT_PRESENCE_DELAY):
+        self.presence = BehaviourPresence().setLocationPresence(BehaviourPresenceType.nobodyInLocation, locationIds,
+                                                                delay)
+        return self
+
+    def setNoEndCondition(self):
+        self.endCondition = None
+        return self
+
+    def setEndConditionWhilePeopleInSphere(self):
+        self.endCondition = BehaviourPresence().setSpherePresence(BehaviourPresenceType.someoneInSphere)
+        return self
+
+    def setEndConditionWhilePeopleInLocation(self, locationId):
+        self.endCondition = BehaviourPresence().setLocationPresence(BehaviourPresenceType.somoneInLocation,
+                                                                    [locationId])
+        return self
+
+    def getPacket(self):
+        arr = super().getPacket()
+
+        print("new getpacket impl.")
+        if self.presence is not None:
+            arr += self.presence.getPacket()
+        else:
+            anypresence = BehaviourPresence()
+            arr += anypresence.getPacket()
+
+        return arr
+
