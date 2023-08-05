0f344cef2cc0a3a8ee2da4b480dc570af6a0540c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:16 +0000

    Add package alt-model-checkpoint/2.0.3/alt-model-checkpoint-2.0.3.tar.gz

---
+++ packages/alt-model-checkpoint/alt-model-checkpoint-2.0.3.tar.gz/alt-model-checkpoint-2.0.3/alt_model_checkpoint/test__init__.py
@@ -0,0 +1,73 @@
+import unittest
+from typing import Callable, Union
+from unittest import TestCase
+from unittest.mock import Mock
+
+
+class CommonAltModelCheckpointTests(TestCase):
+    cls = None
+    model_cls = None
+
+    def run(self, *args, **kwargs) -> Union[TestCase, Callable]:
+        # Don't run tests on naked base class
+        if type(self) is CommonAltModelCheckpointTests:
+            return unittest.skip('base class test skip')
+
+        return super().run(*args, **kwargs)
+
+    def test_kwargs_pass_through(self):
+        callback = self.cls('path/to/model.hdf5', None, monitor='foobar')
+        self.assertEqual(callback.filepath, 'path/to/model.hdf5')
+        self.assertEqual(callback.monitor, 'foobar')
+
+    def test_on_epoch_end(self):
+        multigpu_model = Mock(_in_multi_worker_mode=lambda: False)
+        multigpu_model_optimizer = Mock()
+        multigpu_model.optimizer = multigpu_model_optimizer
+
+        template_model = Mock(_in_multi_worker_mode=lambda: False)
+
+        # noinspection PyUnusedLocal
+        def save_impl_mock(*args, **kwargs):
+            self.assertIs(template_model.optimizer, multigpu_model_optimizer,
+                          'original optimizer is saved with template model')
+        template_model.save = Mock(side_effect=save_impl_mock)
+
+        template_model_optimizer = Mock()
+        template_model.optimizer = template_model_optimizer
+
+        callback = self.cls('path/to/model.hdf5', template_model)
+        callback.model = multigpu_model
+
+        callback.on_epoch_end(42)
+        self.assertIs(callback.model, multigpu_model, 'original model is restored')
+        self.assertIs(template_model.optimizer, template_model_optimizer, 'template model optimizer state restored')
+
+        # template model is saved
+        template_model.save.assert_called_once_with('path/to/model.hdf5', overwrite=True)
+
+    def test_can_opt_out_of_inherited_optimizer(self):
+        multigpu_model = Mock(_in_multi_worker_mode=lambda: False)
+        multigpu_model_optimizer = Mock()
+        multigpu_model.optimizer = multigpu_model_optimizer
+
+        template_model = Mock(_in_multi_worker_mode=lambda: False)
+        template_model_optimizer = Mock()
+        template_model.optimizer = template_model_optimizer
+
+        # noinspection PyUnusedLocal
+        def save_impl_mock(*args, **kwargs):
+            self.assertIs(template_model.optimizer, template_model_optimizer,
+                          'template model optimizer is saved with template model')
+
+        template_model.save = Mock(side_effect=save_impl_mock)
+
+        callback = self.cls('path/to/model.hdf5', template_model, inherit_optimizer=False)
+        callback.model = multigpu_model
+
+        callback.on_epoch_end(42)
+        self.assertIs(callback.model, multigpu_model, 'original model is restored')
+        self.assertIs(template_model.optimizer, template_model_optimizer, 'template model optimizer is preserved')
+
+        # template model is saved
+        template_model.save.assert_called_once_with('path/to/model.hdf5', overwrite=True)
---
+++ packages/alt-model-checkpoint/alt-model-checkpoint-2.0.3.tar.gz/alt-model-checkpoint-2.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='alt-model-checkpoint',
+    version='2.0.3',
+    author='Ezekiel Victor',
+    author_email='zekevictor@gmail.com',
+    description='An adapter callback for Keras ModelCheckpoint that allows checkpointing an alternate model'
+                ' (often submodel of a multi-GPU model).',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/TextpertAi/alt-model-checkpoint',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+    python_requires='>=3',
+    install_requires=[
+        'keras'
+    ],
+)
