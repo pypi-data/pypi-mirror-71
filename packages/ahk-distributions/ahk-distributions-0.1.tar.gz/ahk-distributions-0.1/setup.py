460d3322d6b03c3714f3adc7b884a0c153506c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:51 +0000

    Add package ahk-distributions/0.1/ahk-distributions-0.1.tar.gz

---
+++ packages/ahk-distributions/ahk-distributions-0.1.tar.gz/ahk-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ahk-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = "Aye Hninn Khine",
+      author_email = "ayehninnkhine93@gmail.com",
+      zip_safe=False)
