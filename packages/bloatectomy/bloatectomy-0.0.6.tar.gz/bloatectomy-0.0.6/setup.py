8da21e37744212a380f34d991741c941cbb40d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:27 +0000

    Add package bloatectomy/0.0.6/bloatectomy-0.0.6.tar.gz

---
+++ packages/bloatectomy/bloatectomy-0.0.6.tar.gz/bloatectomy-0.0.6/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bloatectomy", # Replace with your own username
+    version="0.0.6",
+    author="Summer Rankin, Roselie Bright, Katherine Dowdy",
+    author_email="summerKRankin@gmail.com",
+    description="Bloatectomy: a method for the identification and removal of duplicate text in the bloated notes of electronic health records and other documents.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MIT-LCP/mimic-code",
+    packages=setuptools.find_packages(exclude=['tests','input','output','paper']),
+    keywords=["python", "medical informatics","electronic health records",
+    "electronic medical records", "public health informatics",
+    "clinical information extraction", "informatics", "natural language processing"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing",
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.7',
+)
