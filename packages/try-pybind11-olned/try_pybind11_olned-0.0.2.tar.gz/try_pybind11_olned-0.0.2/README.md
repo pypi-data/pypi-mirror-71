bea6f5eab418b7377b20e437b80b3101f7bf7630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:13 +0000

    Add package try-pybind11-olned/0.0.2/try_pybind11_olned-0.0.2.tar.gz

---
+++ packages/try-pybind11-olned/try_pybind11_olned-0.0.2.tar.gz/try_pybind11_olned-0.0.2/README.md
@@ -0,0 +1,6 @@
+# vscode-try-pybind11
+
+This is a training project
+
+# License
+The project uses [pibind11](https://github.com/pybind/pybind11), pybind11 is provided under a BSD-style license that can be found in the [LICENSE file](https://github.com/pybind/pybind11/blob/master/LICENSE).
---
+++ packages/try-pybind11-olned/try_pybind11_olned-0.0.2.tar.gz/try_pybind11_olned-0.0.2/src/example.cpp
@@ -0,0 +1,38 @@
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+int add(int i, int j)
+{
+    return i + j;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(try_pybind11_olned, m) {
+    m.doc() = R"pbdoc(
+        Pybind11 example plugin
+        -----------------------
+        .. currentmodule:: try_pypind11_olned
+        .. autosummary::
+           :toctree: _generate
+           add
+           subtract
+    )pbdoc";
+
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = VERSION_INFO;
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
