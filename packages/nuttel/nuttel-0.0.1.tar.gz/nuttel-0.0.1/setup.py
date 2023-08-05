17f28744af47908a06f12f19b92aaabe4c0bbc69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package nuttel/0.0.1/nuttel-0.0.1.tar.gz

---
+++ packages/nuttel/nuttel-0.0.1.tar.gz/nuttel-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nuttel",
+    version="0.0.1",
+    author="Brett Beeson",
+    author_email="brettbeeson@fastmail.com",
+    include_package_data=True,
+    description="SSH Tunnel Utilties",
+    long_description="Client makes ssh port to server at unknown port, server writes ssh config to get back to client",
+    # long_description_content_type="text/markdown",
+    url="https://github.com/brettbeeson/nuttel",
+    packages=setuptools.find_packages(),
+    scripts = ['nuttel.py'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
+    # todo: change from boto to minio. boto is massive
+    install_requires=['psutil', 'sshconf'
+    ],
+    python_requires='>=3.6',
+)
