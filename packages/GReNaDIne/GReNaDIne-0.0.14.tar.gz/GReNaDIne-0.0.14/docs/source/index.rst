3d6ded2dacfb7da297b17647ebcc86ed3c498800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:51 +0000

    Add package GReNaDIne/0.0.14/GReNaDIne-0.0.14.tar.gz

---
+++ packages/GReNaDIne/GReNaDIne-0.0.14.tar.gz/GReNaDIne-0.0.14/docs/source/index.rst
@@ -0,0 +1,27 @@
+.. grenadine documentation master file, created by
+   sphinx-quickstart on Mon Jul  1 09:51:38 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to grenadine's documentation!
+=====================================
+
+.. image:: _static/logo_grenadine_white.png
+   :width: 150
+   :align: center
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   grenadine
+   grenadine.Preprocessing
+   grenadine.Inference
+   grenadine.Evaluation
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/GReNaDIne/GReNaDIne-0.0.14.tar.gz/GReNaDIne-0.0.14/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GReNaDIne",
+    version="0.0.14",
+    author="Sergio Peignier, Pauline Schmitt",
+    author_email="sergio.peignier@insa-lyon.fr",
+    description="Data-Driven Gene Regulatory Network Inference",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'tqdm',
+        'scipy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+)
+
+print(setuptools.find_packages())
