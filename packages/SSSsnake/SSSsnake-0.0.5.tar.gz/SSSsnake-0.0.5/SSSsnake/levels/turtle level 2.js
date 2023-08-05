6788e09e1d918f5bf9ac33937f7bcfe7ab912df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package SSSsnake/0.0.5/SSSsnake-0.0.5.tar.gz

---
+++ packages/SSSsnake/SSSsnake-0.0.5.tar.gz/SSSsnake-0.0.5/SSSsnake/levels/turtle level 2.js
@@ -0,0 +1,11 @@
+
+console.log("drawing level 2")
+
+DrawAppleAt(0,4)
+
+DrawDeadlyCellAt(0,3);
+DrawDeadlyCellAt(1,3);
+DrawBridgeAt(1,3,0);
+DrawDeadlyCellAt(3,3);
+
+console.log("level drawing complete!")
