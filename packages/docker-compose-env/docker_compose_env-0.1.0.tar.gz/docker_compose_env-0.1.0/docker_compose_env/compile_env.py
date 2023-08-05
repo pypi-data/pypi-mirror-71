74cc1f62f467412c026fd1aac73f57931869136f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package docker-compose-env/0.1.0/docker_compose_env-0.1.0.tar.gz

---
+++ packages/docker-compose-env/docker_compose_env-0.1.0.tar.gz/docker_compose_env-0.1.0/docker_compose_env/compile_env.py
@@ -0,0 +1,48 @@
+import argparse
+import yaml
+import re
+import os
+
+
+def compile(env_line):
+    regex = r"(export\s*)?([^=]+)=([^=]+)\s*"
+    matches = list(re.finditer(regex, env_line))
+
+    if len(matches) == 1:
+        groups = matches[0].groups()
+        prefix = groups[0] or ""
+        key = groups[1]
+        value = os.path.expandvars(groups[2])
+        os.environ[key] = value
+        return f"{prefix}{key}={value}"
+
+    return None
+
+
+def compile_files(input_files):
+    content = ""
+    for input_file in input_files:
+        with open(input_file) as f:
+            for env_line in f.readlines():
+                output_line = compile(env_line.strip())
+                if output_line:
+                    content += output_line + os.linesep
+    return content
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("spec_file")
+
+    args = parser.parse_args()
+    with open(args.spec_file) as f:
+        spec = yaml.load(f, Loader=yaml.FullLoader)
+
+    for output_filename, input_files in spec.items():
+        content = compile_files(input_files)
+        with open(output_filename, 'w') as f:
+            f.write(content)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/docker-compose-env/docker_compose_env-0.1.0.tar.gz/docker_compose_env-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(name='docker_compose_env',
+      version='0.1.0',
+      description=
+      'Solve the problem that environment variables are not interpolated by docker-compose',
+      url='https://github.com/mnieber/docker_compose_env',
+      download_url='https://github.com/mnieber/docker_compose_env/tarball/0.1.0',
+      author='Maarten Nieber',
+      author_email='hallomaarten@yahoo.com',
+      license='MIT',
+      packages=find_packages(),
+      package_data={
+          'docker_compose_env': [
+              'bin/*.sh',
+              'bin/*.fish',
+          ]
+      },
+      entry_points={'console_scripts': [
+          'compile-env=docker_compose_env.compile_env:main',
+      ]},
+      data_files=[],
+      install_requires=["PyYAML"],
+      zip_safe=False)
