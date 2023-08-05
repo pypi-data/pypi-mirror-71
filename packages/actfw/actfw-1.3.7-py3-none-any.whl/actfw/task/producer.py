91b36d96040ea9fde04b940058c554a8a13b817a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:55 +0000

    Add package actfw/1.3.7/actfw-1.3.7-py3-none-any.whl

---
+++ packages/actfw/actfw-1.3.7-py3-none-any.whl/actfw/task/producer.py
@@ -0,0 +1,17 @@
+from .pipe import Pipe
+
+
+class Producer(Pipe):
+
+    """Producer Task."""
+
+    def __init__(self):
+        """
+        """
+        super(Producer, self).__init__()
+
+    def _add_in_queue(self, q):
+        raise NotImplementedError('This is producer')
+
+    def _inlet(self):
+        yield ()
