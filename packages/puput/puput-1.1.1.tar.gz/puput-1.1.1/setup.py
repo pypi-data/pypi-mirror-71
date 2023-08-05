7bb49161b05fe504c3b12cec20401c71bdfd227a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:22 +0000

    Add package puput/1.1.1/puput-1.1.1.tar.gz

---
+++ packages/puput/puput-1.1.1.tar.gz/puput-1.1.1/setup.py
@@ -0,0 +1,65 @@
+import os
+import re
+import codecs
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = codecs.open(os.path.join(package, '__init__.py'), encoding='utf-8').read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_author(package):
+    """
+    Return package author as listed in `__author__` in `init.py`.
+    """
+    init_py = codecs.open(os.path.join(package, '__init__.py'), encoding='utf-8').read()
+    return re.search("^__author__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_email(package):
+    """
+    Return package email as listed in `__email__` in `init.py`.
+    """
+    init_py = codecs.open(os.path.join(package, '__init__.py'), encoding='utf-8').read()
+    return re.search("^__email__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+setup(
+    name='puput',
+    version=get_version('puput'),
+    packages=find_packages(),
+    include_package_data=True,
+    keywords="django wagtail puput blog cms app",
+    description='A Django blog app implemented in Wagtail.',
+    long_description=codecs.open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8').read(),
+    install_requires=[
+        'Django>=2.1.6,<3.1',
+        'wagtail>=2.7,<3.0',
+        'django-el-pagination>=3.2.4',
+        'django-social-share>=1.3.0',
+        'django-colorful>=1.3'
+    ],
+    url='http://github.com/APSL/puput',
+    author=get_author('puput'),
+    author_email=get_email('puput'),
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ]
+)
