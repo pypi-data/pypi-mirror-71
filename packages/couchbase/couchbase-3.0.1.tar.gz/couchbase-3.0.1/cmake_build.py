4be87701a0a74dc2d27900ad202b5b89677ac288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:57 +0000

    Add package couchbase/3.0.1/couchbase-3.0.1.tar.gz

---
+++ packages/couchbase/couchbase-3.0.1.tar.gz/couchbase-3.0.1/cmake_build.py
@@ -0,0 +1,213 @@
+#
+# Copyright 2019, Couchbase, Inc.
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import os.path
+import platform
+import re
+import subprocess
+import sys
+
+from distutils.version import LooseVersion
+
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext
+
+import cbuild_config
+from cbuild_config import couchbase_core, build_type
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir='', **kw):
+        Extension.__init__(self, name, **kw)
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(cbuild_config.CBuildCommon):
+    hasrun = False
+    hasbuilt = False
+    hybrid = False
+    info = None  # type: cbuild_config.CBuildInfo
+
+    def run(self):
+        if not CMakeBuild.hasrun:
+            out = self.check_for_cmake()
+            if not out:
+                raise RuntimeError(
+                    "CMake must be installed to build the following extensions: " +
+                    ", ".join(e.name for e in self.extensions))
+
+            if platform.system() == "Windows":
+                cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
+                                                       out.decode()).group(1))
+                if cmake_version < '3.1.0':
+                    raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+            CMakeBuild.hasrun = True
+            if CMakeBuild.hybrid:
+                build_ext.run(self)
+
+            for ext in self.extensions:
+                self.build_extension(ext)
+
+    @staticmethod
+    def check_for_cmake():
+        try:
+            return subprocess.check_output(['cmake', '--version'])
+        except:
+            return None
+
+    @staticmethod
+    def requires():
+        return "" if CMakeBuild.check_for_cmake() else ["cmake"]
+
+    def prep_build(self, ext):
+        if not CMakeBuild.hasbuilt:
+            from distutils.sysconfig import get_python_inc
+            import distutils.sysconfig as sysconfig
+            cfg = self.cfg_type()
+            extdir = os.path.abspath(
+                os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+            lcb_api_flags = self.get_lcb_api_flags()
+            cmake_args = lcb_api_flags + ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                                          '-DPYTHON_EXECUTABLE=' + sys.executable]
+            cmake_args += ['-DPYTHON_INCLUDE_DIR={}'.format(get_python_inc())]
+            self.info.setbase(self.build_temp)
+            self.info.cfg=cfg
+            from distutils import sysconfig
+            import os.path as op
+            v = sysconfig.get_config_vars()
+            print("LIBDIR {}, LIBPL {}".format(v.get("LIBDIR"), v.get("LIBPL")))
+            fpaths = [op.join(v.get(pv, ''), v.get('LDLIBRARY', '')) for pv in ('LIBDIR', 'LIBPL')] + [os.path.normpath(
+                os.path.join(get_python_inc(), "..", "..", "lib",
+                             "libpython{}.dylib".format('.'.join(map(str, sys.version_info[0:2]))))),
+                os.path.join(get_python_inc(), "..", "..", "lib")]
+            python_lib = None
+            python_libdir = None
+            for entry in fpaths:
+                if not op.exists(entry):
+                    print("fpath {} does not exist".format(entry))
+                    continue
+                try:
+                    print("got fpath {}:".format(entry))
+                    if op.isfile(entry):
+                        print("fpath {} is file, selecting".format(entry))
+                        python_lib = python_lib or entry
+                        continue
+                    else:
+                        entries = os.listdir(entry)
+                        print("fpath {} is directory, contents {}".format(entry, entries))
+                        for subentry in entries:
+                            fullname = op.normpath(op.join(entry, subentry))
+                            try:
+                                fullname = op.readlink(fullname)
+                            except:
+                                pass
+                            print("trying subentry:{}".format(fullname))
+
+                            if op.exists(fullname):
+                                python_lib = python_lib or fullname
+                                python_libdir = op.normpath(entry)
+                                print("got match {}, breaking out".format(fullname))
+                                continue
+
+                except:
+                    pass
+            cmake_args += ['-DHYBRID_BUILD=TRUE'] if CMakeBuild.hybrid else []
+            cmake_args += ['-DPYTHON_LIBFILE={}'.format(python_lib)] if python_lib else []
+            cmake_args += ['-DPYTHON_LIBDIR={}'.format(python_libdir)] if python_libdir else []
+            cmake_args += [
+                '-DPYTHON_VERSION_EXACT={}'.format('.'.join(map(str, sys.version_info[0:2])))] if python_libdir else []
+            build_args = ['--config', cfg]
+            if platform.system() == "Windows":
+                cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
+                    cfg.upper(),
+                    extdir), '-DLCB_NO_MOCK=1', '-DLCB_NO_SSL=1']
+                if sys.maxsize > 2 ** 32:
+                    cmake_args += ['-A', 'x64']
+                build_args += ['--', '/m']
+            else:
+                cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg.upper()]
+                build_args += ['--', '-j2']
+
+            env = os.environ.copy()
+            cmake_args += ['-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON']
+            cxx_compile_args=filter(re.compile(r'^(?!-std\s*=\s*c(11|99)).*').match, ext.extra_compile_args)
+            env['CXXFLAGS'] = '{} {} -DVERSION_INFO=\\"{}\\"'.format(
+                env.get('CXXFLAGS', ''), ' '.join(cxx_compile_args),
+                self.distribution.get_version())
+
+            env['CFLAGS'] = '{} {}'.format(
+                env.get('CFLAGS', ''), ' '.join(ext.extra_compile_args),
+                self.distribution.get_version())
+            print("Launching build with env: {}, build_args: {}, cmake_args: {}".format(env, build_args, cmake_args))
+            if not os.path.exists(self.build_temp):
+                os.makedirs(self.build_temp)
+            subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, stdout=sys.stdout, stderr=sys.stdout,
+                                  cwd=self.build_temp, env=env)
+            subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                                  cwd=self.build_temp)
+            CMakeBuild.hasbuilt = True
+            build_dir = os.path.realpath(self.build_lib)
+
+            if CMakeBuild.hybrid:
+                if not self.compiler:
+                    self.run()
+
+            for name in self.info.entries():
+                try:
+                    pkg_build_dir=os.path.join(build_dir, cbuild_config.couchbase_core)
+                    self.copy_binary_to(cfg, pkg_build_dir, self.info.lcb_pkgs_srcs(), name)
+                    self.copy_binary_to(cfg, self.info.pkg_data_dir, self.info.lcb_pkgs_srcs(), name)
+                except:
+                    print("failure")
+                    raise
+
+
+def gen_cmake_build(extoptions, pkgdata):
+    from cmake_build import CMakeExtension, CMakeBuild
+
+    class LazyCommandClass(dict):
+        """
+        Lazy command class that defers operations requiring given cmdclass until
+        they've actually been downloaded and installed by setup_requires.
+        """
+        def __init__(self, cmdclass_real):
+            self.cmdclass_real=cmdclass_real
+
+        def __contains__(self, key):
+            return (
+                    key == 'build_ext'
+                    or super(LazyCommandClass, self).__contains__(key)
+            )
+
+        def __setitem__(self, key, value):
+            if key == 'build_ext':
+                raise AssertionError("build_ext overridden!")
+            super(LazyCommandClass, self).__setitem__(key, value)
+
+        def __getitem__(self, key):
+            if key != 'build_ext':
+                return super(LazyCommandClass, self).__getitem__(key)
+            return self.cmdclass_real
+
+    CMakeBuild.hybrid = build_type in ['CMAKE_HYBRID']
+    CMakeBuild.setup_build_info(extoptions, pkgdata)
+    e_mods = [CMakeExtension(str(couchbase_core+'._libcouchbase'), '', **extoptions)]
+    return e_mods, CMakeBuild.requires(), LazyCommandClass(CMakeBuild)
+
+
---
+++ packages/couchbase/couchbase-3.0.1.tar.gz/couchbase-3.0.1/setup.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+import logging
+import os
+import couchbase_version
+
+from cbuild_config import get_ext_options, couchbase_core, build_type
+from cmodule import gen_distutils_build
+from cmake_build import gen_cmake_build
+
+try:
+    if os.environ.get('PYCBC_NO_DISTRIBUTE'):
+        raise ImportError()
+
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+import os
+import itertools
+import pathlib
+curdir = pathlib.Path(__file__).parent
+
+
+lcb_min_version = (2, 9, 0)
+
+try:
+    from lcb_version import get_lcb_min_version
+    lcb_min_version=get_lcb_min_version()
+except:
+    lcb_min_version=(2,9,0)
+if not os.path.exists("build"):
+    os.mkdir("build")
+
+with open(str(curdir.joinpath("build/lcb_min_version.h")), "w+") as LCB_MIN_VERSION:
+    LCB_MIN_VERSION.write('\n'.join(
+        ["#define LCB_MIN_VERSION 0x{}".format(''.join(map(lambda x: "{0:02d}".format(x), lcb_min_version))),
+         '#define LCB_MIN_VERSION_TEXT "{}"'.format('.'.join(map(str, lcb_min_version))),
+         '#define PYCBC_PACKAGE_NAME "{}"'.format(couchbase_core)]))
+
+try:
+    couchbase_version.gen_version()
+except couchbase_version.CantInvokeGit:
+    pass
+
+pkgversion = couchbase_version.get_version()
+
+
+def handle_build_type_and_gen_deps():
+    cmake_build = build_type in ['CMAKE', 'CMAKE_HYBRID']
+    print("Build type: {}, cmake:{}".format(build_type, cmake_build))
+    general_requires = open(str(curdir.joinpath('requirements.txt'))).readlines()
+    extoptions, pkgdata=get_ext_options()
+
+    if cmake_build:
+        e_mods, extra_requires, cmdclass = gen_cmake_build(extoptions, pkgdata)
+        general_requires += extra_requires
+    else:
+        print("Legacy build")
+        e_mods, cmdclass = gen_distutils_build(extoptions, pkgdata)
+
+    setup_kw = {'ext_modules': e_mods}
+    logging.error(setup_kw)
+
+    setup_kw['setup_requires'] = general_requires
+    setup_kw['install_requires'] = general_requires
+    setup_kw['cmdclass'] = cmdclass
+    setup_kw['package_data'] = pkgdata
+    setup_kw['eager_resources'] = list(itertools.chain.from_iterable(pkgdata.values()))
+    return setup_kw
+
+
+setup_kw = handle_build_type_and_gen_deps()
+
+packages = {
+    'acouchbase',
+    'couchbase',
+    couchbase_core,
+    couchbase_core+'.views',
+    couchbase_core+'.iops',
+    couchbase_core+'.asynchronous',
+    'couchbase.asynchronous',
+    'couchbase.management',
+    'txcouchbase'
+}
+
+setup(
+    name = 'couchbase',
+    version = pkgversion,
+    url="https://github.com/couchbase/couchbase-python-client",
+    author="Couchbase, Inc.",
+    author_email="PythonPackage@couchbase.com",
+    license="Apache License 2.0",
+    description="Python Client for Couchbase",
+    long_description=open(str(curdir.joinpath("README.rst")), "r").read(),
+    long_description_content_type='text/x-rst',
+    keywords=["couchbase", "nosql", "pycouchbase", "libcouchbase"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules"],
+    python_requires=">=3.5",
+    packages = list(packages),
+    tests_require=['utilspie','nose', 'testresources>=0.2.7', 'basictracer==2.2.0'],
+    test_suite='couchbase_tests.test_sync',
+    **setup_kw
+)
