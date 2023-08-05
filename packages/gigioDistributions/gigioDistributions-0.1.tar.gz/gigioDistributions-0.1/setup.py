a862d8f55d073aa71a24b99723e851c47701659b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:41 +0000

    Add package gigioDistributions/0.1/gigioDistributions-0.1.tar.gz

---
+++ packages/gigioDistributions/gigioDistributions-0.1.tar.gz/gigioDistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gigioDistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gigioDistributions'],
+      author = 'Giovani F, Machado',
+      author_email = 'giovani.machado@gmail.com',
+      zip_safe=False)
