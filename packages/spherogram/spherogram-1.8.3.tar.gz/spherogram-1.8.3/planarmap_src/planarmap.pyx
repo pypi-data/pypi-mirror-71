4e940a8e67c1aa8767e430a0212a161470a30941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:31 +0000

    Add package spherogram/1.8.3/spherogram-1.8.3.tar.gz

---
+++ packages/spherogram/spherogram-1.8.3.tar.gz/spherogram-1.8.3/planarmap_src/planarmap.pyx
@@ -0,0 +1,131 @@
+#cython: language_level=3
+
+from libc.stdlib cimport malloc, free
+import random
+
+cdef extern from 'PMdef.h':
+    ctypedef struct pmSize:
+        char m, b  #  map and basic map type
+        long e, v, f  # edges, vertices, faces 
+        long r, g, d  # red and black vertices, max degree
+        long t        # tolerence on e
+        long *dgArr   # pt on vertex list
+
+    ctypedef struct pmMethod:
+        char core, pic
+        long seed
+        char verbose
+
+    ctypedef struct pmMemory:
+        char dTree
+        long sTree, rTree, gTree, sWrd, sEdge, sVtx, sLeaf
+
+    ctypedef struct pm_vertex:
+        pm_edge* root
+        pm_vertex* next
+        long mark
+        short type
+        long label
+        
+    ctypedef struct pm_edge:
+        pm_vertex* c_from "from"
+        pm_vertex* face
+        pm_edge* prev
+        pm_edge* next
+        pm_edge* oppo
+        long mark
+        short type
+        long label
+
+    ctypedef struct pmMap:
+        pm_edge *root
+        long e, v, f, i
+
+    void set_pmRandom_callback(long (*function)(long))
+
+cdef extern from 'PMplanmap.h':
+    extern int pmMemoryInit(pmSize *S, pmMethod *Meth, pmMemory *M)
+    extern int pmPlanMap(pmSize *S, pmMethod *Meth,
+                         pmMemory *M, pmMap *Map)
+    extern int pmFreeMap(pmMap *Map)
+
+cdef extern from 'stats.h':
+    extern long pmStatGauss(pmMap *Map)
+
+# We want Planarmap to use the same pseudo-random number generator as Python.
+
+cdef long randrange_callback(long n):
+    return random.randrange(n) + 1
+
+set_pmRandom_callback(randrange_callback)
+
+
+# The main function 
+        
+def random_map(num_vertices, int edge_connectivity=4,
+               int num_link_comps=0, int max_tries=100):
+    """
+    Use Gilles Schaeffer's "Planarmap program" to generate
+    a random 4-valent planar graph with the given number
+    of vertices.
+
+    The "edge_connectivity" parameter can be any of 2, 4, or 6.
+    Recall that a graph is k-edge-connected if removing any set
+    of *less than* k edges disconnects the graph.  In particular,
+    for 4-valent graphs, being 2-connected is just the same as
+    being connected.  In particuar, a 2-connected graph can
+    (and frequently do) have looped edges.
+
+    Under the hood, it uses Python's pseudo-random number
+    generator.
+    """
+    cdef pmSize size
+    cdef pmMethod method
+    cdef pmMemory memory
+    cdef pmMap the_map
+    cdef pm_edge *edge
+    cdef pm_vertex *vert
+
+    if edge_connectivity==2:
+        size.m, size.b = 4, 4
+    elif edge_connectivity==4:
+        size.m, size.b = 5, 5
+    elif edge_connectivity==6:
+        size.m, size.b = 6, 5
+    else:
+        raise ValueError("Invalid edge_connectivity parameter")
+
+    size.v = num_vertices
+    size.e, size.f, size.r, size.g, size.d = 0, 0, 0, 0, 0
+    size.t = -1
+    size.dgArr = NULL
+
+    method.core, method.pic = 0, 0
+    method.verbose = 0
+    pmMemoryInit(&size, &method, &memory)
+    pmPlanMap(&size, &method, &memory, &the_map)
+    if num_link_comps > 0:
+        tries = 0
+        while pmStatGauss(&the_map) != num_link_comps and tries < max_tries:
+            pmFreeMap(&the_map)
+            pmPlanMap(&size, &method, &memory, &the_map)
+            tries += 1
+
+        if tries==max_tries:
+            return 
+
+    ans = []
+    vert = the_map.root.c_from
+    while vert != NULL:
+        edges_at_vert = []
+        edge = vert.root
+        while edge != vert.root.prev:
+            edges_at_vert.append(edge.label)
+            edge = edge.next
+        edges_at_vert.append(edge.label)
+        ans.append( (vert.label, tuple(edges_at_vert)) )
+        vert = vert.next
+    pmFreeMap(&the_map)
+    return ans
+    
+    
---
+++ packages/spherogram/spherogram-1.8.3.tar.gz/spherogram-1.8.3/setup.py
@@ -0,0 +1,235 @@
+import os, shutil, sys, sysconfig, subprocess
+from glob import glob
+from distutils.util import get_platform
+from setuptools import setup, Command, Extension
+
+
+# Defensive linker flags for Linux:
+if sys.platform.startswith('linux'):
+    extra_link_args=['-Wl,-Bsymbolic-functions', '-Wl,-Bsymbolic']
+else:
+    extra_link_args=[]
+
+
+# The planarity extension
+
+try:
+    import sage.libs
+    ext_modules = []
+except ImportError:    
+    planarity_dir = 'planarity_src/c/'
+    planarity_ui_sources = glob(planarity_dir + 'planarity*.c')
+    planarity_sources = [file for file in glob('planarity_src/c/*.c')
+                         if not file in planarity_ui_sources]
+
+    if sys.platform.startswith('win'):
+        extra_compile_args = ['-D_CRT_SECURE_NO_WARNINGS']
+    else:
+        extra_compile_args = []
+    
+    Planarity = Extension(
+        name = 'spherogram.planarity',
+        sources = ['planarity_src/planarity.c'] + planarity_sources, 
+        include_dirs = [planarity_dir],
+        extra_compile_args = extra_compile_args,
+        extra_link_args = extra_link_args
+        )
+
+    ext_modules = [Planarity]
+
+# A real clean
+
+class SpherogramClean(Command):
+    user_options = []
+    def initialize_options(self):
+        pass 
+    def finalize_options(self):
+        pass
+    def run(self):
+        junkdirs = (glob('build/lib*') +
+                    glob('build/bdist*') +
+                    glob('snappy*.egg-info') +
+                    ['__pycache__', os.path.join('python', 'doc')]
+        )
+        for dir in junkdirs:
+            try:
+                shutil.rmtree(dir)
+            except OSError:
+                pass
+        junkfiles = glob('*/*.so*') + glob('*/*.pyc') + glob('*/*.c') 
+        for file in junkfiles:
+            try:
+                os.remove(file)
+            except OSError:
+                pass
+        
+class SpherogramTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass 
+    def finalize_options(self):
+        pass
+    def run(self):
+        build_lib_dir = os.path.join(
+            'build',
+            'lib.{platform}-{version_info[0]}.{version_info[1]}'.format(
+                platform=sysconfig.get_platform(),
+                version_info=sys.version_info)
+        )
+        sys.path.insert(0, os.path.abspath(build_lib_dir))
+        from spherogram.test import run_all_tests
+        sys.exit(run_all_tests())
+
+def check_call(args):
+    try:
+        subprocess.check_call(args)
+    except subprocess.CalledProcessError:
+        executable = args[0]
+        command = [a for a in args if not a.startswith('-')][-1]
+        raise RuntimeError(command + ' failed for ' + executable)
+
+# For manylinux1 wheels, need to set the platform name manually to
+# avoid having to "repair" the wheels later. 
+try:
+    from wheel.bdist_wheel import bdist_wheel
+    class SpherogramBuildWheel(bdist_wheel):
+        def initialize_options(self):
+            bdist_wheel.initialize_options(self)
+            if sys.platform.startswith('linux'):
+                plat = get_platform().replace('linux', 'manylinux1')
+                plat = plat.replace('-', '_')
+                self.plat_name = plat
+        
+except ImportError:
+    SpherogramBuildWheel = None
+        
+class SpherogramRelease(Command):
+    user_options = [('install', 'i', 'install the release into each Python')]
+    def initialize_options(self):
+        self.install = False
+    def finalize_options(self):
+        pass
+    def run(self):
+        if os.path.exists('build'):
+            shutil.rmtree('build')
+        if os.path.exists('dist'):
+            shutil.rmtree('dist')
+
+        pythons = os.environ.get('RELEASE_PYTHONS', sys.executable).split(',')
+        for python in pythons:
+            check_call([python, 'setup.py', 'build'])
+            check_call([python, 'setup.py', 'test'])
+            if sys.platform.startswith('linux'):
+                check_call([python, 'setup.py', 'bdist_egg'])
+            if self.install:
+                check_call([python, 'setup.py', 'pip_install'])
+            else:
+                check_call([python, 'setup.py', 'bdist_wheel'])
+
+        # Build sdist using the *first* specified Python
+        check_call([pythons[0], 'setup.py', 'sdist'])
+
+        # Double-check the Linux wheels
+        if sys.platform.startswith('linux'):
+            for name in os.listdir('dist'):
+                if name.endswith('.whl'):
+                    subprocess.check_call(['auditwheel', 'repair', os.path.join('dist', name)])
+
+class SpherogramPipInstall(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        python = sys.executable
+        check_call([python, 'setup.py', 'bdist_wheel'])
+        egginfo = 'spherogram.egg-info'
+        if os.path.exists(egginfo):
+            shutil.rmtree(egginfo)
+        wheels = glob('dist' + os.sep + '*.whl')
+        new_wheel = max(wheels, key=os.path.getmtime)            
+        check_call([python, '-m', 'pip', 'install', '--upgrade',
+                    '--upgrade-strategy', 'only-if-needed',
+                    new_wheel])
+
+# The planarmap extension
+
+pmap_dir = 'planarmap_src/'
+pmap_src_dir = pmap_dir + '/src/'
+pmap_src_files = [pmap_src_dir + file for file in
+                  ['PMdef.c', 'PMplanmap.c', 'PMenlight.c',
+                   'PMconjugation.c', 'PMextract.c', 'stats.c']]
+
+Planarmap = Extension(
+    name = 'spherogram.planarmap',
+    sources =  [pmap_dir + 'planarmap.c'] + pmap_src_files, 
+    include_dirs = [pmap_src_dir],
+    extra_link_args = extra_link_args
+    )
+
+ext_modules.append(Planarmap)
+
+
+# If have Cython, check that .c files are up to date:
+
+try:
+    from Cython.Build import cythonize
+    if 'clean' not in sys.argv:
+        targets = ['planarity_src/planarity.pyx', pmap_dir + 'planarmap.pyx']
+        targets = [file for file in targets if os.path.exists(file)]
+        cythonize(targets)
+except ImportError:
+    pass 
+
+# Main module
+
+exec(open('spherogram_src/version.py').read())
+
+# Get long description from README
+long_description = open('README.rst').read()
+long_description = long_description.split('==\n')[1]
+long_description = long_description.split('\nDeveloped')[0]
+
+install_requires = ['decorator', 'future', 'snappy_manifolds>=1.1']
+if sys.version_info < (3, 5):
+    install_requires.append('networkx<=2.2.9,>=1.3')
+else:
+    install_requires.append('networkx>=1.3')
+
+setup( name = 'spherogram',
+       version = version,
+       install_requires = install_requires,
+       dependency_links = [],
+       packages = ['spherogram', 'spherogram.links',
+                   'spherogram.links.test', 'spherogram.codecs',
+                   'spherogram.dev', 'spherogram.dev.dev_jennet'],
+       package_dir = {'spherogram' : 'spherogram_src', 'spherogram.dev':'dev'},
+       package_data = {'spherogram.links'  :  ['doc.pdf']}, 
+       ext_modules = ext_modules,
+       cmdclass =  {'clean': SpherogramClean,
+                    'test': SpherogramTest,
+                    'release': SpherogramRelease,
+                    'bdist_wheel':SpherogramBuildWheel,
+                    'pip_install':SpherogramPipInstall,
+       },
+       zip_safe = False,
+
+       description= 'Spherical diagrams for 3-manifold topology', 
+       long_description = long_description,
+       author = 'Marc Culler and Nathan M. Dunfield',
+       author_email = 'culler@marc-culler.info, nathan@dunfield.info',
+       license='GPLv2+',
+       url = 'https://github.com/3-manifolds/Spherogram',
+       classifiers = [
+           'Development Status :: 5 - Production/Stable',
+           'Intended Audience :: Science/Research',
+           'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+           'Operating System :: OS Independent',
+           'Programming Language :: C',
+           'Programming Language :: Python',
+           'Topic :: Scientific/Engineering :: Mathematics',
+        ],
+        keywords = 'knot, link, SnapPy',
+)
+
