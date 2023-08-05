72f96e030ec93c3791303e829ee619bae11ba6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:18 +0000

    Add package nurbspy/1.1.1/nurbspy-1.1.1-py3-none-any.whl

---
+++ packages/nurbspy/nurbspy-1.1.1-py3-none-any.whl/nurbspy/__init__.py
@@ -0,0 +1,13 @@
+# Import geometry modules
+from .nurbs_basis_functions    import *
+from .nurbs_curve              import *
+from .nurbs_curve_circular_arc import *
+from .nurbs_surface            import *
+from .nurbs_surface_bilinear   import *
+from .nurbs_surface_ruled      import *
+from .nurbs_surface_extruded   import *
+from .nurbs_surface_revolution import *
+from .nurbs_surface_coons      import *
+
+# Import minimum working example
+from .minimal_example          import *
