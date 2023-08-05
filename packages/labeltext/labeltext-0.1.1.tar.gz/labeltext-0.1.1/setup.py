a0d90afbc45bd117fd9d3acf24a2ad80d76dcdd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:18 +0000

    Add package labeltext/0.1.1/labeltext-0.1.1.tar.gz

---
+++ packages/labeltext/labeltext-0.1.1.tar.gz/labeltext-0.1.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['labeltext']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['attrdict>=2.0.1,<3.0.0',
+ 'loguru>=0.5.1,<0.6.0',
+ 'pandas>=1.0.4,<2.0.0',
+ 'pyfiglet>=0.8.post1,<0.9',
+ 'termcolor>=1.1.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['fix = scripts:fix']}
+
+setup_kwargs = {
+    'name': 'labeltext',
+    'version': '0.1.1',
+    'description': 'labeltext is a simple command-line utility to annotate large amounts of text quickly for text classification tasks.',
+    'long_description': None,
+    'author': 'Soumendra Dhanee',
+    'author_email': 'soumendra@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
