99e9b5892d6136643ac8d8a1a2f12da37bce771b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:44 +0000

    Add package bitbnspy/0.1.16/bitbnspy-0.1.16.tar.gz

---
+++ packages/bitbnspy/bitbnspy-0.1.16.tar.gz/bitbnspy-0.1.16/setup.py
@@ -0,0 +1,38 @@
+# from distutils.core import setup
+from setuptools import setup
+
+from pkg_resources import parse_requirements
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='bitbnspy',
+    packages=['bitbnspy'],
+    version='0.1.16',
+    license='MIT',
+    description='This project is designed to assist you make your own projects that interact with the Bitbns API. '
+                'This project seeks to have complete API coverage excluding WebSockets which would be released in '
+                'the future version.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Prashant Singh',
+    author_email='prashant@buyhatke.com',
+    url='https://github.com/bitbns-official/bitbnspy',
+    # Provide either the link to your github or to your website
+    download_url='',
+    keywords=['npm', 'bitbns', 'crypto', 'btc', 'eth', 'neo', 'orderbook', 'crypto trading', 'bitbns api'],
+    install_reqs=parse_requirements('requirements.txt'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6'
+    ],
+)
