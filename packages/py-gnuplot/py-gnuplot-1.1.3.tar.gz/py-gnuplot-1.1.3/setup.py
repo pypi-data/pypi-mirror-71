9db36c3246b2778e91c234b3ddc3d5f2262d776a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:58 +0000

    Add package py-gnuplot/1.1.3/py-gnuplot-1.1.3.tar.gz

---
+++ packages/py-gnuplot/py-gnuplot-1.1.3.tar.gz/py-gnuplot-1.1.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='py-gnuplot',
+      version='1.1.3',
+      keywords = ["gnuplot", "pandas"],
+      author='Yongping Guo',
+      author_email='guoyoooping@163.com',
+      description='py-gnuplot is a python plot tools based on gnuplot.',
+      long_description=open('README.rst').read(),
+      install_requires = ["pandas"],
+      url='http://www.gnuplot.info',
+      license='GPLv3',
+      scripts=[],
+      packages=[ 'pygnuplot' ],
+      #packages = find_packages(),
+      )
