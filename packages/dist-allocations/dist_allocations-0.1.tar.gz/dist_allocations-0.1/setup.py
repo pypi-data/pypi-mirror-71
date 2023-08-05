7525e91fa190c06f3688f479aa8200ea123977bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:33 +0000

    Add package dist-allocations/0.1/dist_allocations-0.1.tar.gz

---
+++ packages/dist-allocations/dist_allocations-0.1.tar.gz/dist_allocations-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_allocations',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_allocations'],
+      author = 'Charles Arnold',
+      author_email = 'kingme@email.com',
+      zip_safe=False)
