35044e3a9c12b6b2bb51b95612a71ed426bb8771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:51 +0000

    Add package aegea/3.6.25/aegea-3.6.25-py2.py3-none-any.whl

---
+++ packages/aegea/aegea-3.6.25-py2.py3-none-any.whl/aegea/util/aws/_boto3_loader.py
@@ -0,0 +1,21 @@
+class Loader:
+    cache = dict(resource={}, client={})
+    client_kwargs = dict(default={})
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def __getattr__(self, attr):
+        if attr == "__all__":
+            return list(self.cache[self.factory])
+        if attr == "__path__" or attr == "__loader__":
+            return None
+        if attr not in self.cache[self.factory]:
+            if self.factory == "client" and attr in self.cache["resource"]:
+                self.cache["client"][attr] = self.cache["resource"][attr].meta.client
+            else:
+                import boto3
+                factory = getattr(boto3, self.factory)
+                self.cache[self.factory][attr] = factory(attr,
+                                                         **self.client_kwargs.get(attr, self.client_kwargs["default"]))
+        return self.cache[self.factory][attr]
