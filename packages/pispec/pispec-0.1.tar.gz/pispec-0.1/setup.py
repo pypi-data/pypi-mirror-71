0449625c470af7a72644467b99585a2bc8affcba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package pispec/0.1/pispec-0.1.tar.gz

---
+++ packages/pispec/pispec-0.1.tar.gz/pispec-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="pispec",
+    version="0.1",
+    description="Prototype package for computing photon injection spectra",
+    zip_safe=False,
+    packages=["pispec"],
+    author = 'Boris Bolliet',
+    author_email = 'boris.bolliet@gmail.com',
+    url = 'https://github.com/borisbolliet/pi_spec',
+    download_url = 'https://github.com/borisbolliet/pi_spec/archive/master.zip',
+    # package_data={
+    #     "pispec": ["ct_database/case_1_040520/*txt"]
+    #},
+
+
+)
