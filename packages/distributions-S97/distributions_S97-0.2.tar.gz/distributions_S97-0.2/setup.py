e51a0054a9a9fa0ac696fdd45367dd278e475cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:46 +0000

    Add package distributions-S97/0.2/distributions_S97-0.2.tar.gz

---
+++ packages/distributions-S97/distributions_S97-0.2.tar.gz/distributions_S97-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_S97',
+      version='0.2',
+      description='Gaussian and Boolean distributions',
+      packages=['distributions_S97'],
+      author = 'Salma Tarek',
+      zip_safe=False)
