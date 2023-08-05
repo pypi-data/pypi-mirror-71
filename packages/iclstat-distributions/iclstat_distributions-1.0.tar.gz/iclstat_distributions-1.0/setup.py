df224655a4c5299e029d1a5bb6ebe19b75dffaa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:50 +0000

    Add package iclstat-distributions/1.0/iclstat_distributions-1.0.tar.gz

---
+++ packages/iclstat-distributions/iclstat_distributions-1.0.tar.gz/iclstat_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='iclstat_distributions',
+      version=1.0,
+      description='Binomial and Gaussian distributions',
+      packages=['iclstat_distributions'],
+      author='Iker Chasco Llorente',
+      author_email='i_chasco@hotmail.com',
+      zip_safe=False)
