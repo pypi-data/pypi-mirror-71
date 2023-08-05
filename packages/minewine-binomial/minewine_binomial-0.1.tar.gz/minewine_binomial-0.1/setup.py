24cc2d53c7e08dabe9904183808d9aa938fa95f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package minewine-binomial/0.1/minewine_binomial-0.1.tar.gz

---
+++ packages/minewine-binomial/minewine_binomial-0.1.tar.gz/minewine_binomial-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='minewine_binomial',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
