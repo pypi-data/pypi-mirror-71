26c7304763ef4b92e1cee919cfd4a552cb76ddf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:37 +0000

    Add package distributions-Eiad/0.1/distributions-Eiad-0.1.tar.gz

---
+++ packages/distributions-Eiad/distributions-Eiad-0.1.tar.gz/distributions-Eiad-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-Eiad',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-Eiad'],
+      zip_safe=False)
