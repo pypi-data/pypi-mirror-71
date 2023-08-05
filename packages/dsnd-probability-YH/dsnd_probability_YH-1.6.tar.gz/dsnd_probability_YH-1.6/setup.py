d31afb50690d55746c22c85b3253d8363984a737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:03 +0000

    Add package dsnd-probability-YH/1.6/dsnd_probability_YH-1.6.tar.gz

---
+++ packages/dsnd-probability-YH/dsnd_probability_YH-1.6.tar.gz/dsnd_probability_YH-1.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_YH',
+      version='1.6',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_YH'],
+      author = 'Yasin Hassan',
+      author_email = 'yasinhassan1243@yahoo.com',
+      zip_safe=False)
+
+
+
+
