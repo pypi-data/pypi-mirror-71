756e34d9e4b47083e842b77b12a1127720b51c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:36 +0000

    Add package pipestat/0.0.1/pipestat-0.0.1.tar.gz

---
+++ packages/pipestat/pipestat-0.0.1.tar.gz/pipestat-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pipestat',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/pepkit/pipestat',
+    author='Michal Stolarczyk',
+    author_email='michal@virginia.edu',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
