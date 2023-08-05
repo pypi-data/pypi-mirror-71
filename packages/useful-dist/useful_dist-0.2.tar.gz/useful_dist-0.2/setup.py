4888c30c27746bb2f62d67c5222e993c8e3741af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:50 +0000

    Add package useful-dist/0.2/useful_dist-0.2.tar.gz

---
+++ packages/useful-dist/useful_dist-0.2.tar.gz/useful_dist-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='useful_dist',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['useful_dist'],
+      zip_safe=False)
