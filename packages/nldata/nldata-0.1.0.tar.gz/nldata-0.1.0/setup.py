2125ad93e1593d83aababf1fcf9f4671c1d649d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:57 +0000

    Add package nldata/0.1.0/nldata-0.1.0.tar.gz

---
+++ packages/nldata/nldata-0.1.0.tar.gz/nldata-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nldata']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'nldata',
+    'version': '0.1.0',
+    'description': 'NLData is a library of Natural Language Datasetspoe  .',
+    'long_description': None,
+    'author': 'Davide Nunes',
+    'author_email': 'mail@davidenunes.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
