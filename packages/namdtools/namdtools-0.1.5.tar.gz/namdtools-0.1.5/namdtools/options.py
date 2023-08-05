e90e36ea82732bd1356169e608064f3e98f97a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:03 +0000

    Add package namdtools/0.1.5/namdtools-0.1.5.tar.gz

---
+++ packages/namdtools/namdtools-0.1.5.tar.gz/namdtools-0.1.5/namdtools/options.py
@@ -0,0 +1,16 @@
+"""
+options.py
+written in Python3
+author: C. Lockhart <chris@lockhartlab.org>
+
+>>> import namdtools
+>>> namdtools.options.namd_path = '/usr/bin/namd2'
+>>> namdtools.options.namd_args =
+
+"""
+
+charmrun_path = None
+namd_path = 'namd'
+
+charmrun_args = []
+namd_args = []
