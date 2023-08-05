8a5f21af44837b695463b4029c5761344b4b03aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package jt-dist20/0.1/jt_dist20-0.1.tar.gz

---
+++ packages/jt-dist20/jt_dist20-0.1.tar.gz/jt_dist20-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jt_dist20',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['jt_dist20'],
+      author='Jenish Thapa',
+      author_email='jenisht1@gmail.com',
+      zip_safe=False)
