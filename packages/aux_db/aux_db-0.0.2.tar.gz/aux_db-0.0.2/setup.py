699fe9f85e176e25ba38657a3ea878205202852f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package aux_db/0.0.2/aux_db-0.0.2.tar.gz

---
+++ packages/aux_db/aux_db-0.0.2.tar.gz/aux_db-0.0.2/setup.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    packages=find_packages(),
+    name='aux_db',
+    version='0.0.2',
+    author='LiaoDingyi',
+    author_email='731737792@qq.com',
+    description="auxiliary sqlserver tool",
+    platforms='window',
+    install_requires=['pymssql'],
+    classifiers=[],
+    url='https://github.com/yituocpuls'
+)
