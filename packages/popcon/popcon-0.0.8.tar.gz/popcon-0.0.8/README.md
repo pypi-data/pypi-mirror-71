a7146ec1a8be9773d65b712b1e0ea14a250a5a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:20 +0000

    Add package popcon/0.0.8/popcon-0.0.8.tar.gz

---
+++ packages/popcon/popcon-0.0.8.tar.gz/popcon-0.0.8/README.md
@@ -0,0 +1,4 @@
+popcon
+=========
+
+`popcon` is a Python package for analyzing populations of connectomes. It is built on top of Neurodata's `graspy`.
---
+++ packages/popcon/popcon-0.0.8.tar.gz/popcon-0.0.8/popcon/utils/__init__.py
@@ -0,0 +1,4 @@
+from .checks import check_input_graphs, check_multigraphs
+from .fmri import timeseries_to_connectome
+from .load import load_edgelists, load_metrics, parse_participants
+from .multigraph import Multigraph
