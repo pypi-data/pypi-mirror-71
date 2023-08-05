4af68c0c86b1f746865cf1bd858e3d9f733b68de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:00 +0000

    Add package cs-distributions/0.1/cs_distributions-0.1.tar.gz

---
+++ packages/cs-distributions/cs_distributions-0.1.tar.gz/cs_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='cs_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['cs_distributions'],
+      zip_safe=False)
