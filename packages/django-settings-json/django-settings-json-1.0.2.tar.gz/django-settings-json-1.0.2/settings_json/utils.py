f4979f47157d7499d462cd234ea75b082b6ad02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package django-settings-json/1.0.2/django-settings-json-1.0.2.tar.gz

---
+++ packages/django-settings-json/django-settings-json-1.0.2.tar.gz/django-settings-json-1.0.2/settings_json/utils.py
@@ -0,0 +1,32 @@
+from json import loads
+from os import environ, path
+from traceback import extract_stack
+
+from django.core.exceptions import ImproperlyConfigured
+
+SETTINGS_DIR = environ.get('SETTINGS_DIR')
+
+
+def get_settings_dir_from_traceback():
+    for file, _, _, _ in extract_stack():
+        if file.endswith('/settings.py'):
+            return path.dirname(file)
+
+
+if not SETTINGS_DIR:
+    SETTINGS_DIR = get_settings_dir_from_traceback()
+
+with open(path.join(SETTINGS_DIR, 'settings.json')) as file_handler:
+    settings = loads(file_handler.read())
+
+
+def get_setting(setting, settings_json=settings):
+    """
+    Get the setting variable or return explicit exception
+    """
+
+    try:
+        return settings_json[setting]
+    except KeyError:
+        error_message = "Set the {setting} environment variable on settings.json".format(setting=setting)
+        raise ImproperlyConfigured(error_message)
