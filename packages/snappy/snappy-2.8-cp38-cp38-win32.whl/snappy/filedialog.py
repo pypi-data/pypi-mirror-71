3433bfe83f5a4256ec9360c7bec3f1731a7cac31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:14 +0000

    Add package snappy/2.8/snappy-2.8-cp38-cp38-win32.whl

---
+++ packages/snappy/snappy-2.8-cp38-cp38-win32.whl/snappy/filedialog.py
@@ -0,0 +1,27 @@
+import sys, platform
+if sys.version_info.major < 3:
+    import tkFileDialog
+else:
+    import tkinter.filedialog as tkFileDialog
+
+def askopenfile(**options):
+    if sys.platform == 'darwin' and platform.mac_ver()[0] < '10.15.2':
+        options.pop('parent', None)
+    return tkFileDialog.askopenfile(**options)
+
+def asksaveasfile(mode='w',**options):
+    """
+    Ask for a filename to save as, and returned the opened file.
+    Modified from tkFileDialog to more intelligently handle
+    default file extensions.
+    """
+    if sys.platform == 'darwin':
+        if platform.mac_ver()[0] < '10.15.2':
+            options.pop('parent', None)
+        if 'defaultextension' in options and not 'initialfile' in options:
+            options['initialfile'] = 'untitled' + options['defaultextension']
+
+    return tkFileDialog.asksaveasfile(mode=mode, **options)
+
+if __name__ == '__main__':
+    asksaveasfile(defaultextension='.txt')
