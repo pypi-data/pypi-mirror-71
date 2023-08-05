dd3bf446565ce052a2463ab19e8b29de22f576e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:26 +0000

    Add package interutils/0.8.3/interutils-0.8.3.tar.gz

---
+++ packages/interutils/interutils-0.8.3.tar.gz/interutils-0.8.3/README.md
@@ -0,0 +1,5 @@
+A collection of handy utilities and snippets for creating interactive programs.
+
+TODO
++ add 'check_dependencies' function & check all over
+       the WD for possible implementations
---
+++ packages/interutils/interutils-0.8.3.tar.gz/interutils-0.8.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="interutils",
+    version="0.8.3",
+    author="Max G",
+    author_email="max3227@gmail.com",
+    description="A collection of handy utilities and snippets for creating interactive programs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'termcolor',
+    ],
+)
