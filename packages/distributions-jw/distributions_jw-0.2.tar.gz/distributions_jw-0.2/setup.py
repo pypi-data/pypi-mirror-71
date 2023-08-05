871086720389daa59f9c9ccb8f1007c08947ae50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:24 +0000

    Add package distributions-jw/0.2/distributions_jw-0.2.tar.gz

---
+++ packages/distributions-jw/distributions_jw-0.2.tar.gz/distributions_jw-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_jw',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='jw',
+      author_email='jw@gmail.com',
+      zip_safe=False)
