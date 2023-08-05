5b5a54f46eb3be66d923ee12ebd5aa1d348b5b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:50 +0000

    Add package pybricks-stubs/2.0.1/pybricks-stubs-2.0.1.tar.gz

---
+++ packages/pybricks-stubs/pybricks-stubs-2.0.1.tar.gz/pybricks-stubs-2.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pybricks-stubs",
+    version="2.0.1",
+    author="Lawrence (Drew) Whisenant",
+    author_email="dwhisenant@firstpartners.org",
+    description="A stubs package to write EV3 Python code.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drewwhis/pybricks-stubs",
+    packages=setuptools.find_packages(exclude=('test*',)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
