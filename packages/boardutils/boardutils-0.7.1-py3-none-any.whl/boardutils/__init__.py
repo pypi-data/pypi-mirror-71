65b7b6ef8a9cc878dbd6acee1dbb25817098a8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:46 +0000

    Add package boardutils/0.7.1/boardutils-0.7.1-py3-none-any.whl

---
+++ packages/boardutils/boardutils-0.7.1-py3-none-any.whl/boardutils/__init__.py
@@ -0,0 +1,7 @@
+"""
+boardutils
+~~~~~~
+
+a set of utilities to run custom python integrations/scripts on the Board Cloud platform
+This is an example package and should not be used to build production grade integrations
+"""
---
+++ packages/boardutils/boardutils-0.7.1-py3-none-any.whl/boardutils/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the boardutils package.
+"""
+
+__title__ = 'boardutils'
+__description__ = 'set of utilities to integrate with Board CPM and BI toolkit'
+__version__ = '0.7.1'
+__author__ = 'Board International SA'
+__author_email__ = 'info@board.com'
+__license__ = 'Usage of the Board API and of boardutils package is granted to Board customers with an active API license'
+__url__ = 'https://www.board.com'
