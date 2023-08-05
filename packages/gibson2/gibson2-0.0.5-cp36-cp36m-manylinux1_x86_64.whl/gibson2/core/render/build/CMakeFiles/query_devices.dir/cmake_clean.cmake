cacdcc3b19d75c17c47c3e49a9287782784ed019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:24 +0000

    Add package gibson2/0.0.5/gibson2-0.0.5-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/gibson2/gibson2-0.0.5-cp36-cp36m-manylinux1_x86_64.whl/gibson2/core/render/build/CMakeFiles/query_devices.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/query_devices.dir/glad/egl.cpp.o"
+  "CMakeFiles/query_devices.dir/glad/gl.cpp.o"
+  "CMakeFiles/query_devices.dir/cpp/query_devices.cpp.o"
+  "query_devices.pdb"
+  "query_devices"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/query_devices.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
