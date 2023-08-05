83ed9a3a0a703a5d5b20c3038176d03bb2136a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:46 +0000

    Add package yaqd-rpi-gpio/2020.6.1/yaqd_rpi_gpio-2020.6.1-py3-none-any.whl

---
+++ packages/yaqd-rpi-gpio/yaqd_rpi_gpio-2020.6.1-py3-none-any.whl/yaqd_rpi_gpio/_gpio_digital_output.py
@@ -0,0 +1,41 @@
+__all__ = ["GpioDigitalOutput"]
+
+import asyncio
+from typing import Dict, Any, List
+
+import gpiozero  # type: ignore
+
+from yaqd_core import DiscreteHardware
+
+from .__version__ import __branch__
+
+
+class GpioDigitalOutput(DiscreteHardware):
+    _kind = "gpio-digital-output"
+    _version = "0.2.0" + f"+{__branch__}" if __branch__ else ""
+    traits: List[str] = []
+    defaults: Dict[str, Any] = {}
+
+    def __init__(self, name, config, config_filepath):
+        super().__init__(name, config, config_filepath)
+        self.index = config["index"]
+        self.controller = gpiozero.DigitalOutputDevice(pin=self.index)
+        self._position_identifiers = {"low": 0, "high": 1}
+
+    def _connection_lost(self, peername):
+        super()._connection_lost(peername)
+        if len(self._clients) == 0:
+            self.controller.value = 0
+
+    def _set_position(self, value):
+        self.controller.value = value
+
+    async def update_state(self):
+        while True:
+            self._position = self.controller.value
+            if self._position:
+                self._position_identifier = "high"
+            else:
+                self._position_identifier = "low"
+            self._busy = False
+            await self._busy_sig.wait()
