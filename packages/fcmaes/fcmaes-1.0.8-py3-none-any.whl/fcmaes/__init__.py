e0ff50f01c66af1e26ce0e6efe49a656fbe9e184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:27 +0000

    Add package fcmaes/1.0.8/fcmaes-1.0.8-py3-none-any.whl

---
+++ packages/fcmaes/fcmaes-1.0.8-py3-none-any.whl/fcmaes/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) Dietmar Wolz.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory.
+
+__version__ = '1.0.8'
+
+__all__ = [
+    'cmaes',
+    'cmaescpp',
+    'dacpp',
+    'decpp',
+    'retry',
+    'advretry',
+    'pygmoretry',
+    'optimizer',
+    'astro',
+    'testfun',
+]
