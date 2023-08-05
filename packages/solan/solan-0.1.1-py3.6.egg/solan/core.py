4c9a2703754cc68fd25a206ee20ab2bc23770448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:00 +0000

    Add package solan/0.1.1/solan-0.1.1-py3.6.egg

---
+++ packages/solan/solan-0.1.1-py3.6.egg/solan/core.py
@@ -0,0 +1,41 @@
+# Copyright 2019 Francesco Apruzzese <cescoap@gmail.com>
+# License GPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import click
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from os import chdir
+from socket import gethostname,gethostbyname
+from pyngrok import ngrok
+
+
+@click.command()
+@click.option('-p','--port', type=int,
+              default=8000, show_default=True)
+@click.option('-r','--remote', is_flag=True)
+@click.argument('directory',
+                type=click.Path(exists=True))
+def run(directory, port, remote):
+    click.echo('Sharing {}'.format(directory))
+    # ----- Change directory
+    chdir(directory)
+    click.echo('Running the server')
+    # ----- Define server
+    solan_server = HTTPServer
+    solan_handler = SimpleHTTPRequestHandler
+    solan_server_address = ('', port)
+    httpd = solan_server(
+        solan_server_address, solan_handler)
+    # ----- Get local IP
+    hostname = gethostname()
+    ip = gethostbyname(hostname)
+    click.echo(
+        "Local service active on {ip}:{port}".format(
+            ip=ip,
+            port=port))
+    if remote:
+        public_url = ngrok.connect(port)
+        click.echo(
+            "Remote service active on {url}".format(
+                url=public_url))
+    # ----- Run server
+    httpd.serve_forever()
