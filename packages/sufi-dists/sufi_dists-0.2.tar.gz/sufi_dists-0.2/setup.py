95e68ec44c06a726a244a732c1b66bcf28801a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:23 +0000

    Add package sufi-dists/0.2/sufi_dists-0.2.tar.gz

---
+++ packages/sufi-dists/sufi_dists-0.2.tar.gz/sufi_dists-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sufi_dists',
+      version='0.2',
+      description='Gaussian and Binomial distributions by Sufi',
+      packages=['sufi_dists'],
+      author='Sufian Kaki Aslam',
+      author_email='sufiankaki@gmail.com',
+      zip_safe=False)
