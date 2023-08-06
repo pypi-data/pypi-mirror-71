c117b78b3bd0a045c3604450b440d49b0c6ceb7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:22 +0000

    Add package distributions-pk/0.2/distributions_pk-0.2.tar.gz

---
+++ packages/distributions-pk/distributions_pk-0.2.tar.gz/distributions_pk-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_pk',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_pk'],
+      author = 'Prabhat Kedia',
+      author_email = 'prabhatkedia07@gmail.com',
+      zip_safe=False)
