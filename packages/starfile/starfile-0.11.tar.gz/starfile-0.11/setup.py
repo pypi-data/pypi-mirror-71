a663d85d6cfce09f62946eb0e72d9c884e46d28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:14 +0000

    Add package starfile/0.11/starfile-0.11.tar.gz

---
+++ packages/starfile/starfile-0.11.tar.gz/starfile-0.11/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from starfile.version import VERSION
+setup(
+  name = 'starfile',
+  packages = ['starfile'],
+  version = f'{VERSION}',
+  license='BSD 3-Clause License',
+  description = 'Read and write STAR files as pandas DataFrames',
+  author = 'Alister Burt',
+  author_email = 'alisterburt@gmail.com',
+  url = 'https://github.com/alisterburt/starfile',
+  download_url = f'https://github.com/alisterburt/starfile/archive/v{VERSION}.tar.gz',
+  keywords = ['io', 'STAR', 'star', 'starfile'],
+  install_requires=[
+          'pandas',
+          'openpyxl',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
