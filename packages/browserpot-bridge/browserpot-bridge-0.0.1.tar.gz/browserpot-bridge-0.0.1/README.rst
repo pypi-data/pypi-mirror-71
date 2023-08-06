edb0a53d98bff83ee08bb5b0b3775ec427982b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:36 +0000

    Add package browserpot-bridge/0.0.1/browserpot-bridge-0.0.1.tar.gz

---
+++ packages/browserpot-bridge/browserpot-bridge-0.0.1.tar.gz/browserpot-bridge-0.0.1/README.rst
@@ -0,0 +1,3 @@
+browserpot-bridge
+=================
+
---
+++ packages/browserpot-bridge/browserpot-bridge-0.0.1.tar.gz/browserpot-bridge-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+  name = 'browserpot-bridge',
+  version = '0.0.1',
+  description = '',
+  author = 'browserpot',
+  author_email = 'browserpot@gmail.com',
+  license = 'GPL-3.0',
+  url = 'https://github.com/browserpot/browserpot-bridge',
+  download_url = 'https://github.com/browserpot/browserpot-bridge/downloads',
+  keywords = ['browserpot'],
+  classifiers = [],
+  packages = setuptools.find_packages(),
+)
