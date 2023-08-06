a121795f0743e037569e3a865821a17cfac4b9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:41 +0000

    Add package instapvapi/0.1.1/instapvapi-0.1.1.tar.gz

---
+++ packages/instapvapi/instapvapi-0.1.1.tar.gz/instapvapi-0.1.1/instapv/exceptions.py
@@ -0,0 +1,24 @@
+class SentryBlockException(Exception):
+    pass
+
+class AccountTypeException(Exception):
+    pass
+
+class UserNotFoundException(Exception):
+    pass
+
+class InvalidCredentialsException(Exception):
+    pass
+
+class RateLimitException(Exception):
+    pass
+
+class AccessDeniedException(Exception):
+    pass
+
+class ChallengeRequiredException(Exception):
+    pass
+
+class LogedOutException(Exception):
+    pass
+
