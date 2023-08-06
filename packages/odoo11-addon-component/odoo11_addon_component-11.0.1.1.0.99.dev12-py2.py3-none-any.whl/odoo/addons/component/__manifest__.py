baaf5a362f93f135d0050be18287404decaa185c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:45 +0000

    Add package odoo11-addon-component/11.0.1.1.0.99.dev12/odoo11_addon_component-11.0.1.1.0.99.dev12-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-component/odoo11_addon_component-11.0.1.1.0.99.dev12-py2.py3-none-any.whl/odoo/addons/component/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+{'name': 'Components',
+ 'version': '11.0.1.1.0',
+ 'author': 'Camptocamp,'
+           'Odoo Community Association (OCA)',
+ 'website': 'https://www.camptocamp.com',
+ 'license': 'LGPL-3',
+ 'category': 'Generic Modules',
+ 'depends': ['base',
+             ],
+ 'external_dependencies': {
+     'python': ['cachetools'],
+ },
+ 'data': [],
+ 'installable': True,
+ }
