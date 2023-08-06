4c3537e39d6ffac788566294a4185c090aaae938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:21 +0000

    Add package xiaocui/0.0.1/xiaocui-0.0.1-py3-none-any.whl

---
+++ packages/xiaocui/xiaocui-0.0.1-py3-none-any.whl/xiaocui/xiaocui.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+def output():
+    print('hello,这是')
+
+
+def output1(data):
+    print('hello,这是：{}'.format(data))
+
+
+def susu(susu):
+    print('susu:{}'.format(susu))
