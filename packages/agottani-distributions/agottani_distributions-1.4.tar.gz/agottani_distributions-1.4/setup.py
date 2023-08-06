324344982d247c8355cc484cacee606ec8532793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:19 +0000

    Add package agottani-distributions/1.4/agottani_distributions-1.4.tar.gz

---
+++ packages/agottani-distributions/agottani_distributions-1.4.tar.gz/agottani_distributions-1.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='agottani_distributions',
+      version='1.4',
+      description='Gaussian and binomial distributions',
+      packages=['agottani_distributions'],
+      author = 'Luis Fernando Rodrigues Agottani',
+      author_email = 'luisfernandoengmec@gmail.com',
+      zip_safe=False)
