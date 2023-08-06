a67df95c934dba3629029064105886fe14dfca5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:54 +0000

    Add package GandB-distributions-chauhan.shi/0.0.1/GandB-distributions-chauhan.shi-0.0.1.tar.gz

---
+++ packages/GandB-distributions-chauhan.shi/GandB-distributions-chauhan.shi-0.0.1.tar.gz/GandB-distributions-chauhan.shi-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="GandB-distributions-chauhan.shi",
+    version="0.0.1",
+    author="Shivam Chauhan",
+    author_email="shivam.chauhan21@gmail.com",
+    description="Guassian and Binomial distribution package",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chauhanshi/GaussianBinomial-distribution-python-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
