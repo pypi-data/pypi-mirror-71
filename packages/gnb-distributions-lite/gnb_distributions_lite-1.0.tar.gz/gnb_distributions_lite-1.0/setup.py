4420d21774e1799ec42bdbef0e20bbaa9c622ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:21 +0000

    Add package gnb-distributions-lite/1.0/gnb_distributions_lite-1.0.tar.gz

---
+++ packages/gnb-distributions-lite/gnb_distributions_lite-1.0.tar.gz/gnb_distributions_lite-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gnb_distributions_lite',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gnb_distributions_lite'],
+      zip_safe=False)
