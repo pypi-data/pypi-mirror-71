40af3cc7615f955a7084ce7174abcad0bd4ec8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:56 +0000

    Add package prob-dist-simplified/1.0/prob_dist_simplified-1.0.tar.gz

---
+++ packages/prob-dist-simplified/prob_dist_simplified-1.0.tar.gz/prob_dist_simplified-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='prob_dist_simplified',
+      version='1.0',
+      description='Probability Distributions',
+      packages=['prob_dist_simplified'],
+      author='Sakzsee',
+      zip_safe=False)
+
+
+
