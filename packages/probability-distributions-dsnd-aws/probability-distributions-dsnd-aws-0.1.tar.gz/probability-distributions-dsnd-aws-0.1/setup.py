ee712b75d95e084b88c832d7417f1a86972b7557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:33 +0000

    Add package probability-distributions-dsnd-aws/0.1/probability-distributions-dsnd-aws-0.1.tar.gz

---
+++ packages/probability-distributions-dsnd-aws/probability-distributions-dsnd-aws-0.1.tar.gz/probability-distributions-dsnd-aws-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability-distributions-dsnd-aws',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probability-distributions-dsnd-aws'],
+      zip_safe=False)
