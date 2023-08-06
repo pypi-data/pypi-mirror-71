a9f10e695e43abc6040803fb5bf0808ce03646e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:43 +0000

    Add package LGNpy/1.0.0/LGNpy-1.0.0-py3-none-any.whl

---
+++ packages/LGNpy/LGNpy-1.0.0-py3-none-any.whl/lgnpy/LinearGaussianExperimental.py
@@ -0,0 +1,149 @@
+import pandas as pd
+import numpy as np
+import copy
+from .Graph import Graph
+from .logging_config import Logger
+
+
+
+
+class LinearGaussianExperimental(Graph):
+    """
+
+    Implements modified Linear Gaussian. Results not guaranteed. Experimental.
+    """
+    def __init__(self):
+        super().__init__()
+        self.log = Logger()
+
+    def get_node_values(self, node,origin):
+        """
+    Get mean and variance of node using Linear Gaussian CPD. Calcluated using finding betas
+    """
+        neighbors = self.get_neighbors(node)
+        if origin in neighbors: neighbors.remove(origin)
+
+        index_to_keep = [self.nodes.index(node)]
+        index_to_reduce = [self.nodes.index(idx) for idx in neighbors]
+        values = self.__get_parent_calculated_means(neighbors)
+        val = {n: round(v, 3) for n, v in zip(neighbors, values)}
+
+        mu_j = self.mean[index_to_keep]
+        mu_i = self.mean[index_to_reduce]
+
+        sig_i_j = self.cov[np.ix_(index_to_reduce, index_to_keep)]
+        sig_j_i = self.cov[np.ix_(index_to_keep, index_to_reduce)]
+        sig_i_i_inv = np.linalg.inv(self.cov[np.ix_(index_to_reduce, index_to_reduce)])
+        sig_j_j = self.cov[np.ix_(index_to_keep, index_to_keep)]
+
+        covariance = sig_j_j - np.dot(np.dot(sig_j_i, sig_i_i_inv), sig_i_j)
+        beta_0 = mu_j - np.dot(np.dot(sig_j_i, sig_i_i_inv), mu_i)
+        beta = np.dot(sig_j_i, sig_i_i_inv)
+
+        new_mu = beta_0 + np.dot(beta, values)
+
+        node_values = {n: round(v, 3) for n, v in zip(neighbors, values)}
+        node_beta = list(np.around(np.array(list(beta_0) + list(beta[0])), 2))
+        self.parameters[node] = {"node_values": node_values, "node_betas": node_beta}
+        return new_mu[0], covariance[0][0]
+
+    def __get_parent_calculated_means(self, nodes):
+        """
+    Get evidences of parents given node name list
+    """
+        pa_e = []
+        for node in nodes:
+            ev = self.calculated_means[node]
+            if ev is None:
+                ev = self.mean[self.nodes.index(node)]
+            else:
+                pass
+            pa_e.append(ev)
+        return pa_e
+
+    def get_model_parameters(self):
+        """
+    Get parameters for each node
+    """
+        return self.parameters
+
+    def __build_results(self):
+        """
+        Make Pandas dataframe with the results.
+
+        """
+
+        self.inf_summary = pd.DataFrame(
+            index=self.nodes,
+            columns=[
+                "Evidence",
+                "Mean",
+                "Mean_inferred",
+                "Variance",
+                "Variance_inferred",
+            ],
+        )
+
+        self.inf_summary.loc[:, "Mean"] = self.mean
+        self.inf_summary["Evidence"] = self.inf_summary.index.to_series().map(
+            self.evidences
+        )
+        self.inf_summary.loc[:, "Variance"] = list(np.around(np.diag(self.cov),3))
+
+        self.inf_summary["Mean_inferred"] = self.inf_summary.index.to_series().map(
+            self.calculated_means
+        )
+        self.inf_summary["Variance_inferred"] = self.inf_summary.index.to_series().map(
+            self.calculated_vars
+        )
+        self.inf_summary["u_%change"] = (
+            (self.inf_summary["Mean_inferred"] - self.inf_summary["Mean"])
+            / self.inf_summary["Mean"]
+        ) * 100
+
+        self.inf_summary = (
+            self.inf_summary.round(4)
+            .replace(pd.np.nan, "", regex=True)
+            .replace(0, "", regex=True)
+        )
+
+        return self.inf_summary
+
+    def __print_message(self,log_instance,node):
+        log_instance.debug(f"Calculated:'{node}'= {round(self.calculated_means[node], 3)}")
+        log_instance.debug(f"Parent nodes used: {self.parameters[node]['node_values']}")
+        log_instance.debug(f"Beta calculated: {self.parameters[node]['node_betas']}")
+
+    def run_inference(self,inf_node,debug=True, return_results=True):
+        """
+        Run Inference on network with given evidences.
+        """
+
+        g_temp = copy.deepcopy(self.g)
+        self._log = self.log.setup_logger(debug=debug)
+        self._log.debug("Started")
+
+        if all(x == None for x in self.evidences.values()):
+            self._log.debug("No evidences were set. Proceeding without evidence")
+
+        self.parameters = dict.fromkeys(self.nodes)
+        self.calculated_means = copy.deepcopy(self.evidences)
+        self.calculated_vars = dict.fromkeys(self.nodes)
+        self.done_flags = dict.fromkeys(self.nodes)
+
+        def recurse(current_node,origin):
+            self._log.debug(f"Recursing for {current_node} with {origin}")
+            for p in self.get_neighbors(current_node):
+                if len(self.get_neighbors(p)) > 1:
+                    if p != origin:
+                        recurse(p,current_node)
+                        self.calculated_means[p], self.calculated_vars[p] = self.get_node_values(p,current_node)
+                        self.__print_message(self._log,p)
+
+        recurse(inf_node,inf_node)
+        self.calculated_means[inf_node], self.calculated_vars[inf_node] = self.get_node_values(inf_node,"")
+        self.__print_message(self._log,inf_node)
+        return self.__build_results()
+
+    def get_inference_results(self):
+        return self.inf_summary
---
+++ packages/LGNpy/LGNpy-1.0.0-py3-none-any.whl/tests/test_lgnpy.py
@@ -0,0 +1,278 @@
+import numpy as np
+import pandas as pd
+from nose import with_setup
+from lgnpy import LinearGaussian,LinearGaussianExperimental,GaussianBP
+
+
+def setup():
+    """
+    Setup.
+    :return: None.
+    """
+    np.random.seed(42)
+
+
+def teardown():
+    """
+    Teardown.
+    :return: None.
+    """
+    pass
+
+
+@with_setup(setup, teardown)
+def test_create_graph():
+    """
+    Tests creating a basic dag.
+    :return: None.
+    """
+    lg = LinearGaussian()
+    lg.set_edge("A", "B")
+    lg.set_edge("C", "B")
+    lg.set_edge("B", "D")
+
+    assert lg.get_edges() == [("A", "B"), ("B", "D"), ("C", "B")]
+    assert lg.get_nodes() == ["A", "B", "C", "D"]
+    assert lg.get_children("D") == []
+    assert lg.get_children("B") == ["D"]
+    assert lg.get_parents("B") == ["A", "C"]
+
+    lg = LinearGaussian()
+    lg.set_edges_from([("A", "C"), ("B", "C"), ("C", "E"), ("D", "E")])
+    assert lg.get_edges() == [("A", "C"), ("C", "E"), ("B", "C"), ("D", "E")]
+    assert lg.get_nodes() == ["A", "C", "B", "E", "D"]
+    assert lg.get_parents("C") == ["A", "B"]
+    assert lg.get_children("C") == ["E"]
+
+
+
+@with_setup(setup,teardown)
+def test_parameters():
+    data = pd.DataFrame(columns=['A','B','C','D','E'])
+    n=100
+    data['A'] = np.random.normal(0,2,n)
+    data['B'] = np.random.normal(5,3,n)
+    data['C'] = 2*data['A'] + 3*data['B'] + np.random.normal(0,2,n)
+    data['D'] = np.random.normal(2,2,n)
+    data['E'] = 3*data['C'] + np.random.normal(0,2,n)
+
+    lg = LinearGaussian()
+    lg.set_edges_from([('A', 'C'), ('B', 'C'), ('C', 'E'), ('D', 'E')])
+    lg.set_data(data)
+
+    np.testing.assert_almost_equal(list(lg.get_mean()),
+                                   [-0.20769303478818774,
+                                    5.066913761149772,
+                                    14.91514772007384,
+                                    2.213680241394609,
+                                    44.63343421920083],
+                                    decimal=3)
+
+    np.testing.assert_almost_equal(list(np.diag(np.array(lg.get_covariance()))),
+                                   [3.29907957452064,
+                                    8.18536047354675,
+                                    84.70325221116876,
+                                    3.126541257985881,
+                                    778.505141031734],
+                                    decimal = 3)
+
+    np.testing.assert_almost_equal(list(np.diag(np.array(lg.get_precision_matrix()))),
+                                   [1.422843910211299,
+                                    2.141985350397759,
+                                    2.29749879236002,
+                                    0.34555297789630945,
+                                    0.24461016431093713],
+                                   decimal=3)
+
+    lg.set_evidences({'A': 6})
+    assert lg.get_evidences() == {'A': 6, 'B': None, 'C': None, 'D': None, 'E': None}
+
+    lg.clear_evidences()
+    assert lg.get_evidences() == {'A': None, 'B': None, 'C': None, 'D': None, 'E': None}
+
+    lg.set_evidences({'A': 7, 'B': 2})
+    assert lg.get_evidences() == {'A': 7, 'B': 2, 'C': None, 'D': None, 'E': None}
+
+
+@with_setup(setup, teardown)
+def test_LinearGaussian_inference():
+    lg1 = LinearGaussian()
+    lg1.set_edges_from([('A', 'E'), ('B', 'E'), ('E', 'F'), ('E', 'G'), ('D', 'G'), ('C', 'H'), ('H', 'G')])
+
+    np.random.seed(42)
+    n = 100
+    data = pd.DataFrame(columns=['A', 'B', 'C', 'D', 'E'])
+
+    # Root nodes data
+    data['A'] = np.random.normal(5, 2, n)
+    data['B'] = np.random.normal(10, 2, n)
+    data['C'] = np.random.normal(50, 2, n)
+    data['D'] = np.random.normal(20, 2, n)
+
+    # Data for nodes with parents
+    data['E'] = 2 * data['A'] + 3 * data['B'] + np.random.normal(3, 5, n)
+    data['F'] = 3 * data['E'] + np.random.normal(-5, 2, n)
+    data['H'] = 0.4 * data['C'] + np.random.normal(0, 2, n)
+    data['G'] = 5 + 3 * data['E'] + 0.5 * data['H'] + 0.2 * data['D'] + np.random.normal(0, 5, n)
+
+    lg1.set_data(data)
+    lg1.set_evidences({'A': 5, 'B': 10})
+    lg1.run_inference(debug=True)
+
+    summary = lg1.inf_summary
+
+    assert summary.loc['A','Evidence'] == 5.0
+    assert summary.loc['G', 'Evidence'] == ''
+    np.testing.assert_almost_equal(summary.loc['B','Mean'],10.0446,decimal=3)
+    np.testing.assert_almost_equal(summary.loc['H','Mean'],20.1001,decimal=3)
+
+    np.testing.assert_almost_equal(summary.loc['A','Mean_inferred'],5.0, decimal=3)
+    np.testing.assert_almost_equal(summary.loc['G', 'Mean_inferred'], 146.9538, decimal=3)
+
+    np.testing.assert_almost_equal(summary.loc['D','Variance'],3.127, decimal=3)
+    np.testing.assert_almost_equal(summary.loc['H', 'Variance'], 4.888, decimal=3)
+
+    assert summary.loc['C', 'Variance_inferred'] == ''
+    np.testing.assert_almost_equal(summary.loc['G', 'Variance_inferred'], 23.2971, decimal=3)
+
+    np.testing.assert_almost_equal(summary.loc['A', 'u_%change'],4.3339, decimal=3)
+    np.testing.assert_almost_equal(summary.loc['E', 'u_%change'],0.4512, decimal=3)
+    assert summary.loc['C', 'u_%change'] == ''
+    pass
+
+@with_setup(setup, teardown)
+def test_LinearGaussianExperimental_inference():
+    lg2 = LinearGaussianExperimental()
+    lg2.set_edges_from([('A', 'E'), ('B', 'E'), ('E', 'F'), ('E', 'G'), ('D', 'G'), ('C', 'H'), ('H', 'G')])
+    np.random.seed(42)
+    n = 100
+    data = pd.DataFrame(columns=['A', 'B', 'C', 'D', 'E'])
+
+    # Root nodes data
+    data['A'] = np.random.normal(5, 2, n)
+    data['B'] = np.random.normal(10, 2, n)
+    data['C'] = np.random.normal(50, 2, n)
+    data['D'] = np.random.normal(20, 2, n)
+
+    # Data for nodes with parents
+    data['E'] = 2 * data['A'] + 3 * data['B'] + np.random.normal(3, 5, n)
+    data['F'] = 3 * data['E'] + np.random.normal(-5, 2, n)
+    data['H'] = 0.4 * data['C'] + np.random.normal(0, 2, n)
+    data['G'] = 5 + 3 * data['E'] + 0.5 * data['H'] + 0.2 * data['D'] + np.random.normal(0, 5, n)
+
+    lg2.set_data(data)
+    lg2.set_evidences({'A': 5, 'B': 10})
+    lg2.run_inference('D',debug=True)
+
+    summary = lg2.inf_summary
+    print(summary)
+
+    assert summary.loc['A', 'Evidence'] == 5.0
+    assert summary.loc['G', 'Evidence'] == ''
+    #np.testing.assert_almost_equal(summary.loc['B', 'Mean'], 10.0446, decimal=3)
+    np.testing.assert_almost_equal(summary.loc['H', 'Mean'], 20.1001, decimal=3)
+
+    np.testing.assert_almost_equal(summary.loc['A', 'Mean_inferred'], 5.0, decimal=3)
+    np.testing.assert_almost_equal(summary.loc['G', 'Mean_inferred'],146.4092, decimal=3)
+
+    np.testing.assert_almost_equal(summary.loc['D', 'Variance'], 3.127, decimal=3)
+    np.testing.assert_almost_equal(summary.loc['H', 'Variance'], 4.888, decimal=3)
+
+    assert summary.loc['C', 'Variance_inferred'] == ''
+    np.testing.assert_almost_equal(summary.loc['G', 'Variance_inferred'], 23.299, decimal=3)
+
+    np.testing.assert_almost_equal(summary.loc['A', 'u_%change'], 4.3339, decimal=3)
+    np.testing.assert_almost_equal(summary.loc['E', 'u_%change'], 0.025, decimal=3)
+    assert summary.loc['C', 'u_%change'] == ''
+    pass
+
+@with_setup(setup, teardown)
+def test_GaussianBP_inference():
+    lg3 = GaussianBP()
+    lg3.set_edges_from([('A', 'E'), ('B', 'E'), ('E', 'F'), ('E', 'G'), ('D', 'G'), ('C', 'H'), ('H', 'G')])
+
+    np.random.seed(42)
+    n = 100
+    data = pd.DataFrame(columns=['A', 'B', 'C', 'D', 'E'])
+
+    # Root nodes data
+    data['A'] = np.random.normal(5, 2, n)
+    data['B'] = np.random.normal(10, 2, n)
+    data['C'] = np.random.normal(50, 2, n)
+    data['D'] = np.random.normal(20, 2, n)
+
+    # Data for nodes with parents
+    data['E'] = 2 * data['A'] + 3 * data['B'] + np.random.normal(3, 5, n)
+    data['F'] = 3 * data['E'] + np.random.normal(-5, 2, n)
+    data['H'] = 0.4 * data['C'] + np.random.normal(0, 2, n)
+    data['G'] = 5 + 3 * data['E'] + 0.5 * data['H'] + 0.2 * data['D'] + np.random.normal(0, 5, n)
+
+
+    lg3.set_data(data)
+    lg3.set_evidences({'A': 5, 'B': 10})
+    lg3.run_inference(iterations=100)
+    #lg3.plot_errors()
+
+    summary = lg3.inf_summary
+
+    assert summary.loc['A', 'Evidence'] == 5.0
+
+    np.testing.assert_almost_equal(summary.loc['B', 'Mean'], 10.0446, decimal=3)
+    np.testing.assert_almost_equal(summary.loc['H', 'Mean'], 20.1001, decimal=3)
+    pass
+
+
+@with_setup(setup, teardown)
+def test_utility_functions():
+    lg4 = LinearGaussian()
+    lg4.set_edges_from([('A', 'E'), ('B', 'E'), ('E', 'F'), ('E', 'G'), ('D', 'G'), ('C', 'H'), ('H', 'G')])
+
+    np.random.seed(42)
+    n = 100
+    data = pd.DataFrame(columns=['A', 'B', 'C', 'D', 'E'])
+    # Root nodes data
+    data['A'] = np.random.normal(5, 2, n)
+    data['B'] = np.random.normal(10, 2, n)
+    data['C'] = np.random.normal(50, 2, n)
+    data['D'] = np.random.normal(20, 2, n)
+
+    # Data for nodes with parents
+    data['E'] = 2 * data['A'] + 3 * data['B'] + np.random.normal(3, 5, n)
+    data['F'] = 3 * data['E'] + np.random.normal(-5, 2, n)
+    data['H'] = 0.4 * data['C'] + np.random.normal(0, 2, n)
+    data['G'] = 5 + 3 * data['E'] + 0.5 * data['H'] + 0.2 * data['D'] + np.random.normal(0, 5, n)
+
+    np.random.seed(42)
+    n = 100
+    data = pd.DataFrame(columns=['A', 'B', 'C', 'D', 'E'])
+
+    # Root nodes data
+    data['A'] = np.random.normal(5, 2, n)
+    data['B'] = np.random.normal(10, 2, n)
+    data['C'] = np.random.normal(50, 2, n)
+    data['D'] = np.random.normal(20, 2, n)
+
+    # Data for nodes with parents
+    data['E'] = 2 * data['A'] + 3 * data['B'] + np.random.normal(3, 5, n)
+    data['F'] = 3 * data['E'] + np.random.normal(-5, 2, n)
+    data['H'] = 0.4 * data['C'] + np.random.normal(0, 2, n)
+    data['G'] = 5 + 3 * data['E'] + 0.5 * data['H'] + 0.2 * data['D'] + np.random.normal(0, 5, n)
+
+    lg4.set_data(data)
+
+    lg4.network_summary()
+    #assert lg4.draw_network('Hello',open=False) == None
+    #assert lg4.plot_distributions() == None
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
