1c7fbcfd7d46c14bf375bad4f8b78b8673a1d1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:29 +0000

    Add package starcli/1.0.0/starcli-1.0.0-py3-none-any.whl

---
+++ packages/starcli/starcli-1.0.0-py3-none-any.whl/starcli/__main__.py
@@ -0,0 +1,63 @@
+""" starcli.__main__ """
+
+# Standard library imports
+from datetime import datetime, timedelta
+
+# Third party imports
+import click
+import requests
+from rich.console import Console
+
+# Internal application imports
+from .utils import colored_output, table_output
+
+API_URL = "https://api.github.com/search/repositories"
+console = Console()
+
+
+@click.command()
+@click.option("--language", "-l", default="", help="language filter (eg: python)")
+@click.option(
+    "--date",
+    "-d",
+    default="",
+    help="date in the ISO8601 format which is YYYY-MM-DD (year-month-day)",
+)
+@click.option(
+    "--layout",
+    "-L",
+    default="list",
+    help="output format, it can be either table or list",
+)
+def search(language, date, layout):
+    """ Returns repositories based on the language.
+        repositories are sorted by stars
+    """
+
+    if not date:
+        start_date = datetime.fromisoformat(
+            datetime.utcnow().date().isoformat()
+        )  # today's timestamp in YYYY-MM-DD:00:00:00 format
+        end_date = datetime.fromisoformat(
+            (datetime.utcnow() + timedelta(days=1)).date().isoformat()
+        )  # next day's timestamp in YYYY-MM-DD:00:00:00 format
+    else:
+        start_date = datetime.fromisoformat(date)
+        end_date = datetime.fromisoformat(
+            (start_date + timedelta(days=1)).date().isoformat()
+        )
+
+    query = f"stars:>0+created:{start_date.isoformat()}..{end_date.isoformat()}"
+    query += f"+language:{language}" if language else ""
+    url = f"{API_URL}?q={query}&sort=stars&order=desc"
+    repositories = requests.get(url).json()
+    # console.print(url) # check if url is valid when debugging
+    if layout == "table":
+        table_output(repositories["items"])
+        return
+    colored_output(repositories["items"])
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    search()
