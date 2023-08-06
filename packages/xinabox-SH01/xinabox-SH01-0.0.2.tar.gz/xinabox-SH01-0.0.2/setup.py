5ebafd27a49a448ea71798f593a3de1c3f01d2fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:31 +0000

    Add package xinabox-SH01/0.0.2/xinabox-SH01-0.0.2.tar.gz

---
+++ packages/xinabox-SH01/xinabox-SH01-0.0.2.tar.gz/xinabox-SH01-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xinabox-SH01",
+    version="0.0.2",
+    author="Luqmaan Baboo",
+    author_email="luqmaanbaboo@gmail.com",
+    description="Capacitive touch sensor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xinabox/Python-SH01",
+    py_modules=["xSH01",],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
