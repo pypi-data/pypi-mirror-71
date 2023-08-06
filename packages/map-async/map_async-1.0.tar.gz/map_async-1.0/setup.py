b0d4772285d374e7df7e9bf9fcf3c8347c10325e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:40 +0000

    Add package map-async/1.0/map_async-1.0.tar.gz

---
+++ packages/map-async/map_async-1.0.tar.gz/map_async-1.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+setuptools.setup(
+    name="map_async",
+    version="1.0",
+    license="MIT",
+    description="Multiprocess mapping with a integrated progress bar.",
+    author='Kang Min Yoo',
+    author_email='kangmin.yoo@gmail.com',
+    url='https://github.com/kaniblu/map-async',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    platforms=[
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+    ],
+    keywords=[
+        "asynchronous",
+        "map",
+        "multiprocessing"
+    ],
+    install_requires=[
+        "tqdm>=4.46.1"
+    ],
+)
