4f963495c06f3f06390a004fb80cd21be30be926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:59 +0000

    Add package importhook/1.0.9/importhook-1.0.9.tar.gz

---
+++ packages/importhook/importhook-1.0.9.tar.gz/importhook-1.0.9/setup.py
@@ -0,0 +1,38 @@
+"""
+importhook
+==========
+"""
+from setuptools import find_packages, setup
+
+
+def get_long_description():
+    with open('README.md') as f:
+        rv = f.read()
+    return rv
+
+
+setup(
+    name='importhook',
+    version='1.0.9',
+    url='https://github.com/brettlangdon/importhook',
+    license='MIT',
+    author='Brett Langdon',
+    author_email='me@brett.is',
+    description='Execute code when certain modules are imported',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+    python_requires=">3.3",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python',
+    ]
+)
