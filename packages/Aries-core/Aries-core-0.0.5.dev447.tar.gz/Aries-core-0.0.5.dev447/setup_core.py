9ad99144b4623fa35f5da74429ffa00b1d3c93ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:24 +0000

    Add package Aries-core/0.0.5.dev447/Aries-core-0.0.5.dev447.tar.gz

---
+++ packages/Aries-core/Aries-core-0.0.5.dev447.tar.gz/Aries-core-0.0.5.dev447/setup_core.py
@@ -0,0 +1,48 @@
+"""Build Script for setuptools
+
+This build script must be executed outside of the Aries directory.
+
+See Also: https://packaging.python.org/tutorials/packaging-projects/
+"""
+import setuptools
+import os
+from Aries.files import Markdown
+
+with open("Aries/README.md", "r") as f:
+    long_description = f.read()
+    long_description = Markdown.from_text(
+        long_description
+    ).make_links_absolute("https://github.com/qiuosier/Aries/blob/master/")
+
+
+with open("Aries/requirements.txt", "r") as f:
+    requirements = f.read().split("\n")
+    requirements = [r.strip() for r in requirements if r.strip()]
+
+release_version = str(os.popen('cd Aries && git tag | tail -1').read()).strip()
+commit_version = str(os.popen('cd Aries && git rev-list --count master').read()).strip()
+
+setuptools.setup(
+    name="Aries-core",
+    version="%s%s" % (release_version, commit_version),
+    author="Qiu Qin",
+    author_email="qiuosier@gmail.com",
+    description="Python package providing shortcuts to small tasks like string manipulation, "
+                "running background tasks, configuring logging, accessing web API etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qiuosier/Aries",
+    packages=setuptools.find_packages(exclude=(
+        "Aries.storage", 
+        "Aries.statistics", 
+        "Aries.visual",
+        "Aries.tests"
+    )),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
