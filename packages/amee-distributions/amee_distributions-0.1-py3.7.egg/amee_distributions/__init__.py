410689a270d8bd904549be11cb502e89a36408c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:24 +0000

    Add package amee-distributions/0.1/amee_distributions-0.1-py3.7.egg

---
+++ packages/amee-distributions/amee_distributions-0.1-py3.7.egg/amee_distributions/__init__.py
@@ -0,0 +1,4 @@
+# note if you want to just import Gaussian, you need the following in this file
+# this is a neat way so the users of your package don't have to know which file has Gaussian class.
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
