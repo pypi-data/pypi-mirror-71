1cea8a625a1e6961e7e4dbedca0719cbb7bd56cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:43 +0000

    Add package wallme/1.3.1/wallme-1.3.1.tar.gz

---
+++ packages/wallme/wallme-1.3.1.tar.gz/wallme-1.3.1/wallme/websites/tetras.py
@@ -0,0 +1,12 @@
+NAME = 'tetras'
+DESCRIPTION = 'Amateur pictures of the Alpes and the Dauphiné'
+URL = 'http://tetras.org/Semaine.jpg'
+
+def pre_process():
+	return None
+    
+def process(date):
+	return URL
+
+def post_process(image):
+	return None
