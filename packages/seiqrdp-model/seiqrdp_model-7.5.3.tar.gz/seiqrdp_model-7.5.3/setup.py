3772717269d6d1c2ccef6f2ee28f4eb31cfd3fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:44 +0000

    Add package seiqrdp-model/7.5.3/seiqrdp_model-7.5.3.tar.gz

---
+++ packages/seiqrdp-model/seiqrdp_model-7.5.3.tar.gz/seiqrdp_model-7.5.3/setup.py
@@ -0,0 +1,33 @@
+
+import setuptools
+
+
+def get_requirements():
+    reqs = []
+    for line in open('requirements.txt', 'r').readlines():
+        reqs.append(line)
+    return reqs
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="seiqrdp_model",
+    version="7.5.3",
+    author="T.Rouabah, N.Belaloui, A.Tounsi",
+    author_email="m.t.rouabah@gmail.com",
+    description="Computational tools to fit data, simulate and calibrate\
+    parameters of the compartmental epidemiological SEIQRDP model.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Taha-Rouabah/COVID-19",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=get_requirements()
+)
