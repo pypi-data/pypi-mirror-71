dffa2e05f688900fa06af0e69a0077c6364b00e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:39 +0000

    Add package ss-distspacepkg/0.1/ss_distspacepkg-0.1.tar.gz

---
+++ packages/ss-distspacepkg/ss_distspacepkg-0.1.tar.gz/ss_distspacepkg-0.1/README.md
@@ -0,0 +1,3 @@
+# distspacepkg- A library for Gaussian and Binomial distribtions
+
+## Since this is was a learning library, I ain't writing much things here
---
+++ packages/ss-distspacepkg/ss_distspacepkg-0.1.tar.gz/ss_distspacepkg-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ss_distspacepkg',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ss_distspacepkg'],
+      zip_safe=False)
