4786d2d929265cbe8b5da58cd41bec8d58d08c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:46 +0000

    Add package rcsbsearch/0.2.1/rcsbsearch-0.2.1-py3-none-any.whl

---
+++ packages/rcsbsearch/rcsbsearch-0.2.1-py3-none-any.whl/rcsbsearch/__init__.py
@@ -0,0 +1,24 @@
+"""RCSB Search API"""
+from .search import (
+    Query,
+    Group,
+    Terminal,
+    TextQuery,
+    Session,
+    Attr,
+    Value,
+)  # noqa: F401
+from .schema import rcsb_attributes
+
+__version__ = "0.2.1"
+
+__all__ = [
+    "Query",
+    "Group",
+    "Terminal",
+    "TextQuery",
+    "Session",
+    "Attr",
+    "Value",
+    "rcsb_attributes",
+]
