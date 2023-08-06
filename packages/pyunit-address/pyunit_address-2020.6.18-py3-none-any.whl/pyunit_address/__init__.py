a82510cf5492ed7e5885bb8955a8563aaae20719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:49 +0000

    Add package pyunit-address/2020.6.18/pyunit_address-2020.6.18-py3-none-any.whl

---
+++ packages/pyunit-address/pyunit_address-2020.6.18-py3-none-any.whl/pyunit_address/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time  : 2020/2/18 15:23
+# @Author: Jtyoui@qq.com
+from .address import Address  # 地址初始化
+from .findAddress import find_address  # 查询地址
+from .supplementAddress import supplement_address  # 补全地址
+from .correctionAddress import correct_address  # 纠错地址
+
+__version__ = '2020.6.18'
+__author__ = 'Jtyoui'
+__description__ = '全国五级地址查询'
+__email__ = 'jtyoui@qq.com'
+__names__ = 'pyUnit_address'
+__url__ = 'https://github.com/PyUnit/pyunit-address'
