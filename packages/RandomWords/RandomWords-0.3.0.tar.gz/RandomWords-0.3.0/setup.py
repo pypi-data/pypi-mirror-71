05ce8ea0bb4cd3c37820670066d6019f97e7a8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:18 +0000

    Add package RandomWords/0.3.0/RandomWords-0.3.0.tar.gz

---
+++ packages/RandomWords/RandomWords-0.3.0.tar.gz/RandomWords-0.3.0/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--strict', '--verbose', '--tb=long']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(
+    name='RandomWords',
+    version='0.3.0',
+    author='Tomek Święcicki',
+    author_email='tomislater@gmail.com',
+    packages=['random_words'],
+    url='https://github.com/tomislater/RandomWords',
+    license='LICENSE.txt',
+    description='A useful module for a random text, e-mails and lorem ipsum.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    include_package_data=True,
+    install_requires=[],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    test_suite='random_words.test.test_random_words',
+    extras_require={
+        'testing': ['pytest'],
+    },
+)
