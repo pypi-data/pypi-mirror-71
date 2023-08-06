d35e5bec9ef16dee90a6f5612df544fb7f1324a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:03 +0000

    Add package mkdocs-codeinclude-plugin/0.0.1/mkdocs-codeinclude-plugin-0.0.1.tar.gz

---
+++ packages/mkdocs-codeinclude-plugin/mkdocs-codeinclude-plugin-0.0.1.tar.gz/mkdocs-codeinclude-plugin-0.0.1/codeinclude/resolver.py
@@ -0,0 +1,89 @@
+import re
+
+
+def select(
+    text,
+    lines=None,
+    from_token=None,
+    to_token=None,
+    block=None,
+    inside_block=None,
+    lang=None,
+):
+
+    selected_lines = []
+
+    if lines:
+        for line_range in lines.split(","):
+            range_match = re.match(r"(\d+)-(\d+)", line_range)
+            if range_match:
+                start = int(range_match.group(1))
+                end = int(range_match.group(2))
+                for i in range(start, end + 1):
+                    selected_lines.append(i)
+            elif line_range.strip() != "":
+                selected_lines.append(int(line_range))
+
+    if block:
+        i = 0
+        delim_count = 0
+        for line in text.splitlines():
+            first_line_of_block = False
+            i = i + 1
+            if block in line and delim_count <= 0:
+                delim_count = 0
+                first_line_of_block = True
+                delim_count += line.count("{")
+
+            if delim_count > 0:
+                if not first_line_of_block:
+                    delim_count += line.count("{")
+                selected_lines.append(i)
+
+            delim_count -= line.count("}")
+
+    if inside_block:
+        i = 0
+        delim_count = 0
+        for line in text.splitlines():
+            first_line_of_block = False
+            i = i + 1
+            if inside_block in line and delim_count <= 0:
+                delim_count = 0
+                first_line_of_block = True
+                delim_count += line.count("{")
+
+            delim_count -= line.count("}")
+
+            if delim_count > 0 and not first_line_of_block:
+                delim_count += line.count("{")
+                selected_lines.append(i)
+
+    if from_token and to_token:
+        i = 0
+        active = False
+        for line in text.splitlines():
+            i = i + 1
+            if not active and from_token in line:
+                active = True
+
+            if active:
+                selected_lines.append(i)
+
+            if active and to_token in line:
+                active = False
+
+    result = ""
+    source_lines = text.splitlines()
+
+    last_selected = 0
+    for i in sorted(selected_lines):
+        if i > (last_selected + 1) and last_selected != 0:
+            result += "\n⋯\n\n"
+        result += source_lines[i - 1] + "\n"
+        last_selected = i
+
+    if result == "":
+        return text
+
+    return result
---
+++ packages/mkdocs-codeinclude-plugin/mkdocs-codeinclude-plugin-0.0.1.tar.gz/mkdocs-codeinclude-plugin-0.0.1/setup.py
@@ -0,0 +1,41 @@
+from os.path import dirname
+from os.path import join
+from setuptools import setup
+
+
+def read_file(fname):
+    "Read a local file"
+    return open(join(dirname(__file__), fname)).read()
+
+
+setup(
+    name='mkdocs-codeinclude-plugin',
+    version='0.0.1',
+    description="A plugin to include code snippets into mkdocs pages",
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    keywords='mkdocs python markdown codeinclude',
+    url='https://github.com/rnorth/mkdocs-codeinclude-plugin',
+    author='Richard North',
+    author_email='rich.north@gmail.com',
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=[
+        'mkdocs>=0.17',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=['codeinclude'],
+    include_package_data=True,
+    entry_points={
+        'mkdocs.plugins': [
+            'codeinclude = codeinclude.plugin:CodeIncludePlugin'
+        ]
+    }
+)
