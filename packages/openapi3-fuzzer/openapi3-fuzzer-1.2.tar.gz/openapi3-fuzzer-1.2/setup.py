cf55ea13b761f7c28633dfed74b101235b945e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:38 +0000

    Add package openapi3-fuzzer/1.2/openapi3-fuzzer-1.2.tar.gz

---
+++ packages/openapi3-fuzzer/openapi3-fuzzer-1.2.tar.gz/openapi3-fuzzer-1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openapi3-fuzzer',
+    packages=find_packages(),
+    package_data={'': ['openapi3-fuzzer/openapi3_fuzzer/fuzz/*.txt']},
+    include_package_data=True,
+    version='1.2',
+    license='gpl-3.0',
+    description='Openapi3 fuzzer',
+    author='VolkerWessels Telecom',
+    author_email='opensource@vwt.digital',
+    url='https://github.com/vwt-digital/openapi3-fuzzer/tree/master',
+    keywords=['Openapi3', 'fuzzer', 'vwt'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'prance==0.17.0'
+    ],
+    python_requires='>=3.6',
+)
