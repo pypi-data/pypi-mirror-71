f8a8fb8c88887dfb785cb50c5527bbed092aee60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:51 +0000

    Add package distributionsYAKS/0.1/distributionsYAKS-0.1.tar.gz

---
+++ packages/distributionsYAKS/distributionsYAKS-0.1.tar.gz/distributionsYAKS-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsYAKS',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsYAKS'],
+      zip_safe=False)
