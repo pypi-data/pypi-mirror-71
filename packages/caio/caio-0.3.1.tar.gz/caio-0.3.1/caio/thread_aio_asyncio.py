4a8f21c92656280363a78d43f88b148a69eca15e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:53 +0000

    Add package caio/0.3.1/caio-0.3.1.tar.gz

---
+++ packages/caio/caio-0.3.1.tar.gz/caio-0.3.1/caio/thread_aio_asyncio.py
@@ -0,0 +1,8 @@
+from .thread_aio import Context, Operation
+from .asyncio_base import AsyncioContextBase
+
+
+class AsyncioContext(AsyncioContextBase):
+    MAX_REQUESTS_DEFAULT = 512
+    OPERATION_CLASS = Operation
+    CONTEXT_CLASS = Context
