851e99facc1b35ad605cbbf46e894e5f208c2b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:02 +0000

    Add package grpu/0.1.dev0/grpu-0.1.dev0.tar.gz

---
+++ packages/grpu/grpu-0.1.dev0.tar.gz/grpu-0.1.dev0/README.md
@@ -0,0 +1,2 @@
+# grp
+Python Utilities
---
+++ packages/grpu/grpu-0.1.dev0.tar.gz/grpu-0.1.dev0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='grpu',
+    version='0.1dev',
+    license='MIT',
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
