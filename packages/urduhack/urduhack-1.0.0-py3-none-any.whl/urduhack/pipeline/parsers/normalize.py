853349cfdeac1cdc96c7782d979ec3bd09ca8aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:39 +0000

    Add package urduhack/1.0.0/urduhack-1.0.0-py3-none-any.whl

---
+++ packages/urduhack/urduhack-1.0.0-py3-none-any.whl/urduhack/pipeline/parsers/normalize.py
@@ -0,0 +1,17 @@
+# coding: utf8
+"""Parser for performing normalization"""
+
+from urduhack.normalization import normalize
+from ..parser import Parser
+
+
+# class for running the tokenizer
+class NormalizeParser(Parser):
+    """pass"""
+
+    def _set_up(self, config):
+        """pass"""
+
+    def parse(self, document):
+        """pass"""
+        return normalize(document)
