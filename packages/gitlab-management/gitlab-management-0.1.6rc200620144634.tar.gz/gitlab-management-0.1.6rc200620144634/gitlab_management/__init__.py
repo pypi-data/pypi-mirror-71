4fb83d8482fc9d9aff17a025ff4708e797114ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:47 +0000

    Add package gitlab-management/0.1.6rc200620144634/gitlab-management-0.1.6rc200620144634.tar.gz

---
+++ packages/gitlab-management/gitlab-management-0.1.6rc200620144634.tar.gz/gitlab-management-0.1.6rc200620144634/gitlab_management/__init__.py
@@ -0,0 +1,17 @@
+
+#-*- coding: utf-8 -*-
+"""
+Todo
+----
+insert module comments in this space.
+
+"""
+
+__title__ = "gitlab-management"
+__version__ = "0.1.6rc200620144634"
+__doc__ = "https://gitlab.com/nofusscomputing/projects/python-gitlab-management"
+__author__ = "No Fuss Computing"
+__email__ = "helpdesk@nofusscomputing.com"
+__license__ = "GNU LGPLv3"
+__copyright__ = "(C) All Rights reserved"
+__source__ = "https://gitlab.com/nofusscomputing/projects/python-gitlab-management/-/tree/901606b960e1ed6793d1347af0e38544895599f5"
