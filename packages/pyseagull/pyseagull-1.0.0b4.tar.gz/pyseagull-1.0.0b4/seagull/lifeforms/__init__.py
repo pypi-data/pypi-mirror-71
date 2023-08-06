7e42a4d54eacf29bfb513e9daa363a8a7c5e6c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:49 +0000

    Add package pyseagull/1.0.0b4/pyseagull-1.0.0b4.tar.gz

---
+++ packages/pyseagull/pyseagull-1.0.0b4.tar.gz/pyseagull-1.0.0b4/seagull/lifeforms/__init__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+"""Lifeforms represent the evolving patterns whenever a rule is applied. In
+Seagull, lifeforms are first-class citizens: you can add them to the board,
+view them independently, compose, customize, and the like. This library
+provides a collection of pre-made lifeforms that you can play around.
+
+Lifeforms are arranged into categories based on their configurations (excluding the Base and Custom lifeforms):
+
+
+.. autosummary::
+    seagull.lifeforms.gliders
+    seagull.lifeforms.growers
+    seagull.lifeforms.oscillators
+    seagull.lifeforms.random
+    seagull.lifeforms.static
+
+
+"""
+
+from .static import Box, Seed, Moon, Kite
+from .oscillators import Blinker, Toad, Pulsar, FigureEight, Beacon
+from .gliders import Glider
+from .growers import Unbounded
+from .random import RandomBox
+from .custom import Custom
+
+__all__ = [
+    "Box",
+    "Seed",
+    "Moon",
+    "Kite",
+    "Blinker",
+    "Toad",
+    "Pulsar",
+    "FigureEight",
+    "Beacon",
+    "Glider",
+    "Unbounded",
+    "RandomBox",
+    "Custom",
+]
