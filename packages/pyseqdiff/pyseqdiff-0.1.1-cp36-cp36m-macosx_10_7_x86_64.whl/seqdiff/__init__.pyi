4e2191b442232d752b1349bf0744d353b1562366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:47 +0000

    Add package pyseqdiff/0.1.1/pyseqdiff-0.1.1-cp36-cp36m-macosx_10_7_x86_64.whl

---
+++ packages/pyseqdiff/pyseqdiff-0.1.1-cp36-cp36m-macosx_10_7_x86_64.whl/seqdiff/__init__.pyi
@@ -0,0 +1,8 @@
+from typing import List, Optional, Sequence, Any, Tuple
+
+def diff(
+    a: Sequence[Any], b: Sequence[Any]
+) -> Tuple[List[Optional[int]], List[Optional[int]]]: ...
+
+__version__: str
+
