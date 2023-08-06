5b75601159ac557a545f0fc920bdc4fd45d5bb38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:53 +0000

    Add package test-distmlcourse/0.2/test_distmlcourse-0.2.tar.gz

---
+++ packages/test-distmlcourse/test_distmlcourse-0.2.tar.gz/test_distmlcourse-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='test_distmlcourse',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['test_distmlcourse'],
+      zip_safe=False)
