23b95b83d26339b31fa14140b20ec960db189486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:03 +0000

    Add package fixedint/0.1.6/fixedint-0.1.6.tar.gz

---
+++ packages/fixedint/fixedint-0.1.6.tar.gz/fixedint-0.1.6/setup.py
@@ -0,0 +1,39 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+readme = open('README.rst')
+desc = []
+for line in readme:
+    if line.strip() == '.. __CUT__':
+        break
+    desc.append(line)
+long_description = ''.join(desc)
+readme.close()
+
+setup(
+    name = 'fixedint',
+    version = '0.1.6',
+    author = 'Robert Xiao',
+    author_email = 'robert.bo.xiao@gmail.com',
+    url = 'https://github.com/nneonneo/fixedint',
+    license = 'PSF',
+    classifiers = [
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Utilities",
+    ],
+    description = "simple fixed-width integers",
+    packages = ['fixedint'],
+    long_description=long_description,
+)
