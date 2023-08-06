714bf878fe1edd2ee03f13e6bb5801dde7bd7e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:16 +0000

    Add package pybbda/0.1.3/pybbda-0.1.3.tar.gz

---
+++ packages/pybbda/pybbda-0.1.3.tar.gz/pybbda-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def process_line(line):
+    return line.strip().split("=")[0].replace('>', '').replace("~", '')
+
+with open("requirements.txt", "r") as fh:
+    install_requires = [process_line(line) for line in fh.readlines() if len(line)>1]
+
+setup(
+    name="pybbda",
+    version="0.1.3",
+    author="Ben Dilday",
+    author_email="ben.dilday.phd@gmail.com",
+    description="Baseball data and analysis in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bdilday/pybbda",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={"pybaseballdatana": ["*.csv"]},
+    include_package_data=True,
+     install_requires=install_requires,
+)
