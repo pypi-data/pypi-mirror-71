f752e4fdb4cec46a3439d3258912a2e626d7a0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:43 +0000

    Add package Astrology-Aries/0.0.5.dev438/Astrology-Aries-0.0.5.dev438.tar.gz

---
+++ packages/Astrology-Aries/Astrology-Aries-0.0.5.dev438.tar.gz/Astrology-Aries-0.0.5.dev438/Aries/gcp/storage.py
@@ -0,0 +1,6 @@
+"""This module has been moved to Aries.storage package.
+The imports in this files are for backward compatibility purpose.
+Use Aries.storage module for new developments.
+This file will be removed in the future.
+"""
+from ..storage.gs import GSObject, GSFolder, GSFile
