cb6ff26106d9025540aca7bb73719f3fdc46ac8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:21 +0000

    Add package win32printing/0.1.3/win32printing-0.1.3.tar.gz

---
+++ packages/win32printing/win32printing-0.1.3.tar.gz/win32printing-0.1.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import os
+from io import open
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), "r", encoding="utf-8") as fobj:
+    long_description = fobj.read()
+
+requires = [
+    "pywin32",
+]
+
+
+setup(
+    name="win32printing",
+    version="0.1.3",
+    description="Printing utils.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="zencore",
+    author_email="dobetter@zencore.cn",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords=["win32printing"],
+    install_requires=requires,
+    packages=find_packages("."),
+    py_modules=["win32printing"],
+)
