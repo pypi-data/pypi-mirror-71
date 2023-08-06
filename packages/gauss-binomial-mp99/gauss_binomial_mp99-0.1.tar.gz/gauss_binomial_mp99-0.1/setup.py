812d08931dcaf12a488618b3769682dabfd5ecbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:27 +0000

    Add package gauss-binomial-mp99/0.1/gauss_binomial_mp99-0.1.tar.gz

---
+++ packages/gauss-binomial-mp99/gauss_binomial_mp99-0.1.tar.gz/gauss_binomial_mp99-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss_binomial_mp99',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_binomial_mp99'],
+	  author = 'Manan Paneri',
+	  author_email = 'mananpaneri28@gmail.com',
+      zip_safe=False)
