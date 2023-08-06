7791d3fd0fc7dddc6f277994d2a2b366f941d710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:14 +0000

    Add package paniagua/0.0.5/paniagua-0.0.5.tar.gz

---
+++ packages/paniagua/paniagua-0.0.5.tar.gz/paniagua-0.0.5/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+setup(
+   name='paniagua', ## Este nombre debe ser único y no estar indexado
+   version='0.0.5',
+   packages=find_packages(),
+   py_modules=['paniagua'] ## Este debe ser el nombre del archivo *.py
+)
