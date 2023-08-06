3667d2591e10750ca5915fe5d003844424a366bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:29 +0000

    Add package dummy-autoupdate/0.2.5/dummy_autoupdate-0.2.5.tar.gz

---
+++ packages/dummy-autoupdate/dummy_autoupdate-0.2.5.tar.gz/dummy_autoupdate-0.2.5/dummy_autoupdate/debug_window.py
@@ -0,0 +1,31 @@
+import logging
+
+from PyQt5.QtCore import QObject, pyqtSignal
+
+from PyQt5.QtWidgets import QPlainTextEdit
+
+
+class QTextEditLogger(logging.Handler, QObject):
+    appendPlainText = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super().__init__()
+        QObject.__init__(self)
+        self.widget = QPlainTextEdit(parent)
+        self.widget.setReadOnly(True)
+        self.widget.show()
+        self.appendPlainText.connect(self.widget.appendPlainText)
+        # self.widget.setMinimumWidth(500)
+        # self.widget.setMinimumHeight(500)
+        self.setFormatter(
+            logging.Formatter(
+                '%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s'))
+        logging.getLogger().addHandler(self)
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    def emit(self, record):
+        msg = self.format(record)
+        try:
+            self.appendPlainText.emit(msg)
+        except RuntimeError:
+            pass
