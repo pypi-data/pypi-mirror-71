827e8e63aff325e0b6707db2df5e3562e1210508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:37 +0000

    Add package quizmake/0.0.5/quizmake-0.0.5.tar.gz

---
+++ packages/quizmake/quizmake-0.0.5.tar.gz/quizmake-0.0.5/quizmake/__main__.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Initialization
+"""
+
+import sys
+
+from . import main
+
+if __name__ == "__main__":
+    sys.exit(main.main(sys.argv))
---
+++ packages/quizmake/quizmake-0.0.5.tar.gz/quizmake-0.0.5/quizmake/quizmake.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Just main stuff
+"""
+
+
+def add_two_numbers(x_val: int, y_val: int) -> int:
+    """
+    Just adds two numbers together
+    """
+    return x_val + y_val
+
+
+def return_string() -> str:
+    """
+    Returns the string 'doggo'
+    """
+    return "string"
