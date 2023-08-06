866ae920563373d49cc28dc1a8048086d550461b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:39 +0000

    Add package properly-model-python/0.4.94/properly_model_python-0.4.94-py3-none-any.whl

---
+++ packages/properly-model-python/properly_model_python-0.4.94-py3-none-any.whl/properly_model_python/favourite_models/models/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from properly_model_python.models.listing_favourites import ListingFavourites
+from properly_model_python.models.listing_favourites_audit_log import ListingFavouritesAuditLog
+from properly_model_python.models.user_favourites import UserFavourites
