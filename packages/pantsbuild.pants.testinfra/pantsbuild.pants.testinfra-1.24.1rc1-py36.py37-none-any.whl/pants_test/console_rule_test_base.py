e5f3bd69e0a130747e86afa420d507fb64ceca1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:00 +0000

    Add package pantsbuild.pants.testinfra/1.24.1rc1/pantsbuild.pants.testinfra-1.24.1rc1-py36.py37-none-any.whl

---
+++ packages/pantsbuild.pants.testinfra/pantsbuild.pants.testinfra-1.24.1rc1-py36.py37-none-any.whl/pants_test/console_rule_test_base.py
@@ -0,0 +1,8 @@
+# Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.testutil.console_rule_test_base import ConsoleRuleTestBase as ConsoleRuleTestBase  # noqa
+from pants_test.deprecated_testinfra import deprecated_testinfra_module
+
+
+deprecated_testinfra_module()
