b8abb5586bf87eae093cb3d58da10f5dec74227e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:17 +0000

    Add package siweilib/0.0.1/siweilib-0.0.1.tar.gz

---
+++ packages/siweilib/siweilib-0.0.1.tar.gz/siweilib-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="siweilib",
+    version="0.0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    python_requires='>=3.5',
+    install_requires=[
+        "termcolor",
+        "pytz",
+    ]
+)
