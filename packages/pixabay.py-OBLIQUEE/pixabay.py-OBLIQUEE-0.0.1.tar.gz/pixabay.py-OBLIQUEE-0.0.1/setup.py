460a8b462087a4a2b13486c7070ece59a9e4d4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:28 +0000

    Add package pixabay.py-OBLIQUEE/0.0.1/pixabay.py-OBLIQUEE-0.0.1.tar.gz

---
+++ packages/pixabay.py-OBLIQUEE/pixabay.py-OBLIQUEE-0.0.1.tar.gz/pixabay.py-OBLIQUEE-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    metadata_version="0.0.1",
+    name="pixabay.py-OBLIQUEE", # Replace with your own username
+    version="0.0.1",
+    author="Obliquee",
+    author_email="jfagg19@icloud.com",
+    description="A pixabay API wrapper.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/obl1que/pixabay.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    python_requires='>=3.6',
+)
