4960102208cb0002f765c8b1623a3c609fbdb51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:52 +0000

    Add package margot/0.5.34/margot-0.5.34-py2.py3-none-any.whl

---
+++ packages/margot/margot-0.5.34-py2.py3-none-any.whl/margot/signals/__init__.py
@@ -0,0 +1,21 @@
+from margot.signals.algos import BaseAlgo
+
+
+class Position(object):
+    """Represents a Position with a symbol and a weight.
+
+    Arguments:
+        symbol (str): The identifier of the symbol. e.g. 'SPY'.
+        weight (float): A value between -1.0 and +1.0 representing
+                        the weight of this symbol in the position list.
+    """
+
+    def __init__(self, symbol: str, weight: float):  # noqa: D107
+        self.symbol = symbol
+        self.weight = weight
+        if self.weight > 1.0 or self.weight < -1.0:
+            raise ValueError('weight must be a value between -1.0 and +1.0')
+
+    def __repr__(self):
+        """Represent."""
+        return str((self.symbol, self.weight))
