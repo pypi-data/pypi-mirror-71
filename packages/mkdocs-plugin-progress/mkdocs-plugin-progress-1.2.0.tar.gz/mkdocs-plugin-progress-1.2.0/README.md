ea843d3e2f8f6c473e4890f9cc2aca207be90bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:13 +0000

    Add package mkdocs-plugin-progress/1.2.0/mkdocs-plugin-progress-1.2.0.tar.gz

---
+++ packages/mkdocs-plugin-progress/mkdocs-plugin-progress-1.2.0.tar.gz/mkdocs-plugin-progress-1.2.0/README.md
@@ -0,0 +1,12 @@
+# mkdocs-plugin-progress
+
+A plugin for MkDocs that lets you know exactly what is happening during the build.
+This is helpful for use cases such as debugging, or trying to find out which pages the builder is getting stuck on for longer periods of time.
+
+## Usage
+
+Please see [the documentation](https://docs.rdil.rocks/libraries/mkdocs-plugin-progress/).
+
+## License
+
+Copyright (c) 2020-current year Reece Dunham - under the Apache license, a copy of which can be found at https://www.apache.org/licenses/LICENSE-2.0
---
+++ packages/mkdocs-plugin-progress/mkdocs-plugin-progress-1.2.0.tar.gz/mkdocs-plugin-progress-1.2.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+URLs = {
+    "Bug Tracker": "https://github.com/rdilweb/mkdocs-plugin-progress/issues",
+    "Source Code": "https://github.com/rdilweb/mkdocs-plugin-progress",
+    "License": "https://github.com/rdilweb/mkdocs-plugin-progress/blob/master/LICENSE",
+}
+
+setuptools.setup(
+    name="mkdocs-plugin-progress",
+    version="1.2.0",
+    packages=setuptools.find_packages(),
+    description="A plugin for MkDocs that lets you know exactly what is happening during the build.",
+    keywords=["mkdocs", "plugin", "progress", "build", "debugging"],
+    author="Reece Dunham",
+    author_email="me@rdil.rocks",
+    license="Apache 2",
+    entry_points={
+        "mkdocs.plugins": [
+            "progress = mkdocs_plugin_progress:Progress"
+        ]
+    },
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rdilweb/mkdocs-plugin-progress",
+    python_requires=">=3.3",
+    include_package_data=True,
+    project_urls=URLs,
+    install_requires=open("requirements.txt", "r").readlines()
+)
