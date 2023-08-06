24d48aecea3b3521cd6b38300cfe5cb053ac72a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:41 +0000

    Add package dist-udacity/0.1/dist_udacity-0.1.tar.gz

---
+++ packages/dist-udacity/dist_udacity-0.1.tar.gz/dist_udacity-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_udacity',
+      version = '0.1',
+      description = 'Gausian & Binomial distributions',
+      packages = ['dist_udacity'],
+      author = 'Shabbir Yamani',
+      author_email = 'myemail@something.com',
+      zip_safe=False)
