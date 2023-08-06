95d3e7ba9c553db4f18a4c726b89ef60d19e2467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:33 +0000

    Add package fractalcam/0.1/fractalcam-0.1.tar.gz

---
+++ packages/fractalcam/fractalcam-0.1.tar.gz/fractalcam-0.1/README.md
@@ -0,0 +1,20 @@
+# fractalcam
+
+Fractals with python and USB webcams!
+
+## Install
+
+Use git and conda to get the code and setup the environment:
+
+```bash
+git clone https://gitlab.com/ryanmcginger/fractalcam.git
+cd fractalcam
+conda env create -f environment.yaml
+```
+
+Then, just activate and run the script 'fracam.py':
+
+```
+conda activate fractalcam-env
+fracam.py --help
+```
---
+++ packages/fractalcam/fractalcam-0.1.tar.gz/fractalcam-0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+print('PACKAGES: ',find_packages())
+
+# Parse the version from the module.
+with open('fractalcam/__init__.py') as f:
+    for line in f:
+        if line.find("__version__") >= 0:
+            version = line.split("=")[1].strip()
+            version = version.strip('"')
+            version = version.strip("'")
+            continue
+
+setup(
+    name='fractalcam',
+    version=version,
+    description='CLI tool to make fractal with USB Webcams',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='numpy opencv fractal',
+    author='Ryan McCarthy',
+    author_email='code@mcginger.net',
+    license='MIT',
+    install_requires=[
+        'numpy',
+        # 'numba',
+        'scipy',
+        'click',
+        'opencv-python'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    package_dir={'': '.'},
+    packages=find_packages(),
+    entry_points='''
+        [console_scripts]
+        fracam=fractalcam.cli.main:run
+    ''',
+    zip_safe=False)
