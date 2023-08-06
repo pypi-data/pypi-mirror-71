86f1874b8e3073b8ab2857db2314685a9feac04e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:14 +0000

    Add package distributions12/0.1/distributions12-0.1.tar.gz

---
+++ packages/distributions12/distributions12-0.1.tar.gz/distributions12-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions12',
+      version='0.1',
+      description='Gaussian and Bionomial distributions',
+      packages=['distributions12'],
+      author = "abc",
+      zip_safe=False)
