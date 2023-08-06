9a637064ac9b0fc73bec586073e755ecc4de0580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:47 +0000

    Add package bg11-distributions/0.2/bg11_distributions-0.2.tar.gz

---
+++ packages/bg11-distributions/bg11_distributions-0.2.tar.gz/bg11_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bg11_distributions',
+      version='0.2',
+      description='Gaussian & Binomial distributions',
+      packages=['bg11_distributions'],
+      zip_safe=False)
