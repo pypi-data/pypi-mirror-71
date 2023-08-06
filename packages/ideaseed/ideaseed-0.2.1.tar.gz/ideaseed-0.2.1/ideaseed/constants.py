2a7cc84cf72f176db668c4814841da2bfece3547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:59 +0000

    Add package ideaseed/0.2.1/ideaseed-0.2.1.tar.gz

---
+++ packages/ideaseed/ideaseed-0.2.1.tar.gz/ideaseed-0.2.1/ideaseed/constants.py
@@ -0,0 +1,19 @@
+from typing import *
+
+COLOR_NAME_TO_HEX_MAP: Dict[str, int] = {
+    "Blue": 0xAECBFA,
+    "Brown": 0xE6C9A8,
+    "DarkBlue": 0xAECBFA,
+    "Gray": 0xE8EAED,
+    "Green": 0xCCFF90,
+    "Orange": 0xFBBC04,
+    "Pink": 0xFDCFE8,
+    "Purple": 0xD7AEFB,
+    "Red": 0xF28B82,
+    "Teal": 0xA7FFEB,
+    "White": 0xFFFFFF,
+    "Yellow": 0xFFF475,
+}
+
+# colors
+C_PRIMARY = 0x268CCE
