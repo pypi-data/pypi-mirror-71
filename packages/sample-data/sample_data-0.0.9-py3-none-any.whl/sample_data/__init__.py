7b89f1e8c6dc81ff6283df89b53d0b19446874a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:10 +0000

    Add package sample-data/0.0.9/sample_data-0.0.9-py3-none-any.whl

---
+++ packages/sample-data/sample_data-0.0.9-py3-none-any.whl/sample_data/__init__.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def _getcsv(name):
+    def fn():
+        return pd.read_csv(f"{dir_path}/data/{name}.csv")
+    return fn
+
+countries = _getcsv('countries')
+airbnb = _getcsv('airbnb')
+corona = _getcsv('corona')
+drinks = _getcsv('drinks')
