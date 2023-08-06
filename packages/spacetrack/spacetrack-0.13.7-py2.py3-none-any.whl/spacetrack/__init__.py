46dbd9042e2caff6fbad32dad75c0bfefba70d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:24 +0000

    Add package spacetrack/0.13.7/spacetrack-0.13.7-py2.py3-none-any.whl

---
+++ packages/spacetrack/spacetrack-0.13.7-py2.py3-none-any.whl/spacetrack/__init__.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+from __future__ import absolute_import, division, print_function
+
+from .base import (  # noqa
+    AuthenticationError, SpaceTrackClient, UnknownPredicateTypeWarning)
+from .operators import (  # noqa
+    greater_than, inclusive_range, less_than, like, not_equal, startswith)
+
+__all__ = (
+    'AuthenticationError',
+    'greater_than',
+    'inclusive_range',
+    'less_than',
+    'like',
+    'not_equal',
+    'SpaceTrackClient',
+    'startswith',
+)
+
+__version__ = '0.13.7'
+__description__ = 'Python client for space-track.org'
+
+__license__ = 'MIT'
+
+__author__ = 'Frazer McLean'
+__email__ = 'frazer@frazermclean.co.uk'
