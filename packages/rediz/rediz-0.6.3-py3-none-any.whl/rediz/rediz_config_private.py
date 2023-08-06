321c7108b76c5c25a4eac7efd2b9968066117ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:35 +0000

    Add package rediz/0.6.3/rediz-0.6.3-py3-none-any.whl

---
+++ packages/rediz/rediz-0.6.3-py3-none-any.whl/rediz/rediz_config_private.py
@@ -0,0 +1,21 @@
+
+
+
+GRAPH_COLLIDER_CONFIG = {"password":"j123klhjdflijsflkjdsflkfjsGQt7iaPcXEtNFB",
+                         "host":"redis-41323.di123.us-east-1-mz.ec2.cloud.rlrcp.com",
+                         "port":"43131",
+                         "delays":[55],
+                         "max_ttl":5*60,
+                         "transactions_ttl":5*60,
+                         "num_predictions":1000,
+                         "obscurity":"gc-obscure"}
+
+REDIZ_TEST_CONFIG = {"password":"5r7asflkjl13245lj123l4;jH",
+                     "host":"redis-23l12.c11.us-east-1-3.ec2.cloud.redislabs.com",
+                     "port":"20020",
+                     "delays": [1, 5],
+                     "obscurity": "0asdfeb-obscurity-testing-only-easdflkjlsadjf93c"
+                     }
+
+
+REDIZ_CONFIG = GRAPH_COLLIDER_CONFIG
