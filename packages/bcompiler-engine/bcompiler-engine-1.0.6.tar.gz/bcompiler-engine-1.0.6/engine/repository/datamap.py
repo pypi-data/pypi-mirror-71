c3ca945c4414f7c57d95ce0d3db6434dac10821e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:18 +0000

    Add package bcompiler-engine/1.0.6/bcompiler-engine-1.0.6.tar.gz

---
+++ packages/bcompiler-engine/bcompiler-engine-1.0.6.tar.gz/bcompiler-engine-1.0.6/engine/repository/datamap.py
@@ -0,0 +1,25 @@
+import json
+from pathlib import Path
+from typing import List, Union
+
+from engine.utils.extraction import datamap_reader
+from ..domain.datamap import DatamapLine
+from ..exceptions import DatamapNotCSVException
+from ..serializers.datamap import DatamapEncoder
+
+
+class InMemorySingleDatamapRepository:
+    def __init__(self, datamap_path: Union[Path, str]):
+        self.datamap_path = datamap_path
+
+    def list_as_json(self) -> str:
+        """Return list of DatamapLine objects parsed from filepath as json."""
+        try:
+            lst_of_objs = datamap_reader(self.datamap_path)
+        except DatamapNotCSVException:
+            raise
+        return json.dumps(lst_of_objs, cls=DatamapEncoder)
+
+    def list_as_objs(self) -> List[DatamapLine]:
+        """Return list of DatamapLine objects parsed from filepath."""
+        return datamap_reader(self.datamap_path)
