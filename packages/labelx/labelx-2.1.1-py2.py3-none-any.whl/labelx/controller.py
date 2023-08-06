86b948f4a528012f9a72260d64c7c484135868c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:41 +0000

    Add package labelx/2.1.1/labelx-2.1.1-py2.py3-none-any.whl

---
+++ packages/labelx/labelx-2.1.1-py2.py3-none-any.whl/labelx/controller.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""This module handles the API calls"""
+
+# Import builtin python libraries
+import json
+import logging
+import sys
+
+# Import external python libraries
+import click
+import requests
+
+# Import custom (local) python packages
+from .api_manager import call_api_endpoint, get_headers
+from .settings import generate_endpoints, accepted_status_codes, generate_payload
+from .utils import goodbye
+
+# Source code meta data
+__author__ = "Dalwar Hossain"
+__email__ = "dalwar23@protonmail.com"
+
+
+def labelx_controller(
+    endpoint=None, project_id=None, custom_config_path=None, custom_labels_path=None
+):
+    """
+    Label creation controller function
+
+    :param endpoint: (str) labels/badges endpoint
+    :param project_id: (int) Numeric project number
+    :param custom_config_path: (str) custom config path
+    :param custom_labels_path: (str) Custom label information yaml path
+    :returns: (stdout) Output on screen
+    """
+
+    skipped = []
+    api_method = "POST"
+    endpoint_url, host = generate_endpoints(endpoint_type=endpoint, project_id=project_id)
+    headers = get_headers()
+    all_data = generate_payload(endpoint_type=endpoint, scm_host=host)
+    for data_key, data_value in all_data.items():
+        data_value["name"] = data_key
+        try:
+            payload = json.dumps(data_value, indent=4)
+        except TypeError:
+            click.secho(
+                f"[x] TypeError detected!. Skipping [{data_key}].....", fg="red"
+            )
+            skipped.append(data_key)
+            payload = None
+        if payload:
+            click.secho(f"[$] Creating {endpoint[:-1]} - ", fg="cyan", nl=False)
+            click.secho(f"[{data_key}]", fg="white", nl=False)
+            click.secho(f" ..... ", fg="yellow", nl=False)
+            logging.debug(f"Payload: {payload}")
+            api_response = call_api_endpoint(
+                method=api_method,
+                api_url=endpoint_url,
+                data=payload,
+                api_headers=headers,
+            )
+            if api_response.status_code in accepted_status_codes:
+                click.secho(f"DONE", fg="green")
+            else:
+                click.secho(f"FAILED ({api_response.reason})", fg="red")
+                skipped.append(data_key)
+        else:
+            skipped.append(data_key)
+    goodbye(before=True, data=skipped)
---
+++ packages/labelx/labelx-2.1.1-py2.py3-none-any.whl/labelx/settings.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Setting parameters module for labelx"""
+
+# Import builtin libraries
+import logging
+import os
+import sys
+
+# Import external python libraries
+import click
+import yaml
+
+# Import custom (local) python packages
+from . import config_manager
+
+# Source code meta data
+__author__ = "Dalwar Hossain"
+__email__ = "dalwar23@protonmail.com"
+
+# Setting parameters
+api_version = "v4"
+accepted_status_codes = [200, 201, 202]
+max_col_length = 88
+allowed_extensions = ["yaml", "yml"]
+
+
+# Default link url modification for badges endpoint
+def mod_defaults(init_dict=None, host=None):
+    """
+    This method adds the host name before all required link_url
+
+    :param init_dict: (dict) a python dictionary that needs modification
+    :param host: (str) Source code management host url
+    :retuns:
+    """
+
+    ret_dict = {}
+    for data_key, data_value in init_dict.items():
+        if "%" in data_value["link_url"]:
+            data_value["link_url"] = f"{host}/{data_value['link_url']}"
+            ret_dict[data_key] = data_value
+        else:
+            ret_dict[data_key] = data_value
+    logging.debug(f"Modified defaults: {ret_dict}")
+    return ret_dict
+
+
+# Read configurations
+def read_yaml(yaml_file_path=None):
+    """
+    This method reads configurations from a .yaml file
+
+    :param yaml_file_path: Configuration files full path (default and custom)
+    :returns: (dict) a python dictionary with yaml file data
+    """
+
+    logging.debug(f"[$] Reading yaml file.....")
+    file_ext = str(yaml_file_path).rsplit(".", 1)[1]
+    if file_ext in allowed_extensions:
+        with open(yaml_file_path, "r") as stream:
+            try:
+                defaults = yaml.load(stream, Loader=yaml.FullLoader)
+            except Exception as err:
+                click.secho(f"[x] ERROR: {err}", fg="red")
+                sys.exit(1)
+        return defaults
+    else:
+        return False
+
+
+# Generate payloads
+def generate_payload(endpoint_type=None, scm_host=None, custom_data_file_path=None):
+    """
+    Parse file or default values to generate a list of labels with attributes
+
+    :param endpoint_type: (str) labels/badges endpoint string
+    :param scm_host: (str) Source code management host url
+    :param custom_data_file_path: (str) Full Path to Labels (YAML) file
+    :returns: (dict) key value pair of the label name and attributes
+    """
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    data_file = f"{endpoint_type}.yaml"
+    data_yaml_file = os.path.join(dir_path, data_file)
+    defaults = read_yaml(yaml_file_path=data_yaml_file)
+    logging.debug(f"[$] Defaults (before custom): {defaults}")
+    if endpoint_type == "badges":
+        default_data = mod_defaults(init_dict=defaults, host=scm_host)
+    else:
+        default_data = defaults
+    if custom_data_file_path:
+        custom_data = read_yaml(yaml_file_path=custom_data_file_path)
+        for c_name, c_value in custom_data.items():
+            default_data[c_name] = c_value
+        logging.debug(f"[$] Default data (after custom): {default_data}")
+    return default_data
+
+
+# Generate Endpoints
+def generate_endpoints(endpoint_type=None, project_id=None, custom_config_path=None):
+    """
+    Get API endpoints
+
+    :param endpoint_type: (str) labels/badges?
+    :param project_id: (int) GitLab Project ID
+    :param custom_config_path: (str) Configuration file path
+    :returns: (str) API endpoint
+    """
+
+    if isinstance(project_id, int):
+        if custom_config_path:
+            all_configs = config_manager.load_config(
+                config_file_paths=[custom_config_path]
+            )
+        else:
+            all_configs = config_manager.load_config()
+        protocol = all_configs["login"]["protocol"]
+        host = all_configs["login"]["host"]
+        host_url = f"{protocol}://{host}"
+        endpoint = f"{host_url}/api/{api_version}/projects/{project_id}/{endpoint_type}"
+        logging.debug(f"Endpoint: {endpoint}")
+        return [endpoint, host_url]
+    else:
+        click.secho(f"[x] No project ID found! Provide project ID!", fg="red")
+        sys.exit(1)
+
+
+# Define authentication
+def get_authentication():
+    """
+    Generates the authentication
+
+    :return: (str/tuple) Authentication
+    """
+
+    configs = config_manager.load_config()
+    if configs:
+        login = configs["login"]
+    else:
+        sys.exit(1)
+
+    token = login["token"]
+    logging.debug(f"Authentication Token: {token}")
+    return token
