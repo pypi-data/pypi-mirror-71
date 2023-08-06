ad92e33e46998bb5a993e844a46ac928147da7b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:36 +0000

    Add package agpypeline/0.0.4/agpypeline-0.0.4-py3-none-any.whl

---
+++ packages/agpypeline/agpypeline-0.0.4-py3-none-any.whl/agpypeline/configuration.py
@@ -0,0 +1,35 @@
+"""Configuration information for an AgPipeline Transformer
+"""
+
+
+class Configuration:
+    # The version number of the transformer
+    transformer_version = None
+
+    # The transformer description
+    transformer_description = None
+
+    # Short name of the transformer
+    transformer_name = None
+
+    # The sensor associated with the transformer
+    transformer_sensor = None
+
+    # The transformer type (eg: 'rgbmask', 'plotclipper')
+    transformer_type = None
+
+    # The name of the author of the extractor
+    author_name = None
+
+    # The email of the author of the extractor
+    author_email = None
+
+    # Contributors to this transformer
+    contributors = []
+
+    # Repository URI of where the source code lives
+    repository = None
+
+    # Override flag for disabling the metadata file requirement.
+    # Uncomment and set to False to override default behavior
+    # metadata_needed = True
