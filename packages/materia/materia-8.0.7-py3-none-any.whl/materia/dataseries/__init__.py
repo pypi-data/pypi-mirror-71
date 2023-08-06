003af88ecda3097c6d56798037bd117ba8f694d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:52 +0000

    Add package materia/8.0.7/materia-8.0.7-py3-none-any.whl

---
+++ packages/materia/materia-8.0.7-py3-none-any.whl/materia/dataseries/__init__.py
@@ -0,0 +1,25 @@
+from .astmg173 import ASTMG173
+from .blackbody_spd import BlackbodySPD
+from .cie_cmfs import (
+    CIE1931ColorMatchingFunctionX,
+    CIE1931ColorMatchingFunctionY,
+    CIE1931ColorMatchingFunctionZ,
+)
+from .cie_illuminants import (
+    CIEIlluminantA,
+    CIEIlluminantD65,
+    CIEIlluminantDSeries,
+    CIEIlluminantF4,
+)
+from .cie_test_color_samples import (
+    CIE1995TestColorSample01,
+    CIE1995TestColorSample02,
+    CIE1995TestColorSample03,
+    CIE1995TestColorSample04,
+    CIE1995TestColorSample05,
+    CIE1995TestColorSample06,
+    CIE1995TestColorSample07,
+    CIE1995TestColorSample08,
+)
+from .dataseries import *
+from .simmons_d_series import SimmonsDSeriesPCA0, SimmonsDSeriesPCA1, SimmonsDSeriesPCA2
