fd1cd35efdf24c5d1496d2a2d6767f73778c22db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:18 +0000

    Add package routingfilter/1.1.0/routingfilter-1.1.0.tar.gz

---
+++ packages/routingfilter/routingfilter-1.1.0.tar.gz/routingfilter-1.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='routingfilter',
+    version='1.1.0',
+    packages=['routingfilter'],
+    include_package_data=True,
+    install_requires=["IPy"],
+    url='https://github.com/certego/RoutingFilter',
+    license='GNU LGPLv3',
+    author='Certego S.r.l.',
+    author_email='support@certego.net',
+    description='Generic Business Logic Implementation for Routing objects as python dictionaries'
+)
