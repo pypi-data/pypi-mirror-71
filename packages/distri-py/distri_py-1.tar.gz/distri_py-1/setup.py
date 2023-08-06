9d74a178f08d4764aedeb837f6df567b5e5324cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:44 +0000

    Add package distri-py/1/distri_py-1.tar.gz

---
+++ packages/distri-py/distri_py-1.tar.gz/distri_py-1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distri_py',
+      version='1',
+      description='Gaussian and Binomial distributions',
+      packages=['distri_py'],
+      zip_safe=False)
