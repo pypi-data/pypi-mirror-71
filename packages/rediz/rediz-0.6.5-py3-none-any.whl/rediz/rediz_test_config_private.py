796fdda023e2ba10bd652207e958f4b2ce9a8af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:54 +0000

    Add package rediz/0.6.5/rediz-0.6.5-py3-none-any.whl

---
+++ packages/rediz/rediz-0.6.5-py3-none-any.whl/rediz/rediz_test_config_private.py
@@ -0,0 +1,5 @@
+
+#raise Exception("testing locally")
+REDIZ_TEST_CONFIG = {"password":"5rlaksdjfljlsdjfljasdfH",
+                     "host":"redis-33223.c11.us-east-1-3.ec2.cloud.redislabs.com",
+                     "port":"31122"}
