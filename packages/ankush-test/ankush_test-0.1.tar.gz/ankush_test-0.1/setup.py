0627632924c7a3a85358b52c1185db8c16fb5b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:47 +0000

    Add package ankush-test/0.1/ankush_test-0.1.tar.gz

---
+++ packages/ankush-test/ankush_test-0.1.tar.gz/ankush_test-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ankush_test',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Ankush Anshuman',
+      author_email = 'ankush3107@gmail.com',
+      zip_safe=False)
