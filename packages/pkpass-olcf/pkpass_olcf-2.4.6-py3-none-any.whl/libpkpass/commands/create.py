9ffb74f62a2f1504a59c94c6107da8ff796f0071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:20 +0000

    Add package pkpass-olcf/2.4.6/pkpass_olcf-2.4.6-py3-none-any.whl

---
+++ packages/pkpass-olcf/pkpass_olcf-2.4.6-py3-none-any.whl/libpkpass/commands/create.py
@@ -0,0 +1,37 @@
+"""This module allows for the creation of passwords"""
+import getpass
+from libpkpass.commands.command import Command
+from libpkpass.errors import CliArgumentError, PasswordMismatchError, BlankPasswordError
+
+    ####################################################################
+class Create(Command):
+    """This class implements the CLI functionality of creation of passwords"""
+    ####################################################################
+    name = 'create'
+    description = 'Create a new password entry and encrypt it for yourself'
+    selected_args = Command.selected_args + ['pwname', 'pwstore', 'overwrite', 'stdin', 'keypath',
+                                             'nopassphrase', 'nosign', 'card_slot', 'escrow_users',
+                                             'min_escrow', 'noescrow']
+
+        ####################################################################
+    def _run_command_execution(self):
+        """ Run function for class.                                      """
+        ####################################################################
+        password1 = getpass.getpass("Enter password to create: ")
+        if password1.strip() == "":
+            raise BlankPasswordError
+        password2 = getpass.getpass("Enter password to create again: ")
+        if password1 != password2:
+            raise PasswordMismatchError
+
+        description = input("Description: ")
+        authorizer = input("Authorizer: ")
+        self.create_or_update_pass(password1, description, authorizer)
+
+        ####################################################################
+    def _validate_args(self):
+        ####################################################################
+        for argument in ['pwname', 'keypath']:
+            if argument not in self.args or self.args[argument] is None:
+                raise CliArgumentError(
+                    "'%s' is a required argument" % argument)
