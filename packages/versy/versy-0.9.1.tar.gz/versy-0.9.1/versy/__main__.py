636fa79f524897e37d30317e1ab741111f259c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:15 +0000

    Add package versy/0.9.1/versy-0.9.1.tar.gz

---
+++ packages/versy/versy-0.9.1.tar.gz/versy-0.9.1/versy/__main__.py
@@ -0,0 +1,40 @@
+from versy import versy
+import argparse
+import sys
+import traceback
+
+ACTIONS = versy.ACTIONS + ('<new version number>',)
+
+
+def main(args=None):
+    p = argparse.ArgumentParser(description=_DESCRIPTION)
+
+    p.add_argument('action', nargs='?', default='show', help=_ACTIONS_HELP)
+    p.add_argument('--changelog', '-c', default=None, help=_CHANGELOG_HELP)
+    p.add_argument('--dry-run', '-d', action='store_true', help=_DRY_RUN_HELP)
+    p.add_argument('--message', '-m', help=_MESSAGE_HELP)
+    p.add_argument('--path', '-p', default='.', help=_PATH_HELP)
+    p.add_argument('--verbose', '-v', action='store_true', help=_VERBOSE_HELP)
+
+    args = p.parse_args(args)
+    try:
+        versy.versy(**vars(args))
+    except Exception as e:
+        print('ERROR:', e, file=sys.stderr)
+        if args.verbose:
+            traceback.print_exc()
+        sys.exit(-1)
+
+
+_DESCRIPTION = """Update the version number and CHANGELOG of the program"""
+_ACTIONS_HELP = 'Actions are: ' + ', '.join(ACTIONS)
+_CHANGELOG_HELP = """Specify the name of a new CHANGELOG"""
+_MESSAGE_HELP = """\
+Override the default CHANGELOG message from the git commit list"""
+_PATH_HELP = """Root directory to search for a version number"""
+_VERBOSE_HELP = """Print more stuff"""
+_DRY_RUN_HELP = """Don't actually make the changes, just print the diffs"""
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/versy/versy-0.9.1.tar.gz/versy-0.9.1/versy/versy.py
@@ -0,0 +1,74 @@
+from . import git
+from . import semver
+from .changelog import ChangeLog
+from .version_file import VersionFile
+import contextlib
+import functools
+import io
+import myers
+import safer
+
+PREFIX = '__version__ = '
+VERSION = 'VERSION'
+ACTIONS = 'patch', 'minor', 'major', 'new', 'show'
+__version__ = '0.9.1'
+
+
+def versy(action, changelog, dry_run, message, path, verbose):
+    printer = _dry_printer if dry_run else _printer
+
+    if not dry_run:
+        git.check_clean_workspace()
+
+    vfile = VersionFile(path, printer)
+    version = semver.semver(vfile.version)
+
+    if action == 'show':
+        print('Version', version, 'found in', vfile.file)
+        return
+
+    cd = verbose and not dry_run
+    cl = ChangeLog(path, str(version), changelog, printer, message, cd)
+
+    if action == 'new':
+        cl.new()
+        msg = 'Version v%s' % version
+        git.commit([cl.file], msg, dry_run, verbose)
+    else:
+        new_version = str(semver.bump(version, action))
+        cl.update(new_version)
+        vfile.write(new_version)
+        if dry_run or verbose:
+            print()
+        print('Version', version, '-->', new_version, 'in', vfile.file)
+
+        if dry_run or verbose:
+            print()
+        msg = 'Version v%s' % new_version
+        git.commit([vfile.file, cl.file], msg, dry_run, verbose)
+
+
+@contextlib.contextmanager
+def _dry_printer(file):
+    fp = io.StringIO()
+
+    yield functools.partial(print, file=fp)
+
+    if file.exists():
+        before = file.read_text().splitlines()
+    else:
+        before = []
+    after = fp.getvalue().splitlines()
+
+    print()
+    print(60 * '-')
+    print()
+    print('%s:' % file)
+    print()
+    print(*myers.diff(before, after, context=2, format=True), sep='\n')
+
+
+@contextlib.contextmanager
+def _printer(file):
+    with safer.printer(file) as _print:
+        yield _print
