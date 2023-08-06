f767834ec5255923cf19da349ef4af3b1fd563ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:45 +0000

    Add package service-stats/1.0.2/service_stats-1.0.2-py3-none-any.whl

---
+++ packages/service-stats/service_stats-1.0.2-py3-none-any.whl/service/__init__.py
@@ -0,0 +1,9 @@
+"""Import Service modules"""
+from .boot_time import Boot
+from .cpu import Cpu
+from .disk import Disk
+from .globals import Global
+from .memory import Memory
+from .network import Network
+from .slack import Slack
+from .system import System
