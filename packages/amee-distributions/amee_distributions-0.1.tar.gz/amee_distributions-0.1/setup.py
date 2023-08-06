736e64fe1c75a5f4e98ef8bb99e5bc9fdc2f0fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:24 +0000

    Add package amee-distributions/0.1/amee_distributions-0.1.tar.gz

---
+++ packages/amee-distributions/amee_distributions-0.1.tar.gz/amee_distributions-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='amee_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['amee_distributions'],
+      zip_safe=False,
+      author='Ameeta Sharma',
+      author_email='amsa@westchestergov.com'
+     )
