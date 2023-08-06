cba3727645059342825d58614b83ba9180366b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:59 +0000

    Add package embers/0.0.3/embers-0.0.3.tar.gz

---
+++ packages/embers/embers-0.0.3.tar.gz/embers-0.0.3/embers/rf_tools/__init__.py
@@ -0,0 +1,7 @@
+"""
+RF Tools
+========
+A subpackage of embers used to pre process and condition and preview raw rf data.
+This validates analysis performed at later stages of the embers pipeline.
+
+"""
