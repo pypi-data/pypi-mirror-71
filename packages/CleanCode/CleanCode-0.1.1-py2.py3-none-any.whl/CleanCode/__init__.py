174f2c017e55a9bf1e581192c4b74a46e0547b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:43 +0000

    Add package CleanCode/0.1.1/CleanCode-0.1.1-py2.py3-none-any.whl

---
+++ packages/CleanCode/CleanCode-0.1.1-py2.py3-none-any.whl/CleanCode/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2018 ZouMingzhe <zoumingzhe@qq.com>
+# This module is part of the CleanCode package, which is released under a MIT licence.
+from  .clean         import  clean
+from  .scan          import  scan
