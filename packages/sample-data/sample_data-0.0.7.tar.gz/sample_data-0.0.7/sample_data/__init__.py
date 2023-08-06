8ae2802dbefc773ab08a10488b2ffe6e73b077c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:03 +0000

    Add package sample-data/0.0.7/sample_data-0.0.7.tar.gz

---
+++ packages/sample-data/sample_data-0.0.7.tar.gz/sample_data-0.0.7/sample_data/__init__.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def _getcsv(name):
+    def fn():
+        return pd.read_csv(f"{dir_path}/data/{name}.csv")
+    return fn
+
+countries = _getcsv('countries')
+airbnb = _getcsv('airbnb')
+corona = _getcsv('corona')
