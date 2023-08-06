67b31a1943d2513efe8deb4c759b97ad855907d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:42 +0000

    Add package ducts/0.0.2/ducts-0.0.2.tar.gz

---
+++ packages/ducts/ducts-0.0.2.tar.gz/ducts-0.0.2/ducts/main_asr.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+
+from concurrent.futures import ProcessPoolExecutor
+
+import asyncio
+
+from ifconf import configure_module, config_callback, configure_main
+
+from ducts.asr import AutomaticSpeechRecognizerManager
+
+def main():
+    configure_main()
+    loop = asyncio.get_event_loop()
+    asr = AutomaticSpeechRecognizerManager(loop)
+    asyncio.ensure_future(asr.run())
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        asr.close()
+        loop.close()
+    
+if __name__ == '__main__':
+    main()
