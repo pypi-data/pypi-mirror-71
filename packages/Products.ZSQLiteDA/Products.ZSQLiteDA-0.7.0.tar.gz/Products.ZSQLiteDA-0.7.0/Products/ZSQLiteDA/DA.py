54f21f2c1329b447426a6c827c12f388ecd336de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:42 +0000

    Add package Products.ZSQLiteDA/0.7.0/Products.ZSQLiteDA-0.7.0.tar.gz

---
+++ packages/Products.ZSQLiteDA/Products.ZSQLiteDA-0.7.0.tar.gz/Products.ZSQLiteDA-0.7.0/Products/ZSQLiteDA/DA.py
@@ -0,0 +1,71 @@
+##############################################################################
+#
+# Copyright (c) 2001 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+database_type='SQLite'
+
+import sys
+from _thread import allocate_lock
+
+from .db import DB, manage_DataSources, data_dir
+import Shared.DC.ZRDB.Connection
+from App.special_dtml import HTMLFile
+import Shared.DC.ZRDB.Connection
+
+_connections={}
+_connections_lock=allocate_lock()
+
+data_sources=manage_DataSources
+
+addConnectionForm=HTMLFile('dtml/connectionAdd',globals())
+
+
+def manage_addZSQLiteConnectionForm(self, REQUEST, *args, **kw):
+    """Add a connection form"""
+    return addConnectionForm(
+        self, self, REQUEST,
+        database_type=database_type,
+        data_dir=data_dir,
+        data_sources=data_sources)
+
+def manage_addZSQLiteConnection(
+    self, id, title, connection, REQUEST=None):
+    """Add a DB connection to a folder"""
+
+    # Note - ZSQLiteDA's connect immediately check is alway false
+    self._setObject(id, Connection(
+        id, title, connection, None))
+    if REQUEST is not None: return self.manage_main(self,REQUEST)
+
+class Connection(Shared.DC.ZRDB.Connection.Connection):
+    " "
+    database_type=database_type
+    id='%s_database_connection' % database_type
+    meta_type=title='Z %s Database Connection' % database_type
+    zmi_icon = 'fas fa-database'
+    data_dir=data_dir
+
+    manage_properties=HTMLFile('dtml/connectionEdit', globals(),
+                                       data_sources=data_sources)
+
+    def connected(self):
+        if hasattr(self, '_v_database_connection'):
+            return self._v_database_connection.opened
+        return ''
+
+    def connect(self,s):
+        _connections_lock.acquire()
+        try:
+            c=_connections
+            self._v_database_connection=c[s]=DB(s)
+            return self
+        finally:
+            _connections_lock.release()
---
+++ packages/Products.ZSQLiteDA/Products.ZSQLiteDA-0.7.0.tar.gz/Products.ZSQLiteDA-0.7.0/Products/ZSQLiteDA/db.py
@@ -0,0 +1,124 @@
+##############################################################################
+#
+# Copyright (c) 2001 Zope Corporation and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+import os
+import sqlite3
+
+from DateTime import DateTime
+import Shared.DC.ZRDB.THUNK
+
+data_dir= os.path.abspath(
+    os.path.join(os.path.dirname(os.environ['CLIENT_HOME']), 'sqlite')
+)
+
+def manage_DataSources():
+
+    if not os.path.exists(data_dir):
+        try:
+            os.mkdir(data_dir)
+        except:
+            raise sqlite3.OperationalError(
+                """
+                The Zope SQLite Database Adapter requires the
+                existence of the directory, <code>%s</code>.  An error
+                occurred  while trying to create this directory.
+                """ % data_dir)
+    if not os.path.isdir(data_dir):
+        raise sqlite3.OperationalError(
+            """
+            The Zope SQLite Database Adapter requires the
+            existence of the directory, <code>%s</code>.  This
+            exists, but is not a directory.
+            """ % data_dir)
+
+    return map(
+        lambda d: (d,''),
+        filter(lambda f, i=os.path.isfile, d=data_dir, j=os.path.join:
+               i(j(d,f)),
+               os.listdir(data_dir))
+        )
+
+class DB(Shared.DC.ZRDB.THUNK.THUNKED_TM):
+
+    opened=''
+
+    def open(self):
+        connection = self.connection
+        if connection != ':memory:':
+            connection = os.path.join(data_dir, connection)
+        self.db = sqlite3.connect(connection, check_same_thread = False)
+        self.opened=DateTime()
+
+    def close(self):
+        self.db.close()
+        self.opened = None
+
+    def __init__(self,connection):
+        self.connection=connection
+        self.open()
+
+    def query(self, query_string, max_rows=None):
+        self._begin()
+        c = self.db.cursor()
+
+        queries=filter(None, [q.strip() for q in query_string.split('\0')])
+        if not queries:
+            raise sqlite3.OperationalError('empty query')
+        desc=None
+        result=[]
+        for qs in queries:
+            c.execute(qs)
+            d=c.description
+            if d is None: continue
+            if desc is None: desc=d
+            elif d != desc:
+                raise sqlite3.OperationalError(
+                    'Multiple incompatible selects in '
+                    'multiple sql-statement query'
+                    )
+
+            if max_rows:
+                if not result: result=c.fetchmany(max_rows)
+                elif len(result) < max_rows:
+                    result=result+c.fetchmany(max_rows-len(result))
+            else:
+                result = c.fetchall()
+
+        self._finish()
+        if desc is None: return (),()
+
+        items=[]
+        for name, type, width, ds, p, scale, null_ok in desc:
+            if type=='NUMBER':
+                if scale==0: type='i'
+                else: type='n'
+            elif type=='DATE':
+                type='d'
+            else: type='s'
+            items.append({
+                'name': name,
+                'type': type,
+                'width': width,
+                'null': null_ok,
+                })
+
+        return items, result
+
+    def _begin(self):
+        #sqlite3.connection don't have begein() method.
+        pass
+
+    def _finish(self):
+        self.db.commit()
+
+    def _abort(self):
+        conn.db.rollback()
