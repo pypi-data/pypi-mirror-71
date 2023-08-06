dbb76633640555c10cfe7fddfd9561d4ade93e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:11 +0000

    Add package asdsnd-distributions/0.1/asdsnd_distributions-0.1.tar.gz

---
+++ packages/asdsnd-distributions/asdsnd_distributions-0.1.tar.gz/asdsnd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='asdsnd_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['asdsnd_distributions'],
+      author = 'Ayod Sanwoolu',
+      author_email = 'asanwoolu@gmail.com',
+      zip_safe=False)
