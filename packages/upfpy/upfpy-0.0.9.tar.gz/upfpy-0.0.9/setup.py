f3c8940b63ccfad7d35b61351dc9b687ea574ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:40 +0000

    Add package upfpy/0.0.9/upfpy-0.0.9.tar.gz

---
+++ packages/upfpy/upfpy-0.0.9.tar.gz/upfpy-0.0.9/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='upfpy',
+    version='0.0.9',
+    author="Rick Honda",
+    author_email="rickhonda7@gmail.com",
+    description='Fully factors given integer: unique prime factorization',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rickhonda/upfpy",
+
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+
+    packages=["upfpy"],
+    entry_points={
+        "console_scripts": [
+            "upfpy=upfpy.__main__:main",
+            ]
+        },
+    extras_require={
+        "dev": [
+            "pytest>=3.8",
+            ],
+        },
+    )
