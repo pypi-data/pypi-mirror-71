9994d7b6b33e8f5278a4517a3e57287f2296df69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:32 +0000

    Add package fcdist-probability/0.1/fcdist_probability-0.1.tar.gz

---
+++ packages/fcdist-probability/fcdist_probability-0.1.tar.gz/fcdist_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='fcdist_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['fcdist_probability'],
+      zip_safe=False)
