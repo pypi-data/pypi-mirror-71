1059a92d997ad2981c5bdd5d18efaa27d095f715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:32 +0000

    Add package sbc-utils/1.0.1/sbc_utils-1.0.1.tar.gz

---
+++ packages/sbc-utils/sbc_utils-1.0.1.tar.gz/sbc_utils-1.0.1/setup.py
@@ -0,0 +1,40 @@
+'''
+pytz setup script
+'''
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+me = 'Abhinav Kotak'
+memail = 'in.abhi9@gmail.com'
+
+packages = ['sbc_utils']
+package_dir = {'sbc_utils': 'src/sbc_utils'}
+
+setup(
+    name='sbc_utils',
+    version='1.0.1',
+    zip_safe=True,
+    description='Utility functions',
+    author=me,
+    author_email=memail,
+    maintainer=me,
+    maintainer_email=memail,
+    url='https://gitlab.stickboycreative.com/sbc-python/sbc_utils',
+    license=open('LICENSE', 'r').read(),
+    keywords=['python'],
+    packages=packages,
+    package_dir=package_dir,
+    platforms=['Independant'],
+    classifiers=[
+        'Development Status :: 1 - beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
