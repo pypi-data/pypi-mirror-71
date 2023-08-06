e2e32a3372e755e0714e35bae8988fcda80af977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:31 +0000

    Add package jeremy-portal-gun/0.4.0/jeremy-portal-gun-0.4.0.tar.gz

---
+++ packages/jeremy-portal-gun/jeremy-portal-gun-0.4.0.tar.gz/jeremy-portal-gun-0.4.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jeremy_portal_gun']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.2.1,<0.3.0']
+
+entry_points = \
+{'console_scripts': ['magic = jeremy_portal_gun.main:app']}
+
+setup_kwargs = {
+    'name': 'jeremy-portal-gun',
+    'version': '0.4.0',
+    'description': '',
+    'long_description': '# Portal Gun\n\nThe awesome Portal FUN!!!!',
+    'author': 'Jeremy Berman',
+    'author_email': 'jerber@sas.upenn.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
