db16ca056c2451fd75c99d791bcb2fd599c0c509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package dataclay/2.4/dataClay-2.4.tar.gz

---
+++ packages/dataclay/dataClay-2.4.tar.gz/dataClay-2.4/README.md
@@ -0,0 +1,27 @@
+
+[![Documentation Status](https://readthedocs.org/projects/pyclay/badge/?version=latest)](https://pyclay.readthedocs.io/en/latest/?badge=latest)
+[![PyPI version](https://badge.fury.io/py/dataClay.png)](https://badge.fury.io/py/dataClay)
+
+# dataClay Python codebase
+
+This repository holds the `pyclay` Python package. This package is used both
+by the dataClay Python clients and also for the dataClay service Execution
+Environment.
+
+## Installation
+
+This package is available from PyPI, so just `pip` it:
+
+    $ pip install dataClay
+
+## Documentation
+
+Official documentation available at [read the docs](https://pyclay.readthedocs.io/en/latest/)
+
+## Other resources
+
+[BSC official dataClay webpage](https://www.bsc.es/dataclay)
+
+---
+
+![dataClay logo](https://www.bsc.es/sites/default/files/public/styles/bscw2_-_simple_crop_style/public/bscw2/content/software-app/logo/logo_dataclay_web_bsc.jpg)
---
+++ packages/dataclay/dataClay-2.4.tar.gz/dataClay-2.4/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os 
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+# parse_requirements
+install_reqs = parse_requirements("./requirements.txt")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("VERSION.txt", "r") as version_file:
+    version = version_file.read().strip()
+
+setup(name='dataClay',
+      version=version,
+      install_requires=install_reqs,
+      description='Python library for dataClay',
+      packages=find_packages("src"),
+      package_dir={'':'src'},
+      package_data={
+        # All .properties files are valuable "package data"
+        '': ['*.properties'],
+      },
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://www.bsc.es/dataclay",
+      project_urls={
+          'Documentation': 'https://pyclay.readthedocs.io/en/latest/',
+          'Source': 'https://github.com/bsc-dom/pyclay',
+      },
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Intended Audience :: Science/Research",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python",
+          "Topic :: Database :: Database Engines/Servers",
+          "Topic :: System :: Distributed Computing",
+          "Topic :: Software Development :: Libraries :: Application Frameworks",
+      ],
+      )
+
