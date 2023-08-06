a86ea4800dd26d437ae0e76e38a19844f3b8e08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:57 +0000

    Add package itay-bardugo-cron-migration/1.0.0/itay_bardugo_cron_migration-1.0.0-py3-none-any.whl

---
+++ packages/itay-bardugo-cron-migration/itay_bardugo_cron_migration-1.0.0-py3-none-any.whl/cron_migration/app/models/environment.py
@@ -0,0 +1,42 @@
+from cron_migration.files.models.path import Path
+import os
+
+
+class Environment:
+    def __init__(self, path: Path = None, template: str = "default"):
+        self._path = path
+        self.template = template
+
+    def get_real_path(self):
+        return self.path.path
+
+    def get_script_path(self) -> Path:
+        return Path(
+            path=os.path.join(
+                self.get_real_path() +
+                os.path.sep +
+                "revision.py.mako"
+            )
+        )
+
+    def path_from_base(self, *args):
+        return self._path.join(*args)
+
+    def get_revisions_path(self, filename=None) -> Path:
+        return Path(
+            path=os.path.join(
+                self.get_real_path() +
+                os.path.sep +
+                "revisions" +
+                (os.path.sep + filename if filename else "")
+
+            )
+        )
+
+    @property
+    def path(self):
+        return self._path
+
+    @path.setter
+    def path(self, p):
+        self._path = p
