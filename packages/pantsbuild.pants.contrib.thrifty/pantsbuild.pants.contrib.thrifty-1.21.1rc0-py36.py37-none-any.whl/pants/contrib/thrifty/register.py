965d819002bf0a32f6c9a5c16568ab8820295f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:21 +0000

    Add package pantsbuild.pants.contrib.thrifty/1.21.1rc0/pantsbuild.pants.contrib.thrifty-1.21.1rc0-py36.py37-none-any.whl

---
+++ packages/pantsbuild.pants.contrib.thrifty/pantsbuild.pants.contrib.thrifty-1.21.1rc0-py36.py37-none-any.whl/pants/contrib/thrifty/register.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.build_graph.build_file_aliases import BuildFileAliases
+from pants.goal.task_registrar import TaskRegistrar as task
+
+from pants.contrib.thrifty.java_thrifty_gen import JavaThriftyGen
+from pants.contrib.thrifty.java_thrifty_library import JavaThriftyLibrary
+
+
+def build_file_aliases():
+  return BuildFileAliases(
+    targets={
+      'java_thrifty_library': JavaThriftyLibrary,
+    }
+  )
+
+
+def register_goals():
+  task(name='thrifty', action=JavaThriftyGen).install('gen')
