37706cfe011f4db6fd9c9e087c5f8c541207e5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:44 +0000

    Add package cloudshell-app-helper/1.1.2/cloudshell-app-helper-1.1.2.tar.gz

---
+++ packages/cloudshell-app-helper/cloudshell-app-helper-1.1.2.tar.gz/cloudshell-app-helper-1.1.2/cloudshell/helpers/save_workflow/deploy_info.py
@@ -0,0 +1,16 @@
+class DeployInfo(dict):
+    def __init__(self, deployment_paths, app_template_image=''):
+
+        display_image = app_template_image
+
+        deploypaths = []
+        for deploy_path in deployment_paths:
+            path = dict()
+            path['name'] = deploy_path.Name
+            path['is_default'] = deploy_path.IsDefault
+            path['service_name'] = deploy_path.DeploymentService.Model
+            path['attributes'] = dict()
+            for attribute in deploy_path.DeploymentService.Attributes:
+                path['attributes'][attribute.Name] = attribute.Value
+            deploypaths.append(path)
+        dict.__init__(self, deploypaths=deploypaths, display_image=display_image)
