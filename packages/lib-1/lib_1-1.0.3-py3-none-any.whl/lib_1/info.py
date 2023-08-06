12f24da0ed2fe04aa21c6889d4c9cd4341c9d2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:46 +0000

    Add package lib-1/1.0.3/lib_1-1.0.3-py3-none-any.whl

---
+++ packages/lib-1/lib_1-1.0.3-py3-none-any.whl/lib_1/info.py
@@ -0,0 +1,13 @@
+def name():
+    print("XYZ")
+    return
+def dob():
+    print("01-01-2000")
+    return
+def address():
+    print("india")
+    return
+def email():
+    print("xyz123456778@gmail.com")
+    return
+    
