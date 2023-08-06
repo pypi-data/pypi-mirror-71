af80ba39ab5717f6dc086b546ce6e55a7d75af9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:41 +0000

    Add package itay-bardugo-cron-migration/1.1.0/itay-bardugo-cron-migration-1.1.0.tar.gz

---
+++ packages/itay-bardugo-cron-migration/itay-bardugo-cron-migration-1.1.0.tar.gz/itay-bardugo-cron-migration-1.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="itay-bardugo-cron-migration", # Replace with your own username
+    version="1.1.0",
+    author="Itay Bardugo",
+    author_email="itaybardugo91@gmail.com",
+    description="manage your cron jobs with python and a revision system(CLI)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/itay-bardugo/python-cron-migration",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+    entry_points={
+        "console_scripts": [
+            'cronmig=cron_migration.cli.commands:cronmig',
+            'cronmig-revision=cron_migration.cli.commands:revision',
+        ]
+    },
+    python_requires='>=3.8.3',
+)
