c4924a2557d4e7d47282d0cf8eeb2cc1dbd95afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:14 +0000

    Add package refnx/0.1.15/refnx-0.1.15-cp36-cp36m-macosx_10_14_x86_64.whl

---
+++ packages/refnx/refnx-0.1.15-cp36-cp36m-macosx_10_14_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.15'
+version = '0.1.15'
+full_version = '0.1.15'
+git_revision = '1ece7ba5ce6c8f34ff0d0212444c8ab2c35c1a00'
+release = True
+if not release:
+    version = full_version
