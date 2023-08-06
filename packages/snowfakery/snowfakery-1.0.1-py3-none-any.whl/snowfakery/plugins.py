6ecdc7553339313b1bdba12e00a3299780869afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:21 +0000

    Add package snowfakery/1.0.1/snowfakery-1.0.1-py3-none-any.whl

---
+++ packages/snowfakery/snowfakery-1.0.1-py3-none-any.whl/snowfakery/plugins.py
@@ -0,0 +1,74 @@
+from typing import Any, Callable
+
+
+class SnowfakeryPlugin:
+    """Base class for all plugins.
+
+    Currently plugins can declare new functions. Over time new superpowers
+    for plugins will be introduced.
+
+    Plugins generally look like this:
+
+    class MyPlugin:
+        class Functions: # must be named `Functions`.
+            def func1(self, arg1, arg2, arg3=default):
+                return something()
+
+    This function would be invoked through the name `MyPlugin.func1()`
+
+    Despite the name, the Functions namespace could also include
+    constants or other namespaces, if that is useful to you.
+
+    A RuntimeContext object is supplied on `self.context` with methods
+    context.field_vars() and context.context_vars().
+
+    context.field_vars() are the same field variables that can be used in
+    templates.
+
+    context.context_vars() is a mutable mapping contaiing
+    values that are available to this object template and that of
+    all children.
+
+    context_vars are unique to each plugin and plugins do not see
+    (or corrupt, or overwrite) context_vars for another plugin.
+
+    Plugins can also keep internal state for global data, like any other
+    Python object.
+    """
+
+    def __init__(self, interpreter):
+        self.interpreter = interpreter
+        self.context = PluginContext(self)
+
+    def custom_functions(self, *args, **kwargs):
+        """Instantiate, contextualize and return a function library
+
+        Default behaviour is to return self.Function()."""
+        functions = self.Functions()
+        functions.context = self.context
+        return functions
+
+
+class PluginContext:
+    "Exposes certain stable internals to plugins"
+
+    def __init__(self, plugin):
+        self.plugin = plugin
+        self.interpreter = plugin.interpreter
+
+    def field_vars(self):
+        return self.interpreter.current_context.field_vars()
+
+    def context_vars(self):
+        return self.interpreter.current_context.context_vars(
+            self.plugin.__class__.__name__
+        )
+
+    def evaluate(self, field_definition):
+        return field_definition.render(self.interpreter.current_context)
+
+
+def lazy(func: Any) -> Callable:
+    """A lazy function is one that expects its arguments to be unparsed"""
+    func.lazy = True
+    return func
