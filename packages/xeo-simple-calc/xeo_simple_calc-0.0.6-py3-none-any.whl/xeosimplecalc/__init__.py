0907197eb8011a77208725535d2d443165e5fa7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:34 +0000

    Add package xeo-simple-calc/0.0.6/xeo_simple_calc-0.0.6-py3-none-any.whl

---
+++ packages/xeo-simple-calc/xeo_simple_calc-0.0.6-py3-none-any.whl/xeosimplecalc/__init__.py
@@ -0,0 +1,15 @@
+def add(num1, num2):
+    return num1 + num2
+def subtract(num1, num2):
+    return num1 - num2
+def multiply(num1, num2):
+    return num1 * num2
+def divide(num1, num2):
+    return num1 / num2
+def exponentiation(num, degree):
+    return num ** degree
+def square_root(num):
+    if num <= 0:
+        return 0
+    else:
+        return num ** 0.5
