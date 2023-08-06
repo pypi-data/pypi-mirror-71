37fcec6dc6334d799795c12d4a7d62d1cf6536f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:02 +0000

    Add package bipbop-microservices/0.0.5/bipbop-microservices-0.0.5.tar.gz

---
+++ packages/bipbop-microservices/bipbop-microservices-0.0.5.tar.gz/bipbop-microservices-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bipbop-microservices",
+    version="0.0.5",
+    author="Lucas Fernando Amorim",
+    author_email="lf.amorim@bipbop.com.br",
+    description="IPC between BIPBOP APPs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    repository_url="https://github.com/bipbop/bipbop-microservices",
+    url="https://github.com/bipbop/bipbop-microservices",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Customer Service",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
