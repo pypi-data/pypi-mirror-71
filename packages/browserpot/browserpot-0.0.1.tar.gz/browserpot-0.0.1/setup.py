c897e9e7b99d8d74b6a1c03e69f450b6cc22040f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:47 +0000

    Add package browserpot/0.0.1/browserpot-0.0.1.tar.gz

---
+++ packages/browserpot/browserpot-0.0.1.tar.gz/browserpot-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+  name = 'browserpot',
+  version = '0.0.1',
+  description = '',
+  author = 'browserpot',
+  author_email = 'browserpot@gmail.com',
+  license = 'GPL-3.0',
+  url = 'https://github.com/browserpot/browserpot',
+  download_url = 'https://github.com/browserpot/browserpot/downloads',
+  keywords = ['browserpot'],
+  classifiers = [],
+  packages = setuptools.find_packages(),
+)
