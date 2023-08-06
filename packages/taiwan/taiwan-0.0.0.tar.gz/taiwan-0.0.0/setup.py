c4d86d60f23a6bf81cd7abc98daccac57e60dd4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:27 +0000

    Add package taiwan/0.0.0/taiwan-0.0.0.tar.gz

---
+++ packages/taiwan/taiwan-0.0.0.tar.gz/taiwan-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+def main():
+    setup(name='taiwan',
+          version='v0.0.0',
+          author='なるみ',
+          author_email='weaper@gamil.com',
+          packages=find_packages())
+
+
+if __name__ == "__main__":
+    main()
