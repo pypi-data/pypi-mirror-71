749fa6f15d209110a8e274e63270d213bd3740e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:38 +0000

    Add package perfectextractor/0.3/perfectextractor-0.3.tar.gz

---
+++ packages/perfectextractor/perfectextractor-0.3.tar.gz/perfectextractor-0.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='perfectextractor',
+    version='0.3',
+    author='Martijn van der Klis',
+    author_email='M.H.vanderKlis@uu.nl',
+    description='Extracting present perfects (and related forms) from parallel corpora',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    url='https://github.com/UUDigitalHumanitieslab/perfectextractor',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+      'click',
+      'lxml',
+      'requests',
+    ],
+)
