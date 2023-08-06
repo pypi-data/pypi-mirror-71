b54e6e39aec7480155fa9b1ca81e77ed90778499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:30 +0000

    Add package ACNLoggerAVConcurrent/1.1.1/ACNLoggerAVConcurrent-1.1.1.tar.gz

---
+++ packages/ACNLoggerAVConcurrent/ACNLoggerAVConcurrent-1.1.1.tar.gz/ACNLoggerAVConcurrent-1.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ACNLoggerAVConcurrent',    # This is the name of your PyPI-package.
+    version='1.1.1',           # The name of your scipt, and also the command you'll be using for calling it
+    author='AIIberia',
+    author_email='acn.av.endesa@gmail.com',
+    packages=['ACNLoggerAVConcurrent']
+)
