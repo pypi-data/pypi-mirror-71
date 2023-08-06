e998e441d8c320ac0107431cab00d911068fbdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:35 +0000

    Add package odoo10-addon-test-connector/10.0.1.0.0.99.dev15/odoo10_addon_test_connector-10.0.1.0.0.99.dev15-py2-none-any.whl

---
+++ packages/odoo10-addon-test-connector/odoo10_addon_test_connector-10.0.1.0.0.99.dev15-py2-none-any.whl/odoo/addons/test_connector/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+{'name': 'Connector Tests',
+ 'summary': 'Automated tests for Connector, do not install.',
+ 'description': 'Automated tests for Connector, do not install.',
+ 'version': '10.0.1.0.0',
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'license': 'LGPL-3',
+ 'category': 'Hidden',
+ 'depends': ['connector',
+             ],
+ 'website': 'https://www.camptocamp.com',
+ 'data': ['security/ir.model.access.csv',
+          ],
+ 'installable': True,
+ }
