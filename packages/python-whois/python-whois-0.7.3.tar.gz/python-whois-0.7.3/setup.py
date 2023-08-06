d3d914e044f72af62a805e0c9400f46bff1648ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:42 +0000

    Add package python-whois/0.7.3/python-whois-0.7.3.tar.gz

---
+++ packages/python-whois/python-whois-0.7.3.tar.gz/python-whois-0.7.3/setup.py
@@ -0,0 +1,40 @@
+import sys, os
+import setuptools
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setuptools.setup(
+    name='python-whois',
+    version='0.7.3',
+    description="Whois querying and parsing of domain registration information.",
+    long_description=read('README.rst'),
+    install_requires=[
+        'future',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='whois, python',
+    author='Richard Penman',
+    author_email='richard.penman@gmail.com',
+    url='https://github.com/richardpenman/whois',
+    license='MIT',
+    packages=['whois'],
+    package_dir={'whois':'whois'},
+    extras_require={
+        'better date conversion': ["python-dateutil"]
+    },
+    test_suite='nose.collector',
+    tests_require=['nose', 'simplejson'],
+    include_package_data=True,
+    zip_safe=False
+)
