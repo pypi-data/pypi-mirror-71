ca041add6588ee4e4ba64318448959bc4f1c66b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:44 +0000

    Add package PyUtilib/6.0.0/PyUtilib-6.0.0.tar.gz

---
+++ packages/PyUtilib/PyUtilib-6.0.0.tar.gz/PyUtilib-6.0.0/README.md
@@ -0,0 +1,42 @@
+[![Travis Build Status](https://travis-ci.org/PyUtilib/pyutilib.png?branch=master)](https://travis-ci.org/PyUtilib/pyutilib)
+[![Appveyor Build status](https://ci.appveyor.com/api/projects/status/c8dh5o1blp0sycr6/branch/master?svg=true)](https://ci.appveyor.com/project/WilliamHart/pyutilib/branch/master)
+[![codecov.io](https://codecov.io/github/PyUtilib/pyutilib/coverage.svg?branch=master)](https://codecov.io/github/PyUtilib/pyutilib?branch=master)
+[![Depsy Software Impact](http://depsy.org/api/package/pypi/PyUtilib/badge.svg)](http://depsy.org/package/python/PyUtilib)
+[![Project Status: Active - The project has reached a stable, usable state and is being actively developed.](http://www.repostatus.org/badges/latest/active.svg)](http://www.repostatus.org/#active)
+
+## PyUtilib Overview
+
+The PyUtilib project supports a collection of Python utilities,
+including a well-developed component architecture and extensions
+to the PyUnit testing framework. PyUtilib has been developed to
+support several Python-centric projects, especially
+[Pyomo](http://pyomo.org).
+
+PyUtilib is available under the BSD License.
+
+### Installation
+
+#### PyPI [![PyPI version](https://img.shields.io/pypi/v/pyutilib.svg?maxAge=3600)](https://pypi.org/project/PyUtilib/) [![PyPI downloads](https://img.shields.io/pypi/dm/pyutilib.svg?maxAge=21600)](https://pypistats.org/packages/pyutilib)
+
+    pip install pyutilib
+    
+#### Anaconda [![Anaconda version](https://anaconda.org/conda-forge/pyutilib/badges/version.svg)](https://anaconda.org/conda-forge/pyutilib) [![Anaconda downloads](https://anaconda.org/conda-forge/pyutilib/badges/downloads.svg)](https://anaconda.org/conda-forge/pyutilib)
+
+    conda install -c https://conda.anaconda.org/conda-forge pyutilib
+
+### Getting Help
+
+* [PyUtilib Forum](https://groups.google.com/forum/?hl=en#!forum/pyutilib-forum)
+* [Add a Ticket](https://github.com/PyUtilib/pyutilib/issues/new)
+* [Find a Ticket](https://github.com/PyUtilib/pyutilib/issues) and **Vote On It**!
+
+### Developers
+
+PyUtilib development moved to this repository in October, 2015 from
+Sandia National Laboratories. Developer discussions are hosted by [google groups](https://groups.google.com/forum/#!forum/pyutilib-developers).
+
+
+By contributing to this software project, you are agreeing to the following terms and conditions for your contributions:
+
+1. You agree your contributions are submitted under the BSD license. 
+2. You represent you are authorized to make the contributions and grant the license. If your employer has rights to intellectual property that includes your contributions, you represent that you have received permission to make contributions and grant the required license on behalf of that employer. 
---
+++ packages/PyUtilib/PyUtilib-6.0.0.tar.gz/PyUtilib-6.0.0/setup.py
@@ -0,0 +1,84 @@
+#  _________________________________________________________________________
+#
+#  PyUtilib: A Python utility library.
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  _________________________________________________________________________
+
+"""
+Setup for PyUtilib package
+"""
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as README:
+        # Strip all leading badges up to, but not including the COIN-OR
+        # badge so that they do not appear in the PyPI description
+        while True:
+            line = README.readline()
+            if 'COIN-OR' in line:
+                break
+            if line.strip() and '[![' not in line:
+                break
+        return line + README.read()
+
+requires=[ 'nose', 'six' ]
+
+setup(name="PyUtilib",
+    version='6.0.0',
+    maintainer='William E. Hart',
+    maintainer_email='wehart@sandia.gov',
+    url = 'https://github.com/PyUtilib/pyutilib',
+    license = 'BSD',
+    platforms = ["any"],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    description = 'PyUtilib: A collection of Python utilities',
+    long_description = read('README.md'),
+    long_description_content_type='text/markdown',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: Jython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'],
+      packages=find_packages(),
+      keywords=['utility'],
+      install_requires=requires,
+      entry_points="""
+        [nose.plugins.0.10]
+        nose.testdata = pyutilib.th.nose_testdata:TestData
+        nose.forcedgc = pyutilib.th.nose_gc:ForcedGC
+        nose.timeout = pyutilib.th.nose_timeout:TestTimeout
+        [console_scripts]
+        test.pyutilib = pyutilib.dev.runtests:runPyUtilibTests
+        lbin = pyutilib.dev.lbin:main
+        lpython = pyutilib.dev.lpython:main
+        pypi_downloads = pyutilib.dev.pypi_downloads:main
+        replaceCopyright = pyutilib.dev.replaceCopyright:main
+        checkCopyright = pyutilib.dev.checkCopyright:main
+        pyutilib_test_driver = pyutilib.autotest.driver:main
+        dispatch_srvr=pyutilib.pyro.dispatch_srvr:main
+      """
+      )
+
