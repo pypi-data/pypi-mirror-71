41beba5bb1246f864b8b5de6072026c7059cc9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:27 +0000

    Add package simple-novel-reader/0.9.184/simple-novel-reader-0.9.184.tar.gz

---
+++ packages/simple-novel-reader/simple-novel-reader-0.9.184.tar.gz/simple-novel-reader-0.9.184/src/snr/__main__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# :::::::::::::::[]::::::::::::: #
+# :::: /_> |U_U| || /_> /_> :::: #
+# :::: <=/ |T-T| || <=/ <=/ :::: #
+# ::::::::SHISS DOTFILES:::::::: #
+# https://github.com/gzygmanski: #
+# gzygmanski@hotmail.com:::::::: #
+
+from snr.snr import snr
+
+def main():
+    snr()
+
+if __name__ == '__main__':
+    main()
