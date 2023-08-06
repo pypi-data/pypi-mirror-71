71d4b078cd99b1b2941b9388174855d4d36a0f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:31 +0000

    Add package wtdojo/0.3/wtdojo-0.3-py2.py3-none-any.whl

---
+++ packages/wtdojo/wtdojo-0.3-py2.py3-none-any.whl/wtdojo/fields/__init__.py
@@ -0,0 +1,7 @@
+from .core import (DojoStringField, DojoJSONField, DojoDateField, DojoTimeField, DojoIntegerField,
+                  DojoSelectField, DojoSelectMultipleField, DojoBooleanField,
+                  DojoDecimalField)
+
+__all__ = ['DojoStringField', 'DojoJSONField', 'DojoDateField', 'DojoTimeField', 'DojoIntegerField',
+           'DojoSelectField', 'DojoSelectMultipleField', 'DojoBooleanField',
+           'DojoDecimalField']
