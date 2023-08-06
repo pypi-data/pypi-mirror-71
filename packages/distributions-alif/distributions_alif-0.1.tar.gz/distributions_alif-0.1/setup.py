a478d88a30d7d26fa972da734695acee36c32ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:18 +0000

    Add package distributions-alif/0.1/distributions_alif-0.1.tar.gz

---
+++ packages/distributions-alif/distributions_alif-0.1.tar.gz/distributions_alif-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_alif',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_alif'],
+      author = 'Alif Ilham Madani',
+      author_email = 'alifilhammadani@gmail.com',
+      zip_safe=False)
