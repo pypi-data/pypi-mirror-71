31f33c9b1f8c82f4586a5a1c1ebfccc48c9fbb92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:41 +0000

    Add package distributions-asif/1.0/distributions_asif-1.0.tar.gz

---
+++ packages/distributions-asif/distributions_asif-1.0.tar.gz/distributions_asif-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_asif',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions_asif'],
+      zip_safe=False)
