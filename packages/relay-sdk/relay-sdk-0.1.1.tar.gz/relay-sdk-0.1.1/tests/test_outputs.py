9b39607b1604eca8b96b7bebf6beed4b74d04561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:14 +0000

    Add package relay-sdk/0.1.1/relay-sdk-0.1.1.tar.gz

---
+++ packages/relay-sdk/relay-sdk-0.1.1.tar.gz/relay-sdk-0.1.1/tests/test_outputs.py
@@ -0,0 +1,15 @@
+from relay_sdk.client import new_session
+from relay_sdk.outputs import Outputs
+from requests_mock import Adapter
+
+
+class TestOutputs:
+
+    def test_set(self, requests_mock: Adapter) -> None:
+        requests_mock.register_uri(
+            'PUT', 'http+api://api/outputs/foo',
+            text='OK',
+            request_headers={'content-type': 'application/json'},
+            additional_matcher=lambda request: request.text == r'"bar"',
+        )
+        Outputs(new_session(api_url='http://api')).set('foo', 'bar')
