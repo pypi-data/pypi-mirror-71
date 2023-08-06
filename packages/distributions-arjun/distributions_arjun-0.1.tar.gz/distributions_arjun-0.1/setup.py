438c3a6a7641b191f483e03daa5c4d57a0470c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:49 +0000

    Add package distributions-arjun/0.1/distributions_arjun-0.1.tar.gz

---
+++ packages/distributions-arjun/distributions_arjun-0.1.tar.gz/distributions_arjun-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_arjun',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_arjun'],
+      zip_safe=False)
