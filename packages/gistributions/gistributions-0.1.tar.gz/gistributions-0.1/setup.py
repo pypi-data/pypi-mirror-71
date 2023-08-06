19fd7f0f0e2c2b7e5001829fa619b267326094d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:42 +0000

    Add package gistributions/0.1/gistributions-0.1.tar.gz

---
+++ packages/gistributions/gistributions-0.1.tar.gz/gistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gistributions'],
+      author='Godwin',
+      author_email='justg.techs@gmail.com',
+      zip_safe=False)
