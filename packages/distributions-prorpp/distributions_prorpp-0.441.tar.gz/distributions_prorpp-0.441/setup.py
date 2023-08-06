3f9e63a7b7fd70f5e6fe92de1f9b0e9eb0a9f8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:42 +0000

    Add package distributions-prorpp/0.441/distributions_prorpp-0.441.tar.gz

---
+++ packages/distributions-prorpp/distributions_prorpp-0.441.tar.gz/distributions_prorpp-0.441/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_prorpp',
+      version='0.441',
+      description='Gaussian distributions',
+      packages=['distributions_prorpp'],
+      author= ' Hassan Algizani ',
+      author_email= 'Hassan.jezo@outlook.com',
+      zip_safe=False)
