84246ce34fad94ecb960a9a22249c07fd71d42db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:20 +0000

    Add package WConfig/1.2.0/WConfig-1.2.0.tar.gz

---
+++ packages/WConfig/WConfig-1.2.0.tar.gz/WConfig-1.2.0/setup.py
@@ -0,0 +1,11 @@
+﻿from setuptools import find_packages, setup
+
+setup(
+    name='WConfig',
+    version='1.2.0',
+    author='Himpq.欣普',
+    author_email='15877161799@163.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
