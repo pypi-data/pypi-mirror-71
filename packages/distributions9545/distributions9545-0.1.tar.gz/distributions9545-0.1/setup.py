6a7f4893b87fac429e87a58c91d0b1f7ce0777c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:19 +0000

    Add package distributions9545/0.1/distributions9545-0.1.tar.gz

---
+++ packages/distributions9545/distributions9545-0.1.tar.gz/distributions9545-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions9545',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions9545'],
+      zip_safe=False)
