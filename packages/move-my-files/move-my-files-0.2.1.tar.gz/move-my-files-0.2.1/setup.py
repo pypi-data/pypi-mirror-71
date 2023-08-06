a12634720671cd7cb799457ec54d019ee3054f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:27 +0000

    Add package move-my-files/0.2.1/move-my-files-0.2.1.tar.gz

---
+++ packages/move-my-files/move-my-files-0.2.1.tar.gz/move-my-files-0.2.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='move-my-files',
+    version='0.2.1',
+    description='CLI tools to organize files on your computer',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jan25/move-my-files',
+    author='Abhilash Gnan',
+    author_email='abhilashgnan@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=find_packages(where='.'),
+    package_dir={'': '.'},
+    python_requires='>=3.5, <4',
+    install_requires=[
+        'watchdog',
+        'Click',
+        'pyyaml',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'mmf=app.cli:mmf',
+        ],
+    },
+)
