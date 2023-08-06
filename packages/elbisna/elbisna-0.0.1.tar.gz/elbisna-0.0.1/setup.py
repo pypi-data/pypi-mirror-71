6c83cacf55bcd94eac142bd38f1bae2d15a2d03d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:09 +0000

    Add package elbisna/0.0.1/elbisna-0.0.1.tar.gz

---
+++ packages/elbisna/elbisna-0.0.1.tar.gz/elbisna-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+  name = 'elbisna',
+  version = '0.0.1',
+  description = '',
+  author = 'elbisna',
+  author_email = 'elbisna@gmail.com',
+  license = 'GPL-3.0',
+  url = 'https://github.com/elbisna/elbisna',
+  download_url = 'https://github.com/elbisna/elbisna/downloads',
+  keywords = ['elbisna', 'ansible'],
+  classifiers = [],
+  packages = setuptools.find_packages(),
+)
