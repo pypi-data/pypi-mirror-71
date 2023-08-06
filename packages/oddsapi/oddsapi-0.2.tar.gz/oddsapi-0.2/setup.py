3687830a88e65e8a27cb941e4698389cff85a846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:08 +0000

    Add package oddsapi/0.2/oddsapi-0.2.tar.gz

---
+++ packages/oddsapi/oddsapi-0.2.tar.gz/oddsapi-0.2/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+  name = 'oddsapi',        
+  packages = ['oddsapi'],  
+  version = '0.2', 
+  license='MIT',       
+  description = 'Python wrapper around The Odds-Api.', 
+  long_description=readme(),
+  author = 'Artur Saradzhyan',                  
+  author_email = 'saradzhyanartur@gmail.com',    
+  url = 'https://github.com/saradzhyanartur/oddsapi', 
+  download_url = 'https://github.com/saradzhyanartur/oddsapi/archive/v_02.tar.gz',   
+  keywords = ['The Odds-Api', 'The Odds-API', 'Sports', 'Betting', 'Odds'], 
+  install_requires=[        
+          'urllib3==1.25.9',
+          'certifi==2020.4.5.1',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',   
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
