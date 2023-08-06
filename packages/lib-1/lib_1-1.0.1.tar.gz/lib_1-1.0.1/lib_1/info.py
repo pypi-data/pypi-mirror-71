6f8371e17ba02c964a9a79f7ed8415b1013a63a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:20 +0000

    Add package lib-1/1.0.1/lib_1-1.0.1.tar.gz

---
+++ packages/lib-1/lib_1-1.0.1.tar.gz/lib_1-1.0.1/lib_1/info.py
@@ -0,0 +1,13 @@
+def name():
+    print("Surendra Kumawat")
+    return
+def dob():
+    print("21-nov-1997")
+    return
+def addres():
+    print("ramkui,pachar")
+    return
+def email():
+    print("surendrakumawatp@gmail.com")
+    return
+    
