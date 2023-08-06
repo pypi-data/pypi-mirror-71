d5a32bd771f0b78f1e7fcb44f0210340cb17551c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:18 +0000

    Add package distributions-rcm/0.2/distributions_rcm-0.2.tar.gz

---
+++ packages/distributions-rcm/distributions_rcm-0.2.tar.gz/distributions_rcm-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_rcm',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_rcm'],
+      zip_safe=False)
