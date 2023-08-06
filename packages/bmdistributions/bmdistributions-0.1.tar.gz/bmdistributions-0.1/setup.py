e173ed9e07180da66ad9a39e9554ec372dae5201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:19 +0000

    Add package bmdistributions/0.1/bmdistributions-0.1.tar.gz

---
+++ packages/bmdistributions/bmdistributions-0.1.tar.gz/bmdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bmdistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['bmdistributions'],
+      zip_safe=False)
