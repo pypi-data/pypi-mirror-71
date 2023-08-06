ccdda9817ad9d35338e195e08f8aaf6cf1110da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:27 +0000

    Add package datamart-profiler/0.6.2/datamart_profiler-0.6.2.tar.gz

---
+++ packages/datamart-profiler/datamart_profiler-0.6.2.tar.gz/datamart_profiler-0.6.2/setup.py
@@ -0,0 +1,46 @@
+import io
+import os
+from setuptools import setup
+
+
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+
+req = [
+    'lazo-index-service==0.5.1',
+    'numpy',
+    'pandas',
+    'prometheus_client',
+    'python-dateutil',
+    'scikit-learn',
+    'regex',
+    'requests',
+    'datamart-geo==0.1',
+]
+with io.open('README.rst', encoding='utf-8') as fp:
+    description = fp.read()
+setup(name='datamart_profiler',
+      version='0.6.2',
+      packages=['datamart_profiler'],
+      install_requires=req,
+      description="Data profiling library for Datamart",
+      author="Remi Rampin",
+      author_email='remi.rampin@nyu.edu',
+      maintainer="Remi Rampin",
+      maintainer_email='remi.rampin@nyu.edu',
+      url='https://gitlab.com/ViDA-NYU/datamart/datamart',
+      project_urls={
+          'Homepage': 'https://gitlab.com/ViDA-NYU/datamart/datamart',
+          'Source': 'https://gitlab.com/ViDA-NYU/datamart/datamart',
+          'Tracker': 'https://gitlab.com/ViDA-NYU/datamart/datamart/issues',
+      },
+      long_description=description,
+      license='BSD-3-Clause',
+      keywords=['datamart'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Scientific/Engineering :: Information Analysis'])
