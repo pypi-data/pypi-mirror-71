9fa6c980c9ddee3828763464948159013fff5afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:30 +0000

    Add package xinabox-CORE/0.0.6/xinabox-CORE-0.0.6.tar.gz

---
+++ packages/xinabox-CORE/xinabox-CORE-0.0.6.tar.gz/xinabox-CORE-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xinabox-CORE",
+    version="0.0.6",
+    author="Luqmaan Baboo",
+    author_email="luqmaanbaboo@gmail.com",
+    description="I2C Core for CC03/CS11/CW03, CW02, CW01, Raspberry Pi and Microbit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xinabox/Python-CORE",
+    py_modules=["xCore",],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
