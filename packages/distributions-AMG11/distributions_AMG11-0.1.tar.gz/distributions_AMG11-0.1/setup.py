66654623f9176c504a9fafa9b9d4a33d597f3773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:49 +0000

    Add package distributions-AMG11/0.1/distributions_AMG11-0.1.tar.gz

---
+++ packages/distributions-AMG11/distributions_AMG11-0.1.tar.gz/distributions_AMG11-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_AMG11',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_AMG11'],
+      zip_safe=False)
