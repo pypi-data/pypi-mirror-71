b5bfc6c5866f221a3178c549e485eb93e163189e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:35 +0000

    Add package imagehat/0.0.12/imagehat-0.0.12.tar.gz

---
+++ packages/imagehat/imagehat-0.0.12.tar.gz/imagehat-0.0.12/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='imagehat',
+    version='0.0.12',
+    author='Richard Waschhauser',
+    keywords='Raspberry Pi, USB',
+    description='Image HAT',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: System :: Hardware',
+    ],
+    python_requires='>=3.6',
+)
