be3fc4c6bfc882abc45e265cee1816a9e089386a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:18 +0000

    Add package dummy-autoupdate/0.2.2/dummy_autoupdate-0.2.2.tar.gz

---
+++ packages/dummy-autoupdate/dummy_autoupdate-0.2.2.tar.gz/dummy_autoupdate-0.2.2/dummy_autoupdate/__init__.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+
+from PyQt5.QtWidgets import QApplication
+
+from dummy_autoupdate.main_window import MainWindow
+from dummy_autoupdate.__version import __version__
+
+
+def run():
+    import sys
+    import logging
+
+    log_file = 'dummy_autoupdate_log.txt'
+    logging.basicConfig(level=logging.DEBUG, filename=log_file)
+    print((Path(__file__).parent / log_file).resolve())
+
+    qapp = QApplication([])
+    window = MainWindow(__version__)
+    reboot_exit = window.EXIT_CODE_REBOOT
+    exit_code = qapp.exec_()
+
+    if exit_code == reboot_exit:
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+
+
+if __name__ == '__main__':
+    run()
