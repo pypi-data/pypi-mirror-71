4fc4ca0c207f87167d3aeb77dd14a0382e5acb92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:24 +0000

    Add package urepr/0.0.1/urepr-0.0.1.tar.gz

---
+++ packages/urepr/urepr-0.0.1.tar.gz/urepr-0.0.1/README.md
@@ -0,0 +1,2 @@
+# urepr
+Uncertainty representation
---
+++ packages/urepr/urepr-0.0.1.tar.gz/urepr-0.0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(name='urepr',
+      version='0.0.1',
+      description='Uncertainty representation',
+      author='João Faria',
+      author_email='joao.faria@astro.up.pt',
+      license='MIT',
+      url='https://github.com/j-faria/urepr',
+      packages=['urepr'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ],
+      )
