27fd3664b0e9949abf8e778ed3e562ae05f7ca65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:02 +0000

    Add package ak7-distributions/0.1/ak7_distributions-0.1.tar.gz

---
+++ packages/ak7-distributions/ak7_distributions-0.1.tar.gz/ak7_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ak7_distributions',
+      version='0.1',
+      description='A test case of Gaussian distributions',
+      packages=['distributions'],
+      author='Aashis Kumar',
+      author_email= 'aashismanav@gmail.com',
+      zip_safe=False)
