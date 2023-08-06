ba270b0955ac71037d8bb23c38187012f4bf71f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:53 +0000

    Add package g-b-probability/0.1/g_b_probability-0.1.tar.gz

---
+++ packages/g-b-probability/g_b_probability-0.1.tar.gz/g_b_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='g_b_probability',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['g_b_probability'],
+      author = 'Apu Chandraw',
+      author_email = 'apu3660@gmail.com', 
+      zip_safe=False)
