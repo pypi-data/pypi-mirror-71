66290156948a939a4275db97d81d905b21f15d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:15 +0000

    Add package Exh/0.6/Exh-0.6.tar.gz

---
+++ packages/Exh/Exh-0.6.tar.gz/Exh-0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name             = "Exh",
+      version          = "0.6",
+      description      = "Computes innocent exclusion/inclusions exhaustivity",
+      url              = "http://github.com/KenyC/Exh",
+      author           = "Keny Chatain",
+      author_email     = "kchatain@mit.edu",
+      license          = "MIT",
+      packages         = ["exh", 
+                          "exh.model", 
+                          "exh.utils", 
+                          "exh.prop", 
+                          "exh.fol"],
+      install_requires = [
+          "numpy",
+          "IPython"
+      ],
+      zip_safe         = True)
