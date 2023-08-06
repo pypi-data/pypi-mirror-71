afcbc1fc6f70337da57fcf85ed923cde6929deb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:44 +0000

    Add package distributions-aws-foundation/0.1/distributions_aws_foundation-0.1.tar.gz

---
+++ packages/distributions-aws-foundation/distributions_aws_foundation-0.1.tar.gz/distributions_aws_foundation-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_aws_foundation',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_aws_foundation'],
+      zip_safe=False)
