8b83144c5b7d1de1f7124449b719edb6329e6384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:52 +0000

    Add package admt-distributions/1.1/admt_distributions-1.1.tar.gz

---
+++ packages/admt-distributions/admt_distributions-1.1.tar.gz/admt_distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='admt_distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['admt_distributions'],
+      author = 'Ayush Deval',
+      author_email = 'ayush.deval2017@gmail.com',
+      zip_safe=False)
