c02ea851d1f9b0c10ae13a7d18ed071dfd5c1dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:19 +0000

    Add package valid-typing/0.0.1/valid_typing-0.0.1.tar.gz

---
+++ packages/valid-typing/valid_typing-0.0.1.tar.gz/valid_typing-0.0.1/valid_typing/check_type.py
@@ -0,0 +1,25 @@
+import typing
+
+
+def check_type(actual, definition):
+    if hasattr(definition, "__origin__") and definition.__origin__ == list:
+        # The only valid case now is that the actual type is also a list, therefore test both types of list
+        if not hasattr(actual, "__origin__") or actual.__origin__ != list:
+            return False
+
+        # The definition accepts any type
+        if not len(definition.__args__):
+            return True
+
+        # Both have list element definitions, so check their equality
+        return (
+            len(definition.__args__)
+            and check_type(actual.__args__[0], definition.__args__[0])
+        )
+
+    if hasattr(definition, "__origin__") and definition.__origin__ == typing.Union:
+        if hasattr(actual, "__origin__") and actual.__origin__ == typing.Union:
+            return all(check_type(arg, definition) for arg in actual.__args__)
+        return any(check_type(actual, arg) for arg in definition.__args__)
+
+    return actual == definition
