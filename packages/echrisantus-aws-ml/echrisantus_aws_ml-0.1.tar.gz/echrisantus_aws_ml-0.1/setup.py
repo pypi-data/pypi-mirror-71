83a8a6e87f7b903810454aa3e90ff8deca6a9a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:02 +0000

    Add package echrisantus-aws-ml/0.1/echrisantus_aws_ml-0.1.tar.gz

---
+++ packages/echrisantus-aws-ml/echrisantus_aws_ml-0.1.tar.gz/echrisantus_aws_ml-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='echrisantus_aws_ml',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['echrisantus_aws_ml'],
+      author='Chrisantus Eze',
+      author_email='ezechris30@gmail.com',
+      zip_safe=False)
