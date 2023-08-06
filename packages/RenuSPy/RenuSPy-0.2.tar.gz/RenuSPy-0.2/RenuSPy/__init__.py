8430924e33a31068460a4c56fd49972551a1426b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:47 +0000

    Add package RenuSPy/0.2/RenuSPy-0.2.tar.gz

---
+++ packages/RenuSPy/RenuSPy-0.2.tar.gz/RenuSPy-0.2/RenuSPy/__init__.py
@@ -0,0 +1,8 @@
+def add(x,y):
+  print("This is  function to find the product of two two numbers")
+  return(x*y)
+
+
+def sub(x,y):
+  print("This function would find the difference of two numbers")
+  return(x-y)
