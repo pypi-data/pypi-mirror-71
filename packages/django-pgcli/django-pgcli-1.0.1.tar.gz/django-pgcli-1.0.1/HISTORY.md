de44a2e7d54bc0d37fdcf75b5a62f6c588c80447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:55 +0000

    Add package django-pgcli/1.0.1/django-pgcli-1.0.1.tar.gz

---
+++ packages/django-pgcli/django-pgcli-1.0.1.tar.gz/django-pgcli-1.0.1/HISTORY.md
@@ -0,0 +1,18 @@
+# History
+
+## 1.0.1 (2020-06-21)
+
+* Rebuild wheel
+
+## 1.0.0 (2020-06-20)
+
+* Upgrades to support Django LTS and above
+* Limits support to Python3
+
+## 0.0.2 (2015-04-26)
+
+* Fixed typo in README.rst
+
+## 0.0.1 (2015-04-25)
+
+* Initial release.
---
+++ packages/django-pgcli/django-pgcli-1.0.1.tar.gz/django-pgcli-1.0.1/django_pgcli/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Ash Christopher'
+__email__ = 'ash.christopher@gmail.com'
+__version__ = '1.0.1'
+
+
+from django.db.backends.postgresql import base
+from django.db.backends.postgresql.client import DatabaseClient
+
+
+class PgCLIDatabaseClient(DatabaseClient):
+    executable_name = 'pgcli'
+    
+    def runshell(self):
+        PgCLIDatabaseClient.runshell_db(self.connection.get_connection_params())
+
+base.DatabaseWrapper.__old_database_client_class = base.DatabaseClient
+base.DatabaseWrapper.client_class = PgCLIDatabaseClient
