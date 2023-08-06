93683c9f1a46842a46c680071ba0b370481a2bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:43 +0000

    Add package iterwrapper/0.1.3/iterwrapper-0.1.3-py3-none-any.whl

---
+++ packages/iterwrapper/iterwrapper-0.1.3-py3-none-any.whl/iterwrapper/data/mutated.py
@@ -0,0 +1,16 @@
+class grouped_dict(dict):
+    '''
+    A grouped dict.
+
+    Mostly similar to a python built-in dict, but will append values
+    instead if a key is already given.
+    '''
+
+    def __setitem__(self, item, value):
+        s = super()
+        if item not in self:
+            s.__setitem__(item, [])
+        s.__getitem__(item).append(value)
+
+    def additem(self, key, value):
+        self[key] = value
