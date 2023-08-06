67c9daded1b41f5f5aa6fce070a06a3be46fa604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:38 +0000

    Add package flask-cloud-services/0.0.1/flask_cloud_services-0.0.1-py3-none-any.whl

---
+++ packages/flask-cloud-services/flask_cloud_services-0.0.1-py3-none-any.whl/flask_cloud_services/config.py
@@ -0,0 +1,8 @@
+import os
+
+PROVIDER = os.environ.get('CLOUD_SERVICES_PROVIDER')
+REGION = os.environ.get('CLOUD_SERVICES_REGION')
+
+# Notification Bus services
+NOTIFICATION = 'Notification'
+SUSCRIPTION = 'Subscription'
