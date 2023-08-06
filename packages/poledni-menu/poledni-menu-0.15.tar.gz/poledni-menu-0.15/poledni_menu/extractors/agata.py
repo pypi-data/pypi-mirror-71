cbc19078fd620b945a05acee02a5b097bc29f2d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:19 +0000

    Add package poledni-menu/0.15/poledni-menu-0.15.tar.gz

---
+++ packages/poledni-menu/poledni-menu-0.15.tar.gz/poledni-menu-0.15/poledni_menu/extractors/agata.py
@@ -0,0 +1,36 @@
+from ..utils import killwhitespace, parsed_html_doc
+
+_BASE_URL = "https://agata.suz.cvut.cz/jidelnicky/index.php?clPodsystem={}"
+
+
+def get_url(place_id=None):
+    return _BASE_URL.format(place_id or "5")
+
+
+def get_name(place_id=None):
+    doc = parsed_html_doc(get_url(place_id))
+    h3 = doc.find('//h3')
+    if h3 is not None:
+        return h3.text_content()
+    else:
+        return "Neznámá menza"
+
+
+def get_menu(place_id=None):
+    doc = parsed_html_doc(get_url(place_id))
+    for meal in doc.findall(
+        '//div[@id="jidelnicek"]/div[@class="data"]'
+        '/table//tr',
+    ):
+        tds = meal.findall('td')
+        if len(tds) == 0:
+            continue
+        name = tds[1].text
+        price = tds[5].text
+        place = tds[6].text_content()
+        name, price, place = (killwhitespace(s) for s in (name, price, place))
+        if name is "" or name is ".":
+            continue
+        if place:
+            name = "{} ({})".format(name, place)
+        yield (name, price)
