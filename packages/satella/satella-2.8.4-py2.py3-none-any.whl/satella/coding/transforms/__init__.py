445910d54b5b0d8360622e7d29f7247c91927784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:42 +0000

    Add package satella/2.8.4/satella-2.8.4-py2.py3-none-any.whl

---
+++ packages/satella/satella-2.8.4-py2.py3-none-any.whl/satella/coding/transforms/__init__.py
@@ -0,0 +1,31 @@
+import collections
+import typing as tp
+
+__all__ = ['stringify']
+
+
+def stringify(obj: tp.Union[tp.Any], stringifier: tp.Callable[[tp.Any], str] = str,
+              recursively: bool = False) -> tp.Dict[str, str]:
+    """
+    Stringify all object:
+
+    ie. if a dict, put every item and key (if a dict is given) through stringify.
+        if a list, put every item through stringify
+        else just call stringify on it.
+
+        Note that if you use recursively, then dicts and lists are allowed to be valid elements of the returned
+        representation!
+
+    :param obj: a list or a dict
+    :param stringifier: function that accepts any arguments and returns a string representation
+    :param recursively: whether to recursively stringify elements, ie. stringify will be called on all the children
+    :return: stringified object
+    """
+    if isinstance(obj, collections.abc.Mapping):
+        make_str = (lambda obj2: stringify(obj2, stringifier, True)) if recursively else stringifier
+        return {make_str(k): make_str(v) for k, v in obj.items()}
+    elif isinstance(obj, collections.abc.Sequence):
+        make_str = (lambda obj2: stringify(obj2, stringifier, True)) if recursively else stringifier
+        return [make_str(v) for v in obj]
+    else:
+        return stringifier(obj)
