a8c16bae50ba9f20839f8b85f0142d360012e40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:30 +0000

    Add package jeremy-portal-gun/0.2.0/jeremy-portal-gun-0.2.0.tar.gz

---
+++ packages/jeremy-portal-gun/jeremy-portal-gun-0.2.0.tar.gz/jeremy-portal-gun-0.2.0/jeremy_portal_gun/main.py
@@ -0,0 +1,28 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    Awesome Portal Gun
+    :return:
+    """
+
+
+@app.command()
+def shoot():
+    """SHOOT GUN"""
+    typer.echo("shoot")
+
+
+@app.command()
+def load():
+    "Load the portal gun"
+    typer.echo("loading the protal gunnnn")
+
+
+@app.command()
+def sowell():
+    typer.echo("IS GOAT")
