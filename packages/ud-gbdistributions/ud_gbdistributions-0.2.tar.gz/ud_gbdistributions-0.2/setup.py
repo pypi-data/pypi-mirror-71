b568e0028cb7f4b705786221fa5bd7d714a7e328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:17 +0000

    Add package ud-gbdistributions/0.2/ud_gbdistributions-0.2.tar.gz

---
+++ packages/ud-gbdistributions/ud_gbdistributions-0.2.tar.gz/ud_gbdistributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ud_gbdistributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
