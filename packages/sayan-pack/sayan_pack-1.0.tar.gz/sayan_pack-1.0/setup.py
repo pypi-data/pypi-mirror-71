9b1821e7a4c9100954793fd74692772785f75015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:23 +0000

    Add package sayan-pack/1.0/sayan_pack-1.0.tar.gz

---
+++ packages/sayan-pack/sayan_pack-1.0.tar.gz/sayan_pack-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sayan_pack',
+      version='1.0',
+      description='Regular',
+      packages=['sayan_pack'],
+      author = 'Osayanhu Imoisili',
+      author_email = 'jerryosaimo@gmail.com',
+      zip_safe=False)
