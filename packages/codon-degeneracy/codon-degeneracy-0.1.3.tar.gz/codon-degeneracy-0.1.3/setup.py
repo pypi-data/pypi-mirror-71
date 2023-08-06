34d55fdb92a85a7e5e8039f89d425f561b33c68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:30 +0000

    Add package codon-degeneracy/0.1.3/codon-degeneracy-0.1.3.tar.gz

---
+++ packages/codon-degeneracy/codon-degeneracy-0.1.3.tar.gz/codon-degeneracy-0.1.3/setup.py
@@ -0,0 +1,59 @@
+import os
+from setuptools import setup, find_packages, Command
+
+
+__version__ = None
+exec(open('version.py').read())
+
+
+class CleanCommand(Command):
+    """
+    Custom clean command to tidy up the project root.
+    """
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system(
+            'rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info ./htmlcov')
+
+
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+
+setup(
+    author='Nick Noel Machnik',
+    author_email='nick.machnik@gmail.com',
+    name='codon-degeneracy',
+    version=__version__,
+    description="""
+        Routines for the extraction of degenerate sides and estimation of numbers neutral substitutions from sequences and alignments.""",
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nickmachnik/codon-degeneracy.git',
+    setup_requires=[
+        'setuptools>=18.0',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.8.0',
+        'scikit-bio',
+        'biopython'
+    ],
+    # scripts=['bin/chess'],
+    cmdclass={
+        'clean': CleanCommand
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
