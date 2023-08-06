a635a0add4d086beabd437b8a30221c756f0e589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:30 +0000

    Add package lib-1/1.0.2/lib_1-1.0.2.tar.gz

---
+++ packages/lib-1/lib_1-1.0.2.tar.gz/lib_1-1.0.2/README.md
@@ -0,0 +1,8 @@
+# info
+
+A Python package to see author details.
+
+## Usage
+
+you know about author
+
---
+++ packages/lib-1/lib_1-1.0.2.tar.gz/lib_1-1.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="lib_1",
+    version="1.0.2",
+    description="information about author",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SurendraKumawat23",
+    author="Surendra Kumawat",
+    author_email="surendrakumawatp@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["lib_1"],
+    include_package_data=True,
+    #install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "lib=lib_1.info:name",
+        ]
+    },
+)
