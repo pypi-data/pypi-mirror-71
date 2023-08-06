bdea50f2670535df933807f6198298ddf57d134e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:42 +0000

    Add package distribution-simba/0.1/distribution_simba-0.1.tar.gz

---
+++ packages/distribution-simba/distribution_simba-0.1.tar.gz/distribution_simba-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distribution_simba',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distribution_simba'],
+      author = 'Sudhanshu Kumar',
+      zip_safe=False)
