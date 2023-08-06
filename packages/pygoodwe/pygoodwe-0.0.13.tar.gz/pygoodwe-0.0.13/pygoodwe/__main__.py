84f87e0a4aa7f0bf3d448453e54e2e5f7c8b126a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:24 +0000

    Add package pygoodwe/0.0.13/pygoodwe-0.0.13.tar.gz

---
+++ packages/pygoodwe/pygoodwe-0.0.13.tar.gz/pygoodwe-0.0.13/pygoodwe/__main__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+import logging
+import argparse
+import locale
+import time
+import pygoodwe
+from datetime import datetime
+from astral import Astral
+
+__version__ = "0.0.1"
+__author__ = "James Hodgkinson"
+__copyright__ = "Copyright 2019-, James Hodgkinson"
+__license__ = "MIT"
+__email__ = "james@terminaloutcomes.com"
+
+last_eday_kwh = 0
+
+if __name__ == "__main__":
+    print("Sorry, nothing to see here.")
