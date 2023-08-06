b67d3d77c44be8a7e0981a0f76a28a7d108a5b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:40 +0000

    Add package django-persistent-settings/0.2.0/django-persistent-settings-0.2.0.tar.gz

---
+++ packages/django-persistent-settings/django-persistent-settings-0.2.0.tar.gz/django-persistent-settings-0.2.0/persistent_settings/models.py
@@ -0,0 +1,66 @@
+import logging
+import pickle
+
+from django.contrib.auth import get_user_model
+from django.db import models
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class _VariableManager(models.Manager):
+    """
+    A special manager for `Variable` model overriding default Django manager.
+    """
+
+    def __getitem__(self, key: str):
+        """
+        Used for PersistentSettingsMiddleware.
+        """
+        return self.get(name=key).value
+
+
+class _PickleField(models.BinaryField):
+    """
+    A field for marshalling with pickle.
+    """
+
+    def from_db_value(self, value, *args, **kwargs):  # pragma: no cover
+        # kwargs has a key called "context" in dj1.11
+        # kept for backwards compatibility
+        if value is None:
+            return None
+
+        return pickle.loads(value)
+
+    def to_python(self, value):  # pragma: no cover
+        if isinstance(value, bytes) or value is None:
+            return value
+
+        return pickle.loads(value)
+
+    def get_prep_value(self, value):  # pragma: no cover
+        if value is None:
+            return None
+
+        return pickle.dumps(value)
+
+
+class Variable(models.Model):
+    """
+    A single setting.
+    """
+
+    name = models.SlugField(max_length=64)
+    value = _PickleField(null=True)
+    user = models.ForeignKey(
+        get_user_model(), models.CASCADE, "variables", null=True, default=None
+    )
+
+    objects = _VariableManager()
+
+    class Meta:
+        ordering = ("name",)
+        unique_together = ("name", "user")
+
+    def __str__(self):
+        return self.name
