92285436b9437afaa294c7105a517c51feb0e7fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:13 +0000

    Add package sphinx-lastupdate/1.0.0/sphinx_lastupdate-1.0.0.tar.gz

---
+++ packages/sphinx-lastupdate/sphinx_lastupdate-1.0.0.tar.gz/sphinx_lastupdate-1.0.0/README.md
@@ -0,0 +1,4 @@
+# sphinx-lastupdate
+
+A plugin adapted from the [sphinx-dev community](https://groups.google.com/forum/#!topic/sphinx-dev/6G8TWtIVN14)
+that pulls the last updated date for each file in `YYYY-mm-dd` format (as opposed to a single date for the entire project).
---
+++ packages/sphinx-lastupdate/sphinx_lastupdate-1.0.0.tar.gz/sphinx_lastupdate-1.0.0/sphinx-lastupdate/lastupdate.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Adapted code provided by the sphinx-dev community [0] to produce a plugin to
+# provide the last updated date for each file in YYYY-mm-dd format instead of a
+# single date for the entire project.
+# [0]: https://groups.google.com/forum/#!topic/sphinx-dev/6G8TWtIVN14
+
+from datetime import datetime
+import os
+import subprocess
+
+
+def _get_last_updated(app, pagename):
+    """
+    Pull the date from a git log call that represents the date a file was last
+    updated.
+    """
+    last_updated = None
+    src_file = app.builder.env.doc2path(pagename)
+    if os.path.exists(src_file):
+        try:
+            last_updated_t = subprocess.check_output(
+            [
+                'git', 'log', '-n1', '--format=%ad', '--date=unix',
+                '--', src_file,
+            ]
+            ).decode('utf-8').strip()
+            last_updated = datetime.fromtimestamp(int(last_updated_t))
+        except (ValueError, subprocess.CalledProcessError):
+            pass
+    return last_updated
+
+
+def html_page_context(app, pagename, templatename, context, doctree):
+    context['last_updated'] = _get_last_updated(app, pagename)
+
+
+def setup(app):
+    app.connect('html-page-context', html_page_context)
+    return {
+        'version': '1.0.0',
+        'parallel_read_safe': True,
+        'parallel_write_safe': True
+    }
