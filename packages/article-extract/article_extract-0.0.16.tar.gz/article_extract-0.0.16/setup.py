3c80f39df050ddc1b605923c1a4aa0e671ef1188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:08 +0000

    Add package article-extract/0.0.16/article_extract-0.0.16.tar.gz

---
+++ packages/article-extract/article_extract-0.0.16.tar.gz/article_extract-0.0.16/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="article_extract",
+    version="0.0.16",
+    author="cuiyonghua",
+    author_email="cui_yonghua6@163.com",
+    description="Article extractor can extract title, time, author, source, article content, etc. according to the specified article URL.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Article extractor",
+    url="https://github.com/cuiyonghua6/article_eatract",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
