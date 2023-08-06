74ad76f48a35f8e00cf25891739b642bdc7cb25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:31 +0000

    Add package pyAB/0.0.2/pyAB-0.0.2.tar.gz

---
+++ packages/pyAB/pyAB-0.0.2.tar.gz/pyAB-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name='pyAB',
+      version='0.0.2',
+      description='A/B Testing using Bayesian & Frequentist Statistics',
+      url='https://github.com/AdiVarma27/pyAB',
+      author='Aditya Varma Kalidindi',
+      author_email='kadityavarma27@gmail.com',
+      packages=find_packages(),
+      install_requires = [
+          'numpy',
+          'matplotlib',
+          'seaborn',
+          'scipy',
+          'statsmodels',
+      ],
+      )
