7ac5192fa391b88f9d026820649e270353dfc23d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:34 +0000

    Add package aihelper/0.1.29/aihelper-0.1.29-py3-none-any.whl

---
+++ packages/aihelper/aihelper-0.1.29-py3-none-any.whl/aihelper/runner.py
@@ -0,0 +1,12 @@
+path = r'C:\Users\mjaquier\PycharmProjects\aithermal\tests'
+
+import aihelper.aifile as afile
+
+def main():
+    allthedata = afile.topic_directories(path)
+    print('h')
+
+
+
+if __name__ == '__main__':
+    main()
