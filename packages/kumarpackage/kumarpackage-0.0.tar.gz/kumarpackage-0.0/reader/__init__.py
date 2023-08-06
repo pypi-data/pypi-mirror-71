eed70d39032298c3476427236954048da990ee58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package kumarpackage/0.0/kumarpackage-0.0.tar.gz

---
+++ packages/kumarpackage/kumarpackage-0.0.tar.gz/kumarpackage-0.0/reader/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 11:31:15 2020
+
+@author: kumar Awanish
+"""
+
+from .model import (  
+      training_features,
+      training_targets,
+      prediction_features,
+      model_training,
+      model_prediction
+)
+#del 4cast_package
+
---
+++ packages/kumarpackage/kumarpackage-0.0.tar.gz/kumarpackage-0.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 11:58:30 2020
+
+@author: 49176
+"""
+
+import setuptools
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Kumar Awanish",
+    author_email="awanish00@gmail.com",
+    name='kumarpackage',
+    license="MIT",
+    description='load csv.',
+    version='v0.0',
+    long_description=README,
+    packages=["reader"],
+    #setuptools.find_packages(where='src'),
+    python_requires=">=3.5",
+    include_package_data=True,
+    install_requires=['pandas','scikit-learn'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
