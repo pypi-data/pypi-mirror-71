6aafb5aa8ba1e2dd34bd872207ac3ad0d561bcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:59 +0000

    Add package djsommo/0.1.0/djsommo-0.1.0.tar.gz

---
+++ packages/djsommo/djsommo-0.1.0.tar.gz/djsommo-0.1.0/djsommo/__init__.py
@@ -0,0 +1,25 @@
+__version__ = VERSION = (0, 1, 0)
+
+import warnings
+
+from django.db.models import options
+
+
+# Django doesn't support adding third-party fields to model class Meta.
+# So lets also allow them to safely exist on models even if app is missing
+class SafeOptionalMetaMeta(type):
+
+    def __new__(cls, name, bases, dct):
+         for name in list(dct.keys()):
+             if name.startswith('__'):
+                 continue
+             if name not in options.DEFAULT_NAMES:
+                 warnings.warn('Discarding unsupported {}'.format(name),
+                               stacklevel=2)
+                 del dct[name]
+
+         return super().__new__(cls, name, bases, dct)
+
+
+class SafeOptionalMeta(metaclass=SafeOptionalMetaMeta):
+      pass
