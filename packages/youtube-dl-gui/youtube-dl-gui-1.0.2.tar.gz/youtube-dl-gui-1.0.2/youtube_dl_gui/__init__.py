bacc8e8682d1b7d58429ebe113cd70f6e61287f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:19 +0000

    Add package youtube-dl-gui/1.0.2/youtube-dl-gui-1.0.2.tar.gz

---
+++ packages/youtube-dl-gui/youtube-dl-gui-1.0.2.tar.gz/youtube-dl-gui-1.0.2/youtube_dl_gui/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Vadym Stupakov"
+__email__ = "vadim.stupakov@gmail.com"
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent))
