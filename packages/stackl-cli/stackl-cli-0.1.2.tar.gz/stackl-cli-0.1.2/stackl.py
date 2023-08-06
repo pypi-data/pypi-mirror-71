6cd05d1064f7c8ba19a840eb471cea88637f01cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:27 +0000

    Add package stackl-cli/0.1.2/stackl-cli-0.1.2.tar.gz

---
+++ packages/stackl-cli/stackl-cli-0.1.2.tar.gz/stackl-cli-0.1.2/stackl.py
@@ -0,0 +1,23 @@
+import click
+
+from commands.apply import apply
+from commands.connect import connect
+from commands.create import create
+from commands.delete import delete
+from commands.get import get
+from commands.info import info
+from commands.update import update
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(connect)
+cli.add_command(get)
+cli.add_command(info)
+cli.add_command(apply)
+cli.add_command(create)
+cli.add_command(update)
+cli.add_command(delete)
