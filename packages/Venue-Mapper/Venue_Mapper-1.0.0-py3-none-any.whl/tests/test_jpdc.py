a42bc13d2085ba00335a6c2d87c4176381297288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:27 +0000

    Add package Venue-Mapper/1.0.0/Venue_Mapper-1.0.0-py3-none-any.whl

---
+++ packages/Venue-Mapper/Venue_Mapper-1.0.0-py3-none-any.whl/tests/test_jpdc.py
@@ -0,0 +1,15 @@
+from tests.BaseRunner import BaseRunner
+
+
+class TestJPDC(BaseRunner):
+
+    def setUp(self):
+        self.expected_venue = "JPDC"
+
+        self.correct_strings = {
+            "Journal of Parallel and Distributed Computing",  # Google Scholar
+            "J. Parallel Distrib. Comput.",  # DBLP
+        }
+
+        self.wrong_strings = {
+        }
