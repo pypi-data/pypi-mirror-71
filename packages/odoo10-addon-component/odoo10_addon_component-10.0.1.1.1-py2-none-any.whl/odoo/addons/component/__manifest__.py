0a0d6c7c917aaa6cf23d07a66aae562daa7da8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:23 +0000

    Add package odoo10-addon-component/10.0.1.1.1/odoo10_addon_component-10.0.1.1.1-py2-none-any.whl

---
+++ packages/odoo10-addon-component/odoo10_addon_component-10.0.1.1.1-py2-none-any.whl/odoo/addons/component/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+
+{'name': 'Components',
+ 'version': '10.0.1.1.1',
+ 'author': 'Camptocamp,'
+           'Odoo Community Association (OCA)',
+ 'website': 'https://www.camptocamp.com',
+ 'license': 'LGPL-3',
+ 'category': 'Generic Modules',
+ 'depends': ['base',
+             ],
+ 'external_dependencies': {
+     'python': ['cachetools'],
+ },
+ 'data': [],
+ 'installable': True,
+ }
