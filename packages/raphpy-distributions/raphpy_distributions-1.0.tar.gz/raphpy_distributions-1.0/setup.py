ea3bdba3393f89257e76e5addb5f23185f6ca6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:13 +0000

    Add package raphpy-distributions/1.0/raphpy_distributions-1.0.tar.gz

---
+++ packages/raphpy-distributions/raphpy_distributions-1.0.tar.gz/raphpy_distributions-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='raphpy_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=find_packages(),
+      install_requires=["matplotlib"],
+      author='Raphael Okeibunor',
+      github='https://github.com/RaphaelNagato',
+      zip_safe=False)
