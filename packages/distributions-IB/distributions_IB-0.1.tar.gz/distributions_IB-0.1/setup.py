8f2a6a6bc37b27e05d77ecae9ae7d7dd23c0803c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:40 +0000

    Add package distributions-IB/0.1/distributions_IB-0.1.tar.gz

---
+++ packages/distributions-IB/distributions_IB-0.1.tar.gz/distributions_IB-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_IB',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_IB'],
+      zip_safe=False)
