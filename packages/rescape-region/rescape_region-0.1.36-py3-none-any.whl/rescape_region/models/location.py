54fb541982ea7dc7255f9927c06488d96a5fe964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:57 +0000

    Add package rescape-region/0.1.36/rescape_region-0.1.36-py3-none-any.whl

---
+++ packages/rescape-region/rescape_region-0.1.36-py3-none-any.whl/rescape_region/models/location.py
@@ -0,0 +1,27 @@
+import reversion
+from django.contrib.postgres.fields import JSONField
+from django.db.models import (
+    CharField,
+    DateTimeField)
+from safedelete.models import SafeDeleteModel
+from rescape_region.model_helpers import region_data_default, feature_collection_default
+from rescape_region.models.revision_mixin import RevisionMixin
+
+@reversion.register()
+class Location(SafeDeleteModel, RevisionMixin):
+    """
+        Models a geospatial location
+    """
+
+    # Unique human readable identifier for URLs, etc
+    key = CharField(max_length=20, unique=True, null=False)
+    name = CharField(max_length=50, null=False)
+    geojson = JSONField(null=False, default=feature_collection_default)
+    data = JSONField(null=False, default=region_data_default)
+
+    class Meta:
+        app_label = "rescape_region"
+
+    def __str__(self):
+        return self.name
+
