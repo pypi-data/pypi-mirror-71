e327bac675e814d53f574554cd8cf3f222a5df05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:04 +0000

    Add package mlaws-distributions/0.1/mlaws_distributions-0.1.tar.gz

---
+++ packages/mlaws-distributions/mlaws_distributions-0.1.tar.gz/mlaws_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mlaws_distributions',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['mlaws_distributions'],
+      author='Mohamed Zeglam',
+      zip_safe=False)
