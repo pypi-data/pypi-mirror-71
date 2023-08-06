0923d3dff976aecebb233fab0669482791514e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:01 +0000

    Add package sfl-distributions/0.1/sfl_distributions-0.1.tar.gz

---
+++ packages/sfl-distributions/sfl_distributions-0.1.tar.gz/sfl_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sfl_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sfl_distributions'],
+      author='Sergio Falcón',
+      zip_safe=False)
