67e38006f5f57dbae23d3e29d6e3735278a251c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:44 +0000

    Add package androidtv/0.0.44/androidtv-0.0.44.tar.gz

---
+++ packages/androidtv/androidtv-0.0.44.tar.gz/androidtv-0.0.44/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='androidtv',
+    version='0.0.44',
+    description='Communicate with an Android TV or Fire TV device via ADB over a network.',
+    long_description=readme,
+    keywords=['adb', 'android', 'androidtv', 'firetv'],
+    url='https://github.com/JeffLIrion/python-androidtv/',
+    license='MIT',
+    author='Jeff Irion',
+    author_email='jefflirion@users.noreply.github.com',
+    packages=['androidtv'],
+    install_requires=['adb-shell>=0.1.3,<=0.1.5', 'pure-python-adb>=0.2.2.dev0'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+    ],
+    test_suite='tests'
+)
