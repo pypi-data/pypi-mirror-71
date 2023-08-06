653bf642cb0f966e539176d06fef4042ee0d0112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:40 +0000

    Add package scrapy-redis-sentinel/0.2.2/scrapy-redis-sentinel-0.2.2.tar.gz

---
+++ packages/scrapy-redis-sentinel/scrapy-redis-sentinel-0.2.2.tar.gz/scrapy-redis-sentinel-0.2.2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='scrapy-redis-sentinel',
+    description="Redis Cluster for Scrapy.",
+    long_description=open('README.rst', encoding="utf-8").read(),
+    version="0.2.2",
+    author="Shi Tao",
+    author_email='shitao0418@gmail.com',
+    url='https://github.com/crawlmap/scrapy-redis-sentinel.git',
+    packages=find_packages(),
+    install_requires=[
+        'Scrapy',
+        'redis==3.0.1',
+        'six>=1.5.2',
+        'redis-py-cluster==2.0.0'
+    ],
+    license="MIT",
+    keywords=[
+        'scrapy-redis',
+        'scrapy-redis-sentinel',
+        'scrapy-redis-cluster'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
