0ebacced6508f4c8280e1b1db5917890f180e874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:31 +0000

    Add package jupyter-flex/0.6.1/jupyter-flex-0.6.1.tar.gz

---
+++ packages/jupyter-flex/jupyter-flex-0.6.1.tar.gz/jupyter-flex-0.6.1/share/jupyter/voila/templates/flex/static/dist/0.FlexRenderer.js
@@ -0,0 +1,23 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./node_modules/codemirror/mode sync recursive ^\\.\\/.*\\/.*\\.js$":
+/*!************************************************************!*\
+  !*** ./node_modules/codemirror/mode sync ^\.\/.*\/.*\.js$ ***!
+  \************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	var e = new Error("Cannot find module '" + req + "'");
+	e.code = 'MODULE_NOT_FOUND';
+	throw e;
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = "./node_modules/codemirror/mode sync recursive ^\\.\\/.*\\/.*\\.js$";
+
+/***/ })
+
+}]);
+//# sourceMappingURL=0.FlexRenderer.js.map
