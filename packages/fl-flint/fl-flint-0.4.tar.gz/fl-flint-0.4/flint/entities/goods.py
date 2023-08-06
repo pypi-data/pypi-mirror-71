b8dd221a42c6f5dcf4f0173deaefd27a54e206d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:58 +0000

    Add package fl-flint/0.4/fl-flint-0.4.tar.gz

---
+++ packages/fl-flint/fl-flint-0.4.tar.gz/fl-flint-0.4/flint/entities/goods.py
@@ -0,0 +1,109 @@
+"""
+Copyright (C) 2016, 2017, 2020 biqqles.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# todo: Equipment.
+"""
+from typing import Dict, List, Any, Tuple, Optional
+import math
+
+from . import Entity
+from .. import paths
+from ..formats import dll, utf
+
+
+class Good(Entity):
+    """A Good is an abstract representation of something that can be bought or sold. Commodities, equipment and ships
+    all have goods."""
+    item_icon: Optional[str]  # path to icon, relative to DATA
+    price: int  # the default price for this good, pre market multiplier
+    _market: Dict[bool, Tuple]
+
+    def icon_path(self) -> str:
+        """The absolute path to the .3db file containing this item's icon."""
+        return paths.construct_path('DATA', self.item_icon or self.DEFAULT_ICON)
+
+    def icon(self) -> bytes:
+        """This good's icon in TGA format."""
+        return utf.extract(self.icon_path(), 'MIP0')
+
+    def sold_at(self) -> Dict[str, int]:
+        """A dict of bases that sell this good of the form {base_nickname: price}."""
+        return dict(self._market[True])
+
+    def bought_at(self) -> Dict[str, int]:
+        """A dict of bases that buy this good of the form {base_nickname: price}."""
+        return dict(self._market[False])
+
+    DEFAULT_ICON = 'EQUIPMENT/MODELS/COMMODITIES/NN_ICONS/blank.3db'
+
+
+class Commodity(Good):
+    """A Commodity is the representation of a good in tradeable/transportable form."""
+    volume: float  # volume of one unit in ship's cargo bay
+
+
+class Ship(Good):
+    """A star ship with a cargo bay and possibly hardpoints for weapons."""
+    ids_info1: int
+    ids_info2: int
+    ids_info3: int
+    ship_class: int
+    hit_pts: int
+    hold_size: int
+    nanobot_limit: int
+    shield_battery_limit: int
+    steering_torque: Tuple[float, float, float]
+    angular_drag: Tuple[float, float, float]
+    _hull: Dict[str, Any]
+    _package: Dict[str, Any]
+
+    def infocard(self, markup='html') -> str:
+        """I have no idea why the order these are displayed in is not ascending, but anyway."""
+        if markup == 'html':
+            lookup = dll.lookup_as_html
+        elif markup == 'plain':
+            lookup = dll.lookup_as_plain
+        elif markup == 'rdl':
+            lookup = dll.lookup
+        else:
+            raise ValueError
+        return '<p>'.join(map(lookup, (self.ids_info1, self.ids_info, self.ids_info2, self.ids_info3)))
+
+    def type(self) -> str:
+        """The name of the type (class) of this ship."""
+        return self.TYPE_ID_TO_NAME.get(self.ship_class)
+
+    def turn_rate(self) -> float:
+        """Turn rate in degrees per second."""
+        avg = lambda i: sum(i) / len(i)
+        return math.degrees(avg(self.steering_torque) / (avg(self.angular_drag) or math.inf))
+
+    def hardpoints(self) -> List[str]:
+        """A list of this ship's hardpoints todo out of the box"""
+        return self._package['addon']
+
+    TYPE_ID_TO_NAME = {0: 'Light Fighter',
+                       1: 'Heavy Fighter',
+                       2: 'Freighter',
+                       # Discovery:
+                       3: 'Very Heavy Fighter',
+                       4: 'Super Heavy Fighter',
+                       5: 'Bomber',
+                       6: 'Transport',  # more specifically;
+                       7: 'Transport',  # trains
+                       8: 'Transport',  # battle-transports
+                       9: 'Transport',
+                       10: 'Transport',  # liners.
+                       11: 'Gunboat',  # gunships
+                       12: 'Gunboat',
+                       13: 'Cruiser',  # destroyers
+                       14: 'Cruiser',
+                       15: 'Cruiser',  # battlecruisers
+                       16: 'Battleship',
+                       17: 'Battleship',  # carriers
+                       18: 'Battleship',  # flagships
+                       19: 'Freighter'}  # repair ships
---
+++ packages/fl-flint/fl-flint-0.4.tar.gz/fl-flint-0.4/flint/routines.py
@@ -0,0 +1,166 @@
+"""
+Copyright (C) 2016, 2017, 2020 biqqles.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+This file contains routines for parsing specific sets of information
+from the game files. All exported functions return EntitySets.
+"""
+from typing import Dict, List, Tuple
+from collections import defaultdict
+
+from . import paths
+from . import cached
+from .formats import ini
+from .entities import EntitySet
+from .entities import Commodity, Ship
+from .entities import Base, System, Faction
+from .entities import Solar, Object, Jump, BaseSolar, Star, Planet, PlanetaryBase, TradeLaneRing, Wreck, Zone
+from .maps import PosVector, RotVector
+
+
+@cached
+def get_systems() -> EntitySet[System]:
+    """All systems defined in the game files."""
+    systems = ini.parse(paths.inis['universe'])['system']
+
+    return EntitySet(System(**s, ids_name=s.pop('strid_name')) for s in systems if 'file' in s)
+
+
+@cached
+def get_bases() -> EntitySet[Base]:
+    """All bases defined in the game files."""
+    bases = ini.parse(paths.inis['universe'])['base']
+
+    return EntitySet(Base(**b, ids_name=b.pop('strid_name'), ids_info=None, _market=_get_markets()[b['nickname']])
+                     for b in bases)
+
+
+@cached
+def get_commodities() -> EntitySet[Commodity]:
+    """All commodities defined in the game files."""
+    path = paths.construct_path('DATA/EQUIPMENT/select_equip.ini')
+    commodities = ini.parse(path)['commodity']
+
+    result = []
+
+    for c in commodities:
+        good = _get_goods()[c['nickname']]
+        market = _get_markets()[c['nickname']]
+        result.append(Commodity(**c, item_icon=good.get('item_icon'), price=good['price'], _market=market))
+
+    return EntitySet(result)
+
+
+@cached
+def get_factions() -> EntitySet[Faction]:
+    """All groups (i.e. factions) defined in the game files."""
+    groups = ini.parse(paths.inis['initial_world'])['group']
+    return EntitySet(Faction(**g) for g in groups)
+
+
+@cached
+def get_ships() -> EntitySet[Ship]:
+    """All ships defined in the game files."""
+    stats = ini.parse(paths.inis['ships'])['ship']
+    result: List[Ship] = []
+
+    for s in stats:
+        try:
+            hull = _get_goods()[s['nickname']]
+            package = _get_goods()[hull['nickname']]
+            market = _get_markets()[package['nickname']]
+            ship = Ship(**s, item_icon=hull['item_icon'], price=hull['price'], _market=market, _hull=hull,
+                        _package=package)
+        except (KeyError, TypeError):
+            # ship not sold anywhere or lacking infocards todo: how should we handle this?
+            continue
+        result.append(ship)
+
+    return EntitySet(result)
+
+
+@cached
+def get_system_contents(system: System) -> EntitySet[Solar]:
+    """All solars (objects and zones) in a given system."""
+    result = []
+    contents = ini.parse(system.definition_path())
+
+    # categorise objects based on their keys
+    def modify_solar(solar: Dict):
+        """Common dict modifications for all solar types."""
+        solar['_system'] = system
+        solar['pos'] = PosVector(*o['pos'])
+        solar.setdefault('ids_info', None)  # not everything that ought to have ids_info does...
+        return solar
+
+    for o in contents.get('object', []):
+        if 'ids_name' not in o:
+            continue
+        o = modify_solar(o)
+        keys = o.keys()
+        if {'base', 'reputation', 'space_costume'} <= keys:
+            result.append(BaseSolar(**o))
+        elif 'goto' in keys:
+            result.append(Jump(**o))
+        elif 'prev_ring' in keys or 'next_ring' in keys:
+            result.append(TradeLaneRing(**o))
+        elif 'loadout' in keys and 'reputation' not in keys:
+            result.append(Wreck(**o))
+        elif 'star' in keys:
+            result.append(Star(**o))
+        elif 'spin' in keys:
+            result.append(PlanetaryBase(**o) if 'base' in keys else Planet(**o))
+        else:
+            result.append(Object(**o))
+
+    for z in contents.get('zone', []):
+        z = modify_solar(z)
+        if 'ids_name' in z.keys():
+            result.append(Zone(**z))
+
+    return EntitySet(result)
+
+
+@cached
+def _get_goods() -> Dict[str, Dict]:
+    # This is an internal method. A good is anything that can be bought or sold.
+    # get_commodities, get_ships and get_equipment link goods up to objects
+    goods = ini.parse(paths.inis['goods'])['good']  # todo: equipment too
+
+    result = {}
+    for g in goods:
+        # fold ship components into each other (this makes loading ships easier)
+        if g['category'] == 'shiphull':
+            key = 'ship'
+        elif g['category'] == 'ship':
+            key = 'hull'
+        else:
+            key = 'nickname'
+        result[g[key]] = g
+    return result
+
+
+@cached
+def _get_markets() -> Dict[str, Dict[bool, List[Tuple[str, int]]]]:
+    """Result is of the form base/good nickname -> {sold -> (good/base nickname, price at base}}"""
+    market = ini.parse(paths.inis['markets'], fold_values=False)['basegood']
+    goods = _get_goods()
+    result = defaultdict(lambda: {True: [], False: []})
+    for b in market:
+        base = b['base'][0]
+        base_market = b['marketgood']
+        for good, min_rank, min_rep, min_stock, max_stock, depreciate, multiplier, *_ in base_market:
+            if not multiplier:
+                continue
+            try:
+                sold = not (min_stock == 0 or max_stock == 0)
+                price_at_base = int(round(goods[good]['price'] * multiplier))
+            except KeyError:
+                continue  # this is expected for ship packages, which do not appear in market sections
+
+            result[base][sold].append((good, price_at_base))
+            result[good][sold].append((base, price_at_base))
+    return result
