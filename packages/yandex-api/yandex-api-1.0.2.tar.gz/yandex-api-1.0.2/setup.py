a95c398a8f20a0b41d4edee6e39102cb8f31daa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:50 +0000

    Add package yandex-api/1.0.2/yandex-api-1.0.2.tar.gz

---
+++ packages/yandex-api/yandex-api-1.0.2.tar.gz/yandex-api-1.0.2/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+
+setup(
+    name='yandex-api',
+    version='1.0.2',
+    packages=['yandex'],
+    url='https://github.com/akshay-movieverse/yandex-api',
+    license='MIT License',
+    author='Akshay Singh',
+    author_email='akshayrameshwar2018@gmail.com',
+    description='Yandex.disk http api client',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries',
+    ],
+    install_requires = [
+        'requests >= 2.9.1',
+    ],
+)
