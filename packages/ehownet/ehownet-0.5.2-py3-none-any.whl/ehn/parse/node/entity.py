e9945e8596549989d089b74ecea2a15fceaf8e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:11 +0000

    Add package ehownet/0.5.2/ehownet-0.5.2-py3-none-any.whl

---
+++ packages/ehownet/ehownet-0.5.2-py3-none-any.whl/ehn/parse/node/entity.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+Please refer the tutorial ":ref:`main-parse_node`".
+"""
+
+__author__ = 'Mu Yang <http://muyang.pro>'
+__copyright__ = '2018-2020 CKIP Lab'
+
+# pylint: disable=too-few-public-methods
+
+from .base import (
+    EhnParseEntityBase,
+    EhnParseFeatureBase,
+
+    EhnParseAnchorBody,
+    EhnParseFeatureBody,
+    EhnParseFunctionHead,
+    EhnParseStrHead,
+)
+
+################################################################################################################################
+
+class EhnParseNormalEntity(EhnParseEntityBase, EhnParseStrHead, EhnParseFeatureBody, EhnParseAnchorBody):
+    """E-HowNet Parsing: Normal Entity Node"""
+
+    node_type = 'Entity'
+    feature_type = EhnParseFeatureBase
+
+    def __init__(self, head, *features, anchor=None):
+        EhnParseEntityBase.__init__(self)
+        EhnParseStrHead.__init__(self, head)
+        EhnParseFeatureBody.__init__(self, *features)
+        EhnParseAnchorBody.__init__(self, anchor)
+
+    def children(self):
+        yield from self.features
+
+    def decode(self):
+        _features = ':' + ','.join(feature.decode() for feature in self.features) if self.features else ''
+        return f'{{{self.head}{self.anchor.decode()}{_features}}}'
+
+################################################################################################################################
+
+class EhnParseFunctionEntity(EhnParseEntityBase, EhnParseFunctionHead, EhnParseFeatureBody, EhnParseAnchorBody):
+    """E-HowNet Parsing: Function Entity Node"""
+
+    node_type = 'FunctionEntity'
+    feature_type = EhnParseFeatureBase
+
+    def __init__(self, function, *features, anchor=None):
+        EhnParseEntityBase.__init__(self)
+        EhnParseFunctionHead.__init__(self, function)
+        EhnParseFeatureBody.__init__(self, *features)
+        EhnParseAnchorBody.__init__(self, anchor)
+
+    def children(self):
+        yield self.function
+        yield from self.features
+
+    def decode(self):
+        _features = ':' + ','.join(feature.decode() for feature in self.features) if self.features else ''
+        return f'{{{self.function.decode()}{self.anchor.decode()}{_features}}}'
+
+################################################################################################################################
+
+class EhnParseAnyEntity(EhnParseEntityBase):
+    """E-HowNet Parsing: Any Entity Node"""
+
+    node_type = 'AnyEntity'
+    def __init__(self):
+        EhnParseEntityBase.__init__(self)
+
+    @property
+    def head(self):
+        return 'ANY'
+
+    def children(self):
+        return []
+
+    @staticmethod
+    def decode():
+        return '{}'
+
+################################################################################################################################
+
+class EhnParseNameEntity(EhnParseEntityBase, EhnParseStrHead):
+    """E-HowNet Parsing: Name Entity Node"""
+
+    node_type = 'NameEntity'
+    def __init__(self, head):
+        EhnParseEntityBase.__init__(self)
+        EhnParseStrHead.__init__(self, head)
+
+    def children(self):
+        return []
+
+    def decode(self):
+        return f'{{"{self.head}"}}'
+
+################################################################################################################################
+
+class EhnParseNumberEntity(EhnParseEntityBase, EhnParseStrHead):
+    """E-HowNet Parsing: Number Entity Node"""
+
+    node_type = 'NumberEntity'
+    def __init__(self, head):
+        EhnParseEntityBase.__init__(self)
+        EhnParseStrHead.__init__(self, head)
+
+    def children(self):
+        return []
+
+    def decode(self):
+        return f'{{{self.head}}}'
+
+################################################################################################################################
+
+class EhnParseTildeEntity(EhnParseEntityBase):
+    """E-HowNet Parsing: Tilde Entity Node
+
+    .. deprecated:: 0.6
+
+    """
+
+    node_type = 'TildeEntity'
+    def __init__(self):
+        EhnParseEntityBase.__init__(self)
+
+    @property
+    def head(self):
+        return '~'
+
+    def children(self):
+        return []
+
+    @staticmethod
+    def decode():
+        return '{~}'
+
+################################################################################################################################
+
+class EhnParseCoindexEntity(EhnParseEntityBase, EhnParseStrHead):
+    """E-HowNet Parsing: Coindex Entity Node"""
+
+    node_type = 'CoindexEntity'
+
+    def __init__(self, head):
+        EhnParseEntityBase.__init__(self)
+        EhnParseStrHead.__init__(self, head)
+
+    def children(self):
+        return []
+
+    @property
+    def _tree_label(self):
+        return f'${self.head}'
+
+    def decode(self):
+        return f'{{{self.head}}}'
---
+++ packages/ehownet/ehownet-0.5.2-py3-none-any.whl/ehn/parse/node/other.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+Please refer the tutorial ":ref:`main-parse_node`".
+"""
+
+__author__ = 'Mu Yang <http://muyang.pro>'
+__copyright__ = '2018-2020 CKIP Lab'
+
+# pylint: disable=too-few-public-methods
+
+from .base import (
+    EhnParseEntityBase,
+    EhnParseFeatureBase,
+    EhnParseFunctionBase,
+    EhnParseRestrictionBase,
+    EhnParseRootBase,
+
+    EhnParseAnchorBody,
+    EhnParseArgumentBody,
+    EhnParseFeatureBody,
+    EhnParseStrHead,
+    EhnParseValueBody,
+)
+
+################################################################################################################################
+# Root
+#
+
+class EhnParseRoot(EhnParseRootBase, EhnParseFeatureBody):
+    """E-HowNet Parsing: Root Node"""
+
+    node_type = 'Root'
+    feature_type = EhnParseFeatureBase
+
+    def __init__(self, *features):
+        EhnParseRootBase.__init__(self)
+        EhnParseFeatureBody.__init__(self, *features)
+
+    @property
+    def head(self):
+        return self.features[0].head
+
+    def children(self):
+        yield from self.features
+
+    def decode(self):
+        return ','.join(feature.decode() for feature in self.features) if self.features else ''
+
+################################################################################################################################
+# Function
+#
+
+class EhnParseFunction(EhnParseFunctionBase, EhnParseArgumentBody, EhnParseStrHead):
+    """E-HowNet Parsing: Function Node"""
+
+    node_type = 'Function'
+    argument_type = (EhnParseEntityBase, EhnParseRestrictionBase,)
+
+    def __init__(self, head, *arguments):
+        EhnParseFunctionBase.__init__(self)
+        EhnParseArgumentBody.__init__(self, *arguments)
+        EhnParseStrHead.__init__(self, head)
+
+    def children(self):
+        yield from self.arguments
+
+    def decode(self):
+        _arguments = ','.join(argument.decode() for argument in self.arguments)
+        return f'{self.head}({_arguments})'
+
+################################################################################################################################
+# Restriction
+#
+
+class EhnParseRestriction(EhnParseRestrictionBase, EhnParseValueBody, EhnParseAnchorBody):
+    """E-HowNet Parsing: Restriction Node"""
+
+    node_type = 'Restriction'
+    value_type = EhnParseEntityBase
+
+    def __init__(self, value, anchor=None):
+        EhnParseRestrictionBase.__init__(self)
+        EhnParseValueBody.__init__(self, value)
+        EhnParseAnchorBody.__init__(self, anchor)
+
+    @property
+    def head(self):
+        return self.value.head
+
+    def children(self):
+        yield self.value
+
+    def decode(self):
+        return f'/{self.value.decode()}{self.anchor.decode()}'
