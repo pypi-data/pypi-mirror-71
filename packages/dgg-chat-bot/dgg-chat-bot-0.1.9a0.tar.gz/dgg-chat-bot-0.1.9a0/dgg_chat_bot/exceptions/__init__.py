45613fc4b37a8d28a1a2716ef42a5fa2bfe5de42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:06 +0000

    Add package dgg-chat-bot/0.1.9a0/dgg-chat-bot-0.1.9a0.tar.gz

---
+++ packages/dgg-chat-bot/dgg-chat-bot-0.1.9a0.tar.gz/dgg-chat-bot-0.1.9a0/dgg_chat_bot/exceptions/__init__.py
@@ -0,0 +1,8 @@
+from dgg_chat.exceptions import InvalidMessageError, AnonymousConnectionError
+from ._exceptions import (
+    CommandDescriptionTooLongError,
+    DuplicateCommandError,
+    InvalidCommandError,
+    InvalidCommandArgumentsError,
+    UnknownCommandError,
+)
