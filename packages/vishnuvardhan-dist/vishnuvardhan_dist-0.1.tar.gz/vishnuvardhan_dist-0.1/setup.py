7da18358204c7bea4495a0cae90dae1031c0e76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:38 +0000

    Add package vishnuvardhan-dist/0.1/vishnuvardhan_dist-0.1.tar.gz

---
+++ packages/vishnuvardhan-dist/vishnuvardhan_dist-0.1.tar.gz/vishnuvardhan_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vishnuvardhan_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vishnuvardhan_dist'],
+      zip_safe=False)
