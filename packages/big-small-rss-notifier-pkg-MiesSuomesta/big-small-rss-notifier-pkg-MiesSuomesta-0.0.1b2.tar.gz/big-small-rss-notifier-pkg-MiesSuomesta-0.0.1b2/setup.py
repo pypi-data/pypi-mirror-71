a935ba41408bf86e6202283600266f0b1dc4d784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:56 +0000

    Add package big-small-rss-notifier-pkg-MiesSuomesta/0.0.1b2/big-small-rss-notifier-pkg-MiesSuomesta-0.0.1b2.tar.gz

---
+++ packages/big-small-rss-notifier-pkg-MiesSuomesta/big-small-rss-notifier-pkg-MiesSuomesta-0.0.1b2.tar.gz/big-small-rss-notifier-pkg-MiesSuomesta-0.0.1b2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="big-small-rss-notifier-pkg-MiesSuomesta", # Replace with your own username
+    version="0.0.1b2",
+    author="Lauri Jakku",
+    author_email="lja@iki.fi",
+    description="RSS Notifier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MiesSuomesta/big-small-rss-notifier",
+    packages=setuptools.find_packages(include=['*.py']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ],
+    python_requires='>=3.6',
+)
