984c172ba54eee26f7710954131833c07428f792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:27 +0000

    Add package cenpy/1.0.0.post4/cenpy-1.0.0.post4.tar.gz

---
+++ packages/cenpy/cenpy-1.0.0.post4.tar.gz/cenpy-1.0.0.post4/cenpy/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "1.0.0post4"
+__author__ = "Levi John Wolf levi.john.wolf@gmail.com"
+
+from . import explorer
+from .remote import APIConnection as _APIConnection
+from .tools import _load_sitekey, set_sitekey
+from .products import *
+
+SITEKEY = _load_sitekey()
+
+# __all__ = ['explorer', 'base']
