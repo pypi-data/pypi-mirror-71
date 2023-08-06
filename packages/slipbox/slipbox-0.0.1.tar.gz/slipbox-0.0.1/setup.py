60489acf5afacb8efd1822a37240ce3f8a0588db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:25 +0000

    Add package slipbox/0.0.1/slipbox-0.0.1.tar.gz

---
+++ packages/slipbox/slipbox-0.0.1.tar.gz/slipbox-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="slipbox",
+    version="0.0.1",
+    author="Levi Gruspe",
+    author_email="mail.levig@gmail.com",
+    description="Tools for managing notes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lggruspe/slipbox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Operating System :: POSIX",
+    ],
+    python_requires=">=3.8",
+)
