4616143be1b95b0c1ca4e4e3a712d27c891ebaef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:56 +0000

    Add package gapfill/0.0.7/gapfill-0.0.7.tar.gz

---
+++ packages/gapfill/gapfill-0.0.7.tar.gz/gapfill-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gapfill",
+    version="0.0.7",
+    author="Moon Ki Jung",
+    author_email="m.k.jung@outlook.com",
+    description="GapFill: A Python module of gap filling functions for motion capture marker data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mkjung99/gapfill",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
