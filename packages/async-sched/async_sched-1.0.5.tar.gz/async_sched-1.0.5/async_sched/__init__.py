6239dbd5f53dcaf44b1c7800b950428cc176bcc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:17 +0000

    Add package async-sched/1.0.5/async_sched-1.0.5.tar.gz

---
+++ packages/async-sched/async_sched-1.0.5.tar.gz/async_sched-1.0.5/async_sched/__init__.py
@@ -0,0 +1,28 @@
+
+from .utils import ScheduleError
+from .schedule import Schedule, RepeatSchedule
+
+try:
+    from .server import get_server, set_server, Scheduler, Client, \
+        Message, Error, Quit, Update, RunCommand, ScheduleCommand, RunningSchedule, ListSchedules, StopSchedule
+except (ImportError, Exception) as err:
+    error = err
+    class ClassEnvironmentError:
+        def __new__(cls, *args, **kwargs):
+            raise EnvironmentError('Dependencies not installed! '
+                                   'Libraries pydantic and pydantic_decoder are required!') from error
+
+    get_server = ClassEnvironmentError
+    set_server = ClassEnvironmentError
+    Scheduler = ClassEnvironmentError
+    Client = ClassEnvironmentError
+
+    Message = ClassEnvironmentError
+    Error = ClassEnvironmentError
+    Quit = ClassEnvironmentError
+    Update = ClassEnvironmentError
+    RunCommand = ClassEnvironmentError
+    ScheduleCommand = ClassEnvironmentError
+    RunningSchedule = ClassEnvironmentError
+    ListSchedules = ClassEnvironmentError
+    StopSchedule = ClassEnvironmentError
