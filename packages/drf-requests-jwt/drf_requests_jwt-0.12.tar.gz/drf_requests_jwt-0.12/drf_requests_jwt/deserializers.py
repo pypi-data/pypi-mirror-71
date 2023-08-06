ce12de1c7e875430278adf963c21e925cd68c655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:09 +0000

    Add package drf-requests-jwt/0.12/drf_requests_jwt-0.12.tar.gz

---
+++ packages/drf-requests-jwt/drf_requests_jwt-0.12.tar.gz/drf_requests_jwt-0.12/drf_requests_jwt/deserializers.py
@@ -0,0 +1,26 @@
+"""
+Deserializers.
+"""
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class ObjectListDeserializerMixin(object):
+    serializer_class = None
+
+    def get_deserialized_object_list(self):
+        object_list = []
+
+        if self.serializer_class is None:
+            raise Exception('`serializer_class` is required')
+
+        for object_json in self.get_results_from_all_pages():
+            logger.debug("Serializing {}".format(object_json))
+            serializer = self.serializer_class(data=object_json)
+            if serializer.is_valid():
+                object_list.append(serializer.save())
+            else:
+                logger.debug("Serializer has errors {}".format(serializer.errors))
+
+        return object_list
---
+++ packages/drf-requests-jwt/drf_requests_jwt-0.12.tar.gz/drf_requests_jwt-0.12/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'requests>=2.18.1',
+    'python-slugify>=1.2.4'
+]
+
+setup(
+    name='drf_requests_jwt',
+    version='0.12',
+    description='Django Rest Framework Requests with JWT support',
+    long_description=open('README.rst').read(),
+    url='https://github.com/sensidev/drf-requests-jwt',
+    author='Sensidev',
+    author_email='lucian.corduneanu@sensidev.com',
+    license='MIT',
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=requires,
+    setup_requires=['pytest-runner', 'requests'],
+    tests_require=['pytest', 'mock'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+)
