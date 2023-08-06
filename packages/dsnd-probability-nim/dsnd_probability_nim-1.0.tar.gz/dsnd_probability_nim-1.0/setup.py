af780f3038b7a1869c1ce4dc413cfb0192eadb0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:28 +0000

    Add package dsnd-probability-nim/1.0/dsnd_probability_nim-1.0.tar.gz

---
+++ packages/dsnd-probability-nim/dsnd_probability_nim-1.0.tar.gz/dsnd_probability_nim-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_nim',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_nim'],
+      author = 'Nimrod Michaeli',
+      author_email = 'nimrodmic@gmail.com',
+      zip_safe=False)
