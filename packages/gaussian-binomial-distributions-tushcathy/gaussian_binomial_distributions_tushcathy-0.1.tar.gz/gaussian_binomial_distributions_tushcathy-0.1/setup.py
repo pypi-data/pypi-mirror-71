5f897547a7206b08b36051748409dd13837e6488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:46 +0000

    Add package gaussian-binomial-distributions-tushcathy/0.1/gaussian_binomial_distributions_tushcathy-0.1.tar.gz

---
+++ packages/gaussian-binomial-distributions-tushcathy/gaussian_binomial_distributions_tushcathy-0.1.tar.gz/gaussian_binomial_distributions_tushcathy-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_distributions_tushcathy',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['gaussian_binomial_distributions_tushcathy'],
+      author= 'Catherine Tushabe',
+      author_email= 'tushcathy@gmail.com',
+      zip_safe=False)
