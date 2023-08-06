554bf0b6d7d853be5182594a08ebf6786eb5b032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:07 +0000

    Add package rubiks-cube-gym/0.2.1/rubiks_cube_gym-0.2.1-py3-none-any.whl

---
+++ packages/rubiks-cube-gym/rubiks_cube_gym-0.2.1-py3-none-any.whl/rubiks_cube_gym/__init__.py
@@ -0,0 +1,31 @@
+from gym.envs.registration import register
+
+register(
+    id='rubiks-cube-222-v0',
+    entry_point='rubiks_cube_gym.envs:RubiksCube222Env',
+    max_episode_steps=250,
+)
+
+register(
+    id='rubiks-cube-222-lbl-v0',
+    entry_point='rubiks_cube_gym.envs:RubiksCube222EnvLBL',
+    max_episode_steps=250,
+)
+
+register(
+    id='rubiks-cube-222-ortega-v0',
+    entry_point='rubiks_cube_gym.envs:RubiksCube222EnvOrtega',
+    max_episode_steps=250,
+)
+
+register(
+    id='pyraminx-wo-tips-v0',
+    entry_point='rubiks_cube_gym.envs:PyraminxWoTipsEnv',
+    max_episode_steps=250,
+)
+
+register(
+    id='skewb-v0',
+    entry_point='rubiks_cube_gym.envs:SkewbEnv',
+    max_episode_steps=250,
+)
