a2c7c631aa270caf716d659adc852da211847c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:02 +0000

    Add package mle-distributions/1.0/mle_distributions-1.0.tar.gz

---
+++ packages/mle-distributions/mle_distributions-1.0.tar.gz/mle_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mle_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      author='Mona Hajimomeni',
+      author_email='mona1983@gmail.com',
+      packages=['mle_distributions'],
+      zip_safe=False)
