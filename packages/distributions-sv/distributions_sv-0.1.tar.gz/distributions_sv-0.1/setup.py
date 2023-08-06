04b6ec518fa72ebde8b1bf9d3d9719914e8475ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:26 +0000

    Add package distributions-sv/0.1/distributions_sv-0.1.tar.gz

---
+++ packages/distributions-sv/distributions_sv-0.1.tar.gz/distributions_sv-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_sv',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_sv'],
+      author='soheyl',
+    author_email='soheyl@gmail.com',
+      zip_safe=False)
