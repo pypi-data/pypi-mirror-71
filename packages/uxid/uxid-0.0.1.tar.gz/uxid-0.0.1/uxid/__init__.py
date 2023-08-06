d31c4b3af14346f26f972ed5c1ed0fa7184fef57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:52 +0000

    Add package uxid/0.0.1/uxid-0.0.1.tar.gz

---
+++ packages/uxid/uxid-0.0.1.tar.gz/uxid-0.0.1/uxid/__init__.py
@@ -0,0 +1,13 @@
+"""
+    uxid
+    ~~~~
+
+    User Experience focused Identifier
+
+    :copyright: (c) 2020 JohnnyT.
+    :license: MIT, see LICENSE for more details
+"""
+
+
+def new():
+    return "uxid"
