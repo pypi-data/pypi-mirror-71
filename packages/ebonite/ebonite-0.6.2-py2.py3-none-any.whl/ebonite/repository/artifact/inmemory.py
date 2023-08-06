fc7e016ac233a0115608b352176f1cddfd567d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:45 +0000

    Add package ebonite/0.6.2/ebonite-0.6.2-py2.py3-none-any.whl

---
+++ packages/ebonite/ebonite-0.6.2-py2.py3-none-any.whl/ebonite/repository/artifact/inmemory.py
@@ -0,0 +1,35 @@
+import typing
+
+from ebonite.core.objects.artifacts import ArtifactCollection, Blob, Blobs, InMemoryBlob
+from ebonite.repository.artifact import ArtifactRepository
+from ebonite.repository.artifact.base import ArtifactExistsError, NoSuchArtifactError
+
+
+class InMemoryArtifactRepository(ArtifactRepository):
+    """
+    :class:`.ArtifactRepository` implementation which stores artifacts in-memory
+    """
+
+    type = 'inmemory'
+
+    def __init__(self):
+        self._cache: typing.Dict[str, ArtifactCollection] = {}
+
+    def _get_artifact(self, model_id: str) -> ArtifactCollection:
+        if model_id not in self._cache:
+            raise NoSuchArtifactError(model_id, self)
+
+        return self._cache[model_id]
+
+    def _push_artifact(self, model_id: str, blobs: typing.Dict[str, Blob]) -> ArtifactCollection:
+        if model_id in self._cache:
+            raise ArtifactExistsError(model_id, self)
+        self._cache[model_id] = Blobs({
+            k: InMemoryBlob(v.bytes()) for k, v in blobs.items()
+        })
+        return self._cache[model_id]
+
+    def _delete_artifact(self, model_id: str):
+        if model_id not in self._cache:
+            raise NoSuchArtifactError(model_id, self)
+        del self._cache[model_id]
