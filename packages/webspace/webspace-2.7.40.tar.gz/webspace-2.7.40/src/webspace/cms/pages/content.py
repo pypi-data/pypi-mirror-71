5002d33a601f51e914626c9f50647b9441785a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:31 +0000

    Add package webspace/2.7.40/webspace-2.7.40.tar.gz

---
+++ packages/webspace/webspace-2.7.40.tar.gz/webspace-2.7.40/src/webspace/cms/pages/content.py
@@ -0,0 +1,20 @@
+from wagtail.admin.edit_handlers import StreamFieldPanel
+
+from webspace.cms.amp.mixins import AmpMixin
+from webspace.cms.bakery.abstract import WagtailPageBakeryModel
+from webspace.cms import constants
+from webspace.loader import get_model
+
+GenericPage = get_model('cms', 'GenericPage')
+
+
+class AContentPage(AmpMixin, WagtailPageBakeryModel, GenericPage):
+    template = '%s/content_page.html' % constants.PAGES_TEMPLATES_PATH
+
+    promote_panels = GenericPage.promote_panels + [
+        StreamFieldPanel('schemas'),
+    ]
+
+    class Meta:
+        abstract = True
+        app_label = 'cms'
