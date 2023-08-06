03eefd85c38c80aed50c4b1702682efe5e254fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:19 +0000

    Add package tabsvsspaces/1.0.0/tabsvsspaces-1.0.0-py3-none-any.whl

---
+++ packages/tabsvsspaces/tabsvsspaces-1.0.0-py3-none-any.whl/tabsvsspaces/__init__.py
@@ -0,0 +1,4 @@
+from .find_stats import find_stats, find_all_files, find_stats_for_file, IGNORED_FOLDERS
+from .print_stats import print_stats
+from .stats import Statistics
+from ._main import main
