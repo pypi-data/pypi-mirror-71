a7220695112f262d7a0c606e0708a1cdc0cc587b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:08 +0000

    Add package distributions-jcp/0.3/distributions_jcp-0.3.tar.gz

---
+++ packages/distributions-jcp/distributions_jcp-0.3.tar.gz/distributions_jcp-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_jcp',
+      version='0.3',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_jcp'],
+      author = 'dan',
+      author_email = 'dan@email.com',
+      zip_safe=False)
