0a6e16650c3a1ba0366198a5ea708c0d3bade63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:17 +0000

    Add package Bilals-pypkg/0.1/Bilals_pypkg-0.1.tar.gz

---
+++ packages/Bilals-pypkg/Bilals_pypkg-0.1.tar.gz/Bilals_pypkg-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Bilals_pypkg',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Bilal Shakeel',
+      author_email='bilal_786shakeel@yahoo.com',
+      zip_safe=False)
