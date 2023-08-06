8ec06c493692c2f25ccf7cd97ce0aef2ccfd5c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:09 +0000

    Add package pitcher/0.0.1/pitcher-0.0.1.tar.gz

---
+++ packages/pitcher/pitcher-0.0.1.tar.gz/pitcher-0.0.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+__version__ = os.getenv("VERSION", "0.0.1")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pitcher",
+    author="Ben Kersten",
+    author_email="ben.kersten@twobulls.com",
+    version=__version__,
+    license="MIT",
+    url="https://github.com/twobulls/pitcher",
+    description="Python web framework for API Gateway",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["tests"]),
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
