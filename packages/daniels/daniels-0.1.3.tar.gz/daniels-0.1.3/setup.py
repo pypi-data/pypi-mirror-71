6e2b292ca8d195b652338fcbf8d1821f22766918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:52 +0000

    Add package daniels/0.1.3/daniels-0.1.3.tar.gz

---
+++ packages/daniels/daniels-0.1.3.tar.gz/daniels-0.1.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='daniels',
+    description='Daniel, the autoreporter.',
+    long_description=long_description,
+    url="https://github.com/harvested-financial/danielstwine check dist/*",
+    version='0.1.3',
+    author='Rory Gwozdz',
+    author_email='rory@harvestedfinancial.com',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+    keywords=['pip', 'slack', 'daniel'],
+    python_requires='>=3.6',
+)
