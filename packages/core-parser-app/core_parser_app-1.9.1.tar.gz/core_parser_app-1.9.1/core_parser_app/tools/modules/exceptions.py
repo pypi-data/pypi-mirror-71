e62ae38ce047c93d00808ff9e4bb536aff99297d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:56 +0000

    Add package core-parser-app/1.9.1/core_parser_app-1.9.1.tar.gz

---
+++ packages/core-parser-app/core_parser_app-1.9.1.tar.gz/core_parser_app-1.9.1/core_parser_app/tools/modules/exceptions.py
@@ -0,0 +1,10 @@
+""" Modules Exceptions
+"""
+
+
+class ModuleError(Exception):
+    """
+        Exception raised by the module system
+    """
+    def __init__(self, message):
+        self.message = message
