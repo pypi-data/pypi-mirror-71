f2307b7f1c3cb79635ed6d8b53f37bdd203d47db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:46 +0000

    Add package iapws/1.5/iapws-1.5.tar.gz

---
+++ packages/iapws/iapws-1.5.tar.gz/iapws-1.5/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+import io  # for backwards compatibility with Python 2
+
+
+with open('VERSION') as version_file:
+    __version__ = version_file.read().strip()
+
+with io.open('README.rst', encoding="utf8") as file:
+    long_description = file.read()
+
+setup(
+    name='iapws',
+    version=__version__,
+    packages=['iapws'],
+    package_data={'': ['LICENSE']},
+    author='jjgomera',
+    author_email='jjgomera@gmail.com',
+    url='https://github.com/jjgomera/iapws',
+    download_url='https://github.com/jjgomera/iapws/tarball/v' + __version__,
+    description='Python implementation of standards from The International'
+                'Association for the Properties of Water and Steam',
+    long_description=long_description,
+    license="gpl v3",
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    install_requires=["scipy"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
