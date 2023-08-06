f4f690721f63d80db3a5351da0e391b85c26d0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:51 +0000

    Add package transforms-for-ndarray/0.0.4/transforms_for_ndarray-0.0.4.tar.gz

---
+++ packages/transforms-for-ndarray/transforms_for_ndarray-0.0.4.tar.gz/transforms_for_ndarray-0.0.4/setup.py
@@ -0,0 +1,35 @@
+# coding=gbk
+"""
+author(作者): Channing Xie(谢琛)
+time(时间): 2020/6/18 10:11
+filename(文件名): setup.py
+function description(功能描述):
+...
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="transforms_for_ndarray",
+    version="0.0.4",
+    description=(
+        "transformations for numpy ndarray."
+    ),
+    long_description=open("README.md", encoding='utf-8').read(),
+    author="ChanningXieChen",
+    author_emain="M201972194@hust.edu.cn",
+    license="MIT License",
+    packages=find_packages(),
+    platforms=["all"],
+    url="https://github/XieChen10983/packages",
+    install_requires=[
+        'torch>=1.5.0',
+        'numpy>=1.18.5',
+        # 'opencv>=3.4.2',
+        'Pillow>=7.1.2',
+    ],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ]
+)
---
+++ packages/transforms-for-ndarray/transforms_for_ndarray-0.0.4.tar.gz/transforms_for_ndarray-0.0.4/transforms/__init__.py
@@ -0,0 +1,8 @@
+# coding=gbk
+"""
+author(作者): Channing Xie(谢琛)
+time(时间): 2020/6/18 10:06
+filename(文件名): __init__.py
+function description(功能描述):
+...
+"""
