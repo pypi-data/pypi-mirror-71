0a3c2bd03f4b057bfcae4a162f781114a841723c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:48 +0000

    Add package idem-solaris/3.1/idem_solaris-3.1.tar.gz

---
+++ packages/idem-solaris/idem_solaris-3.1.tar.gz/idem_solaris-3.1/idem_solaris/grains/system/path.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import sys
+
+
+async def load_cwd(hub):
+    """
+    Current working directory
+    """
+    hub.grains.GRAINS.cwd = os.getcwd()
+
+
+async def load_path(hub):
+    """
+    Return the path
+    """
+    # Provides:
+    #   path
+    hub.grains.GRAINS.path = os.environ.get("PATH", "")
+
+
+async def load_pythonpath(hub):
+    """
+    Return the Python path
+    """
+    # Provides:
+    #   pythonpath
+    hub.grains.GRAINS.pythonpath = sorted((str(p) for p in sys.path), key=str.casefold)
+
+
+async def load_executable(hub):
+    """
+    Return the python executable in use
+    """
+    # Provides:
+    #   pythonexecutable
+    hub.grains.GRAINS.pythonexecutable = sys.executable
+
+
+async def load_shell(hub):
+    hub.grains.GRAINS.shell = os.environ.get("SHELL", "/bin/sh")
+
+
+async def load_ps(hub):
+    ps = shutil.which("ps") or "/usr/ucb/ps"
+    hub.grains.GRAINS.ps = f"{ps} auxwww"
