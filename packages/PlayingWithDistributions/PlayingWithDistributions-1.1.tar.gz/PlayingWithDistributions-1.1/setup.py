bf188dafe3464a710bf82f8d4836b25a829c938a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:38 +0000

    Add package PlayingWithDistributions/1.1/PlayingWithDistributions-1.1.tar.gz

---
+++ packages/PlayingWithDistributions/PlayingWithDistributions-1.1.tar.gz/PlayingWithDistributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='PlayingWithDistributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['PlayingWithDistributions'],
+      author = 'Oritseweyinmi Henry Ajagbawa',
+      author_email = 'weyinmiajagbawa@hotmail.com',
+      zip_safe=False)
