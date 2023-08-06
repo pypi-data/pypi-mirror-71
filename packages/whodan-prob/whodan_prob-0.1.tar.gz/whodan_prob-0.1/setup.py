c21245d3026a50f0d90ba9980410cd8fdc9b2b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:16 +0000

    Add package whodan-prob/0.1/whodan_prob-0.1.tar.gz

---
+++ packages/whodan-prob/whodan_prob-0.1.tar.gz/whodan_prob-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='whodan_prob',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
