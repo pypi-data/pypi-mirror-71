0d363d3e76881932244d7974cb8087632ab5d4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:33 +0000

    Add package chaitu-210-hw-greeting/1.0/chaitu_210_hw_greeting-1.0.tar.gz

---
+++ packages/chaitu-210-hw-greeting/chaitu_210_hw_greeting-1.0.tar.gz/chaitu_210_hw_greeting-1.0/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='chaitu_210_hw_greeting',
+    version='1.0',
+    packages=['hellow'],
+    include_package_data=True,
+    description='A brief description of your package',
+    long_description='',
+    url='https://www.test.com/',
+    author='chaitanya',
+    author_email='chaitu210@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+    install_requires=[
+    ],
+)
