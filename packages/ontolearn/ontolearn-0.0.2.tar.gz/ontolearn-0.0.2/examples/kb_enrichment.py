b69d8b6fb00e548b0b9751cb91026022b20adad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:30 +0000

    Add package ontolearn/0.0.2/ontolearn-0.0.2.tar.gz

---
+++ packages/ontolearn/ontolearn-0.0.2.tar.gz/ontolearn-0.0.2/examples/kb_enrichment.py
@@ -0,0 +1,16 @@
+""" kb_enrichment.py illustrates extending Tbox of given KB and saving KB in different formats."""
+from ontolearn import KnowledgeBase, Refinement,Data
+
+
+kb = KnowledgeBase(path='../data/family-benchmark_rich_background.owl')
+data = Data(knowledge_base=kb)
+rho = Refinement(kb)
+
+enrichments = []
+for ith, refs in enumerate(rho.refine(kb.thing)):
+    if len(refs) == 2 and len(refs.instances) < 150:  # selection criterion
+        enrichments.append(refs)
+
+kb.apply_type_enrichment(enrichments)
+kb.save('enriched_kb.owl',rdf_format="rdfxml")
+kb.save('enriched_kb.nt',rdf_format="ntriples")
---
+++ packages/ontolearn/ontolearn-0.0.2.tar.gz/ontolearn-0.0.2/tests/test_util.py
@@ -0,0 +1,11 @@
+""" Test the util module"""
+
+from ontolearn.util import serializer, deserializer
+
+
+def test_serializer_deserializer():
+    m=list(range(10))
+
+    serializer(object_=m, path='.', serialized_name='concepts')
+    temp = deserializer(path='.', serialized_name='concepts')
+    assert m == temp
