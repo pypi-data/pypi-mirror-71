5711416de9294322086a61a0f51171ac2fc10dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:47 +0000

    Add package distributions-gb-yr/1.0/distributions_gb_yr-1.0.tar.gz

---
+++ packages/distributions-gb-yr/distributions_gb_yr-1.0.tar.gz/distributions_gb_yr-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_gb_yr',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions_gb_yr'],
+      author='Yash',
+      zip_safe=False)
