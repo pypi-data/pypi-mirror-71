87ed2595af766d620d74dc8b3bdbd55d06851c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:20 +0000

    Add package distributions-mutaz/0.3/distributions_mutaz-0.3.tar.gz

---
+++ packages/distributions-mutaz/distributions_mutaz-0.3.tar.gz/distributions_mutaz-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_mutaz',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['distributions_mutaz'],
+      zip_safe=False)
