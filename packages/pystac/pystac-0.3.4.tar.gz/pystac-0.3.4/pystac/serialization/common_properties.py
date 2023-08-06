bb8b358b8d18ae7fe50d49b84328715d9905f536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:03 +0000

    Add package pystac/0.3.4/pystac-0.3.4.tar.gz

---
+++ packages/pystac/pystac-0.3.4.tar.gz/pystac-0.3.4/pystac/serialization/common_properties.py
@@ -0,0 +1,71 @@
+from pystac import Collection
+from pystac.utils import make_absolute_href
+from pystac.stac_io import STAC_IO
+
+
+def merge_common_properties(item_dict, collection_cache=None, json_href=None):
+    """Merges Collection properties into an Item.
+
+    Args:
+        item_dict (dict): JSON dict of the Item which properties should be merged
+            into.
+        collection_cache (CollectionCache): Optional CollectionCache
+            that will be used to read and write cached collections.
+        json_href: The HREF of the file that this JSON comes from. Used
+            to resolve relative paths.
+
+    Returns:
+        bool: True if Collection properties have been merged, otherwise False.
+    """
+    properties_merged = False
+
+    collection = None
+    collection_id = None
+    collection_href = None
+
+    # Try the cache if we have a collection ID.
+    if 'collection' in item_dict:
+        collection_id = item_dict['collection']
+        if collection_cache is not None:
+            collection = collection_cache.get_by_id(collection_id)
+
+    # Next, try the collection link.
+    if collection is None:
+        links = item_dict['links']
+
+        # Account for 0.5 links, which were dicts
+        if isinstance(links, dict):
+            links = list(links.values())
+
+        collection_link = next((l for l in links if l['rel'] == 'collection'), None)
+        if collection_link is not None:
+            collection_href = collection_link['href']
+            if json_href is not None:
+                collection_href = make_absolute_href(collection_href, json_href)
+            if collection_cache is not None:
+                collection = collection_cache.get_by_href(collection_href)
+
+            if collection is None:
+                collection = STAC_IO.read_json(collection_href)
+
+    if collection is not None:
+        collection_id = None
+        collection_props = None
+        if isinstance(collection, Collection):
+            collection_id = collection.id
+            collection_props = collection.properties
+        else:
+            collection_id = collection['id']
+            if 'properties' in collection:
+                collection_props = collection['properties']
+
+        if collection_props is not None:
+            for k in collection_props:
+                if k not in item_dict['properties']:
+                    properties_merged = True
+                    item_dict['properties'][k] = collection_props[k]
+
+        if collection_cache is not None and not collection_cache.contains_id(collection_id):
+            collection_cache.cache(collection, href=collection_href)
+
+    return properties_merged
---
+++ packages/pystac/pystac-0.3.4.tar.gz/pystac-0.3.4/tests/test_writing.py
@@ -0,0 +1,99 @@
+import unittest
+from tempfile import TemporaryDirectory
+
+from pystac import (STAC_IO, STACObject, CatalogType, LinkType)
+from pystac.utils import make_absolute_href
+
+from tests.utils import (TestCases, SchemaValidator)
+
+
+class STACWritingTest(unittest.TestCase):
+    """Tests writing STACs, using JSON Schema validation,
+    and ensure that links are correctly set to relative or absolute.
+    """
+    def setUp(self):
+        self.schema_validator = SchemaValidator()
+
+    def validate_catalog(self, catalog):
+        self.schema_validator.validate_object(catalog)
+        validated_count = 1
+
+        for child in catalog.get_children():
+            validated_count += self.validate_catalog(child)
+
+        for item in catalog.get_items():
+            self.schema_validator.validate_object(item)
+            validated_count += 1
+
+        return validated_count
+
+    def validate_file(self, path, object_type):
+        d = STAC_IO.read_json(path)
+        return self.schema_validator.validate_dict(d, object_type)
+
+    def validate_link_types(self, root_href, catalog_type):
+        def validate_item_link_type(href, link_type, should_include_self):
+            item_dict = STAC_IO.read_json(href)
+            item = STACObject.from_file(href)
+            for link in item.get_links():
+                if not link.rel == 'self':
+                    self.assertEqual(link.link_type, link_type)
+
+            rels = set([l['rel'] for l in item_dict['links']])
+            self.assertEqual('self' in rels, should_include_self)
+
+        def validate_catalog_link_type(href, link_type, should_include_self):
+            cat_dict = STAC_IO.read_json(href)
+            cat = STACObject.from_file(href)
+            for link in cat.get_links():
+                if not link.rel == 'self':
+                    self.assertEqual(link.link_type, link_type)
+
+            rels = set([l['rel'] for l in cat_dict['links']])
+            self.assertEqual('self' in rels, should_include_self)
+
+            for child_link in cat.get_child_links():
+                child_href = make_absolute_href(child_link.target, href)
+                validate_catalog_link_type(child_href, link_type,
+                                           catalog_type == CatalogType.ABSOLUTE_PUBLISHED)
+
+            for item_link in cat.get_item_links():
+                item_href = make_absolute_href(item_link.target, href)
+                validate_item_link_type(item_href, link_type,
+                                        catalog_type == CatalogType.ABSOLUTE_PUBLISHED)
+
+        link_type = LinkType.RELATIVE
+        if catalog_type == CatalogType.ABSOLUTE_PUBLISHED:
+            link_type = LinkType.ABSOLUTE
+
+        root_should_include_href = catalog_type in [
+            CatalogType.ABSOLUTE_PUBLISHED, CatalogType.RELATIVE_PUBLISHED
+        ]
+
+        validate_catalog_link_type(root_href, link_type, root_should_include_href)
+
+    def do_test(self, catalog, catalog_type):
+        with TemporaryDirectory() as tmp_dir:
+            catalog.normalize_hrefs(tmp_dir)
+            self.validate_catalog(catalog)
+
+            catalog.save(catalog_type=catalog_type)
+
+            root_href = catalog.get_self_href()
+            self.validate_link_types(root_href, catalog_type)
+
+            for parent, children, items in catalog.walk():
+                self.validate_file(parent.get_self_href(), type(parent))
+
+                for item in items:
+                    self.validate_file(item.get_self_href(), type(item))
+
+    def test_testcases(self):
+        for catalog in TestCases.all_test_catalogs():
+            catalog = catalog.full_copy()
+            for catalog_type in [
+                    CatalogType.ABSOLUTE_PUBLISHED, CatalogType.RELATIVE_PUBLISHED,
+                    CatalogType.SELF_CONTAINED
+            ]:
+                with self.subTest(title='Catalog {} [{}]'.format(catalog.id, catalog_type)):
+                    self.do_test(catalog, catalog_type)
