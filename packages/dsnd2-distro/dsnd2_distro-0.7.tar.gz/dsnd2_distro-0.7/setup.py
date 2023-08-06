8e759d5e01c44554a9de55c58945920e33c5ea1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:57 +0000

    Add package dsnd2-distro/0.7/dsnd2_distro-0.7.tar.gz

---
+++ packages/dsnd2-distro/dsnd2_distro-0.7.tar.gz/dsnd2_distro-0.7/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd2_distro',
+      version='0.7',
+      description='Gaussian distributions',
+      packages=['dsnd2_distro'],
+      zip_safe=False)
