834b2b8bca1ff6a996250919735eec18912e0608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:02 +0000

    Add package pyfan/0.1.38/pyfan-0.1.38.tar.gz

---
+++ packages/pyfan/pyfan-0.1.38.tar.gz/pyfan-0.1.38/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyfan',
+ 'pyfan.gen',
+ 'pyfan.gen.rand',
+ 'pyfan.util',
+ 'pyfan.util.path',
+ 'pyfan.util.pdf',
+ 'pyfan.util.rmd']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cython>=0.29.20,<0.30.0',
+ 'matplotlib>=3.2.1,<4.0.0',
+ 'numpy>=1.18.5,<2.0.0',
+ 'python-frontmatter>=0.5.0,<0.6.0',
+ 'pyyaml>=5.3.1,<6.0.0',
+ 'scipy>=1.4.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pyfan',
+    'version': '0.1.38',
+    'description': '',
+    'long_description': None,
+    'author': 'Fan Wang',
+    'author_email': 'wangfanbsg75@live.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
