1d51b3e51444ab0f9bd9f1c22fdd6ae207d33a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:53 +0000

    Add package bokcolmaps/1.3.1/bokcolmaps-1.3.1-py3-none-any.whl

---
+++ packages/bokcolmaps/bokcolmaps-1.3.1-py3-none-any.whl/bokcolmaps/Examples/ColourMapLPSliderExample.py
@@ -0,0 +1,22 @@
+"""
+The ColourMap based classes with sliders need the Bokeh server,
+i.e. to run this example at the command line enter:
+bokeh serve --show ColourMapLPSliderExample.py
+
+To use the ColourMapSlider (i.e. without a line plot)
+just import and instantiate that instead (same init parameters)
+To disable the hover tool readout, add kwarg hoverdisp=False.
+"""
+
+from bokeh.io import curdoc
+
+from bokcolmaps.ColourMapLPSlider import ColourMapLPSlider
+from bokcolmaps.Examples import example_data
+
+x, y, z, D = example_data()
+
+cm = ColourMapLPSlider(x, y, z, D, cfile='../jet.txt',
+                       xlab='x val', ylab='y val', zlab='power val',
+                       dmlab='Function val')
+
+curdoc().add_root(cm)
