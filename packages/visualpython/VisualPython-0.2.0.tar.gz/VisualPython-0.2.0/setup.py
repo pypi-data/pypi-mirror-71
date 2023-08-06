fc5867c7d75bc703ff2f67221866a170d3f34e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:58 +0000

    Add package visualpython/0.2.0/VisualPython-0.2.0.tar.gz

---
+++ packages/visualpython/VisualPython-0.2.0.tar.gz/VisualPython-0.2.0/setup.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'VisualPython',
+    version          = '0.2.0',
+    packages         = find_packages(),
+    description      = 'VisualPython',
+    license          = '',
+    author           = 'BlackLogic',
+    author_email     = 'blacklogic.dev@gmail.com',
+    url              = 'https://gitlab.com/blacklogic.dev/visualpython',
+    download_url     = 'https://gitlab.com/blacklogic.dev/visualpython/packages/visualpython.0.2.0.tar.gz',
+    install_requires = [],
+    classifiers      = ['Programming Language :: Python :: 3.6',
+                        'Programming Language :: Python :: 3.7']
+    )
