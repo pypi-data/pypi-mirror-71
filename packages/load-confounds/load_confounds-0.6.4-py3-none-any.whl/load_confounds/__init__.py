8beff8c02872e95621dcf584bd07913c16c69907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:48 +0000

    Add package load-confounds/0.6.4/load_confounds-0.6.4-py3-none-any.whl

---
+++ packages/load-confounds/load_confounds-0.6.4-py3-none-any.whl/load_confounds/__init__.py
@@ -0,0 +1,24 @@
+"""
+loading fMRIprep confounds into python
+"""
+from load_confounds.parser import (
+    Confounds,
+    Params2,
+    Params6,
+    Params9,
+    Params24,
+    Params36,
+    AnatCompCor,
+    TempCompCor,
+)
+
+__all__ = [
+    "Confounds",
+    "Params2",
+    "Params6",
+    "Params9",
+    "Params24",
+    "Params36",
+    "AnatCompCor",
+    "TempCompCor",
+]
