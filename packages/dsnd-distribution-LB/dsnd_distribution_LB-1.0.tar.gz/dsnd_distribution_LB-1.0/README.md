2e7d684f6740a89eb085b2b239d1661d54f58d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:16 +0000

    Add package dsnd-distribution-LB/1.0/dsnd_distribution_LB-1.0.tar.gz

---
+++ packages/dsnd-distribution-LB/dsnd_distribution_LB-1.0.tar.gz/dsnd_distribution_LB-1.0/README.md
@@ -0,0 +1,11 @@
+# dsnd-distribution-LB
+
+Summary of the package
+
+# Files
+
+Explanation of files in the packages
+
+# Installation
+
+Installations instructions
---
+++ packages/dsnd-distribution-LB/dsnd_distribution_LB-1.0.tar.gz/dsnd_distribution_LB-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_distribution_LB',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['dsnd_distribution_LB'],
+      zip_safe=False)
