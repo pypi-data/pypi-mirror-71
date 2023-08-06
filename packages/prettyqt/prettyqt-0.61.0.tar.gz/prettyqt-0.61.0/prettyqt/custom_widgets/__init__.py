5e7d00352431376637f1eaf5f3aad0467c9602f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:08 +0000

    Add package prettyqt/0.61.0/prettyqt-0.61.0.tar.gz

---
+++ packages/prettyqt/prettyqt-0.61.0.tar.gz/prettyqt-0.61.0/prettyqt/custom_widgets/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""custom_widgets module
+
+"""
+
+from .image import Image
+from .iconlabel import IconLabel
+from .flowlayout import FlowLayout
+from .colorchooserbutton import ColorChooserButton
+from .filechooserbutton import FileChooserButton
+from .fontchooserbutton import FontChooserButton
+from .inputandslider import InputAndSlider
+from .spanslider import SpanSlider
+from .labeledslider import LabeledSlider
+from .waitingspinner import WaitingSpinner
+from .markdownwidget import MarkdownWindow
+from .imageviewer import ImageViewer
+from .popupinfo import PopupInfo
+from .buttondelegate import ButtonDelegate
+from .radiodelegate import RadioDelegate
+from .selectionwidget import SelectionWidget
+from .codeeditor import CodeEditor
+
+__all__ = ["Image",
+           "IconLabel",
+           "FlowLayout",
+           "ColorChooserButton",
+           "FileChooserButton",
+           "FontChooserButton",
+           "InputAndSlider",
+           "SpanSlider",
+           "LabeledSlider",
+           "WaitingSpinner",
+           "PopupInfo",
+           "ButtonDelegate",
+           "RadioDelegate",
+           "SelectionWidget",
+           "ImageViewer",
+           "MarkdownWindow",
+           "CodeEditor"]
