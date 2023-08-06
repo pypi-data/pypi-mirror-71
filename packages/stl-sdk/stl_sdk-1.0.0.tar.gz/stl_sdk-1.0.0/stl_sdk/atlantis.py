cd734120093075d13b5adcfed48ec45a292098e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:33 +0000

    Add package stl-sdk/1.0.0/stl_sdk-1.0.0.tar.gz

---
+++ packages/stl-sdk/stl_sdk-1.0.0.tar.gz/stl_sdk-1.0.0/stl_sdk/atlantis.py
@@ -0,0 +1,150 @@
+import jwt
+import requests
+
+
+class AtlantisClient:
+    """Cliente HTTP para comunicação com Atlantis
+
+    :param str atlantis_url: Endereço da API do serviço Atlantis
+    :param str client_id: ID do cliente cadastrado no Atlantis
+    """
+
+    def __init__(self, client_id=None, atlantis_url='https://accounts.spacetimeanalytics.com'):
+        self.atlantis_url = atlantis_url
+        self._client_id = client_id
+
+    @property
+    def client_id(self):
+        if self._client_id is None:
+            raise InvalidInitParameters('invalid initialization of the required parameters.')
+        return self._client_id
+
+    def issue_token(self, code, redirect_uri):
+        """Realiza a troca do code pelo token
+
+        .. deprecated:: 0.0.8
+            Use a biblioteca authlib.
+
+        Exemplo:
+
+        .. code-block:: python
+
+            from stl_sdk.torpedo import AtlantisClient
+
+            client = AtlantisClient('https://accounts.spacetimeanalytics.com', 'client_id_abc123')
+            token = client.issue_token('code123abc')
+
+        :param code: Código concedido pelo Atlantis via redirect (authorization_code)
+        :type code: str
+        :param redirect_uri: Endereço de redirecionamento
+        :type redirect_uri: str
+        :return: Dicionário com os tokens: ``access_token``, ``id_token`` e ``refresh_token``
+        :rtype: dict
+        """
+        response = requests.post(
+            '{}/api/token'.format(self.atlantis_url),
+            data={
+                'code': code,
+                'client_id': self.client_id,
+                'redirect_uri': redirect_uri,
+                'grant_type': 'authorization_code'
+            })
+        response.raise_for_status()
+        return response.json()
+
+    def get_public_key(self):
+        """Obtém a chave pública do Atlantis para validação do token de acesso
+
+        .. deprecated:: 0.0.8
+            Use a biblioteca authlib.
+
+        :return: Chave pública
+        :rtype: str
+        """
+        response = requests.get('{}/api/jwks'.format(self.atlantis_url))
+        response.raise_for_status()
+        return response.json()
+
+    def validate_token(self, id_token):
+        """Valida token de acesso do usuário
+
+        .. deprecated:: 0.0.8
+            Use a biblioteca authlib.
+
+        Exemplo:
+
+        .. code-block:: python
+
+            from stl_sdk.torpedo import AtlantisClient
+
+            client = AtlantisClient('https://accounts.spacetimeanalytics.com', 'client_id_abc123')
+            token_payload = client.validate_token('tokenJwT')
+
+        :param id_token: Token JWT do usuário gerado pelo Atlantis
+        :type id_token: str
+        :raises InvalidTokenError: Se o token estiver expirado ou alterado
+        :return: Retorna o payload do token JWT
+        :rtype: dict
+        """
+        public_key = self.get_public_key()
+
+        try:
+            return jwt.decode(id_token, public_key, audience=self.client_id)
+        except Exception as err:
+            raise InvalidTokenError(err)
+
+    def introspect_token(self, access_token):
+        """Verifica se o access_token está válido
+
+        :param access_token: Token do usuário gerado pelo Atlantis
+        :type access_token: str
+        :return: Retorna dados do token: ``active``, ``email``, ``scope``, ``client_id``, ``exp``, etc..
+        :rtype: dict
+        """
+        response = requests.post(
+            '{}/api/introspect-token'.format(self.atlantis_url),
+            data={
+                'token': access_token,
+                'token_type_hint': 'access_token',
+                'client_id': self.client_id
+            })
+        response.raise_for_status()
+        return response.json()
+
+    def introspect_api_key(self, api_key):
+        """Verifica se a Api key é válida
+
+        :param api_key: Api key gerada pelo Atlantis
+        :type api_key: str
+        :return: Retorna dados da api key: ``actions``, ``description``, ``created_at``, etc..
+        :rtype: dict
+        """
+        response = requests.post(
+            '{}/api/introspect-apikey'.format(self.atlantis_url),
+            data={
+                'apikey': api_key,
+                'client_id': self.client_id
+            })
+        response.raise_for_status()
+        return response.json()
+
+
+class AtlantisClientFlask(AtlantisClient):
+
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+        super().__init__()
+
+    def init_app(self, app):
+        self.atlantis_url = app.config['ATLANTIS_URL']
+        self._client_id = app.config['ATLANTIS_CLIENT_ID']
+
+
+class InvalidTokenError(RuntimeError):
+    pass
+
+
+class InvalidInitParameters(RuntimeError):
+    pass
