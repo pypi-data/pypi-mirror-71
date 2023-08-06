a80175d925e9fe1bed2d1a077bc4dd4abd2aaa27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:13 +0000

    Add package mkdocs-add-number-plugin/1.2.2/mkdocs-add-number-plugin-1.2.2.tar.gz

---
+++ packages/mkdocs-add-number-plugin/mkdocs-add-number-plugin-1.2.2.tar.gz/mkdocs-add-number-plugin-1.2.2/mkdocs_add_number_plugin/utils.py
@@ -0,0 +1,25 @@
+
+def flatten(nav):
+    """
+    Flattens mkdocs navigation to list of markdown files 
+    
+    See tests/test_flatten.py for example
+    
+    Args:
+        nav (list): nested list with dicts
+    
+    Returns:
+        list: list of markdown pages
+    """
+    pages = []
+    for i in nav:
+        # file
+        if type(i) == str:
+            pages.append(i)
+            continue
+        item = list(i.values())[0]
+        if type(item) == list:
+            pages += flatten(item)
+        else:
+            pages.append(item)
+    return pages
---
+++ packages/mkdocs-add-number-plugin/mkdocs-add-number-plugin-1.2.2.tar.gz/mkdocs-add-number-plugin-1.2.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mkdocs-add-number-plugin',
+    version='1.2.2',
+    description='MkDocs Plugin to automatically number the headings (h1-h6) '
+                'in each markdown page and number the nav.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='mkdocs index add-number plugin',
+    url='https://github.com/shihr/mkdocs-add-number-plugin.git',
+    author='ignorantshr',
+    author_email='shrshraa@outlook.com',
+    license='MIT',
+    python_requires='>=3.5',
+    install_requires=[
+        'mkdocs>=1.1'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'mkdocs.plugins': [
+            'mkdocs-add-number-plugin=mkdocs_add_number_plugin.plugin:AddNumberPlugin',
+            'add-number=mkdocs_add_number_plugin.plugin:AddNumberPlugin' 
+        ]
+    }
+)
