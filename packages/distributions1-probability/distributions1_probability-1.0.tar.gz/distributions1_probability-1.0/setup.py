6ec8a59138329e5855fc7c18a9aec4be887a8658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:31 +0000

    Add package distributions1-probability/1.0/distributions1_probability-1.0.tar.gz

---
+++ packages/distributions1-probability/distributions1_probability-1.0.tar.gz/distributions1_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions1_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions1_probability'],
+      author = "Eddy Caldas",
+      author_email = "eddycaldas@live.com",
+      zip_safe=False)
