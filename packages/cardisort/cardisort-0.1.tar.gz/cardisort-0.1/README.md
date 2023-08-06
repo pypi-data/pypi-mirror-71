9ddc7add6aa8e4628f094399eae035bd8f2b437b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:40 +0000

    Add package cardisort/0.1/cardisort-0.1.tar.gz

---
+++ packages/cardisort/cardisort-0.1.tar.gz/cardisort-0.1/README.md
@@ -0,0 +1,6 @@
+# cardisort
+cardisort is a package for **car**diac MR **DI**COM **sort**ing.
+
+
+## License
+This package is released under a Creative Commons license (CC BY-NC-SA 4.0)
---
+++ packages/cardisort/cardisort-0.1.tar.gz/cardisort-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cardisort',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/cianmscannell/cardisortt',
+      author='Cian M. Scannell, Ruth P. Lim',
+      author_email='cian.scannell@kcl.ac.uk',
+      license='CC BY-NC-SA 4.0',
+      packages=['cardisort'],
+      zip_safe=False)
