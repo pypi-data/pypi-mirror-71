1d061d87ff45b3facc589c29f59aee307f9ddd54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:40 +0000

    Add package pyhydllp/1.1.5/pyhydllp-1.1.5.tar.gz

---
+++ packages/pyhydllp/pyhydllp-1.1.5.tar.gz/pyhydllp-1.1.5/pyhydllp/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from pyhydllp import hydllp, util, sql
+except ImportError:
+    from pyhydllp import hydllp, util
+from pyhydllp.hyd import hyd
+
---
+++ packages/pyhydllp/pyhydllp-1.1.5.tar.gz/pyhydllp-1.1.5/pyhydllp/hyd.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 11:30:30 2018
+
+@author: michaelek
+"""
+from pyhydllp.hydllp import Hydllp
+
+
+class hyd(object):
+    """
+    Class to initiate the Hydstra connection and access the extraction functions.
+
+    Parameters
+    ----------
+    ini_path : str or None
+        Path to the Hyaccess.ini file.
+    dll_path : str or None
+        Path to the hydllp.dll file.
+    hydllp_filename : str
+        The hydllp file name.
+    hyaccess_filename : str
+        The hyaccess file name.
+    hyconfig_filename : str
+        The hyconfig file name.
+    username : str
+        The login username for Hydstra. Leave a blank str to have Hydstra use the local user machine username.
+    password : str
+        Same as username, but for password.
+
+    Returns
+    -------
+    hyd object
+    """
+    ### Initialisation
+    def __init__(self, ini_path, dll_path, hydllp_filename='hydllp.dll', hyaccess_filename='Hyaccess.ini', hyconfig_filename='HYCONFIG.INI', username='', password=''):
+
+        hydllp = Hydllp(ini_path=ini_path, dll_path=dll_path, hydllp_filename=hydllp_filename, hyaccess_filename=hyaccess_filename, hyconfig_filename=hyconfig_filename, username=username, password=password)
+        self.hydllp = hydllp
+
+    ### Load functions
+    from pyhydllp.base import get_variable_list, get_ts_blockinfo, get_ts_data, ts_data_changes
+    try:
+        from pyhydllp.combo import get_ts_data_bulk, sites_var_periods
+    except ImportError:
+        None
