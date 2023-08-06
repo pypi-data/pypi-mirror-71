b7ac9c80e5bcbd6705d1b4681b3ad35b9726f208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:25 +0000

    Add package bftool-pkg-sulcud/1.4.2.1/bftool-pkg-sulcud-1.4.2.1.tar.gz

---
+++ packages/bftool-pkg-sulcud/bftool-pkg-sulcud-1.4.2.1.tar.gz/bftool-pkg-sulcud-1.4.2.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bftool-pkg-sulcud",
+    version="1.4.2.1",
+    author="Antonio Donis",
+    author_email="antoniojosedonishung@gmail.com",
+    description="A python module (with script) included to handle different kinds of bruteforce",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shoriwe/bftool",
+    packages=["bftool"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
