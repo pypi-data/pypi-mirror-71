cf76bc8a65b88a18dbea04abe66a8b9f434bddba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:45 +0000

    Add package idem-darwin/7/idem_darwin-7.tar.gz

---
+++ packages/idem-darwin/idem_darwin-7.tar.gz/idem_darwin-7/idem_darwin/grains/os/computer.py
@@ -0,0 +1,9 @@
+import shutil
+
+
+async def load_computer_name(hub):
+    scutil = shutil.which("scutil")
+    if scutil:
+        hub.grains.GRAINS.computer_name = (
+            await hub.exec.cmd.run([scutil, "--get", "ComputerName"])
+        ).stdout.strip()
