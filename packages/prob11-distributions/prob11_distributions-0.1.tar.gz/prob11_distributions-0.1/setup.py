ecc89ef9167a34b343f7b71800b92e7b8d66b067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:39 +0000

    Add package prob11-distributions/0.1/prob11_distributions-0.1.tar.gz

---
+++ packages/prob11-distributions/prob11_distributions-0.1.tar.gz/prob11_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob11_distributions',
+      version='0.1',
+      author='Emad Tolba',
+      author_email='abhathy@yahoo.com',
+      description='Gaussian distributions',
+      packages=['prob11_distributions'],
+      zip_safe=False)
