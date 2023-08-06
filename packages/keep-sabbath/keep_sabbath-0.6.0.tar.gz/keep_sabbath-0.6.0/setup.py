520afb94f24bf9786fb1f40855bb926445665a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:27 +0000

    Add package keep-sabbath/0.6.0/keep_sabbath-0.6.0.tar.gz

---
+++ packages/keep-sabbath/keep_sabbath-0.6.0.tar.gz/keep_sabbath-0.6.0/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'keep_sabbath',   
+  packages = ['keep_sabbath'],   
+  version = '0.6.0',
+  license='BSD 3-Clause',   
+  description = 'Django app to automatically redirect users when it is the Sabbath day, with support for overriding on other Biblical Holy days.',
+  author = 'Correct Syntax, Noah Rahm', 
+  author_email = 'correctsyntax@yahoo.com',     
+  url = 'https://correctsyntax.com/projects/keep-sabbath/', 
+  #download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['sabbath', 'django', 'web', 'redirects'], 
+  install_requires=[           
+          'django>=2.2'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: BSD License',   
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
+
