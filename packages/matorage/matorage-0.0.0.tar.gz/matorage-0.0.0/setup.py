5d58d470d7cebbd527a249e6211b6651a1b84897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:45 +0000

    Add package matorage/0.0.0/matorage-0.0.0.tar.gz

---
+++ packages/matorage/matorage-0.0.0.tar.gz/matorage-0.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="matorage",
+    version="0.0.0",
+    license='MIT',
+    author="TaeHwan-Jung",
+    author_email="nlkey2022@gmail.com",
+    description="",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+)
