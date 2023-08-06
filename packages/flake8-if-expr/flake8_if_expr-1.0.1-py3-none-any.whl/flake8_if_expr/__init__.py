3cb6f1f6e04fa59645ab515f140b27bcdc220db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:35 +0000

    Add package flake8-if-expr/1.0.1/flake8_if_expr-1.0.1-py3-none-any.whl

---
+++ packages/flake8-if-expr/flake8_if_expr-1.0.1-py3-none-any.whl/flake8_if_expr/__init__.py
@@ -0,0 +1,23 @@
+import ast
+
+from flake8_plugin_utils import Error, Plugin, Visitor
+
+__version__ = '1.0.1'
+
+
+class IfExprFound(Error):
+    code = 'IF100'
+    message = (
+        f'{code} don`t use "[on_true] if [expression] else [on_false]" syntax'
+    )
+
+
+class IfExprFinder(Visitor):
+    def visit_IfExp(self, node: ast.IfExp) -> None:
+        self.error_from_node(IfExprFound, node)
+
+
+class IfExprPlugin(Plugin):
+    name = 'flake8-if-expr'
+    version = __version__
+    visitors = [IfExprFinder]
