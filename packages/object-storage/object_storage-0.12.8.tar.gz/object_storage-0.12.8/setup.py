765fc7cd35a0347f3103363070525ad21b871918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:41 +0000

    Add package object-storage/0.12.8/object_storage-0.12.8.tar.gz

---
+++ packages/object-storage/object_storage-0.12.8.tar.gz/object_storage-0.12.8/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+install_requires = [
+    "boto3 >= 1.0.1",
+    "pyrax >= 1.9.2",
+    "google-cloud-storage"
+]
+
+setup(name="object_storage",
+      version="0.12.8",
+      description="Python library for accessing files over various file transfer protocols.",
+      url="https://github.com/ustudio/storage",
+      packages=["storage"],
+      install_requires=install_requires)
