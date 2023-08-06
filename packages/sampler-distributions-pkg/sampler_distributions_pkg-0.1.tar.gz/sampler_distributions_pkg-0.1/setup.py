148015bb1f87e970ac77acc60c73b1df95e69d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:54 +0000

    Add package sampler-distributions-pkg/0.1/sampler_distributions_pkg-0.1.tar.gz

---
+++ packages/sampler-distributions-pkg/sampler_distributions_pkg-0.1.tar.gz/sampler_distributions_pkg-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sampler_distributions_pkg',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sampler_distributions_pkg'],
+      author='Daniel Bailey',
+      zip_safe=False)
