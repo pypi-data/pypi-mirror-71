c2c481f159cc9837a08ef9f9bb2767ec8f9a0fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:06 +0000

    Add package gb-prob-distributions/1.0/gb_prob_distributions-1.0.tar.gz

---
+++ packages/gb-prob-distributions/gb_prob_distributions-1.0.tar.gz/gb_prob_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gb_prob_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gb_prob_distributions'],
+      author = 'Ritvik Iyer',
+      author_email = 'ritvik95.iyer@gmail.com',
+      zip_safe=False)
