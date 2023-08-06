61986ab9e24a8339846f404ae24cec6dd25b6051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:33 +0000

    Add package Probability-Distributions-Musu/0.1/Probability_Distributions_Musu-0.1.tar.gz

---
+++ packages/Probability-Distributions-Musu/Probability_Distributions_Musu-0.1.tar.gz/Probability_Distributions_Musu-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Probability_Distributions_Musu',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Probability_Distributions_Musu'],
+      zip_safe=False)
