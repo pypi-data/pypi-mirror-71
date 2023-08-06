0d70b10cb282e3a2966bb15df2fb4477971ae268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:45 +0000

    Add package bestpix/1.2.8/bestpix-1.2.8.tar.gz

---
+++ packages/bestpix/bestpix-1.2.8.tar.gz/bestpix-1.2.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bestpix',
+    version='1.2.8',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    scripts=['scripts/reveal', 'scripts/cleanup'],
+    license='GNU GPLv3',
+    author='Michael Chuprin',
+    author_email='mkchuprin@gmail.com',
+    url='https://github.com/mkchuprin/BestPix',
+    install_requires=['Flask==1.1.2', 'pyheif==0.4', 'Pillow==7.1.2'],
+    packages=['bestpix'],
+    include_package_data=True,
+)
