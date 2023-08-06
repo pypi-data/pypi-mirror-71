a6309f9806f33d8662c7eea7b2261d86bbb42892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:45 +0000

    Add package bestpix/1.3.0/bestpix-1.3.0.tar.gz

---
+++ packages/bestpix/bestpix-1.3.0.tar.gz/bestpix-1.3.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bestpix',
+    version='1.3.0',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    scripts=['scripts/reveal', 'scripts/cleanup'],
+    license='GNU GPLv3',
+    author='Michael Chuprin',
+    author_email='mkchuprin@gmail.com',
+    url='https://github.com/mkchuprin/BestPix',
+    packages=['bestpix'],
+    include_package_data=True,
+)
