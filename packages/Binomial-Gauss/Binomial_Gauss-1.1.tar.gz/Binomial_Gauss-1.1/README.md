3c40d01cd6aa3051b1ee63ce24953f07fe9d8f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:47 +0000

    Add package Binomial-Gauss/1.1/Binomial_Gauss-1.1.tar.gz

---
+++ packages/Binomial-Gauss/Binomial_Gauss-1.1.tar.gz/Binomial_Gauss-1.1/README.md
@@ -0,0 +1,2 @@
+#Description
+This is a simple package created to find mean and standard variance of binomial distributions and Gaussian distributions
---
+++ packages/Binomial-Gauss/Binomial_Gauss-1.1.tar.gz/Binomial_Gauss-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Binomial_Gauss',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Binomial_Gauss'],
+      author = 'Pankaj Biradar',
+      author_email = 'pankaj28988@gmail.com',
+      zip_safe=False)
