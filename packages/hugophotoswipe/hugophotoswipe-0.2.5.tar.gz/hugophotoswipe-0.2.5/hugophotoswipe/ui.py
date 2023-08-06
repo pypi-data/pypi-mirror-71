88244412160458aebd2db40d6131f87609b22735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:06 +0000

    Add package hugophotoswipe/0.2.5/hugophotoswipe-0.2.5.tar.gz

---
+++ packages/hugophotoswipe/hugophotoswipe-0.2.5.tar.gz/hugophotoswipe-0.2.5/hugophotoswipe/ui.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+"""Contains the functions for handling the user interface
+
+This file contains the functions for handling the command line user interface.  
+It mainly processses the command line arguments, loads the settings, 
+initializes a HugoPhotoSwipe instance, and passes the command to this instance.
+
+Author: Gertjan van den Burg
+License: GPL v3.
+
+"""
+
+from __future__ import print_function
+
+import argparse
+import logging
+
+from . import __version__
+from .hugophotoswipe import HugoPhotoSwipe
+from .conf import settings, SETTINGS_FILENAME
+
+
+def main():
+    """ Main HugoPhotoSwipe function to be called to run the program """
+    command, album = parse_args()
+
+    if command == "init":
+        logging.info("Dumping settings file.")
+        settings.dump(".")
+        print("Created settings file: %s" % SETTINGS_FILENAME)
+        return
+
+    if not settings.validate():
+        return
+
+    hps = HugoPhotoSwipe()
+    if command == "new":
+        logging.info("Creating new album")
+        hps.new(name=album)
+    elif command == "update":
+        logging.info("Running album update")
+        hps.update(name=album)
+    elif command == "clean":
+        logging.info("Running clean")
+        hps.clean(name=album)
+    else:
+        raise ValueError("Unknown command: %s" % command)
+    logging.info("Dumping settings file.")
+    settings.dump(".")
+
+
+def parse_args():
+    """ Parse the command line arguments """
+    parser = argparse.ArgumentParser(
+        prog="HugoPhotoSwipe", description="Integrate Hugo and PhotoSwipe"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Verbose mode",
+        action="store_const",
+        dest="loglevel",
+        const=logging.INFO,
+        default=logging.WARNING,
+    )
+    parser.add_argument(
+        "-f",
+        "--fast",
+        action="store_true",
+        help=("Fast mode " "(tries less potential crops)"),
+    )
+    parser.add_argument(
+        "-V", "--version", action="version", version=__version__,
+    )
+    parser.add_argument(
+        "command",
+        choices=["new", "update", "clean", "init"],
+        help="action to do",
+    )
+    parser.add_argument(
+        "album", nargs="?", help="album to apply the action to"
+    )
+    args = parser.parse_args()
+    logging.basicConfig(
+        level=args.loglevel,
+        datefmt="[%Y-%m-%d %H:%M:%S]",
+        format="%(asctime)s - %(message)s",
+    )
+    settings.verbose = args.loglevel == logging.INFO
+    settings.fast = args.fast
+    return args.command, args.album
+
+
+if __name__ == "__main__":
+    exit(main())
---
+++ packages/hugophotoswipe/hugophotoswipe-0.2.5.tar.gz/hugophotoswipe-0.2.5/hugophotoswipe/utils.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+"""Several utility functions
+
+This file contains several utility functions used throughout HugoPhotoSwipe.
+
+Author: Gertjan van den Burg
+License: GPL v3.
+
+"""
+
+from __future__ import print_function
+
+import errno
+import os
+
+import six
+
+from datetime import datetime
+
+if six.PY2:
+    from tzlocal import get_localzone
+    import pytz
+else:
+    from datetime import timezone
+
+
+def mkdirs(path):
+    """ Create directories recursively and don't complain when they exist """
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def modtime():
+    """ Get the current local time as a string in iso format """
+    if six.PY2:
+        local_tz = get_localzone()
+        now = datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(local_tz)
+    else:
+        now = datetime.now(timezone.utc).astimezone()
+    nowstr = now.replace(microsecond=0).isoformat()
+    return nowstr
+
+
+def yaml_field_to_file(fid, data, field, indent="", force_string=False):
+    """ Handy function for writing pretty yaml """
+    if data is None:
+        fid.write("%s%s:\n" % (indent, field))
+    else:
+        if force_string:
+            fid.write('%s%s: "%s"\n' % (indent, field, data))
+        else:
+            fid.write("%s%s: %s\n" % (indent, field, data))
+
+
+def question_yes_no(question, default=True):
+    """ Ask a yes/no question from the user and be persistent """
+    if default:
+        extension = "[Y/n/q]"
+    else:
+        extension = "[y/N/q]"
+    while True:
+        user_input = six.moves.input("%s %s " % (question, extension))
+        if user_input == "q":
+            raise SystemExit
+        if user_input.lower() in ["y", "yes"]:
+            return True
+        elif user_input.lower() in ["n", "no"]:
+            return False
+        elif not user_input:
+            return default
+        else:
+            print("No valid input, please try again.")
+
+
+class cached_property(object):
+    """Decorator for cached class properties
+
+    Decorator that converts a method with a single self argument into a 
+    property cached on the instance.
+
+    From Django: 
+    https://github.com/django/django/blob/master/django/utils/functional.py
+
+    """
+
+    def __init__(self, func, name=None):
+        self.func = func
+        self.__doc__ = getattr(func, "__doc__")
+        self.name = name or func.__name__
+
+    def __get__(self, instance, cls=None):
+        if instance is None:
+            return self
+        res = instance.__dict__[self.name] = self.func(instance)
+        return res
