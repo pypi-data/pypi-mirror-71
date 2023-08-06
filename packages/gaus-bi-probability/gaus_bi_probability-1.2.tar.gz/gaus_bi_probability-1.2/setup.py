07921869bee87b9be0025e1b2c48d415c6ad045d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:17 +0000

    Add package gaus-bi-probability/1.2/gaus_bi_probability-1.2.tar.gz

---
+++ packages/gaus-bi-probability/gaus_bi_probability-1.2.tar.gz/gaus_bi_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaus_bi_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['gaus_bi_probability'],
+      author= 'Adnan Mostafa',
+      author_email= 'adnanmostafaprofessional@gmail.com',
+      zip_safe=False)
