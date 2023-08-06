8ea35abf698b69d17fda4b595261f857ef461193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:18 +0000

    Add package owlcat/1.6.3/owlcat-1.6.3.tar.gz

---
+++ packages/owlcat/owlcat-1.6.3.tar.gz/owlcat-1.6.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+install_requires = [
+      'astropy',
+      'numpy',
+      'matplotlib',
+      'python-casacore',
+      'meqtrees_cattery',
+      'scipy',
+      'astro-kittens',
+      'six',
+      'future',
+]
+
+setup(name='owlcat',
+      version='1.6.3',
+      description='miscellaneous utility scripts for manipulating radio interferometry data',
+      author='Oleg Smirnov',
+      author_email='Oleg Smirnov <osmirnov@gmail.com>',
+      url='https://github.com/ska-sa/owlcat',
+      packages=['Owlcat'],
+      install_requires=install_requires,
+      scripts=['Owlcat/bin/' + i for i in os.listdir('Owlcat/bin')],
+      data_files=[('Owlcat/bin/', ['Owlcat/bin/commands.list'])],
+)
