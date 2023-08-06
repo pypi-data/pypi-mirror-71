7244dcdbdea83abacf6b7811e202ff82e26a1ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:43 +0000

    Add package wilfred/0.7.1/wilfred-0.7.1-py36-none-any.whl

---
+++ packages/wilfred/wilfred-0.7.1-py36-none-any.whl/wilfred/version.py
@@ -0,0 +1,16 @@
+####################################################################
+#                                                                  #
+# Wilfred                                                          #
+# Copyright (C) 2020, Vilhelm Prytz, <vilhelm@prytznet.se>, et al. #
+#                                                                  #
+# Licensed under the terms of the MIT license, see LICENSE.        #
+# https://github.com/wilfred-dev/wilfred                           #
+#                                                                  #
+####################################################################
+
+# use "0.0.0.dev0" to indicate incremental commit build
+# specify version "0.3.0" to indicate standard release
+# "commit_date" should be replaced upon building
+version = "0.7.1"
+commit_hash = "dbe4dcfd942ff4c7ad65fdeffafff7434ad2d46e"
+commit_date = "2020-06-19"
