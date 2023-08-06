c509a219bc50a235db8d5b960c95104cdc501ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:18 +0000

    Add package asyncdagpi/1.0.0/asyncdagpi-1.0.0.tar.gz

---
+++ packages/asyncdagpi/asyncdagpi-1.0.0.tar.gz/asyncdagpi-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="asyncdagpi",
+    packages=["asyncdagpi"],
+    version="1.0.0",
+    pacakges=find_packages(),
+    license="MIT",
+    description="Asynchronous API wrapper for the Dagbot API (http://dagpi.tk)",
+    url="https://github.com/Daggy1234/asyncdagpi",
+    download_url="https://github.com/Daggy1234/asyncdagpi/archive/1.0.1.tar.gz",
+    author="Daggy1234",
+    install_requires=["aiohttp"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
