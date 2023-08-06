8fa583f62ab5bf464c30a271883c882f16e8fc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:52 +0000

    Add package pyduinobridge/1.0.7/pyduinobridge-1.0.7.tar.gz

---
+++ packages/pyduinobridge/pyduinobridge-1.0.7.tar.gz/pyduinobridge-1.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyduinobridge",
+    version="1.0.7",
+    author="Daniel Saromo",
+    author_email="danielsaromo@gmail.com",
+    description="A transparent library for communication between Python and Arduino",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanielSaromo/PyDuinoBridge",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
