d99efb63651a040b775399b6ad5054fe41cc3b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:44 +0000

    Add package pymocko/0.1.3/pymocko-0.1.3-py3-none-any.whl

---
+++ packages/pymocko/pymocko-0.1.3-py3-none-any.whl/pymocko/model.py
@@ -0,0 +1,45 @@
+import random as r
+from . import char
+from . import date
+from . import num
+from . import price
+from . import word
+
+def mock(schema_json=None, count=10):
+    result = []
+    for i in range(count):
+        data = {}
+        for key, value in schema_json.items():
+            if type(value) is dict:
+                for child_key, child_value in value.items():
+                    data[key] = mock(schema_json=value, count=1)[0]
+            elif type(value) is list:
+                data[key] = mock(schema_json=value[0], count=count)
+            elif value == "char":
+                data[key] = char.mock()
+            elif value == "date":
+                data[key] = str(date.mock())
+            elif value == "num":
+                data[key] = num.mock()
+            elif value == "price":
+                data[key] = price.mock()
+            else:
+                normalize_key = key.replace("_","")
+                if "username" in normalize_key:
+                    data[key] = word.mock(no_of_word=1)
+                elif "name" in normalize_key:
+                    data[key] = word.mock(no_of_word=r.choice([2,3]),upper_first_all=True)
+                elif "gender" in normalize_key:
+                    data[key] = word.mock_gender(upper_first=True)
+                elif "phone" in normalize_key:
+                    data[key] = word.mock_phone()
+                elif "mail" in normalize_key:
+                    data[key] = word.mock_email()
+                elif "ipaddr" in normalize_key:
+                    data[key] = word.mock_ip_address()
+                else:
+                    data[key] = word.mock(upper_first=True)
+        
+        result.append(data)
+
+    return result
