2a83fc822231c27185b18bd65e39e9ef4c746958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:11 +0000

    Add package blc/0.1.2/blc-0.1.2.tar.gz

---
+++ packages/blc/blc-0.1.2.tar.gz/blc-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['blc', 'blc.handlers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cleo>=0.8.1,<0.9.0',
+ 'pendulum>=2.1.0,<3.0.0',
+ 'requests>=2.23.0,<3.0.0',
+ 'rich>=1.3.1,<2.0.0',
+ 'tinydb>=4.1.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'blc',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Dimitrios Strantsalis',
+    'author_email': 'dstrants@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
