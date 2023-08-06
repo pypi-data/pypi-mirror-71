3d16c05a18c86f6e2917d82728d9da0166223cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:35 +0000

    Add package md2book/0.0.1/md2book-0.0.1.tar.gz

---
+++ packages/md2book/md2book-0.0.1.tar.gz/md2book-0.0.1/md2book/convert/forms_text.py
@@ -0,0 +1,103 @@
+import re
+from pathlib import Path
+from urllib.request import urlopen
+
+from .forms import PureCodeData
+from md2book.util.exceptions import ParsingError
+from md2book.config import *
+from md2book.formats.mddocx import purify_for_docx
+from md2book.formats.mdtxt import purify_for_txt
+
+class MarkdownCode(PureCodeData):
+	EXT = "md"
+	
+	def __init__(self, code=""):
+		self.code = code
+		self.meta_code = ""
+
+	def set_conf(self, target):
+		super().set_conf(target)
+		self.fill_template(target)
+
+		if target.mods['metadata']:
+			self.meta_code = target.mods['metadata'].get_yaml_intro()
+
+	def clear_for(self, target_format):
+		""" Remove parts that are not needed in some documents """
+		if target_format in ['docx']:
+			self.code = purify_for_docx(self.code)
+		elif target_format in ['txt']:
+			self.code = purify_for_txt(self.code)
+
+	def get(self):
+		return self.meta_code + self.code
+
+	def get_base_code_only(self):
+		return self.code
+
+class TxtCode(PureCodeData):
+	EXT = "txt"
+
+class HtmlCode(PureCodeData):
+	EXT = "html"
+	def __init__(self, code, title, headers=None):
+		self.code = code
+		self.title = title
+		self.headers = headers or []
+
+	def set_conf(self, target):
+		super().set_conf(target)
+		
+		target.load_linked_files()
+		for style in target.stylesheets:
+			self.addStyle(style)
+		for script in target.scripts:
+			self.addScript(script)
+
+	def getHtmlTag(self, tag, content, **keys_vals):
+		keys = ''.join([' {}="{}"'.format(key, val) for key, val in keys_vals.items()])
+		if content == None:
+			htmltag = '<{tag}{keys} />'
+		else:
+			htmltag = '<{tag}{keys}>{content}</{tag}>'
+		return htmltag.format(tag=tag, content=str(content), keys=keys)
+
+	def addHeader(self, tag, content, **keys_vals):
+		self.headers.append(self.getHtmlTag(tag, content, **keys_vals))
+
+	def addStyle(self, path, base_path=""):
+		path = str(path)
+		try:
+			f = urlopen(path)
+		except ValueError:
+			try:
+				f = open(str(Path(base_path) / path))
+			except:
+				if 'styles/themes/' in path:
+					theme = path.split('/')[-1].split('.')[0]
+					raise ParsingError('The theme "{}" doesn\'t exists'.format(theme))
+				raise ParsingError('Can\'t find the stylesheet {}'.format(path))
+		self.addHeader('style', f.read())
+
+	def addScript(self, path, base_path=""):
+		base_path = Path(base_path or self.target.path.parent)
+		path = str(path)
+		try:
+			f = urlopen(path)
+			self.addHeader('script', '', src=path)
+		except ValueError:
+			try:
+				f = open(str(base_path / path))
+			except:
+				raise ParsingError('Can\'t find the script {}'.format(path))
+			self.addHeader('script', f.read())
+
+	def get(self):
+		with open(HTML_TEMPLATE) as f:
+			html_struct = f.read()
+		code = html_struct.format(
+			headers="\n".join(self.headers),
+			title=self.title,
+			body=self.code
+		)
+		return code
---
+++ packages/md2book/md2book-0.0.1.tar.gz/md2book-0.0.1/md2book/util/common.py
@@ -0,0 +1,80 @@
+import subprocess, os, platform, fnmatch, yaml
+import random, string
+from copy import deepcopy
+from urllib.request import urlretrieve
+from pathlib import Path
+
+from .exceptions import ConfigError
+from md2book.config import *
+
+# -------------------- UTILITY -------------------- #
+
+def merge_dicts_recur(settings, overrride):
+	if overrride is None:
+		return settings
+	for key, val in overrride.items():
+		if isinstance(val, dict) and isinstance(settings.get(key, None), dict):
+			merge_dicts_recur(settings[key], val)
+		else:
+			settings[key] = deepcopy(val)
+	return settings
+
+def rand_str(n):
+	return "".join(random.choices(string.ascii_lowercase, k=n))
+
+def is_int(n):
+	try:
+		m = int(n)
+		return True
+	except:
+		return False
+
+# -------------------- SYSYTEM -------------------- #
+
+def sys_open(filepath):
+	filepath = str(filepath)
+	if platform.system() == 'Darwin':       # macOS
+		subprocess.call(('open', filepath))
+	elif platform.system() == 'Windows':    # Windows
+		os.startfile(filepath)
+	else:                                   # linux variants
+		subprocess.call(('xdg-open', filepath))
+
+# -------------------- FILES -------------------- #
+
+def escapePath(path):
+	path = str(path)
+	path = path.replace(" ", "\\ ")
+	return path
+
+def find_files_matching(path, pattern):
+	if path.is_file():
+		if fnmatch.fnmatch(path.name, pattern):
+			yield path
+	elif path.is_dir():
+		for sub_path in path.iterdir():
+			yield from find_files_matching(sub_path, pattern)
+
+def load_yaml_file(filepath, default=None):
+	filepath = str(filepath)
+	try:
+		with open(filepath, "r", encoding="utf-8") as f:
+			try:
+				return yaml.load(f, Loader=yaml.FullLoader)
+			except:
+				raise ConfigError("Invalid yaml format", filepath)
+	except FileNotFoundError:
+		if default is None:
+			raise ConfigError("This file doesn't exists", filepath)
+	return default
+
+def get_document_local_path(path, base_path=''):
+	complete_path = Path(base_path) / path
+	if complete_path.exists() and complete_path.is_file():
+		return str(complete_path)
+	try:
+		p = TMP_DIRS[0] / rand_str(20)
+		urlretrieve(path, p)
+	except ValueError:
+		return str(path)
+	return str(p)
