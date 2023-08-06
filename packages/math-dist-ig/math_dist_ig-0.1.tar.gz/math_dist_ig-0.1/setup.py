9d8c2a5c3b568670334e0159b599cb459357a324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:35 +0000

    Add package math-dist-ig/0.1/math_dist_ig-0.1.tar.gz

---
+++ packages/math-dist-ig/math_dist_ig-0.1.tar.gz/math_dist_ig-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='math_dist_ig',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['math_dist_ig'],
+      author="Igor Tunde",
+      author_email="igortunde@yahoo.com",
+      zip_safe=False)
