e2ba94ddd41e8a039adc0a50d23e24d2d3cbc937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:25 +0000

    Add package pantsbuild.pants.contrib.go/1.26.1rc0/pantsbuild.pants.contrib.go-1.26.1rc0-py36.py37-none-any.whl

---
+++ packages/pantsbuild.pants.contrib.go/pantsbuild.pants.contrib.go-1.26.1rc0-py36.py37-none-any.whl/pants/contrib/go/tasks/go_checkstyle.py
@@ -0,0 +1,21 @@
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.base.exceptions import TaskError
+from pants.task.lint_task_mixin import LintTaskMixin
+
+from pants.contrib.go.tasks.go_fmt_task_base import GoFmtTaskBase
+
+
+class GoCheckstyle(LintTaskMixin, GoFmtTaskBase):
+    """Checks Go code matches gofmt style."""
+
+    @property
+    def skip_execution(self):
+        return super()._resolve_conflicting_skip(old_scope="lint-go")
+
+    def execute(self):
+        with self.go_fmt_invalid_targets(["-d"]) as output:
+            if output:
+                self.context.log.error(output)
+                raise TaskError("Found style errors. Use `./pants fmt` to fix.")
