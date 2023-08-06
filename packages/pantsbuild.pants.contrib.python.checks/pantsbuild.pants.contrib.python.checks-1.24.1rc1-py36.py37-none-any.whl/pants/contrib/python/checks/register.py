72734cf8c8001aeb3afd9d2236898b3f6fac67d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:59 +0000

    Add package pantsbuild.pants.contrib.python.checks/1.24.1rc1/pantsbuild.pants.contrib.python.checks-1.24.1rc1-py36.py37-none-any.whl

---
+++ packages/pantsbuild.pants.contrib.python.checks/pantsbuild.pants.contrib.python.checks-1.24.1rc1-py36.py37-none-any.whl/pants/contrib/python/checks/register.py
@@ -0,0 +1,12 @@
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from pants.goal.task_registrar import TaskRegistrar as task
+
+from pants.contrib.python.checks.tasks.checkstyle.checkstyle import Checkstyle
+from pants.contrib.python.checks.tasks.python_eval import PythonEval
+
+
+def register_goals():
+  task(name='python-eval', action=PythonEval).install('lint')
+  task(name='pythonstyle', action=Checkstyle).install('lint')
