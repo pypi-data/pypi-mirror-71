fbd5c1ac8b69b3a1b810088b782a4e6c07836ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:58 +0000

    Add package qda-modelos/1.0.2/qda_modelos-1.0.2.tar.gz

---
+++ packages/qda-modelos/qda_modelos-1.0.2.tar.gz/qda_modelos-1.0.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name="qda_modelos",
+    packages=["qda_modelos"],
+    version="1.0.2",
+    license="BSD",
+    description="Implement bio-optic models to evaluate water quality indexes with satellite images.",
+    author="CERTI Foundation",
+    author_email="qda-pypi@certi.org.br",
+    url="https://github.com/fundacaocerti/qda_modelos",
+    download_url="https://github.com/fundacaocerti/qda_modelos/archive/v1.0.0.tar.gz",
+    keywords=[
+        "bio-optic models",
+        "water quality",
+        "indexes",
+        "satellite image",
+        "reservoir",
+        "lake",
+        "water",
+        "remote sensing",
+    ],
+    install_requires=["numpy", "qda_modelos", "rasterio"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
