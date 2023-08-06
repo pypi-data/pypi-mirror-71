9b9e79fdc91dbc15267cbc99e299fd015902bd0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:20 +0000

    Add package distributions-g111/0.1/distributions_g111-0.1.tar.gz

---
+++ packages/distributions-g111/distributions_g111-0.1.tar.gz/distributions_g111-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_g111',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_g111'],
+      zip_safe=False)
