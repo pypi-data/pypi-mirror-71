07c921cd007307fff98956f5caa9a7ddab1513ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:15 +0000

    Add package probpackage/1.2/probpackage-1.2.tar.gz

---
+++ packages/probpackage/probpackage-1.2.tar.gz/probpackage-1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='probpackage',
+      version='1.2',
+      description='Gaussian and Binominal Distributions ',
+      packages=['probpackage'],
+      author= 'mohammed lokma',
+      author_mail ='mesholokma@gmail.com',
+
+      zip_safe=False)
