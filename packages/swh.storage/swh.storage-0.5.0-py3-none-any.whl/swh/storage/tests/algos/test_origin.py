38b461dc09efcd7797974bff7a6ed611de8190dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:35 +0000

    Add package swh.storage/0.5.0/swh.storage-0.5.0-py3-none-any.whl

---
+++ packages/swh.storage/swh.storage-0.5.0-py3-none-any.whl/swh/storage/tests/algos/test_origin.py
@@ -0,0 +1,66 @@
+# Copyright (C) 2019  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from unittest.mock import patch
+
+from swh.storage import get_storage
+from swh.storage.algos.origin import iter_origins
+
+
+def assert_list_eq(left, right, msg=None):
+    assert list(left) == list(right), msg
+
+
+storage_config = {"cls": "validate", "storage": {"cls": "memory",}}
+
+
+def test_iter_origins():
+    storage = get_storage(**storage_config)
+    origins = storage.origin_add([{"url": "bar"}, {"url": "qux"}, {"url": "quuz"},])
+    assert_list_eq(iter_origins(storage), origins)
+    assert_list_eq(iter_origins(storage, batch_size=1), origins)
+    assert_list_eq(iter_origins(storage, batch_size=2), origins)
+
+    for i in range(1, 5):
+        assert_list_eq(iter_origins(storage, origin_from=i + 1), origins[i:], i)
+
+        assert_list_eq(
+            iter_origins(storage, origin_from=i + 1, batch_size=1), origins[i:], i
+        )
+
+        assert_list_eq(
+            iter_origins(storage, origin_from=i + 1, batch_size=2), origins[i:], i
+        )
+
+        for j in range(i, 5):
+            assert_list_eq(
+                iter_origins(storage, origin_from=i + 1, origin_to=j + 1),
+                origins[i:j],
+                (i, j),
+            )
+
+            assert_list_eq(
+                iter_origins(storage, origin_from=i + 1, origin_to=j + 1, batch_size=1),
+                origins[i:j],
+                (i, j),
+            )
+
+            assert_list_eq(
+                iter_origins(storage, origin_from=i + 1, origin_to=j + 1, batch_size=2),
+                origins[i:j],
+                (i, j),
+            )
+
+
+@patch("swh.storage.in_memory.InMemoryStorage.origin_get_range")
+def test_iter_origins_batch_size(mock_origin_get_range):
+    storage = get_storage(**storage_config)
+    mock_origin_get_range.return_value = []
+
+    list(iter_origins(storage))
+    mock_origin_get_range.assert_called_with(origin_from=1, origin_count=10000)
+
+    list(iter_origins(storage, batch_size=42))
+    mock_origin_get_range.assert_called_with(origin_from=1, origin_count=42)
---
+++ packages/swh.storage/swh.storage-0.5.0-py3-none-any.whl/swh/storage/tests/algos/test_snapshot.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2018  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+from hypothesis import given
+import pytest
+
+from swh.model.identifiers import snapshot_identifier, identifier_to_bytes
+from swh.model.hypothesis_strategies import snapshots, branch_names, branch_targets
+
+from swh.storage.algos.snapshot import snapshot_get_all_branches
+
+
+@pytest.fixture
+def swh_storage_backend_config():
+    yield {
+        "cls": "memory",
+        "journal_writer": None,
+    }
+
+
+@given(snapshot=snapshots(min_size=0, max_size=10, only_objects=False))
+def test_snapshot_small(swh_storage, snapshot):  # noqa
+    snapshot = snapshot.to_dict()
+    swh_storage.snapshot_add([snapshot])
+
+    returned_snapshot = snapshot_get_all_branches(swh_storage, snapshot["id"])
+    assert snapshot == returned_snapshot
+
+
+@given(branch_name=branch_names(), branch_target=branch_targets(only_objects=True))
+def test_snapshot_large(swh_storage, branch_name, branch_target):  # noqa
+    branch_target = branch_target.to_dict()
+
+    snapshot = {
+        "branches": {b"%s%05d" % (branch_name, i): branch_target for i in range(10000)}
+    }
+    snapshot["id"] = identifier_to_bytes(snapshot_identifier(snapshot))
+
+    swh_storage.snapshot_add([snapshot])
+
+    returned_snapshot = snapshot_get_all_branches(swh_storage, snapshot["id"])
+    assert snapshot == returned_snapshot
