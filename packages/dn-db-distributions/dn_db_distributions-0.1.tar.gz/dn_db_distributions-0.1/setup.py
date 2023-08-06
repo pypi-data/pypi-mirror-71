dc7f68ff686d1605216243f0f359bce02eac4215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:31 +0000

    Add package dn-db-distributions/0.1/dn_db_distributions-0.1.tar.gz

---
+++ packages/dn-db-distributions/dn_db_distributions-0.1.tar.gz/dn_db_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dn_db_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dn_db_distributions'],
+      autor = 'Andres Viana',
+      autor_email = 'andres.viana.viana@gmail.com',
+    zip_safe=False)
