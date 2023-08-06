3f5b4d361c59eab0c61c46d91bcbdadf0296bbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:41 +0000

    Add package lshashpy3/0.0.5/lshashpy3-0.0.5.tar.gz

---
+++ packages/lshashpy3/lshashpy3-0.0.5.tar.gz/lshashpy3-0.0.5/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import lshashpy3
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst', encoding='utf-8') as f:
+    readme = f.read()
+
+with open('CHANGES.rst', encoding='utf-8') as f:
+    changes = f.read()
+
+required = ['numpy', 'bitarray']
+
+setup(
+    name='lshashpy3',
+    version=lshashpy3.__version__,
+    packages=['lshashpy3'],
+    author='Kay Zhu',
+    author_email='me@kayzhu.com',
+    maintainer='Loreto Parisi',
+    maintainer_email='loretoparisi@gmail.com',
+    description='A fast Python 3 implementation of locality sensitive hashing with persistance support.',
+    long_description=readme + '\n\n' + changes,
+    long_description_content_type='text/x-rst',
+    license='MIT License',
+    requires=required,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        ]
+)
