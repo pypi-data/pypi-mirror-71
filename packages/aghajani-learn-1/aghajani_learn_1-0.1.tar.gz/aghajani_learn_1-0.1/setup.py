a39797364936c463adc01ebad1505fb7d38e9bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:17 +0000

    Add package aghajani-learn-1/0.1/aghajani_learn_1-0.1.tar.gz

---
+++ packages/aghajani-learn-1/aghajani_learn_1-0.1.tar.gz/aghajani_learn_1-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='aghajani_learn_1',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['aghajani_learn_1'],
+      zip_safe=False)
