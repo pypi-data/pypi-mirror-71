4fb94d15e38673ffa89fb8e1a156f0fc7dd0e5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:43 +0000

    Add package legimens/0.2.1/legimens-0.2.1.tar.gz

---
+++ packages/legimens/legimens-0.2.1.tar.gz/legimens-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='legimens',
+    version='0.2.1',
+    license='MIT',
+
+    packages=find_packages(),
+    python_requires='>=3.6',
+
+    install_requires = ['loguru', 'trio-websocket'],
+    setup_requires = ['pytest-runner'],
+    tests_require  = ['pytest'],
+
+    test_suite='tests',
+)
