a81ead4a74fa7795376358ec1d8ba84a4b632999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:44 +0000

    Add package G-Bdistribution/1/G&Bdistribution-1.tar.gz

---
+++ packages/G-Bdistribution/G&Bdistribution-1.tar.gz/G&Bdistribution-1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='G&Bdistribution',
+      version='01',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author ='Ravineel Singhi',
+      author_email ='rajatsinghi89@gmail.com',
+      zip_safe=False)
