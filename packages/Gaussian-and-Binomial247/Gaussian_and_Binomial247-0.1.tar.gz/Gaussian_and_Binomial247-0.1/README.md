460f33ed7d920571d5a9a87efc4ade04b3f70775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:24 +0000

    Add package Gaussian-and-Binomial247/0.1/Gaussian_and_Binomial247-0.1.tar.gz

---
+++ packages/Gaussian-and-Binomial247/Gaussian_and_Binomial247-0.1.tar.gz/Gaussian_and_Binomial247-0.1/README.md
@@ -0,0 +1,3 @@
+# GaussianBinomial Package
+
+This is a simple package which instantiates and visualizes the Gaussian and Binomial distributions. 
---
+++ packages/Gaussian-and-Binomial247/Gaussian_and_Binomial247-0.1.tar.gz/Gaussian_and_Binomial247-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Gaussian_and_Binomial247',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Gaussian_and_Binomial247'],
+      zip_safe=False)
