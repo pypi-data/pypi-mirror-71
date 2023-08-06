da5aed095e0e54c26c62cf0087481f362603c244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:28 +0000

    Add package dis-tri-but-ion/0.1/dis_tri_but_ion-0.1.tar.gz

---
+++ packages/dis-tri-but-ion/dis_tri_but_ion-0.1.tar.gz/dis_tri_but_ion-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dis_tri_but_ion',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
