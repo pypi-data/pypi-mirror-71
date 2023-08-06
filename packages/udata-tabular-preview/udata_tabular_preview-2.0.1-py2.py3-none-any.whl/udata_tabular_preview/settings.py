ca34af92d3ea0eb52f95d51dd46b7ecce6507911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:58 +0000

    Add package udata-tabular-preview/2.0.1/udata_tabular_preview-2.0.1-py2.py3-none-any.whl

---
+++ packages/udata-tabular-preview/udata_tabular_preview-2.0.1-py2.py3-none-any.whl/udata_tabular_preview/settings.py
@@ -0,0 +1,17 @@
+'''
+Default settings for udata-tabular-preview
+'''
+# csvapi instance URL
+TABULAR_CSVAPI_URL = None
+
+# Preview UI, one of 'csvapi-front', 'data
+TABULAR_UI = 'csvapi-front'
+
+# Whether or not to allow remote resources
+TABULAR_ALLOW_REMOTE = True
+
+# Max (included) allowed file size in bytes
+TABULAR_MAX_SIZE = None
+
+# Default page size
+TABULAR_PAGE_SIZE = 50
