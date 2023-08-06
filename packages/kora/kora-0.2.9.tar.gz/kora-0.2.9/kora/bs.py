08a809b465c7b8a30e1e6d7c81113a803bc015a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:18 +0000

    Add package kora/0.2.9/kora-0.2.9.tar.gz

---
+++ packages/kora/kora-0.2.9.tar.gz/kora-0.2.9/kora/bs.py
@@ -0,0 +1,15 @@
+""" What bs4 should have been """
+
+from bs4 import BeautifulSoup
+import requests 
+
+
+def Soup(s, features='lxml', **kw):
+    """ A fake class, of what BeautifulSoup should have been """
+    if s.startswith('http'):
+        src = requests.get(s).text
+        return BeautifulSoup(src, features, **kw)
+    if s.startswith('/') or s.startswith("."):
+        src = open(s).read()
+        return BeautifulSoup(src, features, **kw)
+    return BeautifulSoup(s, features, **kw)
