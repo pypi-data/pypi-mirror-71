61b59fe06ee4c7c16d6b16425a28bb9b38fc6793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:20 +0000

    Add package torchaudio/0.5.1/torchaudio-0.5.1-cp35-cp35m-macosx_10_6_x86_64.whl

---
+++ packages/torchaudio/torchaudio-0.5.1-cp35-cp35m-macosx_10_6_x86_64.whl/torchaudio/datasets/commonvoice.py
@@ -0,0 +1,133 @@
+import os
+
+from torch.utils.data import Dataset
+
+import torchaudio
+from torchaudio.datasets.utils import download_url, extract_archive, unicode_csv_reader
+
+# Default TSV should be one of
+# dev.tsv
+# invalidated.tsv
+# other.tsv
+# test.tsv
+# train.tsv
+# validated.tsv
+
+FOLDER_IN_ARCHIVE = "CommonVoice"
+URL = "english"
+VERSION = "cv-corpus-4-2019-12-10"
+TSV = "train.tsv"
+
+
+def load_commonvoice_item(line, header, path, folder_audio):
+    # Each line as the following data:
+    # client_id, path, sentence, up_votes, down_votes, age, gender, accent
+
+    assert header[1] == "path"
+    fileid = line[1]
+
+    filename = os.path.join(path, folder_audio, fileid)
+
+    waveform, sample_rate = torchaudio.load(filename)
+
+    dic = dict(zip(header, line))
+
+    return waveform, sample_rate, dic
+
+
+class COMMONVOICE(Dataset):
+    """
+    Create a Dataset for CommonVoice. Each item is a tuple of the form:
+    (waveform, sample_rate, dictionary)
+    where dictionary is a dictionary built from the tsv file with the following keys:
+    client_id, path, sentence, up_votes, down_votes, age, gender, accent.
+    """
+
+    _ext_txt = ".txt"
+    _ext_audio = ".mp3"
+    _folder_audio = "clips"
+
+    def __init__(self, root,
+                 tsv=TSV,
+                 url=URL,
+                 folder_in_archive=FOLDER_IN_ARCHIVE,
+                 version=VERSION,
+                 download=False):
+
+        languages = {
+            "tatar": "tt",
+            "english": "en",
+            "german": "de",
+            "french": "fr",
+            "welsh": "cy",
+            "breton": "br",
+            "chuvash": "cv",
+            "turkish": "tr",
+            "kyrgyz": "ky",
+            "irish": "ga-IE",
+            "kabyle": "kab",
+            "catalan": "ca",
+            "taiwanese": "zh-TW",
+            "slovenian": "sl",
+            "italian": "it",
+            "dutch": "nl",
+            "hakha chin": "cnh",
+            "esperanto": "eo",
+            "estonian": "et",
+            "persian": "fa",
+            "portuguese": "pt",
+            "basque": "eu",
+            "spanish": "es",
+            "chinese": "zh-CN",
+            "mongolian": "mn",
+            "sakha": "sah",
+            "dhivehi": "dv",
+            "kinyarwanda": "rw",
+            "swedish": "sv-SE",
+            "russian": "ru",
+            "indonesian": "id",
+            "arabic": "ar",
+            "tamil": "ta",
+            "interlingua": "ia",
+            "latvian": "lv",
+            "japanese": "ja",
+            "votic": "vot",
+            "abkhaz": "ab",
+            "cantonese": "zh-HK",
+            "romansh sursilvan": "rm-sursilv"
+        }
+
+        if url in languages:
+            ext_archive = ".tar.gz"
+            language = languages[url]
+
+            base_url = "https://voice-prod-bundler-ee1969a6ce8178826482b88e843c335139bd3fb4.s3.amazonaws.com"
+            url = os.path.join(base_url, version, language + ext_archive)
+
+        basename = os.path.basename(url)
+        archive = os.path.join(root, basename)
+
+        basename = basename.rsplit(".", 2)[0]
+        folder_in_archive = os.path.join(folder_in_archive, version, basename)
+
+        self._path = os.path.join(root, folder_in_archive)
+
+        if download:
+            if not os.path.isdir(self._path):
+                if not os.path.isfile(archive):
+                    download_url(url, root)
+                extract_archive(archive, self._path)
+
+        self._tsv = os.path.join(root, folder_in_archive, tsv)
+
+        with open(self._tsv, "r") as tsv:
+            walker = unicode_csv_reader(tsv, delimiter="\t")
+            self._header = next(walker)
+            self._walker = list(walker)
+
+    def __getitem__(self, n):
+        line = self._walker[n]
+        return load_commonvoice_item(line, self._header, self._path, self._folder_audio)
+
+    def __len__(self):
+        return len(self._walker)
---
+++ packages/torchaudio/torchaudio-0.5.1-cp35-cp35m-macosx_10_6_x86_64.whl/torchaudio/datasets/vctk.py
@@ -0,0 +1,125 @@
+import os
+import warnings
+
+import torchaudio
+from torch.utils.data import Dataset
+from torchaudio.datasets.utils import download_url, extract_archive, walk_files
+
+URL = "http://homepages.inf.ed.ac.uk/jyamagis/release/VCTK-Corpus.tar.gz"
+FOLDER_IN_ARCHIVE = "VCTK-Corpus"
+
+
+def load_vctk_item(
+    fileid, path, ext_audio, ext_txt, folder_audio, folder_txt, downsample=False
+):
+    speaker_id, utterance_id = fileid.split("_")
+
+    # Read text
+    file_txt = os.path.join(path, folder_txt, speaker_id, fileid + ext_txt)
+    with open(file_txt) as file_text:
+        utterance = file_text.readlines()[0]
+
+    # Read wav
+    file_audio = os.path.join(path, folder_audio, speaker_id, fileid + ext_audio)
+    waveform, sample_rate = torchaudio.load(file_audio)
+    if downsample:
+        # TODO Remove this parameter after deprecation
+        F = torchaudio.functional
+        T = torchaudio.transforms
+        # rate
+        sample = T.Resample(sample_rate, 16000, resampling_method='sinc_interpolation')
+        waveform = sample(waveform)
+        # dither
+        waveform = F.dither(waveform, noise_shaping=True)
+
+    return waveform, sample_rate, utterance, speaker_id, utterance_id
+
+
+class VCTK(Dataset):
+    """
+    Create a Dataset for VCTK. Each item is a tuple of the form:
+    (waveform, sample_rate, utterance, speaker_id, utterance_id)
+
+    Folder `p315` will be ignored due to the non-existent corresponding text files.
+    For more information about the dataset visit: https://datashare.is.ed.ac.uk/handle/10283/3443
+    """
+
+    _folder_txt = "txt"
+    _folder_audio = "wav48"
+    _ext_txt = ".txt"
+    _ext_audio = ".wav"
+    _except_folder = "p315"
+
+    def __init__(
+        self,
+        root,
+        url=URL,
+        folder_in_archive=FOLDER_IN_ARCHIVE,
+        download=False,
+        downsample=False,
+        transform=None,
+        target_transform=None,
+    ):
+
+        if downsample:
+            warnings.warn(
+                "In the next version, transforms will not be part of the dataset. "
+                "Please use `downsample=False` to enable this behavior now, ",
+                "and suppress this warning."
+            )
+
+        if transform is not None or target_transform is not None:
+            warnings.warn(
+                "In the next version, transforms will not be part of the dataset. "
+                "Please remove the option `transform=True` and "
+                "`target_transform=True` to suppress this warning."
+            )
+
+        self.downsample = downsample
+        self.transform = transform
+        self.target_transform = target_transform
+
+        archive = os.path.basename(url)
+        archive = os.path.join(root, archive)
+        self._path = os.path.join(root, folder_in_archive)
+
+        if download:
+            if not os.path.isdir(self._path):
+                if not os.path.isfile(archive):
+                    download_url(url, root)
+                extract_archive(archive)
+
+        if not os.path.isdir(self._path):
+            raise RuntimeError(
+                "Dataset not found. Please use `download=True` to download it."
+            )
+
+        walker = walk_files(
+            self._path, suffix=self._ext_audio, prefix=False, remove_suffix=True
+        )
+        walker = filter(lambda w: self._except_folder not in w, walker)
+        self._walker = list(walker)
+
+    def __getitem__(self, n):
+        fileid = self._walker[n]
+        item = load_vctk_item(
+            fileid,
+            self._path,
+            self._ext_audio,
+            self._ext_txt,
+            self._folder_audio,
+            self._folder_txt,
+        )
+
+        # TODO Upon deprecation, uncomment line below and remove following code
+        # return item
+
+        waveform, sample_rate, utterance, speaker_id, utterance_id = item
+        if self.transform is not None:
+            waveform = self.transform(waveform)
+        if self.target_transform is not None:
+            utterance = self.target_transform(utterance)
+        return waveform, sample_rate, utterance, speaker_id, utterance_id
+
+    def __len__(self):
+        return len(self._walker)
