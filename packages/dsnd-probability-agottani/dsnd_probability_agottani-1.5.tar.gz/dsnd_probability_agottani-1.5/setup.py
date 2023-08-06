02d7508508a396c979cfd239ea32452c183d5e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:35 +0000

    Add package dsnd-probability-agottani/1.5/dsnd_probability_agottani-1.5.tar.gz

---
+++ packages/dsnd-probability-agottani/dsnd_probability_agottani-1.5.tar.gz/dsnd_probability_agottani-1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_agottani',
+      version='1.5',
+      description='Gaussian and Binomial Distributions',
+      packages=['dsnd_probability_agottani'],
+      author = 'Luis Fernando Rodrigues Agottani',
+      author_email = 'luisfernandoengmec@gmail.com',
+      zip_safe=False)
