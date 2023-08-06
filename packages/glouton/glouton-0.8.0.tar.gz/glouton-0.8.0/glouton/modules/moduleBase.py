936eaefe914286c95adbf613381d6ea13652d31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:06 +0000

    Add package glouton/0.8.0/glouton-0.8.0.tar.gz

---
+++ packages/glouton/glouton-0.8.0.tar.gz/glouton-0.8.0/glouton/modules/moduleBase.py
@@ -0,0 +1,8 @@
+import os
+
+class ModuleBase:
+    def __init__(self, working_dir):
+        self.working_dir = working_dir
+
+    def runAfterDownload(self, file_name, full_path, observation):
+        raise NotImplementedError()
---
+++ packages/glouton/glouton-0.8.0.tar.gz/glouton-0.8.0/glouton/services/module/moduleService.py
@@ -0,0 +1,81 @@
+import importlib
+from glouton.shared.logger import logger
+from glouton.shared import config
+
+
+class ModuleService:
+    def __init__(self, cmd):
+        self.__cmd = cmd
+        self.__config = config.read()
+
+    def loadDemoddataModules(self):
+        logger.Info('Demoddata module(s) loading :')
+        self.__cmd.demoddata_modules = self.__getModulesFromConfig(
+            self.__cmd.demoddata_modules, 'DEMODDATA')
+        self.__cmd.demoddata_modules = self.__getModulesFromConfig(
+            self.__cmd.demoddata_modules, 'FOR_ALL_OBSERVATION')
+
+        return self.__loadModule(self.__cmd.demoddata_modules)
+
+    def loadArchiveModules(self):
+        logger.Info('Archive module(s) loading :')
+        self.__cmd.archive_modules = self.__getModulesFromConfig(
+            self.__cmd.archive_modules, 'ARCHIVE')
+        self.__cmd.archive_modules = self.__getModulesFromConfig(
+            self.__cmd.archive_modules, 'FOR_ALL_OBSERVATION')
+
+        return self.__loadModule(self.__cmd.archive_modules)
+
+    def loadWaterfallModules(self):
+        logger.Info('Waterfall module(s) loading :')
+        self.__cmd.waterfall_modules = self.__getModulesFromConfig(
+            self.__cmd.waterfall_modules, 'WATERFALL')
+        self.__cmd.waterfall_modules = self.__getModulesFromConfig(
+            self.__cmd.waterfall_modules, 'FOR_ALL_OBSERVATION')
+
+        return self.__loadModule(self.__cmd.waterfall_modules)
+
+    def loadFrameModules(self):
+        logger.Info('Frame module(s) loading :')
+        self.__cmd.frame_modules = self.__getModulesFromConfig(
+            self.__cmd.frame_modules, 'FRAME')
+
+        return self.__loadModule(self.__cmd.frame_modules)
+
+    def __loadModule(self, modules):
+        if modules is None:
+            logger.Info('No module list found')
+            return None
+
+        loaded_modules = []
+        for name in modules:
+            loaded_module = importlib.import_module(
+                'glouton.modules.' + name.lower())
+            module = getattr(loaded_module, name)
+            loaded_modules.append(module(self.__cmd.working_dir))
+            logger.Info('module : ' + name + ' loaded')
+        return loaded_modules
+
+    def __getModulesFromConfig(self, modules, config_array_name):
+        try:
+            modules_from_config = self.__config['MODULES'][config_array_name]
+        except:
+            logger.Warning('config.json : modules bad format')
+            modules_from_config = []
+
+        if len(modules_from_config) == 0 and modules is None:
+            return None
+
+        if len(modules_from_config) == 0 and modules is not None:
+            return modules
+
+        if modules is None:
+            modules = []
+
+        for module in modules_from_config:
+            if module in modules:
+                logger.Warning('warning : ' + module + ' already referenced.')
+
+            modules.append(module)
+
+        return modules
