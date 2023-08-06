0cf04f8c6d534ba081bc9e3c6763916bf0c255a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:11 +0000

    Add package statistics-generator/0.1/statistics_generator-0.1.tar.gz

---
+++ packages/statistics-generator/statistics_generator-0.1.tar.gz/statistics_generator-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='statistics_generator',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['statistics_generator'],
+      zip_safe=False)
