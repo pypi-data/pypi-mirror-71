3e5435166d50c07018eed41903840ac7f20bc822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:40 +0000

    Add package blaze-distributions/0.1/blaze-distributions-0.1.tar.gz

---
+++ packages/blaze-distributions/blaze-distributions-0.1.tar.gz/blaze-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='blaze-distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['blaze_distributions'],
+      author= 'Toluwalope Oluyipe',
+      author_email= 'kingtoluwalope@gmail.com',
+      zip_safe=False)
