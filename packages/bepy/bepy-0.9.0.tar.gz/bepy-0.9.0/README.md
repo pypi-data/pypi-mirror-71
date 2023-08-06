7845c64f8f4b8cc23941275140a53ea044865ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package bepy/0.9.0/bepy-0.9.0.tar.gz

---
+++ packages/bepy/bepy-0.9.0.tar.gz/bepy-0.9.0/README.md
@@ -0,0 +1,2 @@
+# bepy
+A framework for materials characterization data collected via Resontant Piezoresponse Force Microscopy (R-PFM) that facilitates post collection analysis
---
+++ packages/bepy/bepy-0.9.0.tar.gz/bepy-0.9.0/bepy/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='bepy',
+      version='0.9.0',
+      description="A framework for materials characterization data collected that facilitates post analysis",
+      url='https://github.com/lgriffin39/bepy',
+      author='Lee Griffin',
+      author_email='lgriffin39@gatech.edu',
+      license='MIT',
+      packages=['bepy'],
+      install_requires=['numpy', 'pandas', 'matplotlib', 'scipy'],
+      zip_safe=False)
