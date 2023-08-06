4c4362fbaf3c641f8b00b39c61877cede3fe6aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:31 +0000

    Add package ppeservice/0.0.2/ppeservice-0.0.2.tar.gz

---
+++ packages/ppeservice/ppeservice-0.0.2.tar.gz/ppeservice-0.0.2/setup.py
@@ -0,0 +1,36 @@
+import os
+import re
+"""
+This is an personal website generator(could be any website or small services generator)
+It control build, deploy, url generation and all process needed by having a webpage
+"""
+from setuptools import setup, find_packages
+
+packages_required = []
+with open("requirements.txt", "r") as f:
+    for line in f:
+        packages_required.append(line[:-1])
+
+
+
+setup(
+    name='ppeservice',
+    version="0.0.2",
+    license='GPLv3',
+    author='Ye Zhang, Junqi Zhang',
+    author_email="zhangforchat@gmail.com",
+    url="https://github.com/JunqiZhang0/py-production-experiment",
+    description='Personal website generator(could be any website or small services generator)',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=packages_required,
+
+    classifiers=[
+        'Development Status :: 1 - Planning'
+    ],
+    entry_points={
+        'console_scripts': ['ppe=ppe.manage:main']
+    }
+)
