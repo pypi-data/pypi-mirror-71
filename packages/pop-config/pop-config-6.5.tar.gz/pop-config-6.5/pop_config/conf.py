2ec5c851705c88c587129191580ea6cee8606160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:26 +0000

    Add package pop-config/6.5/pop-config-6.5.tar.gz

---
+++ packages/pop-config/pop-config-6.5.tar.gz/pop-config-6.5/pop_config/conf.py
@@ -0,0 +1,60 @@
+import os
+import sys
+
+CLI_CONFIG = {
+    "log_file": {"dyne": "__cli__"},
+    "log_level": {"dyne": "__cli__"},
+    "log_fmt_logfile": {"dyne": "__cli__"},
+    "log_fmt_console": {"dyne": "__cli__"},
+    "log_datefmt": {"dyne": "__cli__"},
+    "log_plugin": {"dyne": "__cli__"},
+}
+
+CONFIG = {
+    "log_file": {
+        "dyne": "__cli__",
+        "default": f"{os.path.splitext(os.path.split(sys.argv[0])[1])[0]}.log",
+        "os": "POP_LOG_FILE",
+        "help": "The location of the log file",
+        "group": "Logging Options",
+    },
+    "log_level": {
+        "dyne": "__cli__",
+        "default": "warning",
+        "os": "POP_LOG_LEVEL",
+        "help": "Set the log level, either quiet, info, warning, or error",
+        "group": "Logging Options",
+    },
+    "log_fmt_logfile": {
+        "dyne": "__cli__",
+        "default": "%(asctime)s,%(msecs)03d [%(name)-17s][%(levelname)-8s] %(message)s",
+        "os": "POP_LOGFILE_FORMAT",
+        "help": "The format to be given to log file messages",
+        "group": "Logging Options",
+    },
+    "log_fmt_console": {
+        "dyne": "__cli__",
+        "default": "[%(levelname)-8s] %(message)s",
+        "os": "POP_LOG_FORMAT_CONSOLE",
+        "help": "The log formatting used in the console",
+        "group": "Logging Options",
+    },
+    "log_datefmt": {
+        "dyne": "__cli__",
+        "default": "%H:%M:%S",
+        "os": "POP_LOG_DATE_FORMAT",
+        "help": "The date format to display in the logs",
+        "group": "Logging Options",
+    },
+    "log_plugin": {
+        "dyne": "__cli__",
+        "default": "basic",
+        "help": "The logging plugin to use",
+        "group": "Logging Options",
+    },
+}
+SUBCOMMANDS = {}
+DYNE = {
+    "config": ["config"],
+    "log": ["log"],
+}
