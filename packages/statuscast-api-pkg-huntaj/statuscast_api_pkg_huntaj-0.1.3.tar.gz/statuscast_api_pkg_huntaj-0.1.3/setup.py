d8a4afafeca6a725d54ea1014e28312b2004b70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:41 +0000

    Add package statuscast-api-pkg-huntaj/0.1.3/statuscast_api_pkg_huntaj-0.1.3.tar.gz

---
+++ packages/statuscast-api-pkg-huntaj/statuscast_api_pkg_huntaj-0.1.3.tar.gz/statuscast_api_pkg_huntaj-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="statuscast_api_pkg_huntaj", # Replace with your own username
+    version="0.1.3",
+    author="Austin Hunt",
+    author_email="huntaj@cofc.edu",
+    description="Package for automating API calls to https://cofc.statuscast.com/api/v1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://hammond.cofc.edu/huntaj/statuscast_api_package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
