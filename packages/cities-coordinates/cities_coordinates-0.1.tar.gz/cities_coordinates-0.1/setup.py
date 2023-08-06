909bca5c0d72bf033d98a2baeafcf484ed51f96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:29 +0000

    Add package cities-coordinates/0.1/cities_coordinates-0.1.tar.gz

---
+++ packages/cities-coordinates/cities_coordinates-0.1.tar.gz/cities_coordinates-0.1/setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='cities_coordinates',  
+     version='0.1',
+     scripts=['cities_coordinates/__init__.py'] ,
+     author="Sofiene  Memmi",
+     author_email="sofiene.memmi@gmail.com",
+     description="A city coordinator package. The goal behind this package is to map cities arround the world with their coordinations without any need for an internet connection",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/sofzer/cities_coordinates",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
