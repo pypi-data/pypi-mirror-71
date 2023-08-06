4b228ab88d7a1336061dc1f740e906f8a9164436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:03 +0000

    Add package dist-Gaussian/0.1/dist_Gaussian-0.1.tar.gz

---
+++ packages/dist-Gaussian/dist_Gaussian-0.1.tar.gz/dist_Gaussian-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_Gaussian',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_Gaussian'],
+      author='Szilvia Jakab',
+      email='szilvi314@gmail.com',
+      zip_safe=False)
