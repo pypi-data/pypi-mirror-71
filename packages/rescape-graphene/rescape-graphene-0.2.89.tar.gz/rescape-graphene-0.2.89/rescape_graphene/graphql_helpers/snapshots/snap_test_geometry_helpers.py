3349421bffed52d705208b2eece7a3c29ee46d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package rescape-graphene/0.2.89/rescape-graphene-0.2.89.tar.gz

---
+++ packages/rescape-graphene/rescape-graphene-0.2.89.tar.gz/rescape-graphene-0.2.89/rescape_graphene/graphql_helpers/snapshots/snap_test_geometry_helpers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot, GenericRepr
+
+
+snapshots = Snapshot()
+
+snapshots['GeometryHelpersTestCase::test_geometrycollection_from_feature 1'] = GenericRepr("<GeometryCollection object at 0x10ffdce68>")
