aea2d91420b239a5d42dbf67e49367d4b59a5f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package python-openflow/2020.1rc1/python-openflow-2020.1rc1.tar.gz

---
+++ packages/python-openflow/python-openflow-2020.1rc1.tar.gz/python-openflow-2020.1rc1/setup.py
@@ -0,0 +1,139 @@
+"""Setup script.
+
+Run "python3 setup --help-commands" to list all available commands and their
+descriptions.
+"""
+import sys
+from abc import abstractmethod
+# Disabling checks due to https://github.com/PyCQA/pylint/issues/73
+# pylint: disable=import-error,no-name-in-module
+from distutils.command.clean import clean
+# pylint: enable=import-error,no-name-in-module
+from subprocess import CalledProcessError, call, check_call
+
+from setuptools import Command, find_packages, setup
+
+from pyof import __version__
+
+
+class SimpleCommand(Command):
+    """Make Command implementation simpler."""
+
+    user_options = []
+
+    def __init__(self, *args, **kwargs):
+        """Store arguments so it's possible to call other commands later."""
+        super().__init__(*args, **kwargs)
+        self.__args = args
+        self.__kwargs = kwargs
+
+    @abstractmethod
+    def run(self):
+        """Run when command is invoked.
+
+        Use *call* instead of *check_call* to ignore failures.
+        """
+
+    def run_command(self, command_class):
+        """Run another command with same __init__ arguments."""
+        command_class(*self.__args, **self.__kwargs).run()
+
+    def initialize_options(self):
+        """Set default values for options."""
+
+    def finalize_options(self):
+        """Post-process options."""
+
+
+class Cleaner(clean):
+    """Custom clean command to tidy up the project root."""
+
+    description = 'clean build, dist, pyc and egg from package and docs'
+
+    def run(self):
+        """Clean build, dist, pyc and egg from package and docs."""
+        super().run()
+        call('rm -vrf ./build ./dist ./*.pyc ./*.egg-info', shell=True)
+        call('find . -name __pycache__ -type d | xargs rm -rf', shell=True)
+        call('test -d docs && make -C docs/ clean', shell=True)
+
+
+class TestCoverage(SimpleCommand):
+    """Display test coverage."""
+
+    description = 'run unit tests and display code coverage'
+
+    def run(self):
+        """Run unittest quietly and display coverage report."""
+        cmd = 'coverage3 run setup.py test && coverage3 report'
+        check_call(cmd, shell=True)
+
+
+class DocTest(SimpleCommand):
+    """Run documentation tests."""
+
+    description = 'run documentation tests'
+
+    def run(self):
+        """Run doctests using Sphinx Makefile."""
+        cmd = 'make -C docs/ default doctest'
+        check_call(cmd, shell=True)
+
+
+class CITest(SimpleCommand):
+    """Run all CI tests."""
+
+    description = 'run all CI tests: unit and doc tests, linter'
+
+    def run(self):
+        """Run unit tests with coverage, doc tests and linter."""
+        for command in TestCoverage, DocTest, Linter:
+            self.run_command(command)
+
+
+class Linter(SimpleCommand):
+    """Lint Python source code."""
+
+    description = 'Lint Python source code'
+
+    def run(self):
+        """Run yala."""
+        print('Yala is running. It may take several seconds...')
+        try:
+            check_call('yala pyof setup.py', shell=True)
+            print('No linter error found.')
+        except CalledProcessError:
+            print('Linter check failed. Fix the error(s) above and try again.')
+            sys.exit(-1)
+
+
+setup(name='python-openflow',
+      version=__version__,
+      description='Library to parse and generate OpenFlow messages',
+      long_description=open("README.rst", "r").read(),
+      url='http://github.com/kytos/python-openflow',
+      author='Kytos Team',
+      author_email='devel@lists.kytos.io',
+      license='MIT',
+      test_suite='tests',
+      include_package_data=True,
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      extras_require={'dev': ['pip-tools >= 2.0',
+                              'coverage', 'pytest', 'yala', 'tox']},
+      packages=find_packages(exclude=['tests']),
+      cmdclass={
+          'ci': CITest,
+          'clean': Cleaner,
+          'coverage': TestCoverage,
+          'doctest': DocTest,
+          'lint': Linter
+      },
+      zip_safe=False,
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: System :: Networking',
+          'Topic :: Software Development :: Libraries'
+      ])
