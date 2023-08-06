8e9c2379eccb7b0da4e5d9028075751363f4a44c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:46 +0000

    Add package pystegano/0.2/pystegano-0.2.tar.gz

---
+++ packages/pystegano/pystegano-0.2.tar.gz/pystegano-0.2/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'pystegano',
+  packages = ['pystegano'],
+  version = '0.2',
+  license='MIT',
+  description = 'Steganography tools for Python',
+  author = 'Diego Zanchett',
+  author_email = 'diego.zanchett@aluno.cefet-rj.br',
+  url = 'https://github.com/dzanchett/pystegano',
+  download_url = 'https://github.com/dzanchett/pystegano/archive/v_0.2.tar.gz',
+  keywords = ['steganography'],
+  install_requires=[
+          'scipy',
+          'numpy',
+          'pandas',
+          'scikit-image',
+          'bitarray',
+          'opencv-python',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
