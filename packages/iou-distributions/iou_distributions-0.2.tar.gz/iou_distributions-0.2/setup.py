1f0e8185ed31db08c6d7469c0ca17769e1506393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:19 +0000

    Add package iou-distributions/0.2/iou_distributions-0.2.tar.gz

---
+++ packages/iou-distributions/iou_distributions-0.2.tar.gz/iou_distributions-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='iou_distributions',
+      version='0.2',
+      description='Binomial and Gaussian distributions',
+      packages=['iou_distributions'],
+      author='Ikenna Ugwuegbulam',
+      zip_safe=False)
