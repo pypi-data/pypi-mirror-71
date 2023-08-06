8e58729361f91ee0895e0f501892ac55176593c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:06 +0000

    Add package codewatchman/0.1.3/codewatchman-0.1.3.tar.gz

---
+++ packages/codewatchman/codewatchman-0.1.3.tar.gz/codewatchman-0.1.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="codewatchman",
+    version="0.1.3",
+    license="MIT",
+    author="Code Watchman",
+    author_email="codewatchmanapp@gmail.com",
+    description="Code Watchman Python API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thevishnupradeep/CWMPythonLibrary",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers'
+    ],
+    keywords=["code watchman", "codewatchman", "watchman"],
+    install_requires=[
+        'requests'
+    ],
+    python_requires='>=3.4',
+    download_url='https://github.com/thevishnupradeep/CWMPythonLibrary/archive/v0.0.1.tar.gz'
+)
