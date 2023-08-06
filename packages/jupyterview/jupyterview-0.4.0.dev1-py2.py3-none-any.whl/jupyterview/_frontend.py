39fd772fb42df8fba47a0b9ba3f772aeb81a8283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:02 +0000

    Add package jupyterview/0.4.0.dev1/jupyterview-0.4.0.dev1-py2.py3-none-any.whl

---
+++ packages/jupyterview/jupyterview-0.4.0.dev1-py2.py3-none-any.whl/jupyterview/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Trung Le.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "jupyterview"
+module_version = "^0.4.0"
