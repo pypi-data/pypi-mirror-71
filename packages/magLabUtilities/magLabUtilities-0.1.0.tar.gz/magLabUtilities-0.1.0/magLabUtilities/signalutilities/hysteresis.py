d13ef393f55ebfe145221cf5940f565435a528f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:07 +0000

    Add package magLabUtilities/0.1.0/magLabUtilities-0.1.0.tar.gz

---
+++ packages/magLabUtilities/magLabUtilities-0.1.0.tar.gz/magLabUtilities-0.1.0/magLabUtilities/signalutilities/hysteresis.py
@@ -0,0 +1,19 @@
+#!python3
+
+# \name
+#   hysteresis curve utilities
+#
+# \description
+#   Module for common manipulations of hysteresis curves
+#
+# \notes
+#
+# \todo
+#
+# \revised
+#   Mark Travers 6/16/2020      - Original construction
+
+import numpy as np
+from magLabUtilities.signalutilities.signals import SignalThread, Signal, SignalBundle
+
+
