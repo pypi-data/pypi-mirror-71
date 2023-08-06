f9a5a5231ca69b19faab8f631e47533caabe7fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:29 +0000

    Add package crypto-sim/0.0.2/crypto_sim-0.0.2.tar.gz

---
+++ packages/crypto-sim/crypto_sim-0.0.2.tar.gz/crypto_sim-0.0.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="crypto_sim",
+    version="0.0.2",
+    author="Chetan Sharma",
+    author_email="chetansharma67g@gmail.com",
+    description="A API for cryptography lib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
