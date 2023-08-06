1c5178b277c86f0f00ec1f5b114f657cce5a0391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:00 +0000

    Add package es-template-manager/1.3/es-template-manager-1.3.tar.gz

---
+++ packages/es-template-manager/es-template-manager-1.3.tar.gz/es-template-manager-1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(name='es-template-manager',
+                 version='1.3',
+                 author='Alex Corvin',
+                 author_email='accorvin@live.com',
+                 description=('A utility for managing Elasticsearch index'
+                              ' templates in source control'),
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url='https://github.com/accorvin/es-template-manager',
+                 packages=setuptools.find_packages(),
+                 install_requires=['requests', 'prometheus_client'],
+                 entry_points={
+                     'console_scripts': [
+                         ('es-template-manager = '
+                          'es_template_manager.es_template_manager:main')
+                     ]
+                 })
