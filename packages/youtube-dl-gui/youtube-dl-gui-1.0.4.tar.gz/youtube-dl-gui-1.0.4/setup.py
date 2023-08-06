5508952db2e244a2e42877f812454ebe65a13ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:30 +0000

    Add package youtube-dl-gui/1.0.4/youtube-dl-gui-1.0.4.tar.gz

---
+++ packages/youtube-dl-gui/youtube-dl-gui-1.0.4.tar.gz/youtube-dl-gui-1.0.4/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['youtube_dl_gui',
+ 'youtube_dl_gui.GUI',
+ 'youtube_dl_gui.Threads',
+ 'youtube_dl_gui.UI']
+
+package_data = \
+{'': ['*'], 'youtube_dl_gui.UI': ['images/*', 'resources/*']}
+
+install_requires = \
+['pyqt5>=5.15.0,<6.0.0', 'youtube-dl>=2020.6.16,<2021.0.0']
+
+entry_points = \
+{'console_scripts': ['youtube-dl-gui = youtube_dl_gui.main:main']}
+
+setup_kwargs = {
+    'name': 'youtube-dl-gui',
+    'version': '1.0.4',
+    'description': 'Yet another youtube-dl frontend written in python3 and qt5',
+    'long_description': 'youtube-dl-GUI\n==============\n\nYet another `youtube-dl` frontend written in python 3.7 and Qt5  \n\nScreenShot:\n-------------\n![youtube-dl-gui Screenshot in Ubuntu](http://imgur.com/KxTLeYl.png)\n\nInstallation:\n------------\n`pip install youtube-dl-gui`',
+    'author': 'Vadym Stupakov',
+    'author_email': 'vadim.stupakov@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Red-Eyed/youtube-dl-gui',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
