ccb94a5b3ab16ef5ec374da7f10479eb781b4af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:52 +0000

    Add package pycocotools-fix-test/2.0.0.8/pycocotools-fix-test-2.0.0.8.tar.gz

---
+++ packages/pycocotools-fix-test/pycocotools-fix-test-2.0.0.8.tar.gz/pycocotools-fix-test-2.0.0.8/setup.py
@@ -0,0 +1,44 @@
+from setuptools import dist, setup, Extension
+
+# To compile and install locally run "python setup.py build_ext --inplace"
+# To install library to Python site-packages run "python setup.py build_ext install"
+
+install_requires = [
+    'setuptools>=18.0',
+    'cython>=0.27.3',
+    'matplotlib>=2.1.0'
+]
+
+dist.Distribution().fetch_build_eggs(install_requires)
+
+import numpy as np
+ext_modules = [
+    Extension(
+        'pycocotools._mask',
+        sources=['./common/maskApi.c', 'pycocotools/_mask.pyx'],
+        include_dirs=[np.get_include(), './common'],
+        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+    )
+]
+
+setup(
+    name='pycocotools-fix-test',
+    author='Junjue Wang',
+    author_email='junjuew@cs.cmu.edu',
+    description='Fixed pycocotools package installation error of numpy or cython not installed',
+    long_description='Created due to the inactivity of the original repo: https://github.com/cocodataset/cocoapi',
+    url='https://github.com/junjuew/cocoapi',
+    packages=['pycocotools'],
+    package_dir={'pycocotools': 'pycocotools'},
+    install_requires=[
+        'setuptools>=18.0',
+        'cython>=0.27.3',
+        'matplotlib>=2.1.0'
+    ],
+    setup_requires=[
+        'cython>=0.27.3',
+        'numpy'
+    ],
+    version='2.0.0.8',
+    ext_modules=ext_modules
+)
