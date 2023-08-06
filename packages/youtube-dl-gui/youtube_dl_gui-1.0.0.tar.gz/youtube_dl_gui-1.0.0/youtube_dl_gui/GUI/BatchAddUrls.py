ddf58a7845899882e0efef677de763a7c6f8e481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:08 +0000

    Add package youtube-dl-gui/1.0.0/youtube_dl_gui-1.0.0.tar.gz

---
+++ packages/youtube-dl-gui/youtube_dl_gui-1.0.0.tar.gz/youtube_dl_gui-1.0.0/youtube_dl_gui/GUI/BatchAddUrls.py
@@ -0,0 +1,33 @@
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QDialog
+from UI.BatchAddUrls import Ui_BatchAdd
+
+
+class BatchAddDialogue(QDialog):
+    def __init__(self, parent=None):
+        super(BatchAddDialogue, self).__init__(parent, QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint)
+        self.ui = Ui_BatchAdd()
+        self.ui.setupUi(self)
+        self.download = False
+        self.ui.Browse.clicked.connect(self.browse_clicked)
+        self.ui.Close.clicked.connect(self.close)
+        self.ui.Add.clicked.connect(self.add_clicked)
+
+    def browse_clicked(self):
+        file_name = str(QFileDialog.getOpenFileName(
+            self, "Select txt file", filter=str('*.txt')
+        ))
+
+        if file_name is '':
+            return
+        with open(file_name, 'rb') as file_data:
+            for line in file_data.readlines():
+                self.ui.UrlList.append(line.strip())
+
+    def add_clicked(self):
+        if str(self.ui.UrlList.toPlainText()).strip() is '':
+            QMessageBox.information(self, "Error!", "No urls given!")
+            return
+        else:
+            self.download = True
+            self.close()
---
+++ packages/youtube-dl-gui/youtube_dl_gui-1.0.0.tar.gz/youtube_dl_gui-1.0.0/youtube_dl_gui/GUI/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Vadym Stupakov"
+__email__ = "vadim.stupakov@gmail.com"
+
+from .AboutDialog import AboutDialog
+from .BatchAddUrls import BatchAddDialogue
+from .LicenseDialog import LicenseDialogue
