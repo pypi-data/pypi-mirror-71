35d014e5d1e59cb48c2c0d228e5709d463cfe9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:36 +0000

    Add package BinomialandGaussian/1.3/BinomialandGaussian-1.3.tar.gz

---
+++ packages/BinomialandGaussian/BinomialandGaussian-1.3.tar.gz/BinomialandGaussian-1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='BinomialandGaussian',
+    version='1.3',
+    description='Gaussian and Binomial distributions',
+    packages=['BinomialandGaussian'],
+    author='Mosin Mahajan',
+    author_email='mosinmahajan@gmail.com',
+    zip_safe=False
+)
