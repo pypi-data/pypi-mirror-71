11183638484953bd54c8b49cb125a050178482d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:10 +0000

    Add package distributions-test-udacity/0.1/distributions_test_udacity-0.1.tar.gz

---
+++ packages/distributions-test-udacity/distributions_test_udacity-0.1.tar.gz/distributions_test_udacity-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_test_udacity',
+      author='Eldar Mustafayev',
+      licence='MIT',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
