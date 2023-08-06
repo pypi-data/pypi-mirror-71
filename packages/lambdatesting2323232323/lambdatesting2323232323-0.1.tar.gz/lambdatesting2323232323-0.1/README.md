415007b40c62463348370c53c2e53622dceb9409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:39 +0000

    Add package lambdatesting2323232323/0.1/lambdatesting2323232323-0.1.tar.gz

---
+++ packages/lambdatesting2323232323/lambdatesting2323232323-0.1.tar.gz/lambdatesting2323232323-0.1/README.md
@@ -0,0 +1,3 @@
+# Testing
+
+Test various things IaC, aws services, etc
---
+++ packages/lambdatesting2323232323/lambdatesting2323232323-0.1.tar.gz/lambdatesting2323232323-0.1/setup.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+requires = []
+
+setup(
+    name = "lambdatesting2323232323",
+    packages = ["src"],
+    version = '0.1',
+    description = 'Example inherits package',
+    #url = "",
+    #download_url = "",
+    author = "FMU",
+    classifiers = [],
+)
