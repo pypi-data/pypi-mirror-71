513be29d966dc143cf4ea23e9da6305602df0902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:11 +0000

    Add package graafilohi/1.1.0/graafilohi-1.1.0.tar.gz

---
+++ packages/graafilohi/graafilohi-1.1.0.tar.gz/graafilohi-1.1.0/CHANGELOG.md
@@ -0,0 +1,48 @@
+# [1.1.0](https://gitlab.com/blissfulreboot/python/graafilohi/compare/v1.0.5...v1.1.0) (2020-06-21)
+
+
+### Features
+
+* node colors based on whether they are callable ([91aed2e](https://gitlab.com/blissfulreboot/python/graafilohi/commit/91aed2ef307ab0fa5266d045724da1b36eb77e84))
+
+## [1.0.5](https://gitlab.com/blissfulreboot/python/graafilohi/compare/v1.0.4...v1.0.5) (2020-06-21)
+
+
+### Bug Fixes
+
+* Fix package ([6498de2](https://gitlab.com/blissfulreboot/python/graafilohi/commit/6498de2b4ebb935f82fc31ec62728d44dc8a8074))
+
+## [1.0.4](https://gitlab.com/blissfulreboot/python/graafilohi/compare/v1.0.3...v1.0.4) (2020-06-21)
+
+
+### Bug Fixes
+
+* script renamed to match the package ([85c10ef](https://gitlab.com/blissfulreboot/python/graafilohi/commit/85c10efe4969f9d278617f1fe91863715f707ef5))
+
+## [1.0.3](https://gitlab.com/blissfulreboot/python/graafilohi/compare/v1.0.2...v1.0.3) (2020-06-20)
+
+
+### Bug Fixes
+
+* add readme.md to the package_data ([6fb7ff9](https://gitlab.com/blissfulreboot/python/graafilohi/commit/6fb7ff99109d965bbbfc84cf7c3617b89661836c))
+
+## [1.0.2](https://gitlab.com/blissfulreboot/python/graafilohi/compare/v1.0.1...v1.0.2) (2020-06-20)
+
+
+### Bug Fixes
+
+* fix installation issue: Add VERSION and changelog.md to the package ([ffb9feb](https://gitlab.com/blissfulreboot/python/graafilohi/commit/ffb9feb5c0d4a1b121e9ce93bed1df51ba5f0284))
+
+## [1.0.1](https://gitlab.com/blissfulreboot/python/graafilohi/compare/v1.0.0...v1.0.1) (2020-06-20)
+
+
+### Bug Fixes
+
+* fix project name in setup.py ([e5ee4bc](https://gitlab.com/blissfulreboot/python/graafilohi/commit/e5ee4bc7b29082ead27dd55457cc80d25bcc2088))
+
+# 1.0.0 (2020-06-20)
+
+
+### Features
+
+* initial version of graafilohi ([1e099c4](https://gitlab.com/blissfulreboot/python/graafilohi/commit/1e099c40499678aa19d29dd8e55ee8d1786ad340))
---
+++ packages/graafilohi/graafilohi-1.1.0.tar.gz/graafilohi-1.1.0/test_graafilohi.py
@@ -0,0 +1,109 @@
+import unittest
+from unittest.mock import MagicMock
+
+from graafilohi import ExecutableDirectedMultiGraph
+
+
+class TestExecutableDirectedMultiGraph(unittest.TestCase):
+    def test_fully_executable_true(self):
+        nodes = [1, 2, 3, 4]
+        edges = [(1, 2), (2, 3), (3, 4)]
+
+        g = ExecutableDirectedMultiGraph()
+        for n in nodes:
+            g.add_node(n, function=MagicMock())
+
+        g.add_edges_from(edges)
+
+        self.assertTrue(g.is_fully_executable())
+
+    def test_fully_executable_false(self):
+        edges = [(1, 2), (2, 3), (3, 4)]
+
+        g = ExecutableDirectedMultiGraph()
+        g.add_node(1)
+        g.add_node(2, function=MagicMock())
+        g.add_node(3, function=MagicMock())
+        g.add_node(4, function=MagicMock())
+        g.add_edges_from(edges)
+
+        self.assertFalse(g.is_fully_executable())
+
+    def test_node_function_not_callable(self):
+        edges = [(1, 2), (2, 3), (3, 4)]
+
+        g = ExecutableDirectedMultiGraph()
+        g.add_node(1, function=MagicMock())
+        g.add_node(2, function="I am not callable, but a string")
+        g.add_node(3, function=MagicMock())
+        g.add_node(4, function=MagicMock())
+        g.add_edges_from(edges)
+
+        self.assertFalse(g.is_fully_executable())
+
+    def test_graph_has_cycle(self):
+        nodes = [1, 2, 3, 4]
+        edges = [(1, 2), (2, 3), (3, 4), (4, 3)]
+
+        g = ExecutableDirectedMultiGraph()
+        for n in nodes:
+            g.add_node(n, function=MagicMock())
+
+        g.add_edges_from(edges)
+
+        self.assertFalse(g.is_fully_executable())
+
+    def test_execution_order(self):
+        nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        edges = [(1, 2), (2, 4), (2, 5), (3, 4), (4, 6), (5, 6), (6, 7), (8, 7), (9, 7), (7, 0)]
+
+        g = ExecutableDirectedMultiGraph()
+        for n in nodes:
+            g.add_node(n, function=MagicMock())
+
+        g.add_edges_from(edges)
+
+        order = g.get_execution_order(g)
+        first = set(order[:4])
+        second = order[4]
+        third = set(order[5:7])
+        last = set(order[7:])
+
+        self.assertSetEqual(first, {1, 3, 8, 9})
+        self.assertEqual(second, 2)
+        self.assertSetEqual(third, {4, 5})
+        self.assertSetEqual(last, {6, 7, 0})
+
+    def test_execution(self):
+        g = ExecutableDirectedMultiGraph()
+
+        first_mock = MagicMock()
+        second_mock = MagicMock()
+
+        g.add_node(1, function=first_mock, function_arguments=[1, 2])
+        g.add_node(2, function=second_mock)
+
+        g.add_edge(1, 2)
+
+        g.execute()
+
+        first_mock.assert_called_once_with(1, 2, context={})
+        second_mock.assert_called_once()
+
+    def test_execution_context(self):
+        g = ExecutableDirectedMultiGraph()
+
+        first_mock = MagicMock(return_value=4)
+        second_mock = MagicMock()
+
+        g.add_node(1, function=first_mock, function_arguments=[1, 2])
+        g.add_node(2, function=second_mock)
+
+        g.add_edge(1, 2)
+
+        g.execute()
+
+        first_mock.assert_called_once_with(1, 2, context={})
+        second_mock.assert_called_once_with(context={1: 4})
+if __name__ == "__main__":
+    unittest.main()
