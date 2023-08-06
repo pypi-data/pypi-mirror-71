07538daee270bf900bf2be2b70358e424a45192a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:49 +0000

    Add package zoop-wrapper/0.1.0a0/zoop_wrapper-0.1.0a0.tar.gz

---
+++ packages/zoop-wrapper/zoop_wrapper-0.1.0a0.tar.gz/zoop_wrapper-0.1.0a0/zoop_wrapper/__init__.py
@@ -0,0 +1,21 @@
+from .wrapper import ZoopWrapper  # noqa
+from .exceptions import ValidationError  # noqa
+from .models import (
+    Address,
+    BankAccount,
+    BillingConfiguration,
+    BillingInstructions,
+    Buyer,
+    Card,
+    Invoice,
+    Person,
+    Seller,
+    Source,
+    Token,
+    Transaction,
+)  # noqa
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
