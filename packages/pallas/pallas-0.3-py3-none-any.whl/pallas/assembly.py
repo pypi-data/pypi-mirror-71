2d74b74a2f7a1aa061e500c0bd596c8241d9227d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:20 +0000

    Add package pallas/0.3/pallas-0.3-py3-none-any.whl

---
+++ packages/pallas/pallas-0.3-py3-none-any.whl/pallas/assembly.py
@@ -0,0 +1,161 @@
+# Copyright 2020 Akamai Technologies, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Facade that can setup an Athena clients from configuration.
+
+Full-features Athena client is made a layered composition of objects.
+The core is an :class:`.AthenaProxy` instance that issues requests to AWS.
+It is wrapped by decorators for extra functionality like caching.
+
+Function defined in this module can assembly the whole object structure.
+"""
+
+from __future__ import annotations
+
+import os
+from typing import Mapping, Optional
+
+from pallas.base import Athena
+from pallas.caching import AthenaCachingWrapper
+from pallas.interruptions import AthenaKillOnInterruptWrapper
+from pallas.normalization import AthenaNormalizationWrapper
+from pallas.proxies import AthenaProxy
+from pallas.storage import storage_from_uri
+
+
+def setup(
+    *,
+    database: Optional[str] = None,
+    workgroup: Optional[str] = None,
+    output_location: Optional[str] = None,
+    region: Optional[str] = None,
+    cache_remote: Optional[str] = None,
+    cache_local: Optional[str] = None,
+    normalize: bool = True,
+    kill_on_interrupt: bool = True,
+) -> Athena:
+    """
+    Setup :class:`.Athena` instance.
+
+    Initializes :class:`.AthenaProxy` and decorates it by caching wrappers.
+
+    :param environ: Mapping to use instead of ``os.environ``.
+        Set to empty dict to ignore environment variables.
+    :param environ_prefix: Prefix of environment variables.
+    :param database: a name of Athena database.
+        If omitted, database should be specified in SQL.
+    :param workgroup: a name of Athena workgroup.
+        If omitted, default workgroup will be used.
+    :param output_location: an output location at S3 for query results.
+        Optional if a default location is specified for the *workgroup*.
+    :param region: an AWS region.
+        By default, region from AWS config is used.
+    :param cache_remote: an URI of a remote cache.
+        Query execution IDs without results are stored to the remote cache.
+    :param cache_local: an URI of a local cache.
+        Both results and query execution IDs are stored to the local cache.
+    :param normalize: whether to normalize SQL
+        Normalizes whitespace to improve caching.
+    :param kill_on_interrupt: whether to kill queries on KeyboardInterrupt
+        Kills query when interrupted during waiting.
+    :return: an Athena instance
+        A :class:`.AthenaProxy` instance wrapped necessary in decorators.
+    """
+    athena: Athena = AthenaProxy(
+        database=database,
+        workgroup=workgroup,
+        output_location=output_location,
+        region=region,
+    )
+    if cache_remote is not None:
+        storage = storage_from_uri(cache_remote)
+        athena = AthenaCachingWrapper(athena, storage=storage, cache_results=False)
+    if cache_local is not None:
+        storage = storage_from_uri(cache_local)
+        athena = AthenaCachingWrapper(athena, storage=storage, cache_results=True)
+    if normalize:
+        athena = AthenaNormalizationWrapper(athena)
+    if kill_on_interrupt:
+        athena = AthenaKillOnInterruptWrapper(athena)
+    return athena
+
+
+def environ_setup(
+    environ: Optional[Mapping[str, str]] = None, *, prefix: str = "PALLAS"
+) -> Athena:
+    """
+    Setup :class:`.Athena` instance from environment variables.
+
+    Reads the following environment variables: ::
+
+        export PALLAS_DATABASE=
+        export PALLAS_WORKGROUP=
+        export PALLAS_OUTPUT_LOCATION=
+        export PALLAS_REGION=
+        export PALLAS_NORMALIZE=true
+        export PALLAS_KILL_ON_INTERRUPT=true
+        export PALLAS_CACHE_REMOTE=$PALLAS_OUTPUT_LOCATION
+        export PALLAS_CACHE_LOCAL=~/Notebooks/.cache/
+
+    :param environ: A mapping object representing the string environment.
+        Defaults to ``os.environ``.
+    :param prefix: Prefix of environment variables.
+    :return: an Athena instance
+        A :class:`.AthenaProxy` instance wrapped necessary in decorators.
+    """
+    if environ is None:
+        environ = os.environ
+    config = _EnvironConfig(environ, prefix)
+    return setup(
+        database=config.get_str("DATABASE"),
+        workgroup=config.get_str("WORKGROUP"),
+        output_location=config.get_str("OUTPUT_LOCATION"),
+        region=config.get_str("REGION"),
+        cache_remote=config.get_str("CACHE_REMOTE"),
+        cache_local=config.get_str("CACHE_LOCAL"),
+        normalize=config.get_bool("NORMALIZE", True),
+        kill_on_interrupt=config.get_bool("KILL_ON_INTERRUPT", True),
+    )
+
+
+class _EnvironConfig:
+    def __init__(self, environ: Mapping[str, str], prefix: str) -> None:
+        self._environ = environ
+        self._prefix = prefix
+
+    def get_str(self, key: str, default: Optional[str] = None) -> Optional[str]:
+        v = self._get(key)
+        if not v:
+            return default
+        return v
+
+    def get_bool(self, key: str, default: bool = False) -> bool:
+        v = self._get(key)
+        if not v:
+            return default
+        v = v.lower()
+        if v in ("1", "true", "on", "yes"):
+            return True
+        if v in ("0", "false", "off", "no"):
+            return False
+        raise ValueError(f"{self._prefix}_{key}: invalid boolean value: {v}")
+
+    def _get(self, key: str) -> str:
+        v = self._environ.get(f"{self._prefix}_{key}", "")
+        if not isinstance(v, str):
+            # Avoid unexpected behaviour when somebody passes something
+            # like {"PALLAS_KILL_ON_INTERRUPT": False} to environ.
+            raise TypeError("Environ values must be a string.")
+        return v
---
+++ packages/pallas/pallas-0.3-py3-none-any.whl/pallas/caching.py
@@ -0,0 +1,201 @@
+# Copyright 2020 Akamai Technologies, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Decorators for caching Athena queries.
+"""
+
+from __future__ import annotations
+
+import hashlib
+import logging
+import re
+from typing import Optional
+from urllib.parse import urlencode
+
+from pallas.base import Athena, AthenaWrapper, Query, QueryWrapper
+from pallas.results import QueryResults
+from pallas.storage import NotFoundError, Storage
+
+logger = logging.getLogger("pallas")
+
+
+_comment_1 = r"--[^\n]*\n"
+_comment_2 = r"/\*([^*]|\*(?!/))*\*/"
+
+SELECT_RE = re.compile(
+    rf"(\s+|{_comment_1}|{_comment_2})*(SELECT|WITH)\b", re.IGNORECASE
+)
+
+
+def is_cacheable(sql: str) -> bool:
+    """
+    Return whether an SQL statement can be cached.
+
+    Only SELECT statements are considered cacheable.
+    """
+    return SELECT_RE.match(sql) is not None
+
+
+class AthenaCachingWrapper(AthenaWrapper):
+    """
+    Athena wrapper that caches query IDs and optionally results.
+
+    Athena always stores results in S3, so it is possible
+    to retrieve past results without manually copying data.
+
+    The caching wrapper can work two modes,
+    depending on *cache_results* setting:
+
+    1) Remote mode - cache query execution IDs.
+    2) Local mode - Cache query execution IDs and query results.
+
+    I the remote mode, cache is used to recover query execution IDs
+    that are necessary for downloading results of past queries.
+
+    In the local mode, results are stored to cache too.
+    If the cache storage is local then it is possible to retrieve
+    cached results without internet connection.
+    """
+
+    _storage: Storage
+
+    def __init__(
+        self, wrapped: Athena, *, storage: Storage, cache_results: bool = True
+    ) -> None:
+        super().__init__(wrapped)
+        self._storage = storage
+        self._cache_results = cache_results
+
+    def __repr__(self) -> str:
+        return (
+            f"<{type(self).__name__}: {self.wrapped!r} cached at {self.storage.uri!r}>"
+        )
+
+    @property
+    def storage(self) -> Storage:
+        return self._storage
+
+    @property
+    def cache_results(self) -> bool:
+        return self._cache_results
+
+    def submit(self, sql: str, *, ignore_cache: bool = False) -> Query:
+        sql_cacheable = is_cacheable(sql)
+        if sql_cacheable and not ignore_cache:
+            execution_id = self._load_execution_id(sql)
+            if execution_id is not None:
+                return self.get_query(execution_id)
+        query = super().submit(sql, ignore_cache=ignore_cache)
+        if sql_cacheable:
+            self._save_execution_id(sql, query.execution_id)
+            query = self._wrap_query(query)
+        return query
+
+    def get_query(self, execution_id: str) -> Query:
+        query = super().get_query(execution_id)
+        return self._wrap_query(query)
+
+    def _wrap_query(self, query: Query) -> Query:
+        if self._cache_results:
+            query = QueryCachingWrapper(query, self._storage)
+        return query
+
+    def _load_execution_id(self, sql: str) -> Optional[str]:
+        key = self._get_cache_key(sql)
+        try:
+            execution_id = self._storage.get(key)
+        except NotFoundError:
+            return None
+        logger.info(
+            f"Query execution loaded from cache {self._storage}{key}:"
+            f" QueryExecutionId={execution_id!r}"
+        )
+        return execution_id
+
+    def _save_execution_id(self, sql: str, execution_id: str) -> None:
+        key = self._get_cache_key(sql)
+        self._storage.set(key, execution_id)
+        logger.info(
+            f"Query execution saved to cache {self._storage}{key}:"
+            f" QueryExecutionId={execution_id!r}"
+        )
+
+    def _get_cache_key(self, sql: str) -> str:
+        parts = {}
+        if self.database is not None:
+            parts["database"] = self.database
+        parts["sql"] = sql
+        encoded = urlencode(parts).encode("utf-8")
+        hash = hashlib.sha256(encoded).hexdigest()
+        return f"query-{hash}"
+
+
+class QueryCachingWrapper(QueryWrapper):
+    """
+    Query wrapper that caches query results.
+    """
+
+    _storage: Storage
+
+    def __init__(self, wrapped: Query, storage: Storage) -> None:
+        super().__init__(wrapped)
+        self._storage = storage
+
+    def get_results(self) -> QueryResults:
+        results = self._load_results()
+        if results is not None:
+            return results
+        results = super().get_results()
+        # When an old query is accessed using its execution ID,
+        # we have to retrieve its SQL to determine whether it can be cached.
+        # Network should not be used until a cache is checked.
+        # Getting the SQL here should not produce an additional request.
+        if is_cacheable(self.get_info().sql):
+            self._save_results(results)
+        return results
+
+    def join(self) -> None:
+        if self._has_results():
+            # If we have results then we can assume that query has finished.
+            return
+        super().join()
+
+    def _has_results(self) -> bool:
+        return self._storage.has(self._get_cache_key())
+
+    def _load_results(self) -> Optional[QueryResults]:
+        key = self._get_cache_key()
+        try:
+            with self._storage.reader(key) as stream:
+                results = QueryResults.load(stream)
+        except NotFoundError:
+            return None
+        logger.info(
+            f"Query results loaded from cache {self._storage}{key}:"
+            f" QueryExecutionId={self.execution_id!r}: {len(results)} rows"
+        )
+        return results
+
+    def _save_results(self, results: QueryResults) -> None:
+        key = self._get_cache_key()
+        with self._storage.writer(key) as stream:
+            results.save(stream)
+        logger.info(
+            f"Query results saved to cache {self._storage}{key}:"
+            f" QueryExecutionId={self.execution_id!r}: {len(results)} rows"
+        )
+
+    def _get_cache_key(self) -> str:
+        return f"results-{self.execution_id}"
