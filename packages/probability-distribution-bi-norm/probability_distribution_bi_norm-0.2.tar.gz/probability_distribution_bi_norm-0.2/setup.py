4a5cf1c17d42aca04f4a55bfbc0be2233b937889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:03 +0000

    Add package probability-distribution-bi-norm/0.2/probability_distribution_bi_norm-0.2.tar.gz

---
+++ packages/probability-distribution-bi-norm/probability_distribution_bi_norm-0.2.tar.gz/probability_distribution_bi_norm-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distribution_bi_norm',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_distribution_bi_norm'],
+      author="Zainab Ayodimeji",
+      author_email ="zayodimeji@gmail.com",
+      zip_safe=False)
