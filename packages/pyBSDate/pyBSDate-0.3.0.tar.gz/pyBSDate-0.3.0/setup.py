8d5aeb99d01446f27c39a5d3ab0e204dfd3d4340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:42 +0000

    Add package pyBSDate/0.3.0/pyBSDate-0.3.0.tar.gz

---
+++ packages/pyBSDate/pyBSDate-0.3.0.tar.gz/pyBSDate-0.3.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyBSDate',
+    version='0.3.0',
+    packages=['pyBSDate'],
+    url='https://github.com/SushilShrestha/pyBSDate',
+    download_url='https://github.com/SushilShrestha/pyBSDate/tarball/0.3.0rc',
+    license='MIT',
+    author='sushil',
+    author_email='afahocci@gmail.com',
+    package_data={},
+    description='Python BS date conversion utility',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ]
+)
