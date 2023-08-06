0b4d00ae4df3844a435a51e362f44f89a2bdf96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:54 +0000

    Add package navpreet-distributions/0.1/navpreet_distributions-0.1.tar.gz

---
+++ packages/navpreet-distributions/navpreet_distributions-0.1.tar.gz/navpreet_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='navpreet_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['navpreet_distributions'],
+      author = 'Navpreet Singh',
+      zip_safe=False)
