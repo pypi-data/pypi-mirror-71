f3db941e8f489e88a516a69152717a1853d6e729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:03 +0000

    Add package melugd-probability/1.0/melugd_probability-1.0.tar.gz

---
+++ packages/melugd-probability/melugd_probability-1.0.tar.gz/melugd_probability-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='melugd_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      author='Meljohn Ugaddan',
+      packages=['melugd_probability'],
+      zip_safe=False)
