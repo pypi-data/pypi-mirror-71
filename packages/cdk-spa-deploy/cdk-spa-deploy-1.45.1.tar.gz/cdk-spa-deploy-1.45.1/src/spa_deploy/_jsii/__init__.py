c1fd08a34070b5774c768b4ce4a1a65fce42e21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:32 +0000

    Add package cdk-spa-deploy/1.45.1/cdk-spa-deploy-1.45.1.tar.gz

---
+++ packages/cdk-spa-deploy/cdk-spa-deploy-1.45.1.tar.gz/cdk-spa-deploy-1.45.1/src/spa_deploy/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_certificatemanager
+import aws_cdk.aws_cloudfront
+import aws_cdk.aws_route53_patterns
+import aws_cdk.aws_route53_targets
+import aws_cdk.aws_s3
+import aws_cdk.aws_s3_deployment
+import aws_cdk.core
+__all__ = []
+
+publication.publish()
