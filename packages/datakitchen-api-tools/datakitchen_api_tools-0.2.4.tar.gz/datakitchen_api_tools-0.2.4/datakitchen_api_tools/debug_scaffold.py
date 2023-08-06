9c097b4818345384ff922500c701d5795c2c9ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:53 +0000

    Add package datakitchen-api-tools/0.2.4/datakitchen_api_tools-0.2.4.tar.gz

---
+++ packages/datakitchen-api-tools/datakitchen_api_tools-0.2.4.tar.gz/datakitchen_api_tools-0.2.4/datakitchen_api_tools/debug_scaffold.py
@@ -0,0 +1,4 @@
+from DKConnection import DKConnection
+
+conn = DKConnection("cloud.datakitchen.io", "armand+hm@datakitchen.io", "%Gun8D9M")
+results = conn.safeCreateOrder("hemonc_wns_prod", 'hemonc_rems' ,'wns-refresh-inrebic-reblozyl-calls')
