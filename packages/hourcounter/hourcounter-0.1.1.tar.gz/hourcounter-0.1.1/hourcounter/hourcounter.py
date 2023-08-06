bb93cef0d36c738b1025cfa17b6c968b6715d97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:42 +0000

    Add package hourcounter/0.1.1/hourcounter-0.1.1.tar.gz

---
+++ packages/hourcounter/hourcounter-0.1.1.tar.gz/hourcounter-0.1.1/hourcounter/hourcounter.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from tqdm import tqdm
+
+class HourlyCount:
+    print("HourlyCount class is intiated you can use the functoins.")
+    
+    def hour_counter(self,data,column_name):
+            
+            self.__df = data
+            self.__column_name = column_name
+            i=0
+            j=1
+            num = []
+            count = []
+            for i in tqdm(range(0,24)):
+                num.append(i)
+            for i in tqdm(range(len(num)-1)):
+            
+            
+           
+            
+                if i < 9:
+
+                   
+                    hour_start = '0'+str(i)+":"+'00'+':'+"00"
+                    
+                    hour_end = '0'+str(j)+":"+'00'+':'+"00"
+                    
+                    count.append(len(self.__df[(self.__df[self.__column_name]>=hour_start)&(self.__df[self.__column_name]<=hour_end)]))
+
+                elif i==9:
+                        hour_start = '0'+str(i)+":"+'00'+':'+"00"
+                        hour_end = str(j)+":"+'00'+':'+"00"
+                        
+                        count.append(len(self.__df[(self.__df[self.__column_name]>=hour_start)&(self.__df[self.__column_name]<=hour_end)]))
+
+
+                else:
+                    
+                    hour_start = str(i)+":"+'00'+':'+"00"
+                    
+                    hour_end = str(j)+":"+'00'+':'+"00"
+                    
+                    count.append(len(self.__df[(self.__df[self.__column_name]>=hour_start)&(self.__df[self.__column_name]<=hour_end)]))
+
+                i = j
+                j = i+1
+                
+            hour = []
+            for i in tqdm(range(len(count))):
+                hour.append(i)
+            hour_count = pd.DataFrame()
+            hour_count['hour'] = hour
+            hour_count['count'] = count
+    
+            return hour_count
