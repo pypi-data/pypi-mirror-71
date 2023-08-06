0edce2a8539af0bf6b2fb8c4b67338c01753f058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:35 +0000

    Add package alpinepkgs/1.1.3/alpinepkgs-1.1.3.tar.gz

---
+++ packages/alpinepkgs/alpinepkgs-1.1.3.tar.gz/alpinepkgs-1.1.3/alpinepkgs/const.py
@@ -0,0 +1,13 @@
+"""Constants."""
+BASE_URL = 'https://pkgs.alpinelinux.org/packages?name={}&branch={}'
+BRANCHES = ['v3.3',
+            'v3.4',
+            'v3.5',
+            'v3.6',
+            'v3.7',
+            'v3.8',
+            'v3.9',
+            'v3.10',
+            'v3.11',
+            'v3.12',
+            'edge']
