514178b6a1e7f26e3ca64083f6d461a752a0f04e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:10 +0000

    Add package arris-tg3442-reboot/0.2/arris_tg3442_reboot-0.2.tar.gz

---
+++ packages/arris-tg3442-reboot/arris_tg3442_reboot-0.2.tar.gz/arris_tg3442_reboot-0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="arris_tg3442_reboot", # Replace with your own username
+    version="0.2",
+    author="Florian Gabsteiger",
+    author_email="florian.g@me.com",
+    description="Reboot your Arris TG-3442 router via cli",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/diveflo/arris-tg3442-reboot",
+    packages=setuptools.find_packages(),
+    scripts=['arris_tg3442_reboot/arris-tg3442-reboot.py'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
