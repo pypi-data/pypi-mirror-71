339b7c790339677b1c47f579c87f66550349b2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:11 +0000

    Add package pydistribution/1.0/pydistribution-1.0.tar.gz

---
+++ packages/pydistribution/pydistribution-1.0.tar.gz/pydistribution-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pydistribution',
+      version='1.0',
+      description='General ideas about the distributions and their visualizations',
+      packages=['pydistribution'],
+      author = 'Yalim Demirkesen',
+      author_email = 'yalimdemirkesen@gmail.com',
+      zip_safe=False)
