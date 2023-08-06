9f6d2c3a83c25da3d740fa095e815264f11c3914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:31 +0000

    Add package jayasurya-dsnd/0.1/jayasurya_dsnd-0.1.tar.gz

---
+++ packages/jayasurya-dsnd/jayasurya_dsnd-0.1.tar.gz/jayasurya_dsnd-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jayasurya_dsnd',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jayasurya_dsnd'],
+      zip_safe=False)
