5ceec02e911c6757b9de09350420b6a1a0da173c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:24 +0000

    Add package pycuGMRES/1.1.3.9.2/pycuGMRES-1.1.3.9.2-py3-none-any.whl

---
+++ packages/pycuGMRES/pycuGMRES-1.1.3.9.2-py3-none-any.whl/pycuGMRES/Shared object generating/compile_cpp.sh
@@ -0,0 +1,16 @@
+FOLDER=$1
+soFILE="${FOLDER}/Shared\ object\ generating/cuGMRES.so"
+
+if test -f "$soFILE"; then
+    rm $soFILE
+fi
+
+echo importing pycuGMRES ...
+echo nvcc -lcublas -lcufft -lcusolver -O3 --compiler-options '-fPIC' -shared \"CUDA\ C\ ++\ sources\" -o cuGMRES.so
+nvcc -lcublas -lcufft -lcusolver -O3 --compiler-options '-fPIC' -shared ${FOLDER}/CUDA\ C\ ++\ sources/TestGMRES.cu -o $soFILE
+if test -f "$soFILE"; then
+    echo CUDA C++ library has been created!
+else 
+    echo FATAL ERROR: CUDA C++ library creating failed!
+fi
+
