e2ece16802efbc4b9e2f44dd18300b04329352ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:11 +0000

    Add package verta/0.14.7/verta-0.14.7.tar.gz

---
+++ packages/verta/verta-0.14.7.tar.gz/verta-0.14.7/verta/dataset/_dataset.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import os
+
+from .._protos.public.modeldb.versioning import Dataset_pb2 as _DatasetService
+
+from .._internal_utils import _utils
+
+from .._repository import blob
+
+
+class _Dataset(blob.Blob):
+    """
+    Base class for dataset versioning. Not for human consumption.
+
+    """
+    def __init__(self, enable_mdb_versioning=False):
+        super(_Dataset, self).__init__()
+
+        self._msg = _DatasetService.DatasetBlob()
+
+        self._mdb_versioned = enable_mdb_versioning
+        self._components_to_upload = dict()  # component paths to local filepaths
+
+    @property
+    def _path_component_blobs(self):
+        """
+        Returns path components of this dataset.
+
+        Returns
+        -------
+        list of PathDatasetComponentBlob
+            Path components of this dataset.
+
+        """
+        # This shall be implemented by subclasses, but shouldn't halt execution if called.
+        return []
+
+    @staticmethod
+    def _path_component_to_repr_lines(path_component_msg):
+        """
+        Parameters
+        ----------
+        path_component_msg : PathDatasetComponentBlob
+
+        Returns
+        -------
+        lines : list of str
+            Lines to be used in the ``__repr__`` of a dataset blob object.
+
+        """
+        lines = [path_component_msg.path]
+        if path_component_msg.size:
+            lines.append("    {} bytes".format(path_component_msg.size))
+        if path_component_msg.last_modified_at_source:
+            lines.append("    last modified {}".format(_utils.timestamp_to_str(path_component_msg.last_modified_at_source)))
+        if path_component_msg.md5:
+            lines.append("    MD5 checksum: {}".format(path_component_msg.md5))
+        if path_component_msg.sha256:
+            lines.append("    SHA-256 checksum: {}".format(path_component_msg.sha256))
+
+        return lines
