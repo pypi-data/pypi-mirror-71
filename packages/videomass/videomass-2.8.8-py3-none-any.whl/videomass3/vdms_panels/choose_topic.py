7176868a93ca2b0704454c67c4f96cded7b1f3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:33 +0000

    Add package videomass/2.8.8/videomass-2.8.8-py3-none-any.whl

---
+++ packages/videomass/videomass-2.8.8-py3-none-any.whl/videomass3/vdms_panels/choose_topic.py
@@ -0,0 +1,211 @@
+# -*- coding: UTF-8 -*-
+
+#########################################################
+# Name: choose_topic.py
+# Porpose: shows the topics available in the program
+# Compatibility: Python3, wxPython Phoenix
+# Author: Gianluca Pernigoto <jeanlucperni@gmail.com>
+# Copyright: (c) 2018/2020 Gianluca Pernigoto <jeanlucperni@gmail.com>
+# license: GPL3
+# Rev: October.25.2019
+#########################################################
+
+# This file is part of Videomass.
+
+#    Videomass is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    Videomass is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Videomass.  If not, see <http://www.gnu.org/licenses/>.
+
+#########################################################
+import wx
+import wx.lib.agw.hyperlink as hpl
+from videomass3.vdms_io import IO_tools
+from videomass3.vdms_sys.msg_info import current_release
+import os
+import shutil
+
+# get videomass wx.App attribute
+get = wx.GetApp()
+OS = get.OS
+PYLIB_YDL = get.pylibYdl
+EXEC_YDL = get.execYdl
+
+
+MSGREADY = (_('Successful! \n\n'
+              'youtube-dl is ready\n\n'
+              'Important: youtube-dl is very often updated, remember to '
+              'check for updates weekly.\nYou can use the dedicated functions '
+              'in the menu bar of Videomass: Tools/youtube-dl" .\n\n'
+              'Do you want to close Videomass now and restart it manually?'))
+
+if OS == 'Windows':
+    MSG = (_('{}\n\n'
+             'To download video from YouTube.com and other video sites, '
+             'Videomass needs an updated\nversion of youtube-dl.exe from '
+             'https://github.com/ytdl-org/youtube-dl/releases\n\n'
+             '- Requires: Microsoft Visual C++ 2010 Redistributable Package '
+             '(x86)\n   for major information visit http://ytdl-org.'
+             'github.io/youtube-dl/download.html\n\n'
+             'Do you want to download youtube-dl now?')).format(PYLIB_YDL)
+
+elif OS == 'Darwin':
+    MSG = (_('{}\n\n'
+             'To download video from YouTube.com and other video sites, '
+             'Videomass needs an updated version of youtube-dl from '
+             'https://github.com/ytdl-org/youtube-dl/releases\n\n'
+             'Do you want to download youtube-dl now?')).format(PYLIB_YDL)
+else:
+    MSG = (_('{}\n\n'
+             'To download video from YouTube.com and other video sites, '
+             'Videomass needs an updated version of youtube-dl.\n'
+             'Videomass recommends pip to install youtube-dl and keep '
+             'it updated by the user.\n\nHowever, an updated copy of '
+             'youtube-dl can be downloaded locally.\n\n'
+             '...Do you wish to continue?')).format(PYLIB_YDL)
+
+    MSG_ERR = _('{}\n\nyoutube-dl: no library or executable '
+                'found .').format(PYLIB_YDL)
+
+PRST_MNG = _('  Presets Manager - Create, edit and use quickly your favorite\n'
+             '  FFmpeg presets and profiles with full formats support and '
+             'codecs. ')
+AV_CONV = _('  A set of useful tools for audio and video conversions.\n'
+            '  Save your profiles and reuse them with Presets Manager. ')
+YOUTUBE_DL = _('  Easily download videos and audio in different formats\n'
+               '  and quality from YouTube, Facebook and more sites. ')
+
+
+class Choose_Topic(wx.Panel):
+    """
+    Helps to choose a topic.
+    """
+    def __init__(self, parent, OS, videoconv_icn, youtube_icn, prstmng_icn):
+        """
+        This is a home panel shown when start Videomass to choose the
+        appropriate contextual panel.
+        """
+        self.parent = parent
+        self.oS = OS
+        version = current_release()
+
+        wx.Panel.__init__(self, parent, -1,)
+
+        welcome = wx.StaticText(self, wx.ID_ANY, (_("Welcome to Videomass")))
+        version = wx.StaticText(self, wx.ID_ANY, (_('Version {}'
+                                                    ).format(version[2])))
+        sizer_base = wx.BoxSizer(wx.VERTICAL)
+        grid_buttons = wx.FlexGridSizer(5, 0, 20, 20)
+        grid_base = wx.GridSizer(1, 1, 0, 0)
+        if OS == 'Windows':
+            style = wx.BU_LEFT | wx.BORDER_NONE
+        else:
+            style = wx.BU_LEFT
+
+        self.presets_mng = wx.Button(self, wx.ID_ANY, PRST_MNG, size=(-1, -1),
+                                     style=style
+                                     )
+        self.presets_mng.SetBitmap(wx.Bitmap(prstmng_icn), wx.LEFT)
+        self.avconv = wx.Button(self, wx.ID_ANY, AV_CONV,
+                                size=(-1, -1), style=style
+                                )
+        self.avconv.SetBitmap(wx.Bitmap(videoconv_icn), wx.LEFT)
+        self.youtube = wx.Button(self, wx.ID_ANY, YOUTUBE_DL,
+                                 size=(-1, -1), style=style
+                                 )
+        self.youtube.SetBitmap(wx.Bitmap(youtube_icn), wx.LEFT)
+
+        grid_buttons.AddMany([(welcome, 0, wx.ALIGN_CENTER_VERTICAL |
+                               wx.ALIGN_CENTER_HORIZONTAL, 0),
+                              (version, 0, wx.BOTTOM |
+                               wx.ALIGN_CENTER_VERTICAL |
+                               wx.ALIGN_CENTER_HORIZONTAL, 20),
+                              (self.presets_mng, 0, wx.EXPAND, 5),
+                              (self.avconv, 0, wx.EXPAND, 5),
+                              (self.youtube, 0, wx.EXPAND, 5),
+                              ])
+        grid_base.Add(grid_buttons, 0, wx.ALIGN_CENTER_VERTICAL |
+                      wx.ALIGN_CENTER_HORIZONTAL, 5
+                      )
+        sizer_base.Add(grid_base, 1, wx.EXPAND, 5)
+        sizer_hpl = wx.BoxSizer(wx.HORIZONTAL)
+        sizer_base.Add(sizer_hpl, 0, wx.ALL |
+                       wx.ALIGN_CENTER_VERTICAL |
+                       wx.ALIGN_CENTER_HORIZONTAL, 15
+                       )
+        txt_link = wx.StaticText(self, label=_('Download additional presets '
+                                               'or contribute to making new '
+                                               'ones '))
+        link = hpl.HyperLinkCtrl(self, -1, _("Additional Presets"),
+                                 URL="https://github.com/jeanslack/"
+                                     "Videomass-presets")
+        sizer_hpl.Add(txt_link)
+        sizer_hpl.Add(link)
+        self.SetSizerAndFit(sizer_base)
+
+        if OS == 'Darwin':
+            welcome.SetFont(wx.Font(13, wx.SWISS, wx.NORMAL, wx.BOLD))
+            version.SetFont(wx.Font(11, wx.SWISS, wx.NORMAL, wx.NORMAL))
+        else:
+            welcome.SetFont(wx.Font(11, wx.SWISS, wx.NORMAL, wx.NORMAL))
+            version.SetFont(wx.Font(9, wx.SWISS, wx.NORMAL, wx.LIGHT))
+
+        self.Bind(wx.EVT_BUTTON, self.on_Video, self.avconv)
+        self.Bind(wx.EVT_BUTTON, self.on_Prst_mng, self.presets_mng)
+        self.Bind(wx.EVT_BUTTON, self.on_YoutubeDL, self.youtube)
+    # ------------------------------------------------------------------#
+
+    def on_Video(self, event):
+        self.parent.switch_file_import(self, 'Audio/Video Conversions')
+    # ------------------------------------------------------------------#
+
+    def on_YoutubeDL(self, event):
+        """
+        Check the installation of youtube-dl depending on the OS in use.
+        """
+        if PYLIB_YDL is None:
+            self.parent.switch_text_import(self, 'Youtube Downloader')
+            return
+        elif EXEC_YDL:
+            if os.path.isfile(EXEC_YDL):
+                self.parent.switch_text_import(self, 'Youtube Downloader')
+                return
+            else:
+                if wx.MessageBox(MSG, _("Videomass confirmation"),
+                                    wx.ICON_QUESTION |
+                                    wx.YES_NO, self) == wx.NO:
+                    return
+
+                latest = self.parent.ydl_latest(self, msgbox=False)
+                if latest[1]:
+                    return
+                else:
+                    upgrade = IO_tools.youtubedl_upgrade(latest[0],
+                                                         EXEC_YDL)
+                if upgrade[1]:  # failed
+                    wx.MessageBox("%s" % (upgrade[1]),
+                                  "Videomass error", wx.ICON_ERROR, self)
+                    return
+                else:
+                    if wx.MessageBox(MSGREADY, "Videomass",
+                                     wx.ICON_QUESTION |
+                                     wx.YES_NO, self) == wx.NO:
+                        return
+                    self.parent.on_Kill()
+                return
+
+        elif EXEC_YDL is False:
+            wx.MessageBox(MSG_ERR, 'Videomass error', wx.ICON_ERROR)
+            return
+    # ------------------------------------------------------------------#
+
+    def on_Prst_mng(self, event):
+        self.parent.switch_file_import(self, 'Presets Manager')
---
+++ packages/videomass/videomass-2.8.8-py3-none-any.whl/videomass3/vdms_threads/ydl_executable.py
@@ -0,0 +1,272 @@
+# -*- coding: UTF-8 -*-
+
+#########################################################
+# Name: Ydl_DL_Exec.py
+# Porpose: long processing task with youtube-dl executable
+# Compatibility: Python3, wxPython Phoenix
+# Author: Gianluca Pernigoto <jeanlucperni@gmail.com>
+# Copyright: (c) 2018/2019 Gianluca Pernigoto <jeanlucperni@gmail.com>
+# license: GPL3
+# Rev: April.19.2020 *PEP8 compatible*
+#########################################################
+# This file is part of Videomass.
+
+#    Videomass is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    Videomass is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Videomass.  If not, see <http://www.gnu.org/licenses/>.
+
+#########################################################
+import wx
+import subprocess
+import itertools
+import os
+from threading import Thread
+import time
+from pubsub import pub
+
+# get videomass wx.App attribute
+get = wx.GetApp()
+OS = get.OS
+LOGDIR = get.LOGdir
+FFMPEG_URL = get.FFMPEG_url
+execYdl = get.execYdl
+
+if not OS == 'Windows':
+    import shlex
+    LINE_MSG = _('Unrecognized error')
+else:
+    if os.path.isfile(execYdl):
+        LINE_MSG = (_('\nRequires MSVCR100.dll\nTo resolve this problem '
+                      'install: Microsoft Visual C++ 2010 Redistributable '
+                      'Package (x86)'))
+    else:
+        LINE_MSG = _('Unrecognized error')
+
+EXECUTABLE_NOT_FOUND_MSG = _("Is 'youtube-dl' installed on your system?")
+
+
+def logWrite(cmd, sterr, logname):
+    """
+    writes youtube-dl commands and status error during
+    threads below
+    """
+    if sterr:
+        apnd = "...%s\n\n" % (sterr)
+    else:
+        apnd = "%s\n\n" % (cmd)
+
+    with open(os.path.join(LOGDIR, logname), "a") as log:
+        log.write(apnd)
+
+
+class Ydl_DL_Exec(Thread):
+    """
+    Ydl_DL_Exec represents a separate thread for running
+    youtube-dl executable with subprocess class and capturing its
+    stdout/stderr output in real time .
+    """
+    def __init__(self, varargs, logname):
+        """
+        Attributes defined here:
+        self.stop_work_thread:  process terminate value
+        self.urls:          urls list
+        self.opt:           option strings to adding
+        self.outtmpl:       options template to renaming on pathname
+        self.code:          Format Code, else empty string ''
+        self.outputdir:     pathname destination
+        self.count:         increases with the progressive account elements
+        self.countmax:      length of self.urls
+        self.logname:       title log name for logging
+
+        """
+        Thread.__init__(self)
+        """initialize"""
+        self.stop_work_thread = False  # process terminate
+        self.urls = varargs[1]
+        self.opt = varargs[4][0]
+        self.outtmpl = varargs[4][1]
+        self.code = varargs[6]
+        self.outputdir = varargs[3]
+        self.count = 0
+        self.countmax = len(varargs[1])
+        self.logname = logname
+
+        self.start()  # start the thread (va in self.run())
+
+    def run(self):
+        """
+        Subprocess initialize thread.
+        """
+        ssl = '--no-check-certificate' if OS == 'Windows' else ''
+
+        for url, code in itertools.zip_longest(self.urls,
+                                               self.code,
+                                               fillvalue='',
+                                               ):
+            format_code = '--format %s' % (code) if code else ''
+            cmd = ('"{0}" {1} --newline --ignore-errors -o '
+                   '"{2}/{3}" {4} {5} --ignore-config --restrict-filenames '
+                   '"{6}" --ffmpeg-location "{7}"'.format(execYdl,
+                                                          ssl,
+                                                          self.outputdir,
+                                                          self.outtmpl,
+                                                          format_code,
+                                                          self.opt,
+                                                          url,
+                                                          FFMPEG_URL,
+                                                          ))
+            self.count += 1
+            count = 'URL %s/%s' % (self.count, self.countmax,)
+            com = "%s\n%s" % (count, cmd)
+            wx.CallAfter(pub.sendMessage,
+                         "COUNT_EVT",
+                         count=count,
+                         duration=100,
+                         fname=url,
+                         end='',
+                         )
+            logWrite(com, '', self.logname)  # write n/n + command only
+
+            if not OS == 'Windows':
+                cmd = shlex.split(cmd)
+                info = None
+            else:
+                # Hide subprocess window on MS Windows
+                info = subprocess.STARTUPINFO()
+                info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            try:
+                with subprocess.Popen(cmd,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.STDOUT,
+                                      bufsize=1,
+                                      universal_newlines=True,
+                                      startupinfo=info,) as p:
+
+                    for line in p.stdout:
+                        wx.CallAfter(pub.sendMessage,
+                                     "UPDATE_YDL_EXECUTABLE_EVT",
+                                     output=line,
+                                     duration=100,
+                                     status=0,
+                                     )
+                        if self.stop_work_thread:  # break second 'for' loop
+                            p.terminate()
+                            break
+
+                    if p.wait():  # error
+                        if 'line' not in locals():
+                            line = LINE_MSG
+                        wx.CallAfter(pub.sendMessage,
+                                     "UPDATE_YDL_EXECUTABLE_EVT",
+                                     output=line,
+                                     duration=100,
+                                     status=p.wait(),
+                                     )
+                        logWrite('',
+                                 "Exit status: %s" % p.wait(),
+                                 self.logname
+                                 )  # append exit error number
+                    else:  # ok
+                        wx.CallAfter(pub.sendMessage,
+                                     "COUNT_EVT",
+                                     count='',
+                                     duration='',
+                                     fname='',
+                                     end='ok'
+                                     )
+            except (OSError, FileNotFoundError) as err:
+                e = "%s\n  %s" % (err, EXECUTABLE_NOT_FOUND_MSG)
+                wx.CallAfter(pub.sendMessage,
+                             "COUNT_EVT",
+                             count=e,
+                             duration=0,
+                             fname=url,
+                             end='error',
+                             )
+                break
+
+            if self.stop_work_thread:  # break first 'for' loop
+                p.terminate()
+                break
+
+        time.sleep(.5)
+        wx.CallAfter(pub.sendMessage, "END_EVT")
+    # --------------------------------------------------------------------#
+
+    def stop(self):
+        """
+        Sets the stop work thread to terminate the process
+        """
+        self.stop_work_thread = True
+# ------------------------------------------------------------------------#
+
+
+class Ydl_EI_Exec(Thread):
+    """
+    Ydl_EI_Exec it is a separate thread to run youtube-dl executable
+    with subprocess class to get -at the end of the process- 'Format
+    code' data and exit status from stdout/stderr output .
+    """
+    def __init__(self, url):
+        """
+        self.urls:          urls list
+        """
+        Thread.__init__(self)
+        """initialize"""
+        self.url = url
+        self.status = None
+        self.data = None
+
+        self.start()  # start the thread (va in self.run())
+
+    def run(self):
+        """
+        Subprocess initialize thread.
+        """
+        ssl = '--no-check-certificate' if OS == 'Windows' else ''
+        cmd = ('"{0}" {1} --newline --ignore-errors --ignore-config '
+               '--restrict-filenames -F "{2}"'.format(execYdl,
+                                                      ssl,
+                                                      self.url))
+        if not OS == 'Windows':
+            cmd = shlex.split(cmd)
+            info = None
+        else:
+            # Hide subprocess window on MS Windows
+            info = subprocess.STARTUPINFO()
+            info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        try:
+            p = subprocess.Popen(cmd,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.STDOUT,
+                                 universal_newlines=True,
+                                 startupinfo=info,
+                                 )
+            out = p.communicate()
+
+        except OSError as oserr:  # executable do not exist
+            self.status = ('%s' % oserr, 'error')
+        else:
+            if p.returncode:  # if returncode == 1
+                if not out[0] and not out[1] and OS == 'Windows':
+                    self.status = LINE_MSG, 'error'
+                else:
+                    self.status = (out[0], 'error')
+            else:
+                self.status = (out[0], out[1])
+
+        self.data = self.status
+
+        wx.CallAfter(pub.sendMessage,
+                     "RESULT_EVT",
+                     status=''
+                     )
