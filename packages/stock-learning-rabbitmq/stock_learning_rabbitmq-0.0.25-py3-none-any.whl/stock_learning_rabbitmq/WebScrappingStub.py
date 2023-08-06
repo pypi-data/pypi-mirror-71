c49f1128e80d4ca90b9282f9c97ccd98a04b67dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:54 +0000

    Add package stock-learning-rabbitmq/0.0.25/stock_learning_rabbitmq-0.0.25-py3-none-any.whl

---
+++ packages/stock-learning-rabbitmq/stock_learning_rabbitmq-0.0.25-py3-none-any.whl/stock_learning_rabbitmq/WebScrappingStub.py
@@ -0,0 +1,19 @@
+from stock_learning_rabbitmq.Stub import Stub
+
+
+class WebScrappingStub(Stub):
+    
+    def __init__(self, server):
+        super().__init__(server, 'stock-learning-web-scrapping')
+
+    def infomoney_ibovespa_company_data(self):
+        self._send('infomoney-ibovespa-company-data', {})
+
+    def infomoney_ibovespa_historic_data(self, content):
+        self._send('infomoney-ibovespa-historic-data', content)
+
+    def infomoney_ibovespa_live_update(self, content):
+        self._send('infomoney-ibovespa-live-update', content)
+
+    def yahoo_company_historic_data(self, content):
+        self._send('yahoo-company-historic-data', content)
