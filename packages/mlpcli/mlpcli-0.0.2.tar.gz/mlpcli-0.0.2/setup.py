ed992aa6174560d994f9118b4696ab32f948e61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:32 +0000

    Add package mlpcli/0.0.2/mlpcli-0.0.2.tar.gz

---
+++ packages/mlpcli/mlpcli-0.0.2.tar.gz/mlpcli-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='mlpcli',
+    version='0.0.002',    
+    description='MLP test Package',
+    author='Padma Polash Paul',
+    author_email='ppaul@dpami.com',    
+    license='BSD 2-clause',
+    packages=['mlpcli'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)    
